7c7ba55899086871da8856d4c16bb15555fd3b86
ahmed korim <ahmedkorrim@gmail.com>
2021-02-04 22:28:13 +0200

    Added Wasm loader

--- packages/apps/config-overrides.js
+++ packages/apps/config-overrides.js
@@ -27,6 +27,28 @@ module.exports = override(function (config, env) {
     include: /node_modules/,
     type: 'javascript/auto',
   });
+
+  const wasmExtensionRegExp = /\.wasm$/;
+  config.resolve.extensions.push('.wasm');
+
+  config.module.rules.forEach((rule) => {
+    (rule.oneOf || []).forEach((oneOf) => {
+      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+        // make file-loader ignore WASM files
+        oneOf.exclude.push(wasmExtensionRegExp);
+      }
+    });
+  });
+
+  //wasam
+  config.module.rules.push({
+    test: wasmExtensionRegExp,
+    include: /node_modules/,
+    type: 'javascript/auto',
+    // include: path.resolve(__dirname, 'src'),
+    use: [{ loader: require.resolve('wasm-loader'), options: {} }],
+  });
+
   // remove ModuleScoplePlugin
   config.resolve.plugins = config.resolve.plugins.filter((plugin) => !(plugin instanceof ModuleScopePlugin));
   config.resolve.alias = packages.reduce((pre, cur) => {
