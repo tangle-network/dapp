1108d6b84ec77d30e07658cc784b0a1af090ea96
Drew Stone <drewstone329@gmail.com>
2021-02-17 21:18:22 -0500

    Update packages/react-hooks/src/useFetch.ts
    Co-authored-by: Ahmed Korim <ahmedkorrim@gmail.com>

--- packages/react-hooks/src/useFetch.ts
+++ packages/react-hooks/src/useFetch.ts
@@ -4,18 +4,24 @@ export const useFetch = (url: RequestInfo) => {
   const [data, setData] = useState(null);
 
   // empty array as second argument equivalent to componentDidMount
-  useEffect(() => {
+useEffect(() => {
+    const controller = new AbortController();
+    const logger = LoggerService.get('App');
+
     async function fetchData() {
-      console.log('Url', url);
-      const response = await fetch(url);
-
-      console.log(response);
-      const json = await response.json();
-
-      setData(json);
+      try {
+        const response = await fetch(url, { signal: controller.signal });
+        const json = await response.json();
+        logger.trace(`Got data for url ${url}`, json);
+        setData(json);
+      } catch (e) {
+        logger.error(e);
+      }
     }
 
     fetchData();
+    // Aborting the Request if the component is unmounted before Resolving
+    return () => controller.abort();
   }, [url]);
 
   return data;
