a6a1d0da54ecd3433a2def911e2d2623da448f1d
ahmed korim <ahmedkorrim@gmail.com>
2021-03-16 18:09:48 +0200

    added receiver and realer to rpc args

--- packages/page-mixer/src/components/withdraw/WithdrawConsole.tsx
+++ packages/page-mixer/src/components/withdraw/WithdrawConsole.tsx
@@ -1,22 +1,59 @@
-import { Fab, TextField } from '@material-ui/core';
+import { Button, Fab, MenuItem, TextField, Typography } from '@material-ui/core';
 import { useWithdraw } from '@webb-dapp/react-hooks/withdraw/useWithdraw';
 import { SpaceBox } from '@webb-dapp/ui-components';
 import React, { FC, useState } from 'react';
-
+import styled from 'styled-components';
 import { CardRoot, CardTitle } from '../common';
 
+const CallToActionWrapper = styled.div`
+  max-width: 300px;
+  margin: auto;
+`;
 export const WithdrawConsole: FC = () => {
   const [note, setNote] = useState('');
 
-  const { withdraw } = useWithdraw(note);
+  const { accounts, setWithdrawTo, withdraw, withdrawTo } = useWithdraw(note);
+
   return (
     <CardRoot>
       <CardTitle>Withdraw</CardTitle>
-      <TextField fullWidth label={'note'} value={note} onChange={({ target: { value } }) => setNote(value)} />
+      <TextField
+        multiline
+        rows={5}
+        placeholder={`webb.mix-v1-EDG-2-d6292aab18779001a69c18d41a2649b273770d22f2f098b04c1d49a9b94990017ce9c1c8b9ac7a18773a624dafb4bcb7989e63162abe5a22f753f90e42618b0a`}
+        fullWidth
+        label={'Note'}
+        value={note}
+        onChange={({ target: { value } }) => setNote(value)}
+      />
       <SpaceBox height={24} />
-      <Fab onClick={withdraw} variant={'extended'} color='primary'>
-        Withdraw
-      </Fab>
+      <TextField
+        label={'Recipient'}
+        select
+        fullWidth
+        value={withdrawTo?.address || ''}
+        onChange={({ target: { value } }) => {
+          console.log(value);
+          setWithdrawTo(accounts.find((acc) => acc.address === value) || null);
+        }}
+      >
+        {accounts.map((account) => (
+          <MenuItem key={account.address} value={account.address}>
+            <div>
+              <div>{account.name}</div>
+              <Typography variant={'caption'} color={'textSecondary'}>
+                {account.address}
+              </Typography>
+            </div>
+          </MenuItem>
+        ))}
+      </TextField>
+      <SpaceBox height={24} />
+      <CallToActionWrapper>
+        <Button disabled={!withdrawTo || !note} fullWidth variant='contained' onClick={withdraw} color='primary'>
+          Withdrawn
+        </Button>
+      </CallToActionWrapper>
     </CardRoot>
   );
 };
--- packages/react-hooks/src/withdraw/useWithdraw.tsx
+++ packages/react-hooks/src/withdraw/useWithdraw.tsx
@@ -1,10 +1,13 @@
-import { useCall, useMixerInfo, useMixerProvider } from '@webb-dapp/react-hooks';
+import { useAccounts, useApi, useCall, useMixerInfo, useMixerProvider } from '@webb-dapp/react-hooks';
 import { useGroupTree } from '@webb-dapp/react-hooks/merkle';
 import { useTX } from '@webb-dapp/react-hooks/tx/useTX';
 import { LoggerService } from '@webb-tools/app-util';
 import { Note } from '@webb-tools/sdk-mixer';
 import { Block } from '@webb-tools/types/interfaces';
-import { useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import { InjectedAccount } from '@polkadot/extension-inject/types';
+import { decodeAddress } from '@polkadot/keyring';
 
 const logger = LoggerService.get('Withdraw');
 
@@ -25,17 +28,29 @@ export function useWithdraw(noteStr: string) {
     }
   }, [noteStr]);
 
+  const [withdrawTo, _setWithdrawTo] = useState<InjectedAccount | null>(null);
+
   const noteMixerGroupId = useMemo(() => note?.id, [note]);
   const groupTreeWrapper = useGroupTree(noteMixerGroupId?.toString());
   const mixerInfoWrapper = useMixerInfo(noteMixerGroupId?.toString());
 
+  const accounts = useAccounts();
+  const { api } = useApi();
+  useEffect(() => {
+    setWithdrawTo(accounts.active || null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accounts.active?.address]);
+
+  const setWithdrawTo = useCallback((next: InjectedAccount | null) => {
+    console.log(next, 'next');
+    _setWithdrawTo(next);
+  }, []);
   const { executeTX, loading } = useTX({
     method: 'withdraw',
     onExtrinsicSuccess: () => {},
     params: [],
     section: 'mixer',
   });
-
   const withdraw = async () => {
     const root = groupTreeWrapper.rootHashU8a;
     if (!root || !note) {
@@ -51,7 +66,13 @@ export function useWithdraw(noteStr: string) {
       return;
     }
     const leaves = mixerInfoWrapper.leaveU8a;
-    const zk = await mixer.generateZK(note, root, leaves);
+    const zk = await mixer.generateZK({
+      leaves,
+      note: noteStr,
+      recipient: decodeAddress(withdrawTo?.address),
+      relayer: decodeAddress(withdrawTo?.address),
+      root,
+    });
     const withdrawProof = {
       cached_block: blockNumber,
       cached_root: root,
@@ -61,11 +82,16 @@ export function useWithdraw(noteStr: string) {
       nullifier_hash: zk.nullifierHash,
       proof_bytes: zk.proof,
       proof_commitments: zk.proofCommitments,
+      recipient: api.createType('Option<AccountId>', withdrawTo?.address),
+      relayer: api.createType('Option<AccountId>', withdrawTo?.address),
     };
     await executeTX([withdrawProof]);
   };
   return {
+    accounts: accounts.accounts || [],
     loading,
+    setWithdrawTo,
     withdraw,
+    withdrawTo,
   };
 }
