a2bef0bff574ca5b69573f48411ff86e8264cc07
ahmed korim <ahmedkorrim@gmail.com>
2021-04-30 10:49:16 +0200

    setup providers

---
+++ packages/wallet/src/providers/web3-provider.ts
@@ -0,0 +1,33 @@
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Web3 from 'web3';
+
+export class Web3Provider {
+  private constructor(private _inner: Web3) {}
+
+  static fromWindowProvider() {
+    if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+      return new Web3Provider(new Web3(window.web3.currentProvider));
+    }
+    throw Error('Not provider in window');
+  }
+
+  static fromUri(url: string) {
+    const HttpProvider = new Web3.providers.HttpProvider(url);
+    const web3 = new Web3(HttpProvider);
+    return new Web3Provider(web3);
+  }
+
+  static async fromQRCode(WCProvider: WalletConnectProvider) {
+    await WCProvider.enable();
+    const web3 = new Web3(WCProvider);
+    return new Web3Provider(web3);
+  }
+
+  get eth() {
+    return this._inner.eth;
+  }
+
+  get account() {
+    return this._inner.defaultAccount;
+  }
+}
