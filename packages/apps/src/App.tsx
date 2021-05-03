1f1461308c945f72eab32e08880b34189d6a4dfc
ahmed korim <ahmedkorrim@gmail.com>
2021-05-03 19:57:31 +0200

    fix tslint

--- packages/apps/src/App.tsx
+++ packages/apps/src/App.tsx
@@ -5,7 +5,6 @@ import { DAppError } from '@webb-dapp/react-components/utils/Error/DAppError';
 import { RouterProvider, WebbProvider } from '@webb-dapp/react-environment';
 import { UIProvider } from '@webb-dapp/ui-components';
 import Theme from '@webb-dapp/ui-components/styles/Theme';
-import makeTheme from '@webb-dapp/ui-components/styling/themes/makeTheme';
 import { LoggerService } from '@webb-tools/app-util';
 import React, { FC } from 'react';
 import { hot } from 'react-hot-loader/root';
