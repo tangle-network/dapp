b2478d087ba15cfb119096476237b46a7f6cd217
ahmed korim <ahmedkorrim@gmail.com>
2021-06-07 15:54:47 +0200

    wait for api ready state

--- packages/react-environment/src/api-providers/webb-polkadot-provider.ts
+++ packages/react-environment/src/api-providers/webb-polkadot-provider.ts
@@ -1,9 +1,10 @@
-import { WebbApiProvider, WebbMethods } from '@webb-dapp/react-environment/webb-context';
-import { PolkadotProvider } from '@webb-dapp/wallet/providers/polkadot/polkadot-provider';
-import { ApiPromise } from '@polkadot/api';
-import { InjectedExtension } from '@polkadot/extension-inject/types';
 import { PolkadotMixerDeposit } from '@webb-dapp/react-environment/api-providers/polkadot-mixer-deposit';
 import { PolkadotMixerWithdraw } from '@webb-dapp/react-environment/api-providers/polkadot-mixer-withdraw';
+import { WebbApiProvider, WebbMethods } from '@webb-dapp/react-environment/webb-context';
+import { PolkadotProvider } from '@webb-dapp/wallet/providers/polkadot/polkadot-provider';
+
+import { ApiPromise } from '@polkadot/api';
+import { InjectedExtension } from '@polkadot/extension-inject/types';
 
 export class WebbPolkadot extends PolkadotProvider implements WebbApiProvider<WebbPolkadot> {
   readonly methods: WebbMethods<WebbPolkadot>;
@@ -26,6 +27,8 @@ export class WebbPolkadot extends PolkadotProvider implements WebbApiProvider<We
 
   static async init(appName: string, endpoints: string[]): Promise<WebbPolkadot> {
     const [apiPromise, injectedExtension] = await PolkadotProvider.getParams(appName, endpoints);
-    return new WebbPolkadot(apiPromise, injectedExtension);
+    const instance = new WebbPolkadot(apiPromise, injectedExtension);
+    await apiPromise.isReady;
+    return instance;
   }
 }
