0b6706ec241bd17b1c96010b737eac377b9dbae6
ahmed korim <ahmedkorrim@gmail.com>
2021-06-24 14:46:21 +0200

    consume the populated chain config on webbprovider

--- packages/react-environment/src/webb-context/common.ts
+++ packages/react-environment/src/webb-context/common.ts
@@ -1,6 +1,6 @@
 import { ChainConfig } from '@webb-dapp/react-environment/types/chian-config.interface';
-import { WalletConfig } from '@webb-dapp/react-environment/types/wallet-config.interface';
 import { CurrencyConfig } from '@webb-dapp/react-environment/types/currency';
+import { WalletConfig } from '@webb-dapp/react-environment/types/wallet-config.interface';
 
 export type Chain = ChainConfig & {
   wallets: Record<number, Wallet>;
