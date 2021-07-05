a77940c370e9d34e3362b5d77da562727a37b3c2
ahmed korim <ahmedkorrim@gmail.com>
2021-07-05 14:18:17 +0200

    hide navigation

--- packages/react-components/src/layout/index.tsx
+++ packages/react-components/src/layout/index.tsx
@@ -41,7 +41,7 @@ const Main: FC<MainLayoutProps> = memo(({ children }) => {
     <MainContainer>
       <AppBar />
       <ContentWrapper>{content}</ContentWrapper>
-      <BottomNavigation />
+      {/*<BottomNavigation />*/}
     </MainContainer>
   );
 });
