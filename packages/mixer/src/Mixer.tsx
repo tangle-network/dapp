3fad8996f1157aca0a0da3a3ee6da7ce795ccbd7
Nathan Barnavon <nepoche@yahoo.com>
2021-08-23 23:49:13 -0400

    Change IP Display component to use geolocation fetched from relayer

--- packages/mixer/src/Mixer.tsx
+++ packages/mixer/src/Mixer.tsx
@@ -1,18 +1,17 @@
 import { Deposit, MixerTabs, Withdraw } from '@webb-dapp/mixer/components';
-import React from 'react';
-import styled from 'styled-components';
 import IPDisplay from '@webb-dapp/react-components/IPDisplay/IPDisplay';
 import { SpaceBox } from '@webb-dapp/ui-components';
+import React from 'react';
+import styled from 'styled-components';
 
 const MixerWrapper = styled.div``;
 type MixerProps = {};
 
 export const Mixer: React.FC<MixerProps> = () => {
-
   return (
     <MixerWrapper>
       <MixerTabs Withdraw={<Withdraw />} Deposit={<Deposit />} />
-      <SpaceBox height={8}/>
+      <SpaceBox height={8} />
       <IPDisplay />
     </MixerWrapper>
   );
--- packages/react-components/src/IPDisplay/IPDisplay.tsx
+++ packages/react-components/src/IPDisplay/IPDisplay.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import { above } from '@webb-dapp/ui-components/utils/responsive-utils';
-import { FontFamilies } from '@webb-dapp/ui-components/styling/fonts/font-families.enum';
-import { Pallet } from '@webb-dapp/ui-components/styling/colors';
 import { Icon, Typography } from '@material-ui/core';
 import { useFetch } from '@webb-dapp/react-hooks/';
+import { Pallet } from '@webb-dapp/ui-components/styling/colors';
+import { FontFamilies } from '@webb-dapp/ui-components/styling/fonts/font-families.enum';
+import { above } from '@webb-dapp/ui-components/utils/responsive-utils';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+import { getWebbRelayer } from '@webb-dapp/apps/configs/relayer-config';
+import { WebbRelayer } from '@webb-dapp/react-environment/webb-context/relayer';
+import { useWebContext } from '@webb-dapp/react-environment/webb-context';
 
 const IPDisplayWrapper = styled.div`
   padding: 1rem;
@@ -20,11 +23,11 @@ const IPDisplayWrapper = styled.div`
     ${theme.type === 'light' ? `box-shadow: 0px 0px 14px rgba(51, 81, 242, 0.11);` : ''}
     .label-icon {
       font-size: 40px;
-      color: ${theme.primary}
+      color: ${theme.primary};
     }
 
     .tor {
-      color: ${theme.primary}
+      color: ${theme.primary};
     }
   `}
 
@@ -42,20 +45,51 @@ const IPDisplayWrapper = styled.div`
   }
 `;
 
-const IPDisplay: React.FC<IPDisplayProps> = () => {
+type RelayerIpInfo = {
+  ip: String;
+  city: String;
+  country: String;
+};
 
-  const { query, city, countryCode } = useFetch(`http://ip-api.com/json/?fields=countryCode,city,query`, {});
+type IPDisplayProps = {};
+
+const IPDisplay: React.FC<IPDisplayProps> = () => {
+  // const { city, countryCode, query } = useFetch(`http://ip-api.com/json/?fields=countryCode,city,query`, {});
+  const { activeApi } = useWebContext();
+
+  const [geoLocation, setGeoLocation] = useState<RelayerIpInfo>({ ip: '', city: '', country: '' });
+
+  useEffect(() => {
+    async function getIpInfo() {
+      const relayer = await activeApi?.relayingManager.getRelayer({})[0];
+      if (relayer) {
+        const response = await relayer.getIp();
+        console.log(response);
+        setGeoLocation(response);
+      }
+    }
+    getIpInfo();
+  }, [activeApi]);
 
   return (
     <IPDisplayWrapper>
-      <Icon className={'label-icon'} style={{fontSize: 40}}>room</Icon>
-      <div style={{paddingLeft: 5}}>
-        <Typography className={'ip-text'} variant={'h5'}>Your IP Address is: <b>{query} {city}, {countryCode}</b></Typography>
+      <Icon className={'label-icon'} style={{ fontSize: 40 }}>
+        room
+      </Icon>
+      <div style={{ paddingLeft: 5 }}>
+        <Typography className={'ip-text'} variant={'h5'}>
+          Your IP Address is:{' '}
+          <b>
+            {geoLocation.ip} {geoLocation.city}, {geoLocation.country}
+          </b>
+        </Typography>
         <Typography className={'ip-info'}>Please mask your IP address while using our service!</Typography>
-        <Typography className={'ip-info'}>We recommend <span className={'tor'}>TOR</span> </Typography>
+        <Typography className={'ip-info'}>
+          We recommend <span className={'tor'}>TOR</span>{' '}
+        </Typography>
       </div>
     </IPDisplayWrapper>
   );
-}
+};
 
 export default IPDisplay;
