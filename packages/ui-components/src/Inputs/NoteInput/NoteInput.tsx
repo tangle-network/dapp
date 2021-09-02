272fdf82f699cb14af79640d8af568b430d4a1b8
nepoche <barna0308@gmail.com>
2021-09-02 17:56:11 -0400

    Update packages/ui-components/src/Inputs/NoteInput/NoteInput.tsx
    Co-authored-by: drewstone <drewstone329@gmail.com>

--- packages/ui-components/src/Inputs/NoteInput/NoteInput.tsx
+++ packages/ui-components/src/Inputs/NoteInput/NoteInput.tsx
@@ -15,7 +15,7 @@ export const NoteInput: React.FC<NoteInputProps> = ({ error, onChange, value }) 
     <InputLabel label={'Note'}>
       <InputBase
         fullWidth
-        placeholder={`webb.mix:v1:4:Circom:Bn254:Poseidon5:ETH:18:0.1:5:5:dc92b0096b02746362c56dbee8e28a036f29b600b59cad3e4a114af2e2eb094f9878beaf5699f43d789937130e7ee7ca12e0703ce9cc62297bbb0abc864e`}
+        placeholder={`webb.mix:v1:4:Circom:Bn254:Poseidon:ETH:18:0.1:5:5:dc92b0096b02746362c56dbee8e28a036f29b600b59cad3e4a114af2e2eb094f9878beaf5699f43d789937130e7ee7ca12e0703ce9cc62297bbb0abc864e`}
         multiline={true}
         rows={5}
         value={value}
