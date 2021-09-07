6989a1e34ae9aab450c8d38680c165e57a697f15
ahmed korim <ahmedkorrim@gmail.com>
2021-09-07 17:09:54 +0200

    Bridge currency

---
+++ packages/react-environment/src/webb-context/bridge/bridge-currency.ts
@@ -0,0 +1,32 @@
+import { ChainId, WebbCurrencyId, webbCurrencyIdFromString, webbCurrencyIdToString } from '@webb-dapp/apps/configs';
+
+/*
+ *
+ * Bridge Currency (Bridge name)
+ *
+ *  Example:
+ *  A bridge between Ethereum and Edgware with the currency ETH
+ *  will be ${named} = webbETH (The wrapped currency)
+ *  the string that defines the bridge chains is the ${ChainId} of the two anchors
+ *  for Edgware => 0 , for Ethereum(main net)  =>  3
+ *  the token name will be webbEth-0-1  (the wrapped Eth token the is bridged between Ethereum and Edgware)
+ * */
+export class BridgeCurrency {
+  constructor(private paraChains: ChainId[], private wrappedCurrency: WebbCurrencyId) {
+    this.paraChains = this.paraChains.sort((a, b) => (a > b ? 1 : a < b ? -1 : 0));
+  }
+
+  toString(): string {
+    const paraChains = this.paraChains.join('-');
+    const baseCurrency = webbCurrencyIdToString(this.wrappedCurrency);
+    return `webb${baseCurrency}-${paraChains}`;
+  }
+
+  static fromString(strBridgeCurrency: string): BridgeCurrency {
+    const parts = strBridgeCurrency.split('-');
+    let prefix = parts[0].replace('webb', '');
+    let chainIds = parts.slice(1, parts.length).map((c) => Number(c) as ChainId);
+    let wrappedCurrency = webbCurrencyIdFromString(prefix);
+    return new BridgeCurrency(chainIds, wrappedCurrency);
+  }
+}
