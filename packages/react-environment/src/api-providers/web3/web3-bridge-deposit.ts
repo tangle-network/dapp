b983de300c0b5849577d13fdd519b736fef2db4f
ahmed korim <ahmedkorrim@gmail.com>
2021-09-14 20:06:40 +0200

    Deposit feedback  notifications

--- packages/react-environment/src/api-providers/web3/web3-bridge-deposit.ts
+++ packages/react-environment/src/api-providers/web3/web3-bridge-deposit.ts
@@ -1,4 +1,4 @@
-import { ChainId, evmIdIntoChainId } from '@webb-dapp/apps/configs';
+import { ChainId, evmIdIntoChainId, getEVMChainNameFromInternal } from '@webb-dapp/apps/configs';
 import { createTornDeposit, Deposit } from '@webb-dapp/contracts/utils/make-deposit';
 import { BridgeConfig, DepositPayload as IDepositPayload, MixerSize } from '@webb-dapp/react-environment';
 import { WebbWeb3Provider } from '@webb-dapp/react-environment/api-providers/web3/webb-web3-provider';
@@ -7,6 +7,9 @@ import { Note, NoteGenInput } from '@webb-tools/sdk-mixer';
 import { u8aToHex } from '@polkadot/util';
 
 import { BridgeDeposit } from '../../webb-context/bridge/bridge-deposit';
+import { transactionNotificationConfig } from '@webb-dapp/wallet/providers/polkadot/transaction-notification-config';
+import React from 'react';
+import { DepositNotification } from '@webb-dapp/ui-components/notification/DepositNotification';
 
 type DepositPayload = IDepositPayload<Note, [Deposit, number | string]>;
 
@@ -14,26 +17,78 @@ export class Web3BridgeDeposit extends BridgeDeposit<WebbWeb3Provider, DepositPa
   bridgeConfig: BridgeConfig = {};
 
   async deposit(depositPayload: DepositPayload): Promise<void> {
-    // Getting the active bridge
     const bridge = this.activeBridge;
     if (!bridge) {
       throw new Error('api not ready');
     }
+    try {
+      // Getting the active bridge
+      const commitment = depositPayload.params[0].commitment;
+      const note = depositPayload.note.note;
 
-    const commitment = depositPayload.params[0].commitment;
-    const note = depositPayload.note.note;
-    // Find the Anchor for this bridge amount
-    const anchor = bridge.anchors.find((anchor) => anchor.amount == note.amount);
-    if (!anchor) {
-      throw new Error('not Anchor for amount' + note.amount);
+      transactionNotificationConfig.loading?.({
+        address: '',
+        data: React.createElement(DepositNotification, {
+          chain: getEVMChainNameFromInternal(Number(note.chain)),
+          amount: Number(note.amount),
+          currency: bridge.currency.name,
+        }),
+        key: 'bridge-deposit',
+        path: {
+          method: 'deposit',
+          section: bridge.currency.name,
+        },
+      });
+
+      // Find the Anchor for this bridge amount
+      const anchor = bridge.anchors.find((anchor) => anchor.amount == note.amount);
+      if (!anchor) {
+        throw new Error('not Anchor for amount' + note.amount);
+      }
+      // Get the contract address for the destination chain
+      const contractAddress = anchor.anchorAddresses[Number(note.chain) as ChainId];
+      if (!contractAddress) {
+        throw new Error(`No Anchor for the chain ${note.chain}`);
+      }
+      const contract = this.inner.getWebbAnchorByAddress(contractAddress);
+      await contract.deposit(commitment);
+      transactionNotificationConfig.finalize?.({
+        address: '',
+        data: undefined,
+        key: 'bridge-deposit',
+        path: {
+          method: 'deposit',
+          section: bridge.currency.name,
+        },
+      });
+    } catch (e) {
+      if (!e.code) {
+        throw e;
+      }
+      if (e.code == 4001) {
+        transactionNotificationConfig.failed?.({
+          address: '',
+          data: 'User Rejected Deposit',
+          key: 'bridge-deposit',
+
+          path: {
+            method: 'deposit',
+            section: bridge.currency.name,
+          },
+        });
+      } else {
+        transactionNotificationConfig.failed?.({
+          address: '',
+          data: 'Deposit Transaction Failed',
+          key: 'bridge-deposit',
+
+          path: {
+            method: 'deposit',
+            section: bridge.currency.name,
+          },
+        });
+      }
     }
-    // Get the contract address for the destination chain
-    const contractAddress = anchor.anchorAddresses[Number(note.chain) as ChainId];
-    if (!contractAddress) {
-      throw new Error(`No Anchor for the chain ${note.chain}`);
-    }
-    const contract = this.inner.getWebbAnchorByAddress(contractAddress);
-    await contract.deposit(commitment);
   }
 
   async getSizes(): Promise<MixerSize[]> {
