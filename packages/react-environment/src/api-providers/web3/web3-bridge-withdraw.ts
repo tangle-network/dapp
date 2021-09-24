8121f7ef86df48c95e480a6518eaafac7fcc691b
Ahmed Korim <ahmedkorrim@gmail.com>
2021-09-24 18:01:24 +0200

    Same chain flow for withdrawal

--- packages/react-environment/src/api-providers/web3/web3-bridge-withdraw.ts
+++ packages/react-environment/src/api-providers/web3/web3-bridge-withdraw.ts
@@ -4,10 +4,11 @@ import { WebbWeb3Provider } from '@webb-dapp/react-environment/api-providers/web
 import { Note } from '@webb-tools/sdk-mixer';
 
 import { BridgeWithdraw } from '../../webb-context';
-import { chainIdIntoEVMId, evmIdIntoChainId, getEVMChainNameFromInternal } from '@webb-dapp/apps/configs';
+import { ChainId, chainIdIntoEVMId, evmIdIntoChainId, getEVMChainNameFromInternal } from '@webb-dapp/apps/configs';
 import { transactionNotificationConfig } from '@webb-dapp/wallet/providers/polkadot/transaction-notification-config';
 import React from 'react';
 import { LoggerService } from '@webb-tools/app-util';
+import { DepositNote } from '@webb-tools/wasm-utils';
 
 const logger = LoggerService.get('Web3BridgeWithdraw');
 
@@ -18,95 +19,108 @@ export class Web3BridgeWithdraw extends BridgeWithdraw<WebbWeb3Provider> {
     return Promise.resolve(undefined);
   }
 
+  async sameChainWithdraw(note: DepositNote, recipient: string) {
+    // Todo Ensure the current provider is the same as the source
+    const activeChain = await this.inner.getChainId();
+    const internalId = evmIdIntoChainId(activeChain);
+    if (Number(note.chain) !== internalId) {
+      throw new Error(`The provider isn't the connected to the expected provider `);
+    }
+    const bridge = Bridge.from(this.bridgeConfig, BridgeCurrency.fromString(note.tokenSymbol));
+
+    const contractAddresses = bridge.anchors.find((anchor) => anchor.amount === note.amount)!;
+    const contractAddress = contractAddresses.anchorAddresses[internalId]!;
+
+    const contract = this.inner.getWebbAnchorByAddress(contractAddress);
+    const accounts = await this.inner.accounts.accounts();
+    const account = accounts[0];
+
+    const deposit = depositFromAnchor2Preimage(note.secret.replace('0x', ''), Number(activeChain));
+    logger.info(`Commitment for withdraw is ${deposit.commitment}`);
+
+    const input = {
+      destinationChainId: activeChain,
+      secret: deposit.secret,
+      nullifier: deposit.nullifier,
+      nullifierHash: deposit.nullifierHash,
+
+      // Todo change this to the realyer address
+      relayer: account.address,
+      recipient: account.address,
+
+      fee: 0,
+      refund: 0,
+    };
+
+    logger.trace(`input for zkp`, input);
+    this.emit('stateChange', WithdrawState.GeneratingZk);
+    const zkpResults = await contract.generateZKP(deposit, input);
+    transactionNotificationConfig.loading?.({
+      address: recipient,
+      data: React.createElement(
+        'p',
+        { style: { fontSize: '.9rem' } }, // Matches Typography variant=h6
+        `Withdraw in progress`
+      ),
+      key: 'bridge-withdraw-evm',
+      path: {
+        method: 'withdraw',
+        section: `Bridge ${bridge.currency.chainIds.map(getEVMChainNameFromInternal).join('-')}`,
+      },
+    });
+    this.emit('stateChange', WithdrawState.SendingTransaction);
+    await contract.withdraw(
+      zkpResults.proof,
+      {
+        destinationChainId: activeChain,
+        fee: input.fee,
+        nullifier: input.nullifier,
+        nullifierHash: input.nullifierHash,
+        pathElements: zkpResults.input.pathElements,
+        pathIndices: zkpResults.input.pathIndices,
+        recipient: input.recipient,
+        refund: input.refund,
+        relayer: input.relayer,
+        root: zkpResults.root as any,
+        secret: zkpResults.input.secret,
+      },
+      zkpResults.input
+    );
+    this.emit('stateChange', WithdrawState.Ideal);
+    transactionNotificationConfig.finalize?.({
+      address: recipient,
+      data: undefined,
+      key: 'bridge-withdraw-evm',
+      path: {
+        method: 'withdraw',
+        section: `Bridge ${bridge.currency.chainIds.map(getEVMChainNameFromInternal).join('-')}`,
+      },
+    });
+  }
+
+  async crossChainWithdraw(note: DepositNote, recipient: string) {}
+
   async withdraw(note: string, recipient: string): Promise<void> {
     logger.trace(`Withdraw using note ${note} , recipient ${recipient}`);
-    ///--->
+
     const parseNote = await Note.deserialize(note);
-    logger.trace(`Parsed note`, parseNote);
-    this.emit('stateChange', WithdrawState.GeneratingZk);
-    ///--->
-    logger.info(`Commitment is from note ${parseNote.note.secret}`);
-    const evmChainId = chainIdIntoEVMId(parseNote.note.chain);
-    const deposit = depositFromAnchor2Preimage(parseNote.note.secret.replace('0x', ''), Number(evmChainId));
+    const depositNote = parseNote.note;
+    const sourceChainName = getEVMChainNameFromInternal(Number(depositNote.sourceChain) as ChainId);
+    const targetChainName = getEVMChainNameFromInternal(Number(depositNote.chain) as ChainId);
+    logger.trace(`Bridge withdraw from ${sourceChainName} to ${targetChainName}`);
+
     const token = parseNote.note.tokenSymbol;
     const bridgeCurrency = BridgeCurrency.fromString(token);
     const bridge = Bridge.from(this.bridgeConfig, bridgeCurrency);
-    const anchor = bridge.anchors.find((anchor) => anchor.amount == parseNote.note.amount);
-    logger.trace(`Hydrated deposit from secrets`, deposit);
-    logger.info(`Dest chainId ${evmChainId}  tokenSymbol ${token} , anchors`, anchor);
+
     try {
-      transactionNotificationConfig.loading?.({
-        address: recipient,
-        data: React.createElement(
-          'p',
-          { style: { fontSize: '.9rem' } }, // Matches Typography variant=h6
-          `Withdraw in progress`
-        ),
-        key: 'bridge-withdraw-evm',
-        path: {
-          method: 'withdraw',
-          section: `Bridge ${bridge.currency.chainIds.map(getEVMChainNameFromInternal).join('-')}`,
-        },
-      });
-      if (!anchor) {
-        throw new Error('not Anchor for amount' + parseNote.note.chain);
+      if (depositNote.sourceChain === depositNote.chain) {
+        logger.trace(`Same chain flow ${sourceChainName}`);
+        return this.sameChainWithdraw(depositNote, recipient);
+      } else {
+        logger.trace(`cross chain flow ${sourceChainName} ----> ${targetChainName}`);
+        return this.crossChainWithdraw(depositNote, recipient);
       }
-      const activeChainId = await this.inner.getChainId();
-      const internalChainId = evmIdIntoChainId(activeChainId);
-      const contractAddress = anchor.anchorAddresses[internalChainId];
-      logger.trace(
-        `Active chain id ${activeChainId} -> internal ChainId$ {internalChainId} === anchor contract address ${contractAddress}`
-      );
-      if (!contractAddress) {
-        throw new Error(`No Anchor for the chain ${internalChainId}`);
-      }
-      const contract = this.inner.getWebbAnchorByAddress(contractAddress);
-      const accounts = await this.inner.accounts.accounts();
-      logger.info(`Commitment for withdraw is ${deposit.commitment}`);
-      const input = {
-        destinationChainId: evmChainId,
-        secret: deposit.secret,
-        nullifier: deposit.nullifier,
-        nullifierHash: deposit.nullifierHash,
-
-        // Todo change this to the realyer address
-        relayer: accounts[0].address,
-        recipient: accounts[0].address,
-
-        fee: 0,
-        refund: 0,
-      };
-      logger.trace(`input for zkp`, input);
-      const zkpResults = await contract.generateZKP(deposit, input);
-      this.emit('stateChange', WithdrawState.SendingTransaction);
-      logger.trace(`Attempt to withdraw `);
-      await contract.withdraw(
-        zkpResults.proof,
-        {
-          destinationChainId: evmChainId,
-          fee: input.fee,
-          nullifier: input.nullifier,
-          nullifierHash: input.nullifierHash,
-          pathElements: zkpResults.input.pathElements,
-          pathIndices: zkpResults.input.pathIndices,
-          recipient: input.recipient,
-          refund: input.refund,
-          relayer: input.relayer,
-          root: zkpResults.root as any,
-          secret: zkpResults.input.secret,
-        },
-        zkpResults.input
-      );
-      this.emit('stateChange', WithdrawState.Ideal);
-      transactionNotificationConfig.finalize?.({
-        address: recipient,
-        data: undefined,
-        key: 'bridge-withdraw-evm',
-        path: {
-          method: 'withdraw',
-          section: `Bridge ${bridge.currency.chainIds.map(getEVMChainNameFromInternal).join('-')}`,
-        },
-      });
     } catch (e) {
       this.emit('stateChange', WithdrawState.Ideal);
       transactionNotificationConfig.failed?.({
@@ -119,11 +133,5 @@ export class Web3BridgeWithdraw extends BridgeWithdraw<WebbWeb3Provider> {
         },
       });
     }
-    /*
-     * TODO
-     * - Generate ZKP based on the bridge circuit
-     * - emit events for UI
-     *  - submit transaction
-     * */
   }
 }
