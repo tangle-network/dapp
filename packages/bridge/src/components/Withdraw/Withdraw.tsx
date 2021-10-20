afe9deab1bf3c9d45181b130165844e3de896dc3
Alex Veron <69320101+daylight1019@users.noreply.github.com>
2021-10-20 20:55:56 +0300

    Alex.update bridge relayer (#187)
    Co-authored-by: Alex <alexv@commonwealth.im>

--- packages/bridge/src/components/Withdraw/Withdraw.tsx
+++ packages/bridge/src/components/Withdraw/Withdraw.tsx
@@ -1,10 +1,10 @@
+import React, { useCallback, useMemo, useState } from 'react';
 import { FormHelperText, InputBase } from '@material-ui/core';
 import WithdrawingModal from '@webb-dapp/bridge/components/Withdraw/WithdrawingModal';
 import { SpaceBox } from '@webb-dapp/ui-components';
 import { InputLabel } from '@webb-dapp/ui-components/Inputs/InputLabel/InputLabel';
 import { BridgeNoteInput } from '@webb-dapp/ui-components/Inputs/NoteInput/BridgeNoteInput';
 import { Modal } from '@webb-dapp/ui-components/Modal/Modal';
-import React, { useState } from 'react';
 import styled from 'styled-components';
 import { WithdrawState } from '@webb-dapp/react-environment';
 import { InputSection } from '@webb-dapp/ui-components/Inputs/InputSection/InputSection';
@@ -12,6 +12,8 @@ import { useDepositNote } from '@webb-dapp/mixer';
 import { useWithdraw } from '@webb-dapp/bridge/hooks';
 import WithdrawSuccessModal from '@webb-dapp/react-components/Withdraw/WithdrawSuccessModal';
 import { MixerButton } from '@webb-dapp/ui-components/Buttons/MixerButton';
+import RelayerInput, { FeesInfo, RelayerApiAdapter } from '@webb-dapp/ui-components/RelayerInput/RelayerInput';
+import { ActiveWebbRelayer, WebbRelayer } from '@webb-dapp/react-environment/webb-context/relayer';
 
 const WithdrawWrapper = styled.div``;
 type WithdrawProps = {};
@@ -20,10 +22,38 @@ export const Withdraw: React.FC<WithdrawProps> = () => {
   const [note, setNote] = useState('');
   const [recipient, setRecipient] = useState('');
 
-  const { canCancel, cancelWithdraw, receipt, setReceipt, stage, validationErrors, withdraw } = useWithdraw({
+  const { canCancel, cancelWithdraw, receipt, relayerMethods, relayersState, setReceipt, setRelayer, stage, validationErrors, withdraw } = useWithdraw({
     recipient,
     note,
   });
+  console.log("relayerState", relayersState);
+  const feesGetter = useCallback(
+    async (activeRelayer: ActiveWebbRelayer): Promise<FeesInfo> => {
+      const defaultFees: FeesInfo = {
+        totalFees: 0,
+        withdrawFeePercentage: 0,
+      };
+      try {
+        const fees = await activeRelayer.fees(note);
+        return fees || defaultFees;
+      } catch (e) {
+        console.log(e);
+      }
+      return defaultFees;
+    },
+    [note]
+  );
+
+  const relayerApi: RelayerApiAdapter = useMemo(() => {
+    return {
+      getInfo: async (endpoint) => {
+        return relayerMethods?.fetchCapabilities(endpoint) ?? ({} as any);
+      },
+      add(endPoint: string, _persistent: boolean) {
+        return relayerMethods?.addRelayer(endPoint);
+      },
+    };
+  }, [relayerMethods]);
 
   const depositNote = useDepositNote(note);
   return (
@@ -51,6 +81,20 @@ export const Withdraw: React.FC<WithdrawProps> = () => {
       </InputSection>
 
       <SpaceBox height={16} />
+
+      {depositNote && (
+        <>
+          <RelayerInput
+            tokenSymbol={depositNote?.note.tokenSymbol || ''}
+            feesGetter={feesGetter}
+            relayers={relayersState.relayers}
+            setActiveRelayer={setRelayer}
+            relayerApi={relayerApi}
+            activeRelayer={relayersState.activeRelayer}
+          />
+          <SpaceBox height={16} />
+        </>
+      )}
 
       <MixerButton disabled={!recipient} onClick={withdraw} label={'Withdraw'} />
       <Modal open={stage !== WithdrawState.Ideal}>
