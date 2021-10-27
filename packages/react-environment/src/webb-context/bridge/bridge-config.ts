0f661d41a53e6b7ca82004d287ae1a8ae125b4ee
ahmed korim <ahmedkorrim@gmail.com>
2021-10-27 13:13:17 +0200

    Update  bridge config adding dummy tokens

--- packages/react-environment/src/webb-context/bridge/bridge-config.ts
+++ packages/react-environment/src/webb-context/bridge/bridge-config.ts
@@ -19,10 +19,48 @@ const webWEBBRinkebyHarmonyTestnet0 = new BridgeCurrency(
   [ChainId.Rinkeby, ChainId.HarmonyTestnet0],
   WebbCurrencyId.WEBB
 );
+/// test tokens
+const webETHRinkebyHarmonyTestnet0 = new BridgeCurrency([ChainId.Rinkeby, ChainId.HarmonyTestnet0], WebbCurrencyId.ETH);
+const webEDGRinkebyBeresheet = new BridgeCurrency([ChainId.Rinkeby, ChainId.EdgewareLocalNet], WebbCurrencyId.EDG);
+const webOneRinkebyHarmonyTestnet0 = new BridgeCurrency([ChainId.Rinkeby, ChainId.HarmonyTestnet0], WebbCurrencyId.ONE);
 
 export const bridgeConfig: BridgeConfig = {
   [webWEBBRinkebyHarmonyTestnet0.name]: {
-    asset: webWEBBRinkebyHarmonyTestnet0,
+    asset: webETHRinkebyHarmonyTestnet0,
+    tokenAddresses: {
+      [ChainId.HarmonyTestnet0]: '0x9d609F54536Cef34f5F612BD976ca632F1fa208E',
+      [ChainId.Rinkeby]: '0x7Cec2Bf7D9c4C3C96Da8a0BfeBAB1E84b8212394',
+    },
+    anchors: [
+      {
+        anchorAddresses: {
+          [ChainId.HarmonyTestnet0]: '0x64E9727C4a835D518C34d3A50A8157120CAeb32F',
+          [ChainId.Rinkeby]: '0xB42139fFcEF02dC85db12aC9416a19A12381167D',
+        },
+        amount: '.1',
+      },
+    ],
+  },
+
+  [webEDGRinkebyBeresheet.name]: {
+    asset: webEDGRinkebyBeresheet,
+    tokenAddresses: {
+      [ChainId.HarmonyTestnet0]: '0x9d609F54536Cef34f5F612BD976ca632F1fa208E',
+      [ChainId.EdgewareLocalNet]: '0x7Cec2Bf7D9c4C3C96Da8a0BfeBAB1E84b8212394',
+    },
+    anchors: [
+      {
+        anchorAddresses: {
+          [ChainId.HarmonyTestnet0]: '0x64E9727C4a835D518C34d3A50A8157120CAeb32F',
+          [ChainId.EdgewareLocalNet]: '0x7Cec2Bf7D9c4C3C96Da8a0BfeBAB1E84b8212394',
+        },
+        amount: '.1',
+      },
+    ],
+  },
+
+  [webOneRinkebyHarmonyTestnet0.name]: {
+    asset: webOneRinkebyHarmonyTestnet0,
     tokenAddresses: {
       [ChainId.HarmonyTestnet0]: '0x9d609F54536Cef34f5F612BD976ca632F1fa208E',
       [ChainId.Rinkeby]: '0x7Cec2Bf7D9c4C3C96Da8a0BfeBAB1E84b8212394',
