65d8f94b26a701779959ad2e68fe30742e776669
Nathan Barnavon <nepoche@yahoo.com>
2021-11-01 16:36:41 -0400

    Add the newest deployed bridges to the dapp

--- packages/react-environment/src/webb-context/bridge/bridge-config.ts
+++ packages/react-environment/src/webb-context/bridge/bridge-config.ts
@@ -15,26 +15,47 @@ export type BridgeConfigEntry = {
 };
 export type BridgeConfig = Record<string, BridgeConfigEntry>;
 
-const webbETHtest1 = new BridgeCurrency([ChainId.Ropsten, ChainId.Rinkeby, ChainId.Goerli], WebbCurrencyId.WEBB);
+const webWEBBRinkebyHarmonyTestnet0 = new BridgeCurrency(
+  [ChainId.Rinkeby, ChainId.HarmonyTestnet0],
+  WebbCurrencyId.WEBB
+);
+const webbETHtest1 = new BridgeCurrency([ChainId.Ropsten, ChainId.Rinkeby, ChainId.Goerli], WebbCurrencyId.ETH);
 
 export const bridgeConfig: BridgeConfig = {
-  [webbETHtest1.name]: {
-    asset: webbETHtest1,
+  [webWEBBRinkebyHarmonyTestnet0.name]: {
+    asset: webWEBBRinkebyHarmonyTestnet0,
     tokenAddresses: {
-      [ChainId.Ropsten]: '0xE91EB19dd96512D33b7a87A8B56b63AF6db103e4',
-      [ChainId.Rinkeby]: '0xcCD83a6ce5512C8dda9043Fc5F635816F66D8B63',
-      [ChainId.Goerli]: '0x64E9727C4a835D518C34d3A50A8157120CAeb32F',
+      [ChainId.HarmonyTestnet0]: '0x9d609F54536Cef34f5F612BD976ca632F1fa208E',
+      [ChainId.Rinkeby]: '0x7Cec2Bf7D9c4C3C96Da8a0BfeBAB1E84b8212394',
     },
     anchors: [
       {
         anchorAddresses: {
-          [ChainId.Ropsten]: '0xB039952e6A46b890e2a44835d97dE253482b2Ca2',
-          [ChainId.Rinkeby]: '0x33EAD0AE289f172E00BDD2e5c39BDF5b18F9d63A',
-          [ChainId.Goerli]: '0x19b72e48a90975a5B9F88C5ae907607A13bf827A',
+          [ChainId.HarmonyTestnet0]: '0xB4182B3813B7b70BFd3D4d4A5861123F6eC02cfD',
+          [ChainId.Rinkeby]: '0x50B180B63aaacAb76AA029302faaA7d456CFCA64',
+        },
+        amount: '.1',
+      },
+    ],
+  },
+  [webbETHtest1.name]: {
+    asset: webbETHtest1,
+    tokenAddresses: {
+      [ChainId.Ropsten]: '0x0000000000000000000000000000000000000000',
+      [ChainId.Rinkeby]: '0x0000000000000000000000000000000000000000',
+      [ChainId.Goerli]: '0x0000000000000000000000000000000000000000',
+    },
+    anchors: [
+      {
+        anchorAddresses: {
+          [ChainId.Ropsten]: '0x0711Ea63FDEDD2c8a9d3C9340a5A8F6cd84b6A92',
+          [ChainId.Rinkeby]: '0x15A66977f0A9D21e09eB6C1B42b001aF992f0C8f',
+          [ChainId.Goerli]: '0x025348e15e9d5529E5A4A55E8eA7eC923b7fB8b6',
         },
         amount: '.01',
       },
     ],
   },
 };
+
 console.log({ bridgeConfig });
