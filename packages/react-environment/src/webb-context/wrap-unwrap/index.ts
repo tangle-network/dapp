df31ff431b63ef9add54a2ceb47d0b1e8e34fea6
ahmed korim <ahmedkorrim@gmail.com>
2021-11-08 13:49:21 +0200

    Comments

--- packages/react-environment/src/webb-context/wrap-unwrap/index.ts
+++ packages/react-environment/src/webb-context/wrap-unwrap/index.ts
@@ -10,26 +10,59 @@ export abstract class WrapUnWrap<T, WrapPayload extends Object = any, UnwrapPayl
   setCurrentToken(nextTokenAddress: string | null) {
     this._currentTokenAddress.next(nextTokenAddress);
   }
-
+  /**
+   *  Current token
+   *  */
   get currentToken() {
     return this._currentTokenAddress.value;
   }
 
+  /**
+   *  watcher of the current token
+   *  */
   get $currentTokenValue() {
     return this._currentTokenAddress;
   }
 
   abstract getSizes(): Promise<MixerSize[]>;
+
+  /**
+   * Wrapped token that is available for the current token
+   *  */
   abstract getWrappedTokens(): Promise<BridgeCurrency[]>;
+
+  /**
+   * Wrapped token that is available for the current token
+   *  */
   abstract getNativeTokens(): Promise<WebbCurrencyId[]>;
 
+  /**
+   *  For validation
+   * */
   abstract canWrap(wrapPayload: WrapPayload): Promise<boolean>;
+
+  /**
+   *  For validation
+   * */
   abstract canUnWrap(unwrapPayload: UnwrapPayload): Promise<boolean>;
 
+  /**
+   *  Get list of all the Governed tokens
+   * */
   abstract getGovernedTokens(): Promise<BridgeCurrency[]>;
 
-  // transaction has
+  /**
+   *  Wrap call
+   *  - Can wrap a token to a GovernedToken
+   *  - Can wrap a token to an ERC20
+   * */
   abstract wrap(wrapPayload: WrapPayload): Promise<string>;
+
+  /**
+   *  UNwrap call
+   *  - Can Unwrap a token to a GovernedToken
+   *  - Can Unwrap a token to an ERC20
+   * */
   abstract unwrap(unwrapPayload: UnwrapPayload): Promise<string>;
 
   // todo add interfaces for infos , supply balance etc
