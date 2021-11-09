ae8f083fbc630911f622b4e805ee2d898f280f07
Nathan Barnavon <nepoche@yahoo.com>
2021-11-09 15:49:41 -0400

    Add withdraw success modal after successful relayed withdraw

--- packages/react-environment/src/api-providers/web3/web3-mixer-withdraw.ts
+++ packages/react-environment/src/api-providers/web3/web3-mixer-withdraw.ts
@@ -234,6 +234,9 @@ export class Web3MixerWithdraw extends MixerWithdraw<WebbWeb3Provider> {
         logger.trace('Sending transaction');
         relayedWithdraw.send(tx);
         const txHash = await relayedWithdraw.await();
+        if (txHash[1]) {
+          return txHash[1];
+        }
         return '';
       } catch (e) {
         this.emit('stateChange', WithdrawState.Failed);
