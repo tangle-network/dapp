9303b7f7baf488ca84180ef31203d12cbd1d86cd
Nathan Barnavon <nepoche@yahoo.com>
2021-11-10 19:21:04 -0400

    Update adblock text

--- packages/react-components/src/PermissionedAccess/DisableAdblock.tsx
+++ packages/react-components/src/PermissionedAccess/DisableAdblock.tsx
@@ -7,7 +7,11 @@ type DisableAdblockProps = {};
 export const DisableAdblock: React.FC<DisableAdblockProps> = () => {
   return (
     <ContentWrapper>
-      <Typography variant={'h2'}>Disable adblock to use the DApp so we may correctly identify your location</Typography>
+      <Typography variant={'h1'}>Please disable adblock</Typography>
+      <Typography variant={'h3'} style={{ paddingTop: '30px' }}>
+        In order to comply with regulations, we must make calls to third-party APIs to prevent unauthorized users. Your
+        adblock software is preventing these calls from happening.
+      </Typography>
     </ContentWrapper>
   );
 };
