17a79ee537bb5e6280ae12e34f6208fbfdfd7c45
Nathan Barnavon <nepoche@yahoo.com>
2021-11-10 13:06:43 -0400

    Add anchor ipfs configurations

--- packages/apps/src/configs/ipfs/anchors.ts
+++ packages/apps/src/configs/ipfs/anchors.ts
@@ -1,6 +1,6 @@
 
 
-export const fetchWasmForEdges = async (maxEdges: number): Promise<ArrayBuffer> => {
+export const fetchWasmForEdges = async (maxEdges: number) => {
   let ipfsHash: string;
 
   switch (maxEdges) {
@@ -25,8 +25,43 @@ export const fetchWasmForEdges = async (maxEdges: number): Promise<ArrayBuffer> 
   }
 
   try {
-    const ipfsWasmRequest = fetch(`https://ipfs.co`)
+    const ipfsWasmRequest = await fetch(`https://ipfs.io/ipfs/${ipfsHash}`);
+    const wasmBuf = await ipfsWasmRequest.arrayBuffer();
+    return wasmBuf;
   } catch (e) {
     console.log('error when fetching wasm from ipfs: ', e);
   }
 };
+
+export const fetchKeyForEdges = async (maxEdges: number) => {
+  let ipfsHash: string;
+
+  switch (maxEdges) {
+    case 1:
+      ipfsHash = 'QmUBkb5P6CJbtywEdr2xiAY8rJz4Aak1uvu28oxoC3Z5QF';
+      break;
+    case 2:
+      ipfsHash = 'QmRukP3kL2dHQc9jpV4gdwi2WVLsbQk8j72WXeqJY2xqQa';
+      break;
+    case 3:
+      ipfsHash = 'QmWaGocEr1NMKageZmEUCoDxcQVeYCkddcDnW6z4JX4mh7';
+      break;
+    case 4:
+      ipfsHash = 'QmRzqsRmXNCkm9ZrETFygLG6TgRQBnYyhYxAsQvwPsr3dH';
+      break;
+    case 5:
+      ipfsHash = 'QmR6jms4hbvtzore2wWeri1Q9dCt9hoRAAx2jBq9BLg4Ak';
+      break;
+    default:
+      ipfsHash = 'QmUBkb5P6CJbtywEdr2xiAY8rJz4Aak1uvu28oxoC3Z5QF';
+      break;
+  }
+
+  try {
+    const ipfsKeyRequest = await fetch(`https://ipfs.io/ipfs/${ipfsHash}`);
+    const circuitKey = await ipfsKeyRequest.arrayBuffer();
+    return circuitKey;
+  } catch (e) {
+    console.log('error when fetching circuit key from ipfs: ', e);
+  }
+}
