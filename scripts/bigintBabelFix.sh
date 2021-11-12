fe7f423b258742fdaa06f4bc2d9b6ec741ff1239
Nathan Barnavon <nepoche@yahoo.com>
2021-11-12 14:19:19 -0400

    Bash script annotation at top of post-build script file

--- scripts/bigintBabelFix.sh
+++ scripts/bigintBabelFix.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   find ./build/ -type f -name "*.js" -print0 | xargs -0 sed -i '' -e 's/{return Math.pow(BigInt(e),BigInt(t))}/{return BigInt(e)**BigInt(t)}/g'
 else
