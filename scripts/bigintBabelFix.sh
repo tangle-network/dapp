5536efa4eb9ab4f4c48a35a710ee174a7b161dc9
Nathan Barnavon <nepoche@yahoo.com>
2021-11-12 12:18:16 -0400

    Update post-build script for unix platforms

--- scripts/bigintBabelFix.sh
+++ scripts/bigintBabelFix.sh
@@ -1,1 +1,5 @@
-find ./build/ -type f -name "*.js" -print0 | xargs -0 sed -i '' -e 's/{return Math.pow(BigInt(e),BigInt(t))}/{return BigInt(e)**BigInt(t)}/g'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ./build/ -type f -name "*.js" -print0 | xargs -0 sed -i '' -e 's/{return Math.pow(BigInt(e),BigInt(t))}/{return BigInt(e)**BigInt(t)}/g'
+else
+  find ./build/ -type f -name "*.js" -print0 | xargs -0 sed -i -e 's/{return Math.pow(BigInt(e),BigInt(t))}/{return BigInt(e)**BigInt(t)}/g'
+fi
