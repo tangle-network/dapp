25e98e245e83bcdfe952c724c18e497c3d60a0f1
Ahmed Korim <ahmedkorrim@gmail.com>
2022-01-12 14:57:04 +0200

    organise imports

--- packages/mixer/src/hooks/mixer-groups-entry-wrapper.class.ts
+++ packages/mixer/src/hooks/mixer-groups-entry-wrapper.class.ts
@@ -1,8 +1,8 @@
+import { Currency } from '@webb-dapp/mixer/utils/currency';
+import { Token } from '@webb-tools/sdk-core';
 import { Balance, PalletMixerMixerMetadata as MixerInfo } from '@webb-tools/types/interfaces';
 
-import { Currency } from '@webb-dapp/mixer/utils/currency';
 import { StorageKey } from '@polkadot/types';
-import { Token } from '@webb-tools/sdk-core';
 
 export type NativeTokenProperties = {
   ss58Format: number | null;
