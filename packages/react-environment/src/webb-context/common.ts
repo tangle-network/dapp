1e9b6a628bee298438c5d4ee871e7342ed0eb454
ahmed korim <ahmedkorrim@gmail.com>
2022-02-09 12:54:20 +0200

    Added static config helpers to `AppConfigApi`

--- packages/react-environment/src/webb-context/common.ts
+++ packages/react-environment/src/webb-context/common.ts
@@ -5,6 +5,11 @@ import { CurrencyConfig } from '@webb-dapp/react-environment/types/currency-conf
 import { MixerConfig } from '@webb-dapp/react-environment/types/mixer-config.interface';
 import { WalletConfig } from '@webb-dapp/react-environment/types/wallet-config.interface';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { ChainId, chainsConfig, currenciesConfig, WebbCurrencyId } from '@webb-dapp/apps/configs';
+import { LoggerService } from '@webb-tools/app-util';
+import { WebbError, WebbErrorCodes } from '@webb-dapp/utils/webb-error';
+
+const logger = LoggerService.get('config api');
 
 export type Chain = ChainConfig & {
   wallets: Record<number, Wallet>;
@@ -43,5 +48,45 @@ export class AppConfigApi {
       ...partialConfig,
     };
     this._appConfig.next(nextConfig);
+  }
+
+  public getAnchorAddressForBridge(assetId: WebbCurrencyId, chainId: number, amount: number): string | undefined {
+    const linkedAnchorConfig = this.config.bridgeByAsset[assetId]?.anchors.find(
+      (anchor) => anchor.amount === amount.toString()
+    );
+    if (!linkedAnchorConfig) {
+      throw new Error('Unsupported configuration for bridge');
+    }
+
+    const anchorAddress = linkedAnchorConfig.anchorAddresses[chainId as ChainId];
+    logger.log('got anchor address: ', anchorAddress);
+    return anchorAddress;
+  }
+
+  public getNativeCurrencySymbol(evmId: number): string {
+    const chain = Object.values(this.config.chains).find((chainsConfig) => chainsConfig.evmId === evmId);
+    if (chain) {
+      const nativeCurrency = chain.nativeCurrencyId;
+      return currenciesConfig[nativeCurrency].symbol;
+    }
+    return 'Unknown';
+  }
+
+  public getEVMChainName(evmId: number): string {
+    const chain = Object.values(this.config.chains).find((chainsConfig) => chainsConfig.evmId === evmId);
+    if (chain) {
+      return chain.name;
+    } else {
+      throw WebbError.from(WebbErrorCodes.UnsupportedChain);
+    }
+  }
+
+  public getEVMChainNameFromInternal(chainID: number): string {
+    const chain = Object.values(this.config.chains).find((chainsConfig) => chainsConfig.id === chainID);
+    if (chain) {
+      return chain.name;
+    } else {
+      throw WebbError.from(WebbErrorCodes.UnsupportedChain);
+    }
   }
 }
