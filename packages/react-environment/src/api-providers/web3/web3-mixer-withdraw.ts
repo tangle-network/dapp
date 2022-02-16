76c158e5c8d76e2a16a4ecf18ef47e72f4bff386
nepoche <nepoche@yahoo.com>
2022-02-16 19:26:53 -0400

    Add web3 mixer withdraw fix

--- packages/react-environment/src/api-providers/web3/web3-mixer-withdraw.ts
+++ packages/react-environment/src/api-providers/web3/web3-mixer-withdraw.ts
@@ -278,14 +278,23 @@ export class Web3MixerWithdraw extends MixerWithdraw<WebbWeb3Provider> {
       });
       try {
         if (relayers.length) {
-          const relayerLeaves = await relayers[0].getLeaves(chainEvmId.toString(16), contract.inner.address);
-
-          zkp = await contract.generateZKPWithLeaves(
-            deposit,
-            zkpInputWithoutMerkleProof,
-            relayerLeaves.leaves,
-            relayerLeaves.lastQueriedBlock
-          );
+          try {
+            const relayerLeaves = await relayers[0].getLeaves(chainEvmId.toString(16), contract.inner.address);
+            zkp = await contract.generateZKPWithLeaves(
+              deposit,
+              zkpInputWithoutMerkleProof,
+              relayerLeaves.leaves,
+              relayerLeaves.lastQueriedBlock
+            );
+          } catch (e) {
+            // If attempting to fetch leaves from the available relayer failed, query from chain.
+            if ((e as any)?.code === WebbErrorCodes.RelayerMisbehaving) {
+              zkp = await contract.generateZKP(deposit, zkpInputWithoutMerkleProof);
+            } else {
+              logger.log('error in web3 mixer withdraw: ', e);
+              throw e;
+            }
+          }
         } else {
           // This is the part of withdraw that takes a long time
           zkp = await contract.generateZKP(deposit, zkpInputWithoutMerkleProof);
