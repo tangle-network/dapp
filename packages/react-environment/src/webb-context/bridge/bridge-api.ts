d3693af79fa121f8af339c92838cea5fe2215d08
Ahmed Korim <ahmedkorrim@gmail.com>
2022-02-22 21:37:21 +0200

    Update packages/react-environment/src/webb-context/bridge/bridge-api.ts
    Co-authored-by: nepoche <nepoche@yahoo.com>

--- packages/react-environment/src/webb-context/bridge/bridge-api.ts
+++ packages/react-environment/src/webb-context/bridge/bridge-api.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Currency } from '../currency/currency';
 
-export type BridgeCurrencyIndex = string | number | WebbCurrencyId;
+export type BridgeCurrencyIndex = string | WebbCurrencyId;
 
 type BridgeStore<BridgeConfigEntry, BridgeConfig = Record<BridgeCurrencyIndex, BridgeConfigEntry>> = {
   config: BridgeConfig;
