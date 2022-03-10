6cdcccd4f7a31868988af8050eb66da1f48b1bc4
nepoche <nepoche@yahoo.com>
2022-03-10 12:10:55 -0400

    Update the deposit tab

--- packages/ui-components/src/Inputs/MixerGroupSelect/MixerGroupSelect.tsx
+++ packages/ui-components/src/Inputs/MixerGroupSelect/MixerGroupSelect.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/indent */
 import { MixerSize } from '@webb-dapp/react-environment/webb-context';
-import { InputLabel } from '@webb-dapp/ui-components/Inputs/InputLabel/InputLabel';
 import { Pallet } from '@webb-dapp/ui-components/styling/colors';
 import { FontFamilies } from '@webb-dapp/ui-components/styling/fonts/font-families.enum';
 import React, { useEffect, useMemo } from 'react';
 import styled, { css } from 'styled-components';
+import { FlexBox, getRoundedAmountString } from '../..';
 
 import { InputSection } from '../InputSection/InputSection';
 
@@ -22,19 +22,19 @@ type MixerGroupSelectProps = {
   value?: MixerSize;
   onChange?(item: MixerSize): void;
 };
-const AmountChipWrapper = styled.span<{ selected?: boolean }>`
+const AmountChipWrapper = styled.div<{ selected?: boolean }>`
   cursor: pointer;
   transition: all ease 0.3s;
   text-transform: capitalize;
+  height: 50px;
+  max-width: 50px;
   ${({ selected, theme }: { theme: Pallet; selected?: boolean }) => css`
     && {
       border: 1px solid ${selected && theme.type === 'dark' ? theme.accentColor : theme.borderColor};
-      border-radius: 20px;
+      border-radius: 50%;
       background: ${selected && theme.type === 'light' ? theme.accentColor : 'transparent'};
       font-family: ${FontFamilies.AvenirNext};
       color: ${selected && theme.type === 'dark' ? theme.accentColor : theme.primaryText};
-      height: 40px;
-      padding: 0 5px;
       flex: 1;
       margin: 5px;
       white-space: nowrap;
@@ -44,7 +44,7 @@ const AmountChipWrapper = styled.span<{ selected?: boolean }>`
       font-size: 14px;
 
       font-style: normal;
-      font-weight: 500;
+      font-weight: 600;
       line-height: 19px;
       text-align: center;
       letter-spacing: -0.065em;
@@ -74,28 +74,29 @@ export const MixerGroupSelect: React.FC<MixerGroupSelectProps> = ({ items, onCha
 
   return (
     <InputSection>
-      <InputLabel label={'Select Amount'}>
-        <MixerGroupSelectWrapper>
-          {mixerSizes.map(({ amount, asset, id, selected, title }) => {
-            return (
-              <AmountChipWrapper
-                key={id + title}
-                selected={selected}
-                onClick={() => {
-                  onChange?.({
-                    title,
-                    id,
-                    amount,
-                    asset,
-                  });
-                }}
-              >
-                {amount}
-              </AmountChipWrapper>
-            );
-          })}
-        </MixerGroupSelectWrapper>
-      </InputLabel>
+      <MixerGroupSelectWrapper style={{ width: '80%' }}>
+        {mixerSizes.map(({ id, selected, title, amount, asset }) => {
+          return (
+            <AmountChipWrapper
+              key={id + title}
+              selected={selected}
+              onClick={() => {
+                onChange?.({
+                  title,
+                  id,
+                  amount,
+                  asset
+                });
+              }}
+            >
+              {getRoundedAmountString(amount)}
+            </AmountChipWrapper>
+          );
+        })}
+      </MixerGroupSelectWrapper>
+      <FlexBox width='20%' justifyContent='center' alignItems='center'>
+        <p>{items[0] && items[0].asset}</p>
+      </FlexBox>
     </InputSection>
   );
 };
