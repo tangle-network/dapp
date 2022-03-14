05ac78eecf2434240c3c944d5861186415541a7f
nepoche <nepoche@yahoo.com>
2022-03-14 09:14:25 -0400

    Finish wrap/unwrap UI update

--- packages/page-wrap-unwrap/src/index.tsx
+++ packages/page-wrap-unwrap/src/index.tsx
@@ -2,6 +2,7 @@ import { Button, Checkbox, FormControlLabel, IconButton, InputBase, Tooltip } fr
 import Icon from '@material-ui/core/Icon';
 import Typography from '@material-ui/core/Typography';
 import { useWrapUnwrap } from '@webb-dapp/page-wrap-unwrap/hooks/useWrapUnwrap';
+import { RequiredWalletSelection } from '@webb-dapp/react-components/RequiredWalletSelection/RequiredWalletSelection';
 import { useWebContext } from '@webb-dapp/react-environment';
 import { useColorPallet } from '@webb-dapp/react-hooks/useColorPallet';
 import { getRoundedAmountString, SpaceBox } from '@webb-dapp/ui-components';
@@ -9,7 +10,6 @@ import { MixerButton } from '@webb-dapp/ui-components/Buttons/MixerButton';
 import { Flex } from '@webb-dapp/ui-components/Flex/Flex';
 import { TokenInput, TokenInputProps } from '@webb-dapp/ui-components/Inputs/TokenInput/TokenInput';
 import { Pallet } from '@webb-dapp/ui-components/styling/colors';
-import { above } from '@webb-dapp/ui-components/utils/responsive-utils';
 import { LoggerService } from '@webb-tools/app-util';
 import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import styled, { css } from 'styled-components';
@@ -17,16 +17,18 @@ import styled, { css } from 'styled-components';
 const logger = LoggerService.get('page-wrap-unwrap');
 
 const TransferWrapper = styled.div`
-  padding: 1rem;
-  ${above.sm`  padding: 2rem;`}
-  max-width: 500px;
   margin: auto;
-  border-radius: 20px;
-  ${({ theme }: { theme: Pallet }) => css`
-    background: ${theme.layer1Background};
-    border: 1px solid ${theme.borderColor};
-  `}
+  max-width: 500px;
+
+  ${({ theme }) => {
+    return css`
+      background: ${theme.layer2Background};
+      border: 1px solid ${theme.borderColor};
+      border-radius: 0 0 13px 13px;
+    `;
+  }}
 `;
+
 const AmountInputWrapper = styled.div`
   display: flex;
   ${({ theme }: { theme: Pallet }) => css`
@@ -48,47 +50,62 @@ const AmountButton = styled.button`
 const TabHeader = styled.header`
   display: flex;
   align-items: center;
-  background: ${({ theme }: { theme: Pallet }) => theme.tabHeader};
-
-  justify-content: space-between;
-  padding: 7px;
-  border-radius: 32px;
+  max-width: 500px;
+  margin: auto;
 `;
+
 const TabButton = styled.button<{ active?: boolean }>`
-  width: 48%;
+  width: 50%;
   display: flex;
   height: 46px;
   cursor: pointer;
   align-items: center;
   justify-content: center;
   transition: all 0.4s ease-in-out;
-  margin: 0 2px;
-
-  .mixer-tab-icon {
-    display: flex;
-    align-items: center;
-    margin-right: 10px;
-  }
 
   .mixer-tab-label {
     font-size: 16px;
+    color: ${({ active, theme }) => {
+      return !active ? theme.primaryText : theme.type === 'dark' ? theme.accentColor : '#000000';
+    }};
   }
 
-  color: ${({ active, theme }) => {
-    return !active ? (theme as Pallet).primaryText : (theme as Pallet).darkGray;
-  }};
-
-  ${({ active }) => {
-    return active
-      ? css`
-          background: #ffffff;
-          box-shadow: 0px 0px 14px rgba(54, 86, 233, 0.1);
-        `
-      : css`
-          background: transparent;
+  ${({ active, theme }) => {
+    if (active) {
+      if (theme.type === 'dark') {
+        return css`
+          background: #131313;
+          border-right: 1px solid ${theme.accentColor};
+          border-top: 1px solid ${theme.accentColor};
+          border-left: 1px solid ${theme.accentColor};
         `;
+      } else {
+        return css`
+          background: #ffffff;
+          border-right: 1px solid #000000;
+          border-top: 1px solid #000000;
+          border-left: 1px solid #000000;
+        `;
+      }
+    } else {
+      return css`
+        background: transparent;
+        border-right: 1px solid ${theme.borderColor};
+        border-top: 1px solid ${theme.borderColor};
+        border-left: 1px solid ${theme.borderColor};
+      `;
+    }
   }}
-  border-radius: 32px;
+
+  ${({ active, theme }) => {
+    if (active) {
+      return css`
+        border-right: 1px solid #${theme.accentColor};
+      `;
+    }
+  }}
+
+  border-radius: 12px 12px 0px 0px;
 `;
 
 const TokenBalance = styled.div`
@@ -99,7 +116,7 @@ const TokenBalance = styled.div`
 `;
 
 const PageWrapUnwrap: FC = () => {
-  const { activeApi, activeChain } = useWebContext();
+  const { activeApi, activeChain, activeWallet } = useWebContext();
   const {
     amount,
     context,
@@ -185,142 +202,125 @@ const PageWrapUnwrap: FC = () => {
 
   return (
     <div>
+      <TabHeader>
+        <TabButton onClick={switchToWrap} active={context === 'wrap'}>
+          <span className='mixer-tab-label'>Wrap</span>
+        </TabButton>
+        <TabButton onClick={switchToUnwrap} active={context === 'unwrap'}>
+          <span className='mixer-tab-label'>Unwrap</span>
+        </TabButton>
+      </TabHeader>
       <TransferWrapper>
-        <TabHeader>
-          <TabButton onClick={switchToWrap} active={context === 'wrap'}>
-            <span className='mixer-tab-icon'>
-              <svg width='36' height='36' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <circle cx='18' cy='18' r='18' fill='#3351F2' />
-                <path
-                  d='M17.2929 27.1923C17.6834 27.5828 18.3166 27.5828 18.7071 27.1923L25.0711 20.8284C25.4616 20.4378 25.4616 19.8047 25.0711 19.4141C24.6805 19.0236 24.0474 19.0236 23.6569 19.4141L18 25.071L12.3431 19.4141C11.9526 19.0236 11.3195 19.0236 10.9289 19.4141C10.5384 19.8047 10.5384 20.4378 10.9289 20.8284L17.2929 27.1923ZM17 9.51465L17 26.4852L19 26.4852L19 9.51465L17 9.51465Z'
-                  fill='white'
-                />
-              </svg>
-            </span>
-            <span className='mixer-tab-label'>Wrap</span>
-          </TabButton>
-          <TabButton onClick={switchToUnwrap} active={context === 'unwrap'}>
-            <span className='mixer-tab-icon'>
-              <svg width='36' height='36' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <circle cx='18' cy='18' r='18' fill='#52B684' />
-                <path
-                  d='M17.2929 9.29289C17.6834 8.90237 18.3166 8.90237 18.7071 9.29289L25.0711 15.6569C25.4616 16.0474 25.4616 16.6805 25.0711 17.0711C24.6805 17.4616 24.0474 17.4616 23.6569 17.0711L18 11.4142L12.3431 17.0711C11.9526 17.4616 11.3195 17.4616 10.9289 17.0711C10.5384 16.6805 10.5384 16.0474 10.9289 15.6569L17.2929 9.29289ZM17 26L17 10L19 10L19 26L17 26Z'
-                  fill='white'
-                />
-              </svg>
-            </span>
-            <span className='mixer-tab-label'>Unwrap</span>
-          </TabButton>
-        </TabHeader>
-
-        <SpaceBox height={16} />
-
-        <Flex
-          row
-          ai={'center'}
-          flex={1}
-          style={{
-            position: 'relative',
-          }}
-        >
-          <Flex flex={2}>
-            <TokenInput {...leftInputProps} />
-          </Flex>
-          <Flex flex={1} row ai={'center'} jc={'center'}>
-            <span>
-              <Tooltip title={'Swap tokens'}>
-                <IconButton
-                  onMouseEnter={() => {
-                    setIsSwap(true);
-                  }}
-                  onMouseLeave={() => {
-                    setIsSwap(false);
-                  }}
-                  onClick={() => {
-                    swap();
-                  }}
-                >
-                  <Icon>{isSwap ? 'swap_horiz' : 'east'}</Icon>
-                </IconButton>
-              </Tooltip>
-            </span>
-          </Flex>
-          <Flex flex={2}>
-            <TokenInput wrapperStyles={{ top: -25 }} {...rightInputProps} />
-          </Flex>
-        </Flex>
-        <SpaceBox height={16} />
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <div>
-            <Typography
-              variant='body2'
-              style={{ color: palette.type === 'dark' ? palette.accentColor : palette.primaryText }}
+        <RequiredWalletSelection>
+          <div style={{ padding: '25px' }}>
+            <Flex
+              row
+              ai={'center'}
+              flex={1}
+              style={{
+                position: 'relative',
+              }}
             >
-              Your Balance~
-            </Typography>
-          </div>
-          <TokenBalance>
-            {wrappableToken && context === "wrap" && (
-              <Typography variant='body2'>
-                <b>
-                  {getRoundedAmountString(wrappableTokenBalance)} {wrappableToken.view.symbol}
-                </b>
-              </Typography>
-            )}
-            {governedToken && context === "unwrap" && (
-              <Typography variant='body2'>
-                {getRoundedAmountString(governedTokenBalance)} {governedToken.view.symbol}
-              </Typography>
-            )}
-          </TokenBalance>
-        </div>
-        <SpaceBox height={16} />
-        <AmountInputWrapper>
-          <div style={{ width: '100%' }}>
-            <InputBase
-              placeholder={`Enter Amount`}
-              fullWidth
-              value={displayedAmount}
-              inputProps={{ style: { fontSize: 14, paddingLeft: '15px' } }}
-              onChange={(event) => {
-                setDisplayedAmount(event.target.value);
-                let maybeNumber = Number(event.target.value);
-                if (!Number.isNaN(maybeNumber)) {
-                  setAmount(Number(event.target.value));
-                } else {
-                  setAmount(null);
+              <Flex flex={2}>
+                <TokenInput {...leftInputProps} />
+              </Flex>
+              <Flex flex={1} row ai={'center'} jc={'center'}>
+                <span>
+                  <Tooltip title={'Swap tokens'}>
+                    <IconButton
+                      onMouseEnter={() => {
+                        setIsSwap(true);
+                      }}
+                      onMouseLeave={() => {
+                        setIsSwap(false);
+                      }}
+                      onClick={() => {
+                        swap();
+                      }}
+                    >
+                      <Icon>{isSwap ? 'swap_horiz' : 'east'}</Icon>
+                    </IconButton>
+                  </Tooltip>
+                </span>
+              </Flex>
+              <Flex flex={2}>
+                <TokenInput wrapperStyles={{ top: -25 }} {...rightInputProps} />
+              </Flex>
+            </Flex>
+            <SpaceBox height={16} />
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+              <div>
+                <Typography
+                  variant='body2'
+                  style={{ color: palette.type === 'dark' ? palette.accentColor : palette.primaryText }}
+                >
+                  Your Balance~
+                </Typography>
+              </div>
+              <TokenBalance>
+                {wrappableToken && context === "wrap" && (
+                  <Typography variant='body2'>
+                    <b>
+                      {getRoundedAmountString(wrappableTokenBalance)} {wrappableToken.view.symbol}
+                    </b>
+                  </Typography>
+                )}
+                {governedToken && context === "unwrap" && (
+                  <Typography variant='body2'>
+                    {getRoundedAmountString(governedTokenBalance)} {governedToken.view.symbol}
+                  </Typography>
+                )}
+              </TokenBalance>
+            </div>
+            <SpaceBox height={16} />
+            <AmountInputWrapper>
+              <div style={{ width: '100%' }}>
+                <InputBase
+                  placeholder={`Enter Amount`}
+                  fullWidth
+                  value={displayedAmount}
+                  inputProps={{ style: { fontSize: 14, paddingLeft: '15px' } }}
+                  onChange={(event) => {
+                    setDisplayedAmount(event.target.value);
+                    let maybeNumber = Number(event.target.value);
+                    if (!Number.isNaN(maybeNumber)) {
+                      setAmount(Number(event.target.value));
+                    } else {
+                      setAmount(null);
+                    }
+                  }}
+                />
+              </div>
+              <div>
+                <AmountButton color={'primary'} as={Button} onClick={() => {
+                  if (context === 'wrap') {
+                    setDisplayedAmount(wrappableTokenBalance.toString())
+                    setAmount(wrappableTokenBalance)
+                  } else {
+                    setDisplayedAmount(governedTokenBalance.toString())
+                    setAmount(governedTokenBalance)
+                  }
+                }}>
+                  MAX
+                </AmountButton>
+              </div>
+            </AmountInputWrapper>
+            <SpaceBox height={16} />
+
+            <MixerButton
+              disabled={loading || !governedToken || !wrappableToken || !amount}
+              label={buttonText}
+              onClick={async () => {
+                try {
+                  setLoading(true);
+                  await execute();
+                } finally {
+                  setLoading(false);
                 }
               }}
             />
           </div>
-          <div>
-            <AmountButton color={'primary'} as={Button} onClick={() => {
-              if (context === 'wrap') {
-                setDisplayedAmount(wrappableTokenBalance.toString())
-                setAmount(wrappableTokenBalance)
-              } else {
-                setDisplayedAmount(governedTokenBalance.toString())
-                setAmount(governedTokenBalance)
-              }
-            }}>
-              MAX
-            </AmountButton>
-          </div>
-        </AmountInputWrapper>
-        <SpaceBox height={16} />
-
-        <MixerButton
-          disabled={loading || !governedToken || !wrappableToken || !amount}
-          label={buttonText}
-          onClick={async () => {
-            try {
-              setLoading(true);
-              await execute();
-            } finally {
-              setLoading(false);
-            }
-          }}
-        />
+        </RequiredWalletSelection>
       </TransferWrapper>
     </div>
   );
