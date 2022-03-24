15be975ce17901d5fe32f41591129de5ffa5d3fa
nepoche <nepoche@yahoo.com>
2022-03-24 12:41:31 -0400

    Update production webpack builds

--- packages/apps/webpack.prod.js
+++ packages/apps/webpack.prod.js
@@ -8,16 +8,18 @@ const { merge } = require('webpack-merge');
 
 const baseConfig = require('./webpack.base.js');
 
-const context = __dirname;
-const hasPublic = fs.existsSync(path.join(context, 'public'));
+module.exports = (env) => {
+  env.context = __dirname;
+  const hasPublic = fs.existsSync(path.join(env.context, 'public'));
 
-module.exports = merge(baseConfig(context), {
-  devtool: process.env.BUILD_ANALYZE ? 'source-map' : false,
-  plugins: [
-    new HtmlWebpackPlugin({
-      PAGE_TITLE: 'Webb Portal',
-      inject: true,
-      template: path.join(context, `${hasPublic ? 'public/' : ''}index.html`),
-    }),
-  ],
-});
+  return merge(baseConfig(env), {
+    devtool: process.env.BUILD_ANALYZE ? 'source-map' : false,
+    plugins: [
+      new HtmlWebpackPlugin({
+        PAGE_TITLE: 'Webb Portal',
+        inject: true,
+        template: path.join(env.context, `${hasPublic ? 'public/' : ''}index.html`),
+      }),
+    ],
+  });
+};
