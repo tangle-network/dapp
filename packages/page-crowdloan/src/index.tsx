f482bfd73bb9aa026a5fa87dbd9bd5e18629b6fb
AtelyPham <tinpham1412000@gmail.com>
2022-06-15 01:32:44 +0700

    feat: add webb reward section

--- packages/page-crowdloan/src/index.tsx
+++ packages/page-crowdloan/src/index.tsx
@@ -1,6 +1,7 @@
 import { alpha, ButtonBase, Checkbox, InputBase, Typography } from '@material-ui/core';
 import { Currency } from '@webb-dapp/api-providers';
 import { WalletConfig } from '@webb-dapp/api-providers/types';
+import WEBBLogo from '@webb-dapp/apps/configs/logos/chains/WebbLogo';
 import { TokenBalance } from '@webb-dapp/mixer/components';
 import { pageWithFeatures } from '@webb-dapp/react-components/utils/FeaturesGuard/pageWithFeatures';
 import { useAppConfig, useWebContext } from '@webb-dapp/react-environment/webb-context';
@@ -10,13 +11,15 @@ import { MixerButton } from '@webb-dapp/ui-components/Buttons/MixerButton';
 import { Flex } from '@webb-dapp/ui-components/Flex/Flex';
 import { Pallet } from '@webb-dapp/ui-components/styling/colors';
 import { getRoundedAmountString } from '@webb-dapp/ui-components/utils';
-import { above } from '@webb-dapp/ui-components/utils/responsive-utils';
+import { above, useBreakpoint } from '@webb-dapp/ui-components/utils/responsive-utils';
 import { FixedPointNumber } from '@webb-tools/sdk-core';
 import { FC, useEffect, useMemo, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import { useCrowdloan } from './hooks/useCrowdloan';
 import { CrowdloanInfo } from './CrowdloanInfo';
+
+const ALLOTTED_REWARDS_POOL = 5000000;
 
 export const PageCrowdloanWrapper = styled.div`
   display: flex;
@@ -43,7 +46,13 @@ export const AmountInputWrapper = styled.div<{ disabled: boolean }>`
   padding: 8px 16px;
   align-items: center;
   justify-content: space-between;
+  margin: 0 1rem;
   margin-bottom: 8px;
+
+  ${above.sm`
+    margin: 0 2rem; 
+    margin-bottom: 8px;
+  `}
 
   && {
     cursor: ${({ disabled }) => (disabled ? 'no-drop' : 'auto')};
@@ -65,12 +74,10 @@ export const AmountButton = styled.button`
 
 const ContributeWrapper = styled.div<{ wallet: WalletConfig | undefined }>`
   box-sizing: border-box;
-  padding: 1rem;
-  ${above.sm`  padding: 2rem;`}
   width: 90%;
   border-radius: 20px;
   ${({ theme }: { theme: Pallet }) => css`
-    background: ${theme.layer1Background};
+    background: ${theme.layer2Background};
     border: 1px solid ${theme.borderColor};
   `}
 
@@ -96,6 +103,15 @@ const ContributeWrapper = styled.div<{ wallet: WalletConfig | undefined }>`
     }
   }}
 
+  .checkbox-wrapper {
+    margin-left: -9px;
+    padding: 0 1rem;
+
+    ${above.sm`
+      padding: 0 2rem;
+    `}
+  }
+
   .checkbox-label {
     padding: 8px;
     padding-left: 0;
@@ -107,6 +123,61 @@ const ContributeWrapper = styled.div<{ wallet: WalletConfig | undefined }>`
   }
 `;
 
+const MixerButtonWrapper = styled.div`
+  padding: 0 1rem;
+  padding-bottom: 1rem;
+
+  ${above.sm`
+    padding: 0 2rem;
+    padding-bottom: 2rem;
+  `}
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+  padding: 0 1rem;
+  padding-top: 1rem;
+
+  ${above.sm`
+    padding: 0 2rem;
+    padding-top: 2rem;
+  `}
+`;
+
+const RewardWrapper = styled.div`
+  background-color: ${({ theme }) => theme.layer1Background};
+  padding: 12px 1rem;
+
+  ${above.sm`
+    padding: 24px 2rem; 
+  `}
+
+  svg {
+    width: 32px;
+    height: 32px;
+  }
+`;
+
+const RewardContentWrapper = styled.div`
+  margin-top: 8px;
+  margin-left: 12px;
+  width: 80%;
+
+  .content {
+    margin-top: 8px;
+
+    p:not(:last-child) {
+      margin-bottom: 4px;
+
+      ${above.lg`
+        margin-bottom: 8px;
+      `}
+    }
+  }
+`;
+
 export type PageCrowdloanProps = {};
 
 const PageCrowdloan: FC<PageCrowdloanProps> = () => {
@@ -114,6 +185,7 @@ const PageCrowdloan: FC<PageCrowdloanProps> = () => {
   const { amount, contribute, fundInfo, getFundInfo, setAmount } = useCrowdloan();
   const palette = useColorPallet();
   const { currencies: currenciesConfig } = useAppConfig();
+  const { isXsOrAbove } = useBreakpoint();
 
   const [displayedAmount, setDisplayedAmount] = useState<string>('');
   const [error, setError] = useState('');
@@ -130,8 +202,13 @@ const PageCrowdloan: FC<PageCrowdloanProps> = () => {
       : [];
   }, [activeChain, currenciesConfig]);
 
-  const activeToken = useMemo(() => allCurrencies[0], [allCurrencies]);
+  const activeToken = useMemo<Currency | undefined>(() => allCurrencies[0], [allCurrencies]);
   const isFundingEnded = useMemo(() => BigInt(blockNumber) > fundInfo.end, [blockNumber, fundInfo]);
+  const allottedRewardsPool = useMemo(() => FixedPointNumber.fromInner(ALLOTTED_REWARDS_POOL, 10), []);
+  const estReward = useMemo(() => {
+    const raised = FixedPointNumber.fromInner(fundInfo.raised.toString(), activeToken?.getDecimals());
+    return amount.div(raised).times(allottedRewardsPool);
+  }, [amount, fundInfo, activeToken, allottedRewardsPool]);
   const confirmText = useMemo(
     () =>
       "Your KSM will be contributed to the crowdloan for Webb's Kusama parachain slot lease and locked for the duration of the lease. If this crowdloan does not win an auction slot, your KSM will be returned.",
@@ -173,78 +250,115 @@ const PageCrowdloan: FC<PageCrowdloanProps> = () => {
   return (
     <PageCrowdloanWrapper>
       <ContributeWrapper wallet={activeWallet}>
-        <div className='titles-and-information'>
-          <Flex row jc='flex-end' style={{ marginBottom: '8px' }}>
-            <div>
-              <Typography
-                variant='body2'
-                style={{ color: palette.type === 'dark' ? palette.accentColor : palette.primaryText }}
-              >
-                Your Balance~
-              </Typography>
-            </div>
+        <TitleWrapper>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography variant='h6'>
+              <b>{isXsOrAbove ? 'Contribution Amount' : 'Amount'}</b>
+            </Typography>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography
+              variant='body2'
+              style={{ color: palette.type === 'dark' ? palette.accentColor : palette.primaryText }}
+            >
+              Your Balance~
+            </Typography>
             <TokenBalance>
               <Typography variant='body2'>
                 {getRoundedAmountString(Number(tokenBalance))} {activeToken?.view.symbol}
               </Typography>
             </TokenBalance>
-          </Flex>
+          </div>
+        </TitleWrapper>
 
-          <AmountInputWrapper disabled={isFundingEnded}>
-            <InputBase
-              disabled={isFundingEnded}
-              placeholder={`Enter Amount`}
-              fullWidth
-              value={displayedAmount}
-              inputProps={{ style: { fontSize: 14, cursor: isFundingEnded ? 'no-drop' : 'auto' } }}
-              onChange={(event) => {
-                setDisplayedAmount(event.target.value);
-                let maybeNumber = Number(event.target.value);
-                if (!Number.isNaN(maybeNumber)) {
-                  let fixed = new FixedPointNumber(event.target.value, activeToken.getDecimals());
-                  let cap = FixedPointNumber.fromInner(fundInfo.cap.toString(), activeToken.getDecimals());
-                  let raised = FixedPointNumber.fromInner(fundInfo.raised.toString(), activeToken.getDecimals());
-                  if (fixed.isLessThan(new FixedPointNumber(0.1, activeToken.getDecimals()))) {
-                    setError('Amount must be greater than 0.1');
-                  } else if (cap.minus(raised).isGreaterThan(fixed)) {
-                    setError('');
-                    setAmount(fixed);
-                  } else {
-                    setError('Amount exceeds the cap');
-                  }
+        <AmountInputWrapper disabled={isFundingEnded}>
+          <InputBase
+            disabled={isFundingEnded}
+            placeholder={`Enter Amount`}
+            fullWidth
+            value={displayedAmount}
+            inputProps={{ style: { fontSize: 14, cursor: isFundingEnded ? 'no-drop' : 'auto' } }}
+            onChange={(event) => {
+              setDisplayedAmount(event.target.value);
+              let maybeNumber = Number(event.target.value);
+              const decimal = activeToken?.getDecimals();
+
+              if (!Number.isNaN(maybeNumber)) {
+                let fixed = new FixedPointNumber(event.target.value, decimal);
+                let cap = FixedPointNumber.fromInner(fundInfo.cap.toString(), decimal);
+                let raised = FixedPointNumber.fromInner(fundInfo.raised.toString(), decimal);
+                if (fixed.isLessThan(new FixedPointNumber(0.1, decimal))) {
+                  setError('Amount must be greater than 0.1');
+                } else if (cap.minus(raised).isGreaterThan(fixed)) {
+                  setError('');
+                  setAmount(fixed);
                 } else {
-                  setAmount(new FixedPointNumber(tokenBalance, activeToken.getDecimals()));
+                  setError('Amount exceeds the cap');
                 }
-              }}
-            />
-            <AmountButton
-              disabled={isFundingEnded}
-              as={ButtonBase}
-              onClick={() => {
-                setDisplayedAmount(tokenBalance.toString());
-                setAmount(new FixedPointNumber(tokenBalance, activeToken.getDecimals()));
-              }}
-            >
-              MAX
-            </AmountButton>
-          </AmountInputWrapper>
+              } else {
+                setAmount(new FixedPointNumber(tokenBalance, decimal));
+              }
+            }}
+          />
+          <AmountButton
+            disabled={isFundingEnded}
+            as={ButtonBase}
+            onClick={() => {
+              setDisplayedAmount(tokenBalance.toString());
+              setAmount(new FixedPointNumber(tokenBalance, activeToken?.getDecimals()));
+            }}
+          >
+            MAX
+          </AmountButton>
+        </AmountInputWrapper>
 
-          <Flex row ai='flex-start' style={{ marginLeft: '-9px', opacity: isFundingEnded ? 0.4 : 1 }}>
-            <Checkbox
-              disabled={isFundingEnded}
-              size='medium'
-              checked={isConfirmed}
-              onChange={() => setIsConfirmed((p) => !p)}
-              inputProps={{ 'aria-label': 'controlled' }}
-              style={{ color: palette.accentColor, display: 'block' }}
-            />
-            <Typography display='block' variant='caption' className='checkbox-label'>
-              {confirmText}
-            </Typography>
+        <RewardWrapper>
+          <Flex row>
+            <WEBBLogo />
+
+            <RewardContentWrapper>
+              <Typography variant='h6' style={{ fontWeight: '700' }}>
+                Webb Rewards
+              </Typography>
+              <Flex row jc='space-between'>
+                <div className='content'>
+                  <Typography variant='body2'>Your contribution amount:</Typography>
+                  <Typography variant='body2'>Est. Rewards:</Typography>
+                  <Typography variant='body2'>Total contribution amout:</Typography>
+                </div>
+                <div className='content'>
+                  <Typography variant='body2'>
+                    <b>{amount.toNumber().toLocaleString()}</b>
+                  </Typography>
+                  <Typography variant='body2'>
+                    <b>{isNaN(estReward.toNumber()) ? '-' : estReward.toNumber().toLocaleString()}</b>
+                  </Typography>
+                  <Typography variant='body2'>
+                    <b>{fundInfo.raised.toLocaleString()}</b>
+                  </Typography>
+                </div>
+              </Flex>
+            </RewardContentWrapper>
           </Flex>
+        </RewardWrapper>
 
-          <SpaceBox height={16} />
+        <Flex row ai='flex-start' style={{ opacity: isFundingEnded ? 0.4 : 1 }}>
+          <Checkbox
+            disabled={isFundingEnded}
+            size='medium'
+            checked={isConfirmed}
+            onChange={() => setIsConfirmed((p) => !p)}
+            inputProps={{ 'aria-label': 'controlled' }}
+            style={{ color: palette.accentColor, display: 'block' }}
+          />
+          <Typography display='block' variant='caption' className='checkbox-label'>
+            {confirmText}
+          </Typography>
+        </Flex>
 
+        <SpaceBox height={16} />
+
+        <MixerButtonWrapper>
           <MixerButton
             disabled={loading || !amount || !!error || isFundingEnded || !isConfirmed}
             label={error ? 'Amount exceeds cap' : isFundingEnded ? 'Funding ended' : 'Contribute'}
@@ -257,7 +371,7 @@ const PageCrowdloan: FC<PageCrowdloanProps> = () => {
               }
             }}
           />
-        </div>
+        </MixerButtonWrapper>
       </ContributeWrapper>
     </PageCrowdloanWrapper>
   );
