b24c5c8132a886b7db12197faeed79b67a2b223c
AtelyPham <tinpham1412000@gmail.com>
2022-06-28 14:22:01 +0700

    refactor: update rounded amount function for better approximate

--- packages/ui-components/src/utils/index.ts
+++ packages/ui-components/src/utils/index.ts
@@ -1,3 +1,5 @@
+import numbro from 'numbro';
+
 export const randomID = (): string => {
   return Math.random().toString(16).slice(3);
 };
@@ -34,14 +36,32 @@ export function getInputBorder(noBorder: boolean, error: boolean): string {
   return '1px solid var(--input-border-color)';
 }
 
-export function getRoundedAmountString(amount: number) {
-  if (amount >= 10000000) {
-    return `${amount / 1000000}M`;
-  } else if (amount >= 100000) {
-    return `${amount / 1000}K`;
-  } else if (amount.toString().length > 5) {
-    return `${amount.toPrecision(4)}`;
-  } else {
-    return `${amount}`;
+/**
+ *
+ * @param num number to format
+ * @param digits number of digits to display
+ * @returns the formatted number with
+ * abbreviations million - m, billion b, ...
+ */
+export function getRoundedAmountString(num: number | undefined, digits = 3): string {
+  if (num === 0) {
+    return '0';
   }
+
+  if (!num) {
+    return '-';
+  }
+
+  if (num < 0.001) {
+    return '<0.001';
+  }
+
+  return numbro(num).format({
+    average: num > 1000,
+    totalLength: num > 1000 ? 3 : 0,
+    mantissa: digits,
+    optionalMantissa: true,
+    trimMantissa: true,
+    thousandSeparated: true,
+  });
 }
