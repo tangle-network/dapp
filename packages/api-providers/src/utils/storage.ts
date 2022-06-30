8f3fa9d4940288556a49cd1dd87558763e6e2e0d
nepoche <nepoche@yahoo.com>
2022-06-30 17:12:13 -0400

    Update deployments

--- packages/api-providers/src/utils/storage.ts
+++ packages/api-providers/src/utils/storage.ts
@@ -9,28 +9,28 @@ export type KeypairStorage = Record<string, { keypair: string }>;
 
 export const anchorDeploymentBlock: Record<number, Record<string, number>> = {
   [computeChainIdType(ChainType.EVM, EVMChainId.Ropsten)]: {
-    '0x53590FF8d6FddC12e9651017F5399d06Dc925C71': 12483374,
+    '0x66e04f6ae26c310e39f5bf24d873909e6d3b64c7': 12497372,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.Rinkeby)]: {
-    '0x5CaedaFEf2Fd8B4DF34B2d16134204dFC6b3F6b3': 10932204,
+    '0x91127f21d63029eb5b2de05b4b1e9fd3497ee95b': 10943907,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.Goerli)]: {
-    '0xE24A63Ebb690d0d6C241FDd4aA8ad90421f91D8a': 7134900,
+    '0x682faa319bf7bae7f0cb68435e857d22bf976e17': 7146565,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.Kovan)]: {
-    '0x1066C35e76c565cbd65FB3a6CB4C6B3161C302B4': 32437592,
+    '0x148e8037ea12834117f3efd9e8990c16c1ff5653': 32476243,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.PolygonTestnet)]: {
-    '0xe76187266aDFcEcd7CACa83B8F76d7333a592E81': 26946665,
+    '0x1371efed369498718bee3eb5d58e5d3dec86be85': 26974489,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.OptimismTestnet)]: {
-    '0xB6836ace175ca5144b966F0633fCf0057E47595B': 4377500,
+    '0x5353cede4b8fea148fb1f66f45d3ec27bff2224d': 4464058,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.ArbitrumTestnet)]: {
-    '0x5353cede4b8fea148fb1f66f45d3ec27bff2224d': 13299500,
+    '0x4953110789d0cb6de126f4ea88890670ccfe6906': 13335776,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.MoonbaseAlpha)]: {
-    '0x0D8D947EB29284a8f7c28E8bAA978E913bf6F5AF': 2384295,
+    '0x0c5f4951f42eec082bd1356b9b41928b4f0e7542': 2397382,
   },
   [computeChainIdType(ChainType.EVM, EVMChainId.HermesLocalnet)]: {
     '0xbfce6b877ebff977bb6e80b24fbbb7bc4ebca4df': 100,
