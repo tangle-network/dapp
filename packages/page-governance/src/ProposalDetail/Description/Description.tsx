98863860875bbdb01da450ac79c0f36c14e0169e
AtelyPham <tinpham1412000@gmail.com>
2022-07-08 19:06:31 +0700

    fix: change description color to primary text color

--- packages/page-governance/src/ProposalDetail/Description/Description.tsx
+++ packages/page-governance/src/ProposalDetail/Description/Description.tsx
@@ -1,15 +1,18 @@
 import { Typography } from '@material-ui/core';
+import { useColorPallet } from '@webb-dapp/react-hooks/useColorPallet';
 import React from 'react';
 
 import { Wrapper } from '../CastVote/styled';
 
 export const Description: React.FC<{ description: string }> = ({ description }) => {
+  const pallet = useColorPallet();
+
   return (
     <Wrapper>
       <Typography variant='h5' component='h6' style={{ fontWeight: 600 }}>
         Description
       </Typography>
-      <p style={{ marginTop: '12px' }}>{description}</p>
+      <p style={{ marginTop: '12px', color: pallet.primaryText }}>{description}</p>
     </Wrapper>
   );
 };
