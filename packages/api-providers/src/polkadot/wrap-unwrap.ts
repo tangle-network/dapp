959cddda164b7281afb9a3b3d1d9a8f4b549117c
ahmed korim <ahmedkorrim@gmail.com>
2022-08-10 15:40:45 +0200

    Implement the wrap function

--- packages/api-providers/src/polkadot/wrap-unwrap.ts
+++ packages/api-providers/src/polkadot/wrap-unwrap.ts
@@ -1,27 +1,59 @@
 // Copyright 2022 @webb-tools/
 // SPDX-License-Identifier: Apache-2.0
 
+import { ethers } from 'ethers';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 import { WrapUnwrap } from '../abstracts';
-import { Amount, CurrencyType, WrappingEvent } from '../';
+import { Amount, WebbError, WebbErrorCodes, WrappingEvent } from '../';
 import { WebbPolkadot } from './webb-provider';
+
+export type PolkadotWrapPayload = Amount;
+export type PolkadotUnwrapPayload = Amount;
 
 export class PolkadotWrapUnwrap extends WrapUnwrap<WebbPolkadot> {
   private _currentChainId = new BehaviorSubject<number | null>(null);
   private _event = new Subject<Partial<WrappingEvent>>();
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async canWrap(wrapPayload: any): Promise<boolean> {}
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  unwrap(unwrapPayload: any): Promise<string> {
-    return Promise.resolve('');
+  async canWrap(wrapPayload: any): Promise<boolean> {
+    return true;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  wrap(wrapPayload: any): Promise<string> {
-    return Promise.resolve('');
+  unwrap(payload: PolkadotUnwrapPayload): Promise<string> {
+    return true;
+  }
+
+  async wrap(payload: PolkadotWrapPayload): Promise<string> {
+    const { amount: amountNumber } = payload;
+    const governedToken = this.inner.methods.bridgeApi.getBridge()?.currency!;
+    const wrappableToken = this.inner.state.wrappableCurrency!;
+    const bnAmount = ethers.utils.parseUnits(amountNumber.toString(), wrappableToken.getDecimals());
+    const chainID = this.inner.typedChainId;
+    const governableATreeId = governedToken.getAddress(chainID)!;
+    const writableATTreeId = wrappableToken.getAddress(chainID)!;
+
+    const account = await this.inner.accounts.activeOrDefault!;
+    if (!account) {
+      throw WebbError.from(WebbErrorCodes.NoAccountAvailable);
+    }
+    const address = account.address;
+
+    try {
+      const tx = this.inner.txBuilder.build(
+        {
+          method: 'wrap',
+          section: 'tokenWrapper',
+        },
+        [writableATTreeId, governableATreeId, bnAmount, address]
+      );
+      const txHash = await tx.call(account.address);
+      return txHash;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
