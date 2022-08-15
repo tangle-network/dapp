7d3a2fef2a3788624a8e4d65953cdf2d63e4d6e5
ahmed korim <ahmedkorrim@gmail.com>
2022-08-15 08:02:12 +0200

    Fix  comment

--- packages/api-providers/src/abstracts/state/index.ts
+++ packages/api-providers/src/abstracts/state/index.ts
@@ -1,11 +1,9 @@
 // Copyright 2022 @webb-tools/
 // SPDX-License-Identifier: Apache-2.0
-import { CurrencyRole } from '@webb-dapp/api-providers';
-import { calculateTypedChainId } from '@webb-tools/sdk-core';
-import { parseTypedChainId } from '@webb-tools/sdk-core/typed-chain-id';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { CurrencyRole } from "@webb-dapp/api-providers";
+import { BehaviorSubject, Observable } from "rxjs";
 
-import { Currency } from '../currency';
+import { Currency } from "../currency";
 
 export interface WebbStateInterface {
   wrappableCurrency: Currency | null;
