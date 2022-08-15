ba87952e7445110701d221e3b33d16fe3b279d1d
ahmed korim <ahmedkorrim@gmail.com>
2022-08-15 13:52:29 +0200

    Enable unsigned transactions for polkadot

--- packages/api-providers/src/polkadot/transaction.ts
+++ packages/api-providers/src/polkadot/transaction.ts
@@ -76,7 +76,7 @@ export class PolkadotTx<P extends Array<any>> extends EventBus<PolkadotTXEvents>
     super();
   }
 
-  async call(signAddress: AddressOrPair) {
+  async call(signAddress: AddressOrPair | null) {
     const api = this.apiPromise;
 
     for (let i = 0; i < this.path.length; i++) {
@@ -95,7 +95,7 @@ export class PolkadotTx<P extends Array<any>> extends EventBus<PolkadotTXEvents>
       this.notificationKey = uniqueId(`${path.section}-${path.method}`);
     }
 
-    if ((signAddress as IKeyringPair)?.address === undefined) {
+    if (signAddress && (signAddress as IKeyringPair)?.address === undefined) {
       // passed an account id or string of address
       const { web3FromAddress } = await import('@polkadot/extension-dapp');
       const injector = await web3FromAddress(signAddress as string);
@@ -108,17 +108,21 @@ export class PolkadotTx<P extends Array<any>> extends EventBus<PolkadotTXEvents>
       const path = this.path[0];
       const parms = this.parms[0];
 
-      txResults = await api.tx[path.section][path.method](...parms).signAsync(signAddress, {
-        nonce: -1,
-      });
+      txResults = signAddress
+        ? await api.tx[path.section][path.method](...parms).signAsync(signAddress, {
+            nonce: -1,
+          })
+        : api.tx[path.section][path.method](...parms);
     } else {
       const parms = this.parms;
       const txs = this.path.map((path, index) => {
         return api.tx[path.section][path.method](...parms[index]);
       });
-      txResults = await api.tx.utility.batch(txs).signAsync(signAddress, {
-        nonce: -1,
-      });
+      txResults = signAddress
+        ? await api.tx.utility.batch(txs).signAsync(signAddress, {
+            nonce: -1,
+          })
+        : api.tx.utility.batch(txs);
     }
 
     await this.emitWithPayload('beforeSend', undefined);
