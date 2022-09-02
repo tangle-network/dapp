0ce73479e5c23e2d4d205f4cd9105c8547854089
AtelyPham <tinpham1412000@gmail.com>
2022-09-02 10:19:43 +0700

    feat: setup new `Typography` component

---
+++ packages/webb-ui-components/src/typograhy/Typography/Typography.tsx
@@ -0,0 +1,51 @@
+import cx from 'classnames';
+import React, { createElement } from 'react';
+
+import { WebbTypographyProps } from '../types';
+import { getFontWeightClassName, getTextAlignClassName, getTextColorClassName } from '../utils';
+
+const defaultComponent = {
+  h1: 'h1' as const,
+  h2: 'h2' as const,
+  h3: 'h3' as const,
+  h4: 'h4' as const,
+  h5: 'h5' as const,
+  body1: 'p' as const,
+  body2: 'p' as const,
+  body3: 'p' as const,
+  body4: 'p' as const,
+  mono1: 'span' as const,
+  mono2: 'span' as const,
+};
+
+/**
+ * The Webb Typography component
+ *
+ * Props:
+ * - `variant`: Represent diffferent variants of the component
+ * - `component`: The html tag (default: same as `variant` prop)
+ * - `fw`: Represent the **font weight** of the component (default: `left`)
+ * - `ta`: Text align (default: `inherit`)
+ * - `darkMode`: Control component dark mode display in `js`, leave it's empty if you want to control dark mode in `css`
+ *
+ * @example
+ *
+ * ```jsx
+ * <Typography variant="h1" fw="semibold" ta="center">This is heading 1</Typography>
+ * <Typography variant="h2" component="h3">This is heading 3 with variant h2</Typography>
+ * ```
+ */
+export const Typography: React.FC<WebbTypographyProps> = (props) => {
+  const { children, className, component, darkMode, fw = 'normal', ta = 'left', variant, ...restProps } = props;
+
+  const _component = component ?? defaultComponent[variant];
+  const _className = cx(
+    className,
+    `.${variant}` as const,
+    getTextAlignClassName(ta),
+    getFontWeightClassName(variant, fw),
+    getTextColorClassName(variant, darkMode)
+  );
+
+  return createElement(_component, { ...restProps, className: _className }, children);
+};
