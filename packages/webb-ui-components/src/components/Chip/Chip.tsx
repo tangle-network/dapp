e630481278e513c7730e703f09a4426f6dbe16cd
AtelyPham <tinpham1412000@gmail.com>
2022-09-05 16:50:05 +0700

    chore: change the variable name

--- packages/webb-ui-components/src/components/Chip/Chip.tsx
+++ packages/webb-ui-components/src/components/Chip/Chip.tsx
@@ -20,14 +20,14 @@ import { getChipClassName } from './utils';
  * ```
  */
 export const Chip: React.FC<ChipProps> = (props) => {
-  const { children, className, color = 'green', isDisabled } = props;
+  const { children, className: classNameProp, color = 'green', isDisabled } = props;
 
   const baseClsx = useMemo(() => 'box-border inline-block px-3 py-1 rounded-full font-bold body4', []);
 
-  const _className = useMemo(
-    () => twMerge(baseClsx, getChipClassName(color, isDisabled), className),
-    [baseClsx, color, isDisabled, className]
+  const className = useMemo(
+    () => twMerge(baseClsx, getChipClassName(color, isDisabled), classNameProp),
+    [baseClsx, color, isDisabled, classNameProp]
   );
 
-  return <span className={_className}>{children}</span>;
+  return <span className={className}>{children}</span>;
 };
