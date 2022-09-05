c02a218b76a992ddb206747324be32efa6f6f0b1
AtelyPham <tinpham1412000@gmail.com>
2022-09-05 15:53:12 +0700

    doc: add doc for `Chip` component

--- packages/webb-ui-components/src/components/Chip/Chip.tsx
+++ packages/webb-ui-components/src/components/Chip/Chip.tsx
@@ -4,6 +4,21 @@ import { twMerge } from 'tailwind-merge';
 import { ChipProps } from './types';
 import { getChipClassName } from './utils';
 
+/**
+ * The `Chip` component
+ *
+ * Props:
+ *
+ * - `color`: The visual style of the badge (default: "green")
+ * - `isDisabled`: If `true`, the chip will display as disabled state
+ *
+ * @example
+ *
+ * ```jsx
+ *  <Chip>Active</Chip>
+ *  <Chip color="red" isDisabled>Disabled</Chip>
+ * ```
+ */
 export const Chip: React.FC<ChipProps> = (props) => {
   const { children, className, color = 'green', isDisabled } = props;
 
