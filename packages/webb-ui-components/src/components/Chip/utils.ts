a416fd7f708f04e37876ae1dd317afa3b86bc1ff
AtelyPham <tinpham1412000@gmail.com>
2022-09-09 14:38:26 +0700

    style: re-style disabled state of `Chip` component

--- packages/webb-ui-components/src/components/Chip/utils.ts
+++ packages/webb-ui-components/src/components/Chip/utils.ts
@@ -3,23 +3,23 @@ import { ChipClassNames, ChipColors } from './types';
 const classNames: ChipClassNames = {
   blue: {
     active: 'bg-blue-0 text-blue-90 dark:bg-blue-120 dark:text-blue-30',
-    disabled: 'bg-blue-0 text-blue-30 dark:bg-blue-120 dark:text-blue-100 cursor-not-allowed',
+    disabled: 'bg-blue-0 text-blue-30 dark:bg-blue-120 dark:text-blue-90 cursor-not-allowed',
   },
   green: {
     active: 'bg-green-0 text-green-100 dark:bg-green-120 dark:text-green-30',
-    disabled: 'bg-green-0 text-green-20 dark:bg-green-120 dark:text-green-100 cursor-not-allowed',
+    disabled: 'bg-green-0 text-green-20 dark:bg-green-120 dark:text-green-90 cursor-not-allowed',
   },
   purple: {
     active: 'bg-purple-0 text-purple-100 dark:bg-purple-120 dark:text-purple-30',
-    disabled: 'bg-purple-0 text-purple-30 dark:bg-purple-120 dark:text-purple-100 cursor-not-allowed',
+    disabled: 'bg-purple-0 text-purple-30 dark:bg-purple-120 dark:text-purple-90 cursor-not-allowed',
   },
   red: {
     active: 'bg-red-0 text-red-100 dark:bg-red-120 dark:text-red-20',
-    disabled: 'bg-red-0 text-red-20 dark:bg-red-120 dark:text-red-100 cursor-not-allowed',
+    disabled: 'bg-red-0 text-red-20 dark:bg-red-120 dark:text-red-90 cursor-not-allowed',
   },
   yellow: {
     active: 'bg-yellow-0 text-yellow-100 dark:bg-yellow-120 dark:text-yellow-30',
-    disabled: 'bg-yellow-0 text-yellow-30 dark:bg-yellow-120 dark:text-yellow-110 cursor-not-allowed',
+    disabled: 'bg-yellow-0 text-yellow-30 dark:bg-yellow-120 dark:text-yellow-90 cursor-not-allowed',
   },
 };
 
