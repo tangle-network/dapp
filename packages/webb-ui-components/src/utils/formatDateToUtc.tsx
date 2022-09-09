f163e3b03e0366f035a511b03de4e385a2126fb6
AtelyPham <tinpham1412000@gmail.com>
2022-09-10 03:48:40 +0700

    chore: add `formatDateToUtc` function

---
+++ packages/webb-ui-components/src/utils/formatDateToUtc.tsx
@@ -0,0 +1,21 @@
+import { isValid, parseISO } from 'date-fns';
+import { format, utcToZonedTime } from 'date-fns-tz';
+
+/**
+ * Format a `date` to UTC string
+ * @param dateArg Represents the `value` to parse to UTC format
+ * @returns the string represents the utc format of the given date
+ */
+export const formatDateToUtc = (dateArg: string | Date): string => {
+  if (!isValid(dateArg)) {
+    throw new Error('Please provide valid date object');
+  }
+
+  let dateISO: Date;
+  if (typeof dateArg === 'string') {
+    dateISO = parseISO(new Date(dateArg).toISOString());
+  } else {
+    dateISO = parseISO(dateArg.toISOString());
+  }
+  return format(utcToZonedTime(dateISO, 'UTC'), "MMM dd yyyy (HH:mm:ss a 'UTC')", { timeZone: 'UTC' });
+};
