ae25ba83b89f1b9e83f0fecddc8f3c240280400c
AtelyPham <tinpham1412000@gmail.com>
2022-09-09 14:38:38 +0700

    chore: test new updates

--- packages/mixer/src/Mixer.tsx
+++ packages/mixer/src/Mixer.tsx
@@ -43,6 +43,30 @@ export const Mixer: React.FC<MixerProps> = () => {
       <MixerTabs Withdraw={<Withdraw />} Deposit={<Deposit />} />
 
       <div className='mt-8'>
+        <Button varirant='link' isDisabled size='sm'>
+          Button
+        </Button>
+      </div>
+
+      <div className='mt-8'>
+        <Chip color='green' isDisabled>
+          Green
+        </Chip>
+        <Chip color='blue' isDisabled className='inline-block ml-2'>
+          Blue
+        </Chip>
+        <Chip color='red' isDisabled className='inline-block ml-2'>
+          Red
+        </Chip>
+        <Chip color='purple' isDisabled className='inline-block ml-2'>
+          Purple
+        </Chip>
+        <Chip color='yellow' isDisabled className='inline-block ml-2'>
+          Yellow
+        </Chip>
+      </div>
+
+      <div className='mt-8'>
         <h6 className='mt-5'>Small</h6>
         <Progress size='sm' value={60} className='w-[100px]' />
         <h6 className='mt-5'>Medium (default)</h6>
