e6ff72cceec92598fd63afe0f0700cd842d21d20
ahmed korim <ahmedkorrim@gmail.com>
2022-09-14 10:07:55 +0200

    Fix types for keygen

---
+++ packages/page-statistics/src/provider/hooks/mappers/index.ts
@@ -0,0 +1,28 @@
+import { SessionAuthFragment } from '@webb-dapp/page-statistics/generated/graphql';
+
+export type Authority = {
+  id: string;
+  sessionId: string;
+  reputation: string;
+  isBest: boolean;
+  isNext: boolean;
+  isNextBest: boolean;
+  bestOrder: number;
+  nextBestOrder: number;
+  authorityId: string;
+};
+export function mapAuthorities(data: SessionAuthFragment): Authority[] {
+  return data.sessionValidators.edges.map(
+    (item): Authority => ({
+      id: item.node?.validator?.id!,
+      sessionId: item.node?.sessionId!,
+      reputation: item.node?.reputation!,
+      isBest: item.node?.isBest!,
+      isNext: item.node?.isNext!,
+      isNextBest: item.node?.isNextBest!,
+      bestOrder: item.node?.bestOrder!,
+      nextBestOrder: item.node?.nextBestOrder!,
+      authorityId: item.node?.validator?.authorityId!,
+    })
+  );
+}
