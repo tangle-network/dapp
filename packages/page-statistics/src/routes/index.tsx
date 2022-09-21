11d163c6b52abb82412d7603964b14863af31275
AtelyPham <tinpham1412000@gmail.com>
2022-09-21 21:41:43 +0700

    chore: remove deprecated files

--- packages/page-statistics/src/routes/index.tsx
+++ packages/page-statistics/src/routes/index.tsx
@@ -3,7 +3,9 @@ import React, { FC, lazy, Suspense } from 'react';
 
 import { Layout } from '../containers';
 
+const PageAuthorities = lazy(() => import('@webb-dapp/page-statistics/pages/Authorities'));
 const PageKeys = lazy(() => import('@webb-dapp/page-statistics/pages/Keys'));
+const PageProposals = lazy(() => import('@webb-dapp/page-statistics/pages/Proposals'));
 
 const CSuspense: FC = ({ children }) => {
   return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
@@ -15,10 +17,26 @@ export const routes: RouterConfigData[] = [
       {
         element: (
           <CSuspense>
+            <PageAuthorities />
+          </CSuspense>
+        ),
+        path: 'authorities/*',
+      },
+      {
+        element: (
+          <CSuspense>
             <PageKeys />
           </CSuspense>
         ),
         path: 'keys/*',
+      },
+      {
+        element: (
+          <CSuspense>
+            <PageProposals />
+          </CSuspense>
+        ),
+        path: 'proposals/*',
       },
       {
         path: '*',
