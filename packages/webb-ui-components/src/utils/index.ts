fe34f660f178e7e73a623db34399162675471a90
AtelyPham <tinpham1412000@gmail.com>
2022-09-24 19:20:43 +0700

    feat: add `randomEnum` function

--- packages/webb-ui-components/src/utils/index.ts
+++ packages/webb-ui-components/src/utils/index.ts
@@ -2,6 +2,7 @@ export * from './arrayFrom';
 export * from './calculateDateProgress';
 export * from './formatDateToUtc';
 export * from './getPaginationItems';
+export * from './randomEnum';
 export * from './range';
 export * from './shortenHex';
 export * from './shorttenString';
