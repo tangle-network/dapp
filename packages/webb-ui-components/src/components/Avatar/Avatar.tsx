2e34d00de8d35cf47d4f93d3029557ced08eeead
Ahmed Korim <ahmedkorrim@gmail.com>
2022-09-27 09:00:56 +0200

    Added default value for `sourceVariant` to be `uri`

--- packages/webb-ui-components/src/components/Avatar/Avatar.tsx
+++ packages/webb-ui-components/src/components/Avatar/Avatar.tsx
@@ -32,7 +32,7 @@ export const Avatar: React.FC<AvatarProps> = (props) => {
     darkMode,
     fallback,
     size = 'md',
-    sourceVariant,
+    sourceVariant = 'uri',
     src,
     theme = 'polkadot',
     value: valueProp,
