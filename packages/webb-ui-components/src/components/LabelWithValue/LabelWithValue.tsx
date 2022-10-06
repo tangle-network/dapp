922e76db152fc2c4a74f3f45ae5cbd9b7648226c
AtelyPham <tinpham1412000@gmail.com>
2022-10-06 19:02:18 +0700

    cast NaN to string to fix warings

--- packages/webb-ui-components/src/components/LabelWithValue/LabelWithValue.tsx
+++ packages/webb-ui-components/src/components/LabelWithValue/LabelWithValue.tsx
@@ -43,7 +43,7 @@ export const LabelWithValue = forwardRef<HTMLSpanElement, LabelWithValueProps>(
         {!valueTooltip &&
           (typeof value === 'string' || typeof value === 'number' ? (
             <Typography component='span' variant={valueVariant}>
-              {value}
+              {value.toString()}
             </Typography>
           ) : (
             value
