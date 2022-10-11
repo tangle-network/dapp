c55f215c35d34aafb3b18e2fa1dfa0e6acbe188a
AtelyPham <tinpham1412000@gmail.com>
2022-10-11 19:50:49 +0700

    add list cards showcase component

---
+++ packages/page-statistics/src/containers/ListCardsShowcase/ListCardsShowcase.tsx
@@ -0,0 +1,10 @@
+import { Card, TitleWithInfo } from '@webb-dapp/webb-ui-components/components';
+
+export const ListCardsShowcase = () => {
+  return (
+    <Card>
+      <TitleWithInfo title='List Cards' variant='h4' />
+      <div className='flex flex-col items-center space-y-4'></div>
+    </Card>
+  );
+};
