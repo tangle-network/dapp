70558372ed73a476a697601323842e9ebfa74bc8
ahmed korim <ahmedkorrim@gmail.com>
2022-10-17 21:10:41 +0200

    Added docs

--- packages/webb-ui-components/src/components/CheckBoxMenu/CheckBoxMenuGroup.tsx
+++ packages/webb-ui-components/src/components/CheckBoxMenu/CheckBoxMenuGroup.tsx
@@ -9,7 +9,17 @@ export type CheckBoxMenuGroupProps<T> = {
   labelGetter(value: T): JSX.Element | string;
   keyGetter(value: T): string;
 };
-
+/**
+ * React component for building toggleable checkbox menu
+ *
+ * iconGetter - Getter function for the icon
+ * keyGetter - Getter for list key
+ * labelGetter - Label for the menu item
+ * onChange - Change handler for the menu group
+ * options - Full list of options available
+ * value - selected value
+ *
+ * */
 export function CheckBoxMenuGroup<T>({
   iconGetter,
   keyGetter,
