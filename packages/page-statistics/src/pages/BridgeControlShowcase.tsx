5a4ea3f073cd9fa2c1a0688ba2460086ad4ffe11
AtelyPham <tinpham1412000@gmail.com>
2022-10-17 19:58:55 +0700

    feat: add transfer control card

--- packages/page-statistics/src/pages/BridgeControlShowcase.tsx
+++ packages/page-statistics/src/pages/BridgeControlShowcase.tsx
@@ -1,9 +1,16 @@
-import { DepositCard, WithdrawCard } from '@webb-dapp/webb-ui-components/containers';
+import { DepositCard, TransferCard, WithdrawCard } from '@webb-dapp/webb-ui-components/containers';
 import { Typography } from '@webb-dapp/webb-ui-components/typography';
 
 const BridgeControlShowcase = () => {
   return (
     <div className='p-4 space-y-10 justify-items-stretch bg-mono-0 dark:bg-mono-200 rounded-xl'>
+      <div>
+        <Typography variant='h3' ta='center' fw='bold' className='mb-4'>
+          Withdraw Card
+        </Typography>
+        <WithdrawCard className='mx-auto' />
+      </div>
+
       <div>
         <Typography variant='h3' ta='center' fw='bold' className='mb-4'>
           Deposit Card
@@ -13,9 +20,9 @@ const BridgeControlShowcase = () => {
 
       <div>
         <Typography variant='h3' ta='center' fw='bold' className='mb-4'>
-          Withdraw Card
+          Transfer Card
         </Typography>
-        <WithdrawCard className='mx-auto' />
+        <TransferCard className='mx-auto' />
       </div>
     </div>
   );
--- packages/webb-ui-components/src/containers/index.ts
+++ packages/webb-ui-components/src/containers/index.ts
@@ -1,3 +1,4 @@
 export * from './DepositCard';
+export * from './TransferCard';
 export * from './WebbUIErrorBoudary';
 export * from './WithdrawCard';
