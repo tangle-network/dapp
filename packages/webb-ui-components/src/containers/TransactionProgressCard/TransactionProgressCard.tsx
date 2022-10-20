6f1d4d5b4cdbbf6b7d9edadcdfcb82e079918a2d
ahmed korim <ahmedkorrim@gmail.com>
2022-10-20 02:16:00 +0200

    Layout

--- packages/webb-ui-components/src/containers/TransactionProgressCard/TransactionProgressCard.tsx
+++ packages/webb-ui-components/src/containers/TransactionProgressCard/TransactionProgressCard.tsx
@@ -1,9 +1,11 @@
-import { TransactionProgressCardProps } from '@webb-dapp/webb-ui-components/containers/TransactionProgressCard/types';
-import { forwardRef } from 'react';
+import { BridgeLabel, TransactionItem } from "@webb-dapp/webb-ui-components/containers/TransactionProgressCard/types";
+import { forwardRef, useMemo } from "react";
+
 /**
  *
  * TransactionProgressCard
  * */
-export const TransactionProgressCard = forwardRef<HTMLDivElement, TransactionProgressCardProps>(({ ...props }, ref) => {
+export const TransactionProgressCard = forwardRef<HTMLDivElement, TransactionItem>(({ label, ...props }, ref) => {
+  const labelVariant = useMemo(() => ((label as BridgeLabel).tokenURI ? 'bridge' : 'native'), [label]);
   return <div {...props} ref={ref}></div>;
 });
--- packages/webb-ui-components/src/containers/TransactionProgressCard/types.ts
+++ packages/webb-ui-components/src/containers/TransactionProgressCard/types.ts
@@ -1,19 +1,46 @@
 export type TransactionItemVariant = 'Withdraw' | 'Deposit' | 'Transfer ';
 export type TransactionItemStatus = 'In-progress' | 'warning' | 'completed';
+
+export type BridgeLabel = {
+  amount: string;
+  token: string;
+  tokenURI: string;
+};
+export type NativeLabel = {
+  amount: string;
+  nativeValue: string;
+};
+type StepInfo =
+  | string
+  | {
+      label: string;
+      value: string;
+      uri: string;
+    };
 /**
  * Transaction item
  * @param method - Transaction method
  * @param status - in
+ * @param note - Information note for the notification item
  * */
 export type TransactionItem = {
   method: string;
-  status: TransactionItemStatus;
-  token: string;
-  tokenIcons: JSX.Element[];
-  metaData: string;
   firedAt: Date;
+  note?: string;
+  stepInfo?: StepInfo;
+  status: TransactionItemStatus;
+  tokens: JSX.Element[];
+  headerAction?: JSX.Element;
+  wallets: {
+    src: JSX.Element;
+    dist: JSX.Element;
+  };
+  label: BridgeLabel | NativeLabel;
+  onDismiss(): void;
+  onDetails(): void;
 };
 
 export type TransactionProgressCardProps = {
   transactions: TransactionItem[];
+  collapsed?: boolean;
 };
