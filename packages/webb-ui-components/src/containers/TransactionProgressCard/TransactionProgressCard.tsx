a30df3f72a137820320958d3c7012efd59f016a1
ahmed korim <ahmedkorrim@gmail.com>
2022-10-24 15:15:29 +0200

    Styling card

--- packages/webb-ui-components/src/containers/TransactionProgressCard/TransactionProgressCard.tsx
+++ packages/webb-ui-components/src/containers/TransactionProgressCard/TransactionProgressCard.tsx
@@ -1,4 +1,7 @@
-import { Chip } from '@webb-dapp/webb-ui-components';
+import OptimismLogo from '@webb-dapp/apps/configs/logos/chains/OptimismLogo';
+import PolygonLogo from '@webb-dapp/apps/configs/logos/chains/PolygonLogo';
+import { Avatar, Chip } from '@webb-dapp/webb-ui-components';
+import { ArrowLeft, ArrowRight, TokenIcon } from '@webb-dapp/webb-ui-components/icons';
 import { Typography } from '@webb-dapp/webb-ui-components/typography';
 import { FC, forwardRef, useMemo } from 'react';
 import { twMerge } from 'tailwind-merge';
@@ -15,30 +18,64 @@ export const TransactionProgressCard = forwardRef<HTMLDivElement, TransactionCar
     return (
       <div
         className={twMerge(
-          `
-      px-2 py-4
-      flex flex-col space-y-4 max-w-[318px] bg-red-10`,
+          `rounded-b-lg shadow-xl py-4  px-4
+border-t-4 border-indigo-500
+       flex flex-col space-y-4 max-w-[318px] dark:bg-mono-160`,
           className
         )}
         {...props}
         ref={ref}
       >
         {/*Card Header*/}
-        <div className={'flex items-center'}>
+        <div className={'my-0 flex items-center'}>
           <div className={'basis-full'}>
             <Chip>{method}</Chip>
           </div>
-          <Typography variant={'body3'} className={'whitespace-nowrap'}>
+          <Typography fw={'bold'} variant={'body3'} className={'whitespace-nowrap'}>
             JUST NOW
           </Typography>
         </div>
-        {/*Card label*/}
+        {/*Card Content*/}
+        <div className={'my-0 flex items-center'}>
+          <div className={'h-full self-start py-2'}>
+            <TokenIcon size={'lg'} name={'ETH'} />
+          </div>
+          <div className={'px-2'}>
+            <Typography
+              variant={'h4'}
+              fw={'bold'}
+              className={'py-0'}
+              style={{
+                color: '#000000',
+              }}
+            >
+              0.990
+            </Typography>
+            <Typography
+              style={{
+                color: '#000000',
+              }}
+              fw={'bold'}
+              className={'py-0'}
+              variant={'body2'}
+            >
+              1789.12 USD
+            </Typography>
+          </div>
+          <div className={'h-full self-start py-2 flex items-center'}>
+            <div className={`w-6  h-6 rounded-full flex items-center justify-center`} children={<PolygonLogo />} />
+            <ArrowRight />
+            <div className={`w-6  h-6 rounded-full flex items-center justify-center`} children={<OptimismLogo />} />
+          </div>
+        </div>
         <div></div>
+        {/*Card Info*/}
+        {/*Card Footer*/}
       </div>
     );
   }
 );
 
-const NativeLabel: FC<{ label: NativeLabel }> = ({ label }) => {
+const NativeTokenLabel: FC<{ label: NativeLabel }> = ({ label }) => {
   const amount = label.amount;
 };
