668ce0498f23a4b7c73bdc1b1d7da2e2a33f34f3
AtelyPham <tinpham1412000@gmail.com>
2022-10-28 03:15:01 +0700

    feat: add radix tabs components

--- libs/webb-ui-components/src/components/Tabs/Tabs.tsx
+++ libs/webb-ui-components/src/components/Tabs/Tabs.tsx
@@ -15,60 +15,72 @@ import { TabProps, TabsProps } from './types';
  *
  * ```
  */
-export const Tabs = forwardRef<HTMLDivElement, TabsProps>(({ className, onChange, value, ...props }, ref) => {
-  const [selectedTab, setSelectedTab] = useState(value[0]);
+export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
+  ({ className, onChange, value, ...props }, ref) => {
+    const [selectedTab, setSelectedTab] = useState(value[0]);
 
-  const onTabChange = useCallback(
-    (nextValue: string) => {
-      setSelectedTab(nextValue);
+    const onTabChange = useCallback(
+      (nextValue: string) => {
+        setSelectedTab(nextValue);
 
-      onChange?.(nextValue);
-    },
-    [onChange]
-  );
+        onChange?.(nextValue);
+      },
+      [onChange]
+    );
 
-  const tabs = useMemo(() => value, [value]);
+    const tabs = useMemo(() => value, [value]);
 
-  const mergedClsx = useMemo(() => twMerge('flex items-center space-x-2', className), [className]);
+    const mergedClsx = useMemo(
+      () => twMerge('flex items-center space-x-2', className),
+      [className]
+    );
 
-  // Update the default selected tab when `value` prop change
-  useEffect(() => {
-    setSelectedTab(value[0]);
-  }, [value]);
+    // Update the default selected tab when `value` prop change
+    useEffect(() => {
+      setSelectedTab(value[0]);
+    }, [value]);
 
-  return (
-    <div {...props} className={mergedClsx} ref={ref}>
-      {tabs.map((tab, idx) => {
-        return (
-          <Tab key={`${tab}-${idx}`} isActive={selectedTab === tab} onClick={() => onTabChange(tab)}>
-            {tab}
-          </Tab>
-        );
-      })}
-    </div>
-  );
-});
+    return (
+      <div {...props} className={mergedClsx} ref={ref}>
+        {tabs.map((tab, idx) => {
+          return (
+            <Tab
+              key={`${tab}-${idx}`}
+              isActive={selectedTab === tab}
+              onClick={() => onTabChange(tab)}
+            >
+              {tab}
+            </Tab>
+          );
+        })}
+      </div>
+    );
+  }
+);
 
 /**
  * The `Tab` component
  */
-const Tab = forwardRef<HTMLButtonElement, TabProps>(({ children, className, isActive, ...props }, ref) => {
-  return (
-    <button
-      {...props}
-      disabled={isActive}
-      className={twMerge(
-        cx(
-          'flex items-center justify-center grow shrink basis-0',
-          'py-2 font-bold rounded-lg body2',
-          'text-mono-120 dark:text-mono-80 bg-mono-0 dark:bg-mono-180',
-          'disabled:bg-blue-0 dark:disabled:bg-blue-120 disabled:text-blue-70 dark:disabled:text-blue-50'
-        ),
-        className
-      )}
-      ref={ref}
-    >
-      <span className='inline-block !text-inherit'>{children}</span>
-    </button>
-  );
-});
+export const Tab = forwardRef<HTMLButtonElement, TabProps>(
+  ({ children, className, isActive, ...props }, ref) => {
+    return (
+      <button
+        {...props}
+        disabled={isActive}
+        className={twMerge(
+          cx(
+            'flex items-center justify-center grow shrink basis-0',
+            'py-2 font-bold rounded-lg body2',
+            'text-mono-120 dark:text-mono-80 bg-mono-0 dark:bg-mono-180',
+            'disabled:bg-blue-0 dark:disabled:bg-blue-120 disabled:text-blue-70 dark:disabled:text-blue-50',
+            'radix-state-active:bg-blue-0 dark:radix-state-active:bg-blue-120 radix-state-active:text-blue-70 dark:radix-state-active:text-blue-50'
+          ),
+          className
+        )}
+        ref={ref}
+      >
+        <span className="inline-block !text-inherit">{children}</span>
+      </button>
+    );
+  }
+);
--- libs/webb-ui-components/src/components/Tabs/index.ts
+++ libs/webb-ui-components/src/components/Tabs/index.ts
@@ -1,1 +1,5 @@
+export * from './TabContent';
 export * from './Tabs';
+export * from './TabsList';
+export * from './TabsRoot';
+export * from './TabTrigger';
