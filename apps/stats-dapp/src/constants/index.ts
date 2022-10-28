2d0a1d07e4fea2f3f99974ec2e357ef5cdf8f0c7
Dustin Brickwood <dustinbrickwood204@gmail.com>
2022-10-28 14:56:49 -0500

    chore: update constants

--- apps/stats-dapp/src/constants/index.ts
+++ apps/stats-dapp/src/constants/index.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 export const defaultEndpoint = isDev
-  ? 'http://localhost:4000'
+  ? 'https://subquery-dev.webb.tools/graphql'
   : 'https://subquery-dev.webb.tools/graphql';
