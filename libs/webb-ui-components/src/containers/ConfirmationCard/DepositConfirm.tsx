beaab9346cf70ef7426da63c1964421e922aa2da
AtelyPham <tinpham1412000@gmail.com>
2022-11-04 22:00:13 +0700

    style: re-style deposit confirm card

--- libs/webb-ui-components/src/containers/ConfirmationCard/DepositConfirm.tsx
+++ libs/webb-ui-components/src/containers/ConfirmationCard/DepositConfirm.tsx
@@ -13,6 +13,7 @@ import {
   TokenWithAmount,
 } from '../../components';
 import { DepositConfirmProps } from './types';
+import { PropsOf } from '../../types';
 
 export const DepositConfirm = forwardRef<HTMLDivElement, DepositConfirmProps>(
   (
@@ -42,15 +43,15 @@ export const DepositConfirm = forwardRef<HTMLDivElement, DepositConfirmProps>(
       }
 
       if (token1Symbol && token2Symbol) {
-        return `${amount} ${token1Symbol}/${token2Symbol}`;
+        return `${amount} ${token1Symbol.toUpperCase()}/${token2Symbol.toUpperCase()}`;
       }
 
       if (token1Symbol) {
-        return `${amount} ${token1Symbol}`;
+        return `${amount} ${token1Symbol.toUpperCase()}`;
       }
 
       if (token2Symbol) {
-        return `${amount} ${token2Symbol}`;
+        return `${amount} ${token2Symbol.toUpperCase()}`;
       }
 
       return amount.toString();
@@ -95,52 +96,101 @@ export const DepositConfirm = forwardRef<HTMLDivElement, DepositConfirmProps>(
         {progress && <Progress value={progress} />}
 
         {/** Unwrapping info */}
-        <div className="space-y-4">
-          <TitleWithInfo
-            titleComponent="h6"
-            title="Unwrapping"
-            variant="utility"
-            info="Unwrapping"
-            titleClassName="text-mono-100 dark:text-mono-80"
-            className="text-mono-100 dark:text-mono-80"
-          />
-          {token1Symbol && (
-            <div className="flex items-center space-x-4">
-              <TokenWithAmount token1Symbol={token1Symbol} amount={amount} />
-              {token2Symbol && (
-                <>
-                  <ArrowRight />
-                  <TokenWithAmount
-                    token1Symbol={token1Symbol}
-                    token2Symbol={token2Symbol}
-                    amount={amount}
-                  />
-                </>
-              )}
+        <WrapperCard>
+          <div className="space-y-4">
+            <TitleWithInfo
+              titleComponent="h6"
+              title="Unwrapping"
+              variant="utility"
+              info="Unwrapping"
+              titleClassName="text-mono-100 dark:text-mono-80"
+              className="text-mono-100 dark:text-mono-80"
+            />
+            {token1Symbol && (
+              <div className="flex items-center space-x-4">
+                <TokenWithAmount token1Symbol={token1Symbol} amount={amount} />
+                {token2Symbol && (
+                  <>
+                    <ArrowRight />
+                    <TokenWithAmount
+                      token1Symbol={token1Symbol}
+                      token2Symbol={token2Symbol}
+                      amount={amount}
+                    />
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+        </WrapperCard>
+
+        {/** New spend note */}
+        <WrapperCard>
+          <div className="space-y-2">
+            <div className="flex items-center justify-between">
+              <TitleWithInfo
+                titleComponent="h6"
+                title="New Spend Note"
+                info="New Spend Note"
+                variant="utility"
+                titleClassName="text-mono-100 dark:text-mono-80"
+                className="text-mono-100 dark:text-mono-80"
+              />
+              <div className="flex space-x-2">
+                <Button
+                  variant="utility"
+                  size="sm"
+                  className="p-2"
+                  onClick={onCopy}
+                >
+                  <FileCopyLine className="!fill-current" />
+                </Button>
+                <Button
+                  variant="utility"
+                  size="sm"
+                  className="p-2"
+                  onClick={onDownload}
+                >
+                  <Download className="!fill-current" />
+                </Button>
+              </div>
             </div>
-          )}
-        </div>
+
+            <div className="flex items-center justify-between max-w-[470px]">
+              <Typography
+                variant="mono1"
+                fw="bold"
+                className="block truncate text-mono-140 dark:text-mono-0"
+              >
+                {note}
+              </Typography>
+            </div>
+
+            <CheckBox
+              {...checkboxProps}
+              wrapperClassName={twMerge(
+                'flex items-center',
+                checkboxProps?.wrapperClassName
+              )}
+            >
+              {checkboxProps?.children ?? 'I have copied the spend note'}
+            </CheckBox>
+          </div>
+        </WrapperCard>
 
         {/** Transaction Details */}
-        <div className="space-y-2">
-          <TitleWithInfo
-            titleComponent="h6"
-            title={`Transaction Details`}
-            variant="utility"
-            titleClassName="text-mono-100 dark:text-mono-80"
-            className="text-mono-100 dark:text-mono-80"
-          />
+        <div className="px-4 space-y-2">
           <div className="space-y-1">
             <InfoItem
               leftTextProps={{
-                variant: 'body1',
+                variant: 'utility',
                 title: 'Depositing',
               }}
               rightContent={amountValue}
             />
             <InfoItem
               leftTextProps={{
-                variant: 'body1',
+                variant: 'utility',
                 title: 'Fees',
                 info: 'Fees',
               }}
@@ -149,57 +199,32 @@ export const DepositConfirm = forwardRef<HTMLDivElement, DepositConfirmProps>(
           </div>
         </div>
 
-        {/** New spend note */}
-        <div className="space-y-2">
-          <TitleWithInfo
-            titleComponent="h6"
-            title="New Spend Note"
-            info="New Spend Note"
-            variant="utility"
-            titleClassName="text-mono-100 dark:text-mono-80"
-            className="text-mono-100 dark:text-mono-80"
-          />
-          <div className="flex items-center justify-between">
-            <div className="px-4 py-1.5 bg-mono-20 dark:bg-mono-160 rounded-lg grow max-w-[438px] truncate">
-              <Typography
-                variant="mono1"
-                fw="bold"
-                className="text-mono-140 dark:text-mono-0"
-              >
-                {note}
-              </Typography>
-            </div>
-            <Button
-              variant="utility"
-              size="sm"
-              className="p-2"
-              onClick={onCopy}
-            >
-              <FileCopyLine className="!fill-current" />
-            </Button>
-            <Button
-              variant="utility"
-              size="sm"
-              className="p-2"
-              onClick={onDownload}
-            >
-              <Download className="!fill-current" />
-            </Button>
-          </div>
-          <CheckBox
-            {...checkboxProps}
-            wrapperClassName={twMerge(
-              'flex items-center',
-              checkboxProps?.wrapperClassName
-            )}
-          >
-            {checkboxProps?.children ?? 'I have copied the spend note'}
-          </CheckBox>
-        </div>
-
         <Button {...actionBtnProps} isFullWidth className="justify-center">
           {actionBtnProps?.children ?? 'Deposit'}
         </Button>
+      </div>
+    );
+  }
+);
+
+/***********************
+ * Internal components *
+ ***********************/
+
+const WrapperCard = forwardRef<HTMLDivElement, PropsOf<'div'>>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div
+        {...props}
+        className={twMerge(
+          'p-2 bg-mono-20 dark:bg-mono-160 rounded-lg',
+          className
+        )}
+        ref={ref}
+      >
+        <div className="px-4 py-2 rounded-lg bg-mono-0 dark:bg-mono-140">
+          {children}
+        </div>
       </div>
     );
   }
