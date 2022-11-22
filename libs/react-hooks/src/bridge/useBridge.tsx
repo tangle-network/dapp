de92039e6b21b6eb3c2a2b80ff30b9a0210236a9
ahmed korim <ahmedkorrim@gmail.com>
2022-11-22 14:58:50 +0200

    Enhance bridge api hook

--- libs/react-hooks/src/bridge/useBridge.tsx
+++ libs/react-hooks/src/bridge/useBridge.tsx
@@ -1,14 +1,21 @@
 import { Currency } from '@webb-tools/abstract-api-provider';
 import { useWebContext } from '@webb-tools/api-provider-environment';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export interface BridgeApi {
-  setGovernedCurrency(currency: Currency): Promise<void>;
+  setGovernedCurrency(currency: Currency | null): Promise<void>;
+
   setWrappableCurrency(currency: Currency | null): Promise<void>;
+  wrappableCurrency: Currency | null;
+  governedCurrency: Currency | null;
 }
 
 export const useBridge = (): BridgeApi => {
   const { activeApi } = useWebContext();
+  const [governedCurrency, setGovernedCurrencyState] =
+    useState<Currency | null>(activeApi?.state.activeBridge.currency ?? null);
+  const [wrappableCurrency, setWrapableCurrencyState] =
+    useState<Currency | null>(activeApi?.state.wrappableCurrency ?? null);
 
   const setWrappableCurrency = useCallback(
     async (currency: Currency | null) => {
@@ -20,7 +27,7 @@ export const useBridge = (): BridgeApi => {
   );
 
   const setGovernedCurrency = useCallback(
-    async (currency: Currency) => {
+    async (currency: Currency | null) => {
       if (!activeApi) {
         return;
       }
@@ -29,9 +36,21 @@ export const useBridge = (): BridgeApi => {
     },
     [activeApi]
   );
+  useEffect(() => {
+    if (activeApi) {
+      activeApi.state.$activeBridge.subscribe((bridge) =>
+        setWrapableCurrencyState(bridge?.currency)
+      );
+      activeApi.state.$wrappableCurrency.subscribe((currency) => {
+        setWrapableCurrencyState(currency);
+      });
+    }
+  }, [activeApi, setWrapableCurrencyState, setGovernedCurrencyState]);
 
   return {
     setWrappableCurrency,
     setGovernedCurrency,
+    wrappableCurrency,
+    governedCurrency,
   };
 };
