34b9918e0734dcec750a049340ff1d3df18c654f
AtelyPham <tinpham1412000@gmail.com>
2022-12-02 13:32:15 +0700

    update the transfer container

--- apps/bridge-dapp/src/containers/TransferContainer/TransferContainer.tsx
+++ apps/bridge-dapp/src/containers/TransferContainer/TransferContainer.tsx
@@ -1,11 +1,49 @@
+import { useBridge, useCurrencies } from '@webb-tools/react-hooks';
 import { TransferCard } from '@webb-tools/webb-ui-components';
-import React, { forwardRef } from 'react';
+import { AssetType } from '@webb-tools/webb-ui-components/components/ListCard/types';
+import { forwardRef, useCallback, useMemo } from 'react';
 import { TransferContainerProps } from './types';
 
 export const TransferContainer = forwardRef<
   HTMLDivElement,
   TransferContainerProps
 >((props, ref) => {
+  const { governedCurrencies, wrappableCurrencies } = useCurrencies();
+
+  const { governedCurrency, setGovernedCurrency } = useBridge();
+
+  const bridgingAssets = useMemo((): AssetType[] => {
+    return Object.values(governedCurrencies).map((currency) => {
+      return {
+        name: currency.view.name,
+        symbol: currency.view.symbol,
+      };
+    });
+  }, [governedCurrencies]);
+
+  const handleGovernedTokenChange = useCallback(
+    async (newToken: AssetType) => {
+      const selectedToken = Object.values(governedCurrencies).find(
+        (currency) => currency.view.symbol === newToken.symbol
+      );
+      if (selectedToken) {
+        setGovernedCurrency(selectedToken);
+      }
+    },
+    [governedCurrencies, setGovernedCurrency]
+  );
+
+  const selectedBridgingAsset = useMemo<AssetType | undefined>(() => {
+    if (!governedCurrency) {
+      return undefined;
+    }
+    return {
+      symbol: governedCurrency.view.symbol,
+      name: governedCurrency.view.name,
+      balance: availableAmount,
+    };
+  }, [availableAmount, governedCurrency]);
+
   return (
     <div>
       <TransferCard />
