02e24d6742d7c6cf2abb7ef6fda663043e75386f
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2022-12-14 02:11:01 +0700

    Setup Static Marketing Page (#808)

---
+++ apps/webbsite/src/components/Heading4.tsx
@@ -0,0 +1,22 @@
+import { PropsOf } from '@webb-tools/webb-ui-components/types';
+import React, { forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+const Heading4 = forwardRef<HTMLHeadingElement, PropsOf<'h1'>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <h1
+        {...props}
+        className={twMerge(
+          'text-[24px] leading-[36px] text-mono-200 font-black',
+          className
+        )}
+        ref={ref}
+      />
+    );
+  }
+);
+
+Heading4.displayName = 'Heading4';
+
+export default Heading4;
--- apps/webbsite/src/pages/index.tsx
+++ apps/webbsite/src/pages/index.tsx
@@ -1,7 +1,28 @@
-import { Typography } from '@webb-tools/webb-ui-components';
+import ApplicationsAndInfrastructureSection from '../components/sections/ApplicationsAndInfrastructureSection';
+import HeroSection from '../components/sections/HeroSection';
+import InActionSection from '../components/sections/InActionSection';
+import PrivacyConnectedSection from '../components/sections/PrivacyConnectedSection';
+import PrivacyScaleSection from '../components/sections/PrivacyScaleSection';
+import ResearchAndDevelopmentSection from '../components/sections/ResearchAndDevelopmentSection';
 
 export function Index() {
-  return <Typography variant="h1">Welcome to Webbsite!</Typography>;
+  return (
+    <>
+      <div className="relative object-cover bg-no-repeat bg-cover bg-hero_bg_image min-h-[1400px] h-[2250px] bg-top">
+        <HeroSection />
+
+        <PrivacyConnectedSection />
+      </div>
+
+      <PrivacyScaleSection />
+
+      <ApplicationsAndInfrastructureSection />
+
+      <InActionSection />
+
+      <ResearchAndDevelopmentSection />
+    </>
+  );
 }
 
 export default Index;
