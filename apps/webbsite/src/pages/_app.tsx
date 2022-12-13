a6d85cfc8a04979058f5b261d563b6365be2719c
AtelyPham <tinpham1412000@gmail.com>
2022-12-13 17:24:15 +0700

    feat: add footer component

--- apps/webbsite/src/pages/_app.tsx
+++ apps/webbsite/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,6 +20,8 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <main className="app">
         <Component {...pageProps} />
       </main>
+
+      <Footer />
     </>
   );
 }
