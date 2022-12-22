8a79fe61009b47dee0f0753f4bce21edc7d482dd
AtelyPham <tinpham1412000@gmail.com>
2022-12-22 20:00:50 +0700

    refactor: parameterize tabs content in privacy connected section

--- apps/webbsite/src/components/sections/PrivacyConnectedSection.tsx
+++ apps/webbsite/src/components/sections/PrivacyConnectedSection.tsx
@@ -9,8 +9,31 @@ import {
 import Heading2 from '../Heading2';
 import SubHeading from '../SubHeading';
 import useDynamicLottiePlayer from '../../hooks/useDynamicLottiePlayer';
+import { FC, useState } from 'react';
+import { Transition } from '@headlessui/react';
+
+const tabsContent = {
+  ownership: {
+    animationUrl: '/animations/deposit.lottie',
+    title: 'Proof of Ownership',
+  },
+  identity: {
+    animationUrl: '/animations/kyc.lottie',
+    title: 'Proof of Identity',
+  },
+  privacy: {
+    animationUrl: '/animations/ecosystem.lottie',
+    title: 'Privacy Ecosystems',
+  },
+};
+
+type TabTypes = 'ownership' | 'identity' | 'privacy';
 
 const PrivacyConnectedSection = () => {
+  // State for the selected tab
+  const [activeTab, setActiveTab] = useState<TabTypes>('ownership');
+
+  // Dynamic loading the lottie player in client side
   useDynamicLottiePlayer();
 
   return (
@@ -25,41 +48,51 @@ const PrivacyConnectedSection = () => {
         Web3 ecosystem.
       </SubHeading>
       <TabsRoot
-        defaultValue="ownership"
+        defaultValue={activeTab}
+        value={activeTab}
+        onValueChange={(nextTab) => setActiveTab(nextTab as TabTypes)}
         className="p-4 space-y-4 rounded-lg mt-9 bg-mono-0"
       >
         <TabsList aria-label="tabs" className="mb-4">
-          <TabTrigger value="ownership">Proof of Ownership</TabTrigger>
-          <TabTrigger value="identity">Proof of Identity</TabTrigger>
-          <TabTrigger value="privacy">Privacy Ecosystems</TabTrigger>
+          {Object.entries(tabsContent).map(([key, value]) => (
+            <TabTrigger key={key} value={key}>
+              {value.title}
+            </TabTrigger>
+          ))}
         </TabsList>
-        <TabContent className="w-[900px] h-[340px]" value="ownership">
-          <dotlottie-player
-            src="/animations/deposit.lottie"
-            autoplay
-            loop
-            style={{ height: '100%', width: '100%' }}
-          />
-        </TabContent>
-        <TabContent className="w-[900px] h-[340px]" value="identity">
-          <dotlottie-player
-            src="/animations/kyc.lottie"
-            autoplay
-            loop
-            style={{ height: '100%', width: '100%' }}
-          />
-        </TabContent>
-        <TabContent className="w-[900px] h-[340px]" value="privacy">
-          <dotlottie-player
-            src="/animations/ecosystem.lottie"
-            autoplay
-            loop
-            style={{ height: '100%', width: '100%' }}
-          />
-        </TabContent>
+
+        {/* Tabs content */}
+        {Object.entries(tabsContent).map(([key, value]) => (
+          <Transition
+            appear
+            show={key === activeTab}
+            enter="transition-opacity duration-[1000]"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-[1000]"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            key={key}
+          >
+            <TabContent className="w-[900px] h-[340px]" value={key}>
+              <LottiePlayer animationUrl={value.animationUrl} />
+            </TabContent>
+          </Transition>
+        ))}
       </TabsRoot>
     </section>
   );
 };
 
 export default PrivacyConnectedSection;
+
+const LottiePlayer: FC<{ animationUrl: string }> = ({ animationUrl }) => {
+  return (
+    <dotlottie-player
+      src={animationUrl}
+      autoplay
+      loop
+      style={{ height: '100%', width: '100%' }}
+    />
+  );
+};
