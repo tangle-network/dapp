397dee717912d6133eccaf9168b0bd784d900179
AtelyPham <tinpham1412000@gmail.com>
2023-01-06 00:52:34 +0700

    add shadow to header when user scrolls the page

--- apps/webbsite/src/components/Header.tsx
+++ apps/webbsite/src/components/Header.tsx
@@ -1,10 +1,35 @@
 import { Logo } from '@webb-tools/webb-ui-components/components/Logo/Logo';
+import cx from 'classnames';
 
+import { useEffect, useState } from 'react';
 import Navbar from './Navbar';
-
+// webb-shadow-sm
 const Header = () => {
+  // State for tracking whether the user has scrolled down the page
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    // Listen for scroll events
+    window.addEventListener('scroll', handleScroll);
+
+    handleScroll();
+
+    return () => {
+      // Clean up the event listener
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed z-50 w-full bg-mono-0">
+    <header
+      className={cx('fixed z-50 w-full bg-mono-0', {
+        'webb-shadow-sm': scrolled,
+      })}
+    >
       <div className="max-w-[1200px] mx-auto p-4 flex items-center justify-between">
         <Logo />
 
