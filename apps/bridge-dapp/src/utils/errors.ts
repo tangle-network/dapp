d4a38310046c3544aa8d336ea39ad33856b571d7
AtelyPham <tinpham1412000@gmail.com>
2023-01-18 15:53:41 +0700

    feat: add get error message function from unknown error

--- apps/bridge-dapp/src/utils/errors.ts
+++ apps/bridge-dapp/src/utils/errors.ts
@@ -5,3 +5,33 @@ export const DEPOSIT_FAILURE_MSG: Omit<SnackBarOpts, 'close'> = {
   message: 'Deposit failed',
   secondaryMessage: 'Something went wrong when depositing',
 };
+
+/**
+ * Check if an unknown error has a `message` property
+ * @param error The `unknown` error to check
+ * @returns true if the error has a `message` property
+ */
+const hasMessage = (error: unknown): error is { message: string } => {
+  return typeof error === 'object' && error !== null && 'message' in error;
+};
+
+/**
+ * Get the error message from an unknown type error
+ * @param error The `unknown` error to check
+ * @returns the error mssage from the unknown error
+ */
+export const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (typeof error === 'string') {
+    return error;
+  }
+
+  if (hasMessage(error)) {
+    return error.message;
+  }
+
+  return 'An unknown error occurred';
+};
