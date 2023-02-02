81d0198dec94063594aa517663ccf77c739b6671
AtelyPham <tinpham1412000@gmail.com>
2023-02-02 23:27:33 +0700

    feat: read content from notion and add privacy policy page

--- apps/webbsite/src/pages/privacy-policy.tsx
+++ apps/webbsite/src/pages/privacy-policy.tsx
@@ -1,12 +1,28 @@
-import { FC } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { ExtendedRecordMap } from 'notion-types';
+import { FC } from 'react';
+import { NotionRenderer } from 'react-notion-x';
 
+import Heading2 from '../components/Heading2';
 import getNotionPage from '../libs/notion/getNotionPage';
 import { PRIVACY_AND_POLICY_PAGE_ID } from '../libs/notion/server-constants';
 
 const PrivacyPolicy: FC<{ data: ExtendedRecordMap }> = ({ data }) => {
-  console.log(data);
-  return <div>PrivacyPolicy</div>;
+  return (
+    <NotionRenderer
+      bodyClassName="!mt-[68px]"
+      fullPage
+      disableHeader
+      components={{
+        Header: Heading2,
+        nextImage: Image,
+        nextLink: Link,
+      }}
+      pageCover={<div />} // Keep the content inside the <main></main>
+      recordMap={data}
+    />
+  );
 };
 
 export default PrivacyPolicy;
