4e0b3c70dc8c1da889ee9f501c96146c0b791e14
AtelyPham <tinpham1412000@gmail.com>
2023-02-03 01:23:39 +0700

    chore: correct links

--- apps/webbsite/src/constants/index.ts
+++ apps/webbsite/src/constants/index.ts
@@ -1,3 +1,12 @@
 export const BRIDGE_URL = 'https://development-hubble-bridge.netlify.app/';
 
 export const STATS_URL = 'https://www.stats-dev.webb.tools/';
+
+export const TANGLE_NETWORK_URL =
+  'https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Ftangle-archive.webb.tools#/explorer';
+
+export const CONNECTED_SHIELDED_POOLS_URL =
+  'https://docs.webb.tools/docs/protocols/asset-transfer/overview/';
+
+export const CONNECTED_SHIELDED_IDENTITY_URL =
+  'https://docs.webb.tools/docs/protocols/identity/';
