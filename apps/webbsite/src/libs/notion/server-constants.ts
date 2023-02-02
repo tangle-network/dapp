911155870b6c77e4026e758150663659edc20a82
AtelyPham <tinpham1412000@gmail.com>
2023-02-03 00:53:36 +0700

    feat: add terms and conditions page

--- apps/webbsite/src/libs/notion/server-constants.ts
+++ apps/webbsite/src/libs/notion/server-constants.ts
@@ -5,7 +5,7 @@ export const NOTION_API_KEY = process.env.NOTION_API_KEY;
 export const NOTION_TOKEN_V2 = process.env.NOTION_TOKEN_V2;
 export const NOTION_ACTIVE_USER = process.env.NOTION_ACTIVE_USER;
 
-export const PRIVACY_AND_POLICY_PAGE_ID =
-  process.env.PRIVACY_AND_POLICY_PAGE_ID;
+export const PRIVACY_POLICY_PAGE_ID = process.env.PRIVACY_POLICY_PAGE_ID;
 
-export const TERMS_OF_CONDITIONS_PAGE_ID = process.env.TERMS_OF_SERVICE_PAGE_ID;
+export const TERMS_AND_CONDITIONS_PAGE_ID =
+  process.env.TERMS_AND_CONDITIONS_PAGE_ID;
--- apps/webbsite/src/pages/terms-and-conditions.tsx
+++ apps/webbsite/src/pages/terms-and-conditions.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { ExtendedRecordMap } from 'notion-types';
-import { FC } from 'react';
+import { FC, RefAttributes } from 'react';
 import { NotionRenderer } from 'react-notion-x';
 
 import Heading2 from '../components/Heading2';
 import getNotionPage from '../libs/notion/getNotionPage';
-import { PRIVACY_POLICY_PAGE_ID } from '../libs/notion/server-constants';
+import { TERMS_AND_CONDITIONS_PAGE_ID } from '../libs/notion/server-constants';
+import { Button, ButtonProps } from '@webb-tools/webb-ui-components';
 
-const PrivacyPolicy: FC<{ data: ExtendedRecordMap }> = ({ data }) => {
+const TermsAndConditions: FC<{ data: ExtendedRecordMap }> = ({ data }) => {
   return (
     <NotionRenderer
       bodyClassName="!mt-[68px]"
@@ -16,6 +17,9 @@ const PrivacyPolicy: FC<{ data: ExtendedRecordMap }> = ({ data }) => {
       disableHeader
       components={{
         Header: Heading2,
+        Link: (props: any) => (
+          <Button className="inline" variant="link" {...props} />
+        ),
         nextImage: Image,
         nextLink: Link,
       }}
@@ -25,10 +29,10 @@ const PrivacyPolicy: FC<{ data: ExtendedRecordMap }> = ({ data }) => {
   );
 };
 
-export default PrivacyPolicy;
+export default TermsAndConditions;
 
 export const getStaticProps = async () => {
-  const pageId = PRIVACY_POLICY_PAGE_ID;
+  const pageId = TERMS_AND_CONDITIONS_PAGE_ID;
   if (!pageId) {
     throw new Error('Missing Notion page ID.'); // Only for development
   }
