7c4d0d833175fc1bd8e6f3d3a7a308edf771f68a
Pavan Soratur <pavanvsoratur@gmail.com>
2023-02-14 06:47:33 -0800

    chore: updates token format function

--- libs/webb-ui-components/src/utils/formatTokenAmount.ts
+++ libs/webb-ui-components/src/utils/formatTokenAmount.ts
@@ -1,12 +1,13 @@
 /**
- * Format token amount to 3 decimal places and add `<` if amount is less than 0.001
+ * Formats a token amount to a given number of decimal places and adds a < symbol if the amount is less than 0.001
  * @param amount The amount to format
+ * @param decimalPlaces (Optional) The number of decimal places to format to - defaults to 3
  * @returns The formatted amount
  */
-export const formatTokenAmount = (amount: string) => {
-  const formattedAmount = parseFloat(amount).toFixed(2);
-  if (Number(formattedAmount) < 0.01) {
-    return `< 0.01`;
+export const formatTokenAmount = (amount: string, decimalPlaces = 3) => {
+  const formattedAmount = parseFloat(amount).toFixed(decimalPlaces);
+  if (Number(formattedAmount) < 0.001) {
+    return `< 0.001`;
   }
   return formattedAmount;
 };
