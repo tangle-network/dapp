9fe50c473c4565767be5ea571f6a2b07080d75ed
AtelyPham <tinpham1412000@gmail.com>
2023-02-16 23:40:30 +0700

    add transition to education card

--- apps/bridge-dapp/src/components/EducationCard/EducationCard.tsx
+++ apps/bridge-dapp/src/components/EducationCard/EducationCard.tsx
@@ -1,9 +1,10 @@
-import { Close, UsageGuideIcon } from '@webb-tools/icons';
+import { Close, HelpLineIcon, UsageGuideIcon } from '@webb-tools/icons';
 import { Button, Typography } from '@webb-tools/webb-ui-components';
 import cx from 'classnames';
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { EducationCardProps } from './types';
+import { Transition } from '@headlessui/react';
 
 const links = [
   {
@@ -70,107 +71,150 @@ const howItWorksContent: {
   ],
 };
 
+const commonTransitionClass = cx('transition-all duration-200');
+
 export const EducationCard = forwardRef<HTMLDivElement, EducationCardProps>(
   ({ currentTab, ...props }, ref) => {
+    const [isOpen, setIsOpen] = useState(true);
+
     return (
-      <div
-        {...props}
-        className={twMerge(
-          'flex flex-row-reverse items-start',
-          props.className
-        )}
-        ref={ref}
-      >
-        <button>
-          <Close size="lg" />
-        </button>
-
-        <div className="space-y-4 grow">
-          <Typography variant="h3" fw="bold">
-            Privacy for everyone, everything, everywhere.
-          </Typography>
-
-          <div className="flex space-x-9">
-            {links.map(({ label, href, getLink, Icon }) => (
-              <Button
-                leftIcon={<Icon className="!fill-current" size="lg" />}
-                href={href ?? getLink?.(currentTab)}
-                rel="noopener noreferrer"
-                target="_blank"
-                variant="link"
-              >
-                {label}
-              </Button>
-            ))}
-          </div>
-
-          <Typography
-            variant="body1"
-            fw="semibold"
-            component="p"
-            className="text-mono-120 dark:text-mono-100"
+      <>
+        <Transition
+          enter={commonTransitionClass}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave={commonTransitionClass}
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          show={!isOpen}
+        >
+          <button
+            className="py-2 px-2.5 space-x-1 flex items-center ml-auto"
+            onClick={() => setIsOpen(true)}
           >
-            How it works?
-          </Typography>
+            <HelpLineIcon size="lg" />
 
-          <div className="space-y-2">
-            {howItWorksContent[currentTab].map((content, index) => {
-              return (
-                <div className="relative flex items-start space-x-2">
-                  {/** Vertical line */}
-                  <span
-                    className={cx(
-                      {
-                        'content-[""] absolute w-0.5 h-[calc(100%-16px)]':
-                          index !== howItWorksContent[currentTab].length - 1,
-                      },
-                      {
-                        'translate-y-6 translate-x-[18px] bg-mono-120 dark:bg-mono-100':
-                          index !== howItWorksContent[currentTab].length - 1,
-                      }
-                    )}
-                  />
+            <Typography component="span" variant="body1" fw="bold">
+              Guide
+            </Typography>
+          </button>
+        </Transition>
 
-                  <Typography
-                    component="p"
-                    className={cx(
-                      'flex items-center justify-center w-6 h-6 border-2 rounded-full',
-                      { 'border-mono-200 dark:border-mono-0': !index },
-                      {
-                        'border-mono-120 dark:border-mono-100 text-mono-120 dark:text-mono-100':
-                          index,
-                      }
-                    )}
-                    variant="mono2"
-                    fw="bold"
+        <Transition
+          enter={cx(commonTransitionClass, 'origin-top-right delay-200')}
+          enterFrom={cx('opacity-0 scale-0')}
+          enterTo={cx('opacity-100 scale-100')}
+          leave={cx(commonTransitionClass, 'origin-top-right')}
+          leaveFrom={cx('opacity-100 scale-100')}
+          leaveTo={cx('opacity-0 scale-0')}
+          show={isOpen}
+        >
+          <div
+            {...props}
+            className={twMerge(
+              'flex flex-row-reverse items-start',
+              props.className
+            )}
+            ref={ref}
+          >
+            <button onClick={() => setIsOpen(false)}>
+              <Close size="lg" />
+            </button>
+
+            <div className="space-y-4 grow">
+              <Typography variant="h3" fw="bold">
+                Privacy for everyone, everything, everywhere.
+              </Typography>
+
+              <div className="flex space-x-9">
+                {links.map(({ label, href, getLink, Icon }, idx) => (
+                  <Button
+                    key={`${label}-${idx}`}
+                    leftIcon={<Icon className="!fill-current" size="lg" />}
+                    href={href ?? getLink?.(currentTab)}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    variant="link"
                   >
-                    {index + 1}
-                  </Typography>
+                    {label}
+                  </Button>
+                ))}
+              </div>
 
-                  <div className="space-y-1">
-                    <Typography
-                      variant="body1"
-                      fw="bold"
-                      className={cx({
-                        'text-mono-120 dark:text-mono-100': index,
-                      })}
-                    >
-                      {content.title}
-                    </Typography>
+              <Typography
+                variant="body1"
+                fw="semibold"
+                component="p"
+                className="text-mono-120 dark:text-mono-100"
+              >
+                How it works?
+              </Typography>
 
-                    <Typography
-                      variant="body1"
-                      className="text-mono-120 dark:text-mono-100"
+              <div className="space-y-2">
+                {howItWorksContent[currentTab].map((content, index) => {
+                  return (
+                    <div
+                      key={`${content.title}-${index}`}
+                      className="relative flex items-start space-x-2"
                     >
-                      {content.description}
-                    </Typography>
-                  </div>
-                </div>
-              );
-            })}
+                      {/** Vertical line */}
+                      <span
+                        className={cx(
+                          {
+                            'content-[""] absolute w-0.5 h-[calc(100%-16px)]':
+                              index !==
+                              howItWorksContent[currentTab].length - 1,
+                          },
+                          {
+                            'translate-y-6 translate-x-[18px] bg-mono-120 dark:bg-mono-100':
+                              index !==
+                              howItWorksContent[currentTab].length - 1,
+                          }
+                        )}
+                      />
+
+                      <Typography
+                        component="p"
+                        className={cx(
+                          'flex items-center justify-center w-6 h-6 border-2 rounded-full',
+                          { 'border-mono-200 dark:border-mono-0': !index },
+                          {
+                            'border-mono-120 dark:border-mono-100 text-mono-120 dark:text-mono-100':
+                              index,
+                          }
+                        )}
+                        variant="mono2"
+                        fw="bold"
+                      >
+                        {index + 1}
+                      </Typography>
+
+                      <div className="space-y-1">
+                        <Typography
+                          variant="body1"
+                          fw="bold"
+                          className={cx({
+                            'text-mono-120 dark:text-mono-100': index,
+                          })}
+                        >
+                          {content.title}
+                        </Typography>
+
+                        <Typography
+                          variant="body1"
+                          className="text-mono-120 dark:text-mono-100"
+                        >
+                          {content.description}
+                        </Typography>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </Transition>
+      </>
     );
   }
 );
