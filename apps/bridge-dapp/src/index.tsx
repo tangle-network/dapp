b2f5af101430fdce7a14ba7fe7466255ef629857
Pavan Soratur <pavanvsoratur@gmail.com>
2023-02-17 10:43:30 -0800

    chore: exports app component with sentry profiler

--- apps/bridge-dapp/src/index.tsx
+++ apps/bridge-dapp/src/index.tsx
@@ -16,5 +16,6 @@ Sentry.init({
 });
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App />);
