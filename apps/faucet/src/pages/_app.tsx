741b94ece02d4fc3b7577f6c3e8331d1e47e37c5
AtelyPham <tinpham1412000@gmail.com>
2023-03-13 12:19:45 +0700

    chore: add metadata for faucet app

--- apps/faucet/src/pages/_app.tsx
+++ apps/faucet/src/pages/_app.tsx
@@ -1,21 +1,61 @@
 import '@webb-tools/webb-ui-components/tailwind.css';
 
+import { WebbUIProvider } from '@webb-tools/webb-ui-components';
+import { DefaultSeo, DefaultSeoProps } from 'next-seo';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { useDarkMode, WebbUIProvider } from '@webb-tools/webb-ui-components';
 
 import '../styles/globals.css';
 
+const defaultMetadata: DefaultSeoProps = {
+  defaultTitle: 'Webb Faucet',
+  titleTemplate: 'Webb | %s',
+  description:
+    'Our easy-to-use crypto faucet website allows you to claim free WebTNT with just a few clicks. Start earning fast and hassle-free payouts today!',
+  themeColor: '#ffffff',
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/static/assets/favicon.png',
+    },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://faucet.webb.tools/',
+    siteName: 'Webb Faucet',
+    images: [
+      {
+        url: '/static/assets/og.png',
+        width: 2400,
+        height: 1800,
+        alt: 'Og Image Alt',
+      },
+      {
+        url: '/static/assets/og-optimize.png',
+        width: 1200,
+        height: 630,
+        alt: 'Optimized Og Image Alt',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@webbprotocol',
+    site: '@webbprotocol',
+    cardType: 'summary_large_image',
+  },
+};
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <WebbUIProvider defaultThemeMode="light">
-      <Head>
-        <title>Welcome to faucet!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </WebbUIProvider>
+    <>
+      <DefaultSeo {...defaultMetadata} />
+
+      <WebbUIProvider defaultThemeMode="light">
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </WebbUIProvider>
+    </>
   );
 }
 
--- apps/faucet/src/pages/index.tsx
+++ apps/faucet/src/pages/index.tsx
@@ -1,10 +1,28 @@
 import { Typography } from '@webb-tools/webb-ui-components';
+import { NextSeo, NextSeoProps } from 'next-seo';
+
+const metadata: NextSeoProps = {
+  additionalMetaTags: [
+    {
+      property: 'keywords',
+      content: 'Faucet,Webb Faucet,Webb Protocol,Crypto Faucet',
+    },
+    {
+      property: 'creator',
+      content: 'Webb Developers',
+    },
+  ],
+};
 
 export function Index() {
   return (
-    <Typography variant="h1" className="text-blue-50">
-      Welcome to Webb Faucet!
-    </Typography>
+    <>
+      <NextSeo {...metadata} />
+
+      <Typography variant="h1" className="text-blue-50">
+        Welcome to Webb Faucet!
+      </Typography>
+    </>
   );
 }
 
