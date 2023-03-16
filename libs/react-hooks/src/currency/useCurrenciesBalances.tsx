8f528bc67200682537b664f05485d8aaee0b42ee
AtelyPham <tinpham1412000@gmail.com>
2023-03-16 23:10:28 +0700

    fix: auto select wrappable and optimized the balances hook

--- libs/react-hooks/src/currency/useCurrenciesBalances.tsx
+++ libs/react-hooks/src/currency/useCurrenciesBalances.tsx
@@ -1,7 +1,7 @@
 import { Currency } from '@webb-tools/abstract-api-provider';
 import { useWebContext } from '@webb-tools/api-provider-environment';
 import { calculateTypedChainId } from '@webb-tools/sdk-core';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * Fetch the balances of the currencies list
@@ -13,29 +13,17 @@ export const useCurrenciesBalances = (
 ): Record<Currency['id'], number> => {
   const { activeApi, activeChain, activeAccount } = useWebContext();
 
-  const currencyIds = useRef(
-    new Set<number>(currencies.map((currency) => currency.id))
-  );
-
   // Balances object map currency id and its balance
   const [balances, setBalances] = useState<Record<number, number>>({});
-
-  // Filter the currencies that already in the currencyIds
-  const newCurrencies = useMemo(() => {
-    return currencies.filter((c) => !currencyIds.current.has(c.id));
-  }, [currencies]);
 
   useEffect(() => {
     let isSubscribe = true;
 
-    if (!activeApi || !activeChain || !newCurrencies.length) {
+    if (!activeApi || !activeChain) {
       return;
     }
 
-    const subscriptions = newCurrencies.map((currency) => {
-      // Add the new currency id to the currencyIds
-      currencyIds.current.add(currency.id);
-
+    const subscriptions = currencies.map((currency) => {
       return activeApi.methods.chainQuery
         .tokenBalanceByCurrencyId(
           calculateTypedChainId(activeChain.chainType, activeChain.chainId),
@@ -43,10 +31,16 @@ export const useCurrenciesBalances = (
         )
         .subscribe((currencyBalance) => {
           if (isSubscribe) {
-            setBalances((prev) => ({
-              ...prev,
-              [currency.id]: Number(currencyBalance),
-            }));
+            setBalances((prev) => {
+              if (prev[currency.id] === Number(currencyBalance)) {
+                return prev;
+              }
+
+              return {
+                ...prev,
+                [currency.id]: Number(currencyBalance),
+              };
+            });
           }
         });
     });
@@ -55,9 +49,7 @@ export const useCurrenciesBalances = (
       isSubscribe = false;
       subscriptions.forEach((subscription) => subscription.unsubscribe());
     };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [!activeApi || !activeChain || !newCurrencies.length, activeAccount]);
+  }, [activeApi, activeChain, activeAccount, currencies]);
 
   return balances;
 };
