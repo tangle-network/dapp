e3a9c078b9ae5f15501e07e051048d628f69ca8f
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-03-26 11:09:13 +0700

    refactor: add Tangle Logo to webb ui components

--- apps/tangle-website/src/components/Header.tsx
+++ apps/tangle-website/src/components/Header.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
-import { Navbar, TangleLogo } from '.';
+import { TangleLogo } from '@webb-tools/webb-ui-components';
+import { Navbar } from '.';
 
 export const Header = () => {
   return (
     <header className="fixed z-50 w-full bg-mono-0 min-h-[72px] webb-shadow-sm">
       <div className="max-w-[1200px] mx-auto p-4 flex items-center justify-between">
         <Link href="/">
-          <TangleLogo />
+          <TangleLogo hideNameOnMobile />
         </Link>
         <Navbar />
       </div>
