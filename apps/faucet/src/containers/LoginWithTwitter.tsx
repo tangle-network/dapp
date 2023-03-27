535c978dde1734047f25dfc29068618910103f01
AtelyPham <tinpham1412000@gmail.com>
2023-03-28 00:28:16 +0700

    chore: mocked twitter login

--- apps/faucet/src/containers/LoginWithTwitter.tsx
+++ apps/faucet/src/containers/LoginWithTwitter.tsx
@@ -1,9 +1,51 @@
 import { Button } from '@webb-tools/webb-ui-components';
+import { useObservableState } from 'observable-hooks';
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import { useFaucetContext } from '../provider';
+
+// The mocked login with twitter function
+const mockedLoginWithTwitter = (): Promise<string> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('@alice.eth');
+    }, 1500);
+  });
+};
 
 const LoginWithTwitter = () => {
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+  const { twitterHandle$ } = useFaucetContext();
+
+  const handleLoginWithTwitter = useCallback(async () => {
+    try {
+      setIsLoggingIn(true);
+      const twitterHandle = await mockedLoginWithTwitter();
+      twitterHandle$.next(twitterHandle);
+    } catch (error) {
+      console.error('Error logging in with Twitter', error);
+    } finally {
+      setIsLoggingIn(false);
+    }
+  }, [twitterHandle$]);
+
   return (
     <div>
-      <Button isFullWidth>Login with Twitter</Button>
+      <Button
+        isLoading={isLoggingIn}
+        loadingText="Logging In"
+        onClick={handleLoginWithTwitter}
+        isFullWidth
+      >
+        Login with Twitter
+      </Button>
     </div>
   );
 };
