42c070a290a98ac25c1acf8634c5576800830115
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2023-03-29 14:25:53 +0700

    Feat: add website header (#1073)
    Co-authored-by: Pavan Soratur <pavanvsoratur@gmail.com>
    Co-authored-by: Dustin Brickwood <dustinbrickwood204@gmail.com>
    Co-authored-by: Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>

---
+++ apps/tangle-website/src/components/index.ts
@@ -0,0 +1,8 @@
+// General components
+export * from './Header';
+export * from './Navbar';
+export * from './Heading1';
+export * from './SubHeading1';
+export * from './InternalOrExternalLink';
+// Section components
+export * from './sections';
