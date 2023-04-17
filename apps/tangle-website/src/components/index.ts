a50eeadf7b16d7cb73bc4268b2f0d01d5e79a648
Pavan Soratur <pavanvsoratur@gmail.com>
2023-04-17 09:58:48 -0700

    chore: adds usecases section

--- apps/tangle-website/src/components/index.ts
+++ apps/tangle-website/src/components/index.ts
@@ -7,6 +7,7 @@ export * from './Navbar';
 export * from './SectionDescription';
 export * from './SectionHeader';
 export * from './SectionTitle';
+export * from './SectionTitle2';
 export * from './TangleFeatureCard';
 // Section components
 export * from './sections';
--- apps/tangle-website/src/components/svgs/index.tsx
+++ apps/tangle-website/src/components/svgs/index.tsx
@@ -1,3 +1,7 @@
 export * from './HowTssWorksSvg';
 export * from './IntroducingTangleSvg';
 export * from './WhatIsTssSvg';
+export * from './UseCase1Svg'
+export * from './UseCase2Svg'
+export * from './UseCase3Svg'
+export * from './UseCase4Svg'
