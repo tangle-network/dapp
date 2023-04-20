bc276b626ccc6d9b5f145e8a1bb15cd427d939b4
Pavan Soratur <pavanvsoratur@gmail.com>
2023-04-20 10:15:35 -0700

    chore: adds seo and plausible

--- apps/tangle-website/src/pages/_app.tsx
+++ apps/tangle-website/src/pages/_app.tsx
@@ -1,14 +1,51 @@
+import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { WebbUIProvider, WebsiteFooter } from '@webb-tools/webb-ui-components';
 import '@webb-tools/webb-ui-components/tailwind.css';
 import '../styles/globals.css';
-
 import { Header } from '../components';
+import Script from 'next/script';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <WebbUIProvider defaultThemeMode="light">
+      <DefaultSeo
+        defaultTitle="Welcome to Tangle Network!"
+        titleTemplate="Tangle Network | %s"
+        themeColor="#ffffff"
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: '/static/assets/favicon.png',
+          },
+        ]}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://tangle.webb.tools/',
+          images: [
+            {
+              url: 'https://webb-assets.s3.amazonaws.com/og.png',
+              width: 2400,
+              height: 1800,
+              alt: 'Og Image Alt',
+            },
+            {
+              url: 'https://webb-assets.s3.amazonaws.com/og-optimize.png',
+              width: 1200,
+              height: 630,
+              alt: 'Optimized Og Image Alt',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@webbprotocol',
+          site: '@webbprotocol',
+          cardType: 'summary_large_image',
+        }}
+      />
+
       <Head>
         <title>Welcome to tangle-website!</title>
       </Head>
@@ -17,6 +54,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
 
       <main className="app">
         <Component {...pageProps} />
+
+        <script
+          defer
+          data-domain="tangle.webb.tools"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </main>
 
       <WebsiteFooter type="tangle" />
--- apps/tangle-website/src/pages/index.tsx
+++ apps/tangle-website/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo';
 import {
   CommunitySection,
   HeroSection,
@@ -9,25 +10,41 @@ import {
   UseCasesSection,
 } from '../components';
 
+const description =
+  'Webb builds infrastructure for connecting zero-knowledge applications empowering developers to unlock user privacy in the Web3 ecosystem.';
+
 export function Index() {
   return (
-    <div className="bg-body bg-top block max-w-[1440px] mx-auto">
-      <HeroSection />
+    <>
+      <NextSeo
+        description={description}
+        canonical="https://tangle.webb.tools/"
+        openGraph={{
+          title:
+            'The next-generation TSS based blockchain powering cross-chain zero-knowledge messaging and applications',
+          description,
+          url: 'https://tangle.webb.tools/',
+        }}
+      />
 
-      <IntroducingSection />
+      <div className="bg-body bg-top block max-w-[1440px] mx-auto">
+        <HeroSection />
 
-      <FeaturesSection />
+        <IntroducingSection />
 
-      <SupportedBySection />
+        <FeaturesSection />
 
-      <GovernanceSystemSection />
+        <SupportedBySection />
 
-      <ParticipationMechanicsSection />
+        <GovernanceSystemSection />
 
-      <UseCasesSection />
+        <ParticipationMechanicsSection />
 
-      <CommunitySection />
-    </div>
+        <UseCasesSection />
+
+        <CommunitySection />
+      </div>
+    </>
   );
 }
 
