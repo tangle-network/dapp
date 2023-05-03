9a61551aeffb519578fae64550faa66742d703a4
Pavan Soratur <pavanvsoratur@gmail.com>
2023-05-02 22:49:51 -0700

    chore: removes notion constants from webb-cms lib

--- apps/webbsite/src/libs/webb-cms/constants.ts
+++ apps/webbsite/src/libs/webb-cms/constants.ts
@@ -1,24 +1,3 @@
-export const NOTION_API_KEY = process.env.NOTION_API_KEY;
-// Get these values by following the instructions here
-// https://github.com/ggiande/react-notion-x/tree/ggiande/doc-private-page-auth#private-pages
-export const NOTION_TOKEN_V2 = process.env.NOTION_TOKEN_V2;
-
-export const NOTION_ACTIVE_USER = process.env.NOTION_ACTIVE_USER;
-
-export const PRIVACY_POLICY_PAGE_ID = process.env.PRIVACY_POLICY_PAGE_ID;
-
-export const TERMS_AND_CONDITIONS_PAGE_ID =
-  process.env.TERMS_AND_CONDITIONS_PAGE_ID;
-
-export const NOTION_BLOG_INTEGRATION_TOKEN =
-  process.env.NOTION_BLOG_INTEGRATION_TOKEN;
-
-export const NOTION_BLOG_INTEGRATION_POSTS_DATABASE_ID =
-  process.env.NOTION_BLOG_INTEGRATION_POSTS_DATABASE_ID;
-
-export const NOTION_BLOG_INTEGRATION_VIDEOS_DATABASE_ID =
-  process.env.NOTION_BLOG_INTEGRATION_VIDEOS_DATABASE_ID;
-
 export const WEBB_CMS_URL = 'https://webb-cms.webb.tools/api';
 
 export const WEBB_CMS_TOKEN = process.env.WEBB_CMS_TOKEN;
