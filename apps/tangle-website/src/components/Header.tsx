a7d75eac1ba43fd4f7a807de8b70e90891e27fbc
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-05-09 12:30:27 +0700

    chore: fix header and hero padding

--- apps/tangle-website/src/components/Header.tsx
+++ apps/tangle-website/src/components/Header.tsx
@@ -5,11 +5,13 @@ import { Navbar } from '.';
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-mono-0 min-h-[72px] webb-shadow-sm">
-      <div className="mx-auto max-w-[1440px] px-[20px] md:px-[24px] lg:px-[156.5px] py-4 flex items-center justify-between">
-        <Link href="/">
-          <TangleLogo hideNameOnMobile />
-        </Link>
-        <Navbar />
+      <div className="max-w-[1440px] mx-auto px-[20px] lg:px-0 py-4">
+        <div className="lg:px-[11.25%] flex items-center justify-between">
+          <Link href="/">
+            <TangleLogo hideNameOnMobile />
+          </Link>
+          <Navbar />
+        </div>
       </div>
     </header>
   );
