524c1484a3179cf5ecca0fc5b7f3605deef63af0
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-05-09 16:47:26 +0700

    feat: add header banner

--- apps/tangle-website/src/components/Header.tsx
+++ apps/tangle-website/src/components/Header.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import { TangleLogo } from '@webb-tools/webb-ui-components';
-import { Navbar } from '.';
+import { TangleLogo, Typography } from '@webb-tools/webb-ui-components';
+import { Close } from '@webb-tools/icons';
+import { Navbar, InternalOrExternalLink } from '.';
 
 export const Header = () => {
+  const [showBanner, setShowBanner] = useState(true);
+
   return (
-    <header className="sticky top-0 z-50 bg-mono-0 min-h-[72px] webb-shadow-sm">
-      <div className="max-w-[1440px] mx-auto px-[20px] lg:px-0 py-4">
+    <header className="sticky flex flex-col top-0 z-50 bg-mono-0 min-h-[72px] webb-shadow-sm">
+
+      {/* Banner */}
+      {showBanner && (
+        <div className="bg-[#624FBE] p-2 lg:order-2 relative">
+          <Typography
+            variant="mkt-utility"
+            className="block text-center max-w-[80%] mx-auto !text-mono-0 !font-bold uppercase leading-[24px]"
+          >
+            TANGLE NETWORK CROWDLOAN IS HAPPENING SOON ✨ |{' '}
+            <InternalOrExternalLink url="#" className="inline-block">
+              <Typography
+                variant="body1"
+                className="!text-mono-0 capitalize underline"
+              >
+                Join Waitlist!
+              </Typography>
+            </InternalOrExternalLink>
+          </Typography>
+          <Close
+            onClick={() => {
+              setShowBanner(false);
+            }}
+            className="fill-mono-0 absolute top-1.5 right-2 md:right-5 w-6 h-6"
+          />
+        </div>
+      )}
+
+      {/* Nav */}
+      <div className="w-full max-w-[1440px] mx-auto px-[20px] lg:px-0 py-4 lg:order-1">
         <div className="lg:px-[11.25%] flex items-center justify-between">
           <Link href="/">
             <TangleLogo hideNameOnMobile />
