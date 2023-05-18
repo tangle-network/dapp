97bbabcf5b00ba61948c03b6a91f0f36aee32477
Pavan Soratur <pavanvsoratur@gmail.com>
2023-05-18 05:29:10 -0700

    chore: updates InActionSection of webbsite

--- apps/webbsite/src/pages/index.tsx
+++ apps/webbsite/src/pages/index.tsx
@@ -43,7 +43,7 @@ export function Index() {
 
       <ApplicationsAndInfrastructureSection />
 
-      {/* <InActionSection /> */}
+      <InActionSection />
 
       {/* <ResearchAndDevelopmentSection /> */}
     </>
