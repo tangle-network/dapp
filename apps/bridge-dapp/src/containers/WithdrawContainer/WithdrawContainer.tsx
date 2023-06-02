260e6d362c411ec5d75de577895b61c58d531bbe
AtelyPham <tinpham1412000@gmail.com>
2023-06-02 08:46:21 +0700

    fix: ci failing

--- apps/bridge-dapp/src/containers/WithdrawContainer/WithdrawContainer.tsx
+++ apps/bridge-dapp/src/containers/WithdrawContainer/WithdrawContainer.tsx
@@ -105,17 +105,6 @@ export const WithdrawContainer = forwardRef<
     [activeApi, currentTypedChainId]
   );
 
-  const useRelayersArgs = useMemo(
-    () => ({
-      typedChainId: currentTypedChainId ?? undefined,
-      target:
-        activeApi?.state.activeBridge && currentTypedChainId
-          ? activeApi.state.activeBridge.targets[currentTypedChainId]
-          : undefined,
-    }),
-    [activeApi, currentTypedChainId]
-  );
-
   // Given the user inputs above, fetch relayers state
   const {
     relayersState: { activeRelayer, relayers },
