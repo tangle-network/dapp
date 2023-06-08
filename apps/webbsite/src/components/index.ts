2d5e8906e116770ec4ed827c0b8c3a7572d5e425
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-06-08 16:12:07 +0700

    feat: add How Shielded Pool Works section + refactor Swiper component

--- apps/webbsite/src/components/index.ts
+++ apps/webbsite/src/components/index.ts
@@ -1,10 +1,11 @@
 // General components
 export * from './Header';
-export * from './PrivacyScaleIllustration';
-export * from './PrivacyScaleSwiper';
 
 // Section components
 export * from './sections';
 
 // Brandkit components
 export * from './brandkit';
+
+// Swiper components
+export * from './swipers';
--- apps/webbsite/src/components/shielded-pool/HowShieldedPoolWorksSection.tsx
+++ apps/webbsite/src/components/shielded-pool/HowShieldedPoolWorksSection.tsx
@@ -1,19 +1,23 @@
 import cx from 'classnames';
 import { Typography } from '@webb-tools/webb-ui-components';
 
+import { ShieldedPoolSwiper } from '../../components';
+
 export const HowShieldedPoolWorksSection = () => {
   return (
-    <section className="dark bg-mono-200 py-[64px] md:py-[156px] px-4 lg:px-0">
-      <div className="max-w-[900px] mx-auto">
+    <section className="dark bg-mono-200 py-[64px] md:py-[156px]">
+      <div className="max-w-[1000px] mx-auto">
         <Typography
           variant="mkt-h3"
           className={cx(
-            'text-center !text-mono-0 font-black mb-6',
+            'text-center !text-mono-0 font-black mb-9',
             '!text-[36px] !leading-[48px] md:!text-[48px] md:!leading-[60px]'
           )}
         >
           How Shielded Pool Works
         </Typography>
+
+        <ShieldedPoolSwiper />
       </div>
     </section>
   );
