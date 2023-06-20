640a35da0dc96108a778f3fcce4ab8b5d0e748d8
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-06-19 20:14:00 -0500

    Update FAQSection.tsx
    Copy for FAQ section. We will need to update these later.

--- apps/tangle-website/src/components/leaderboard/FAQSection.tsx
+++ apps/tangle-website/src/components/leaderboard/FAQSection.tsx
@@ -4,24 +4,27 @@ import { FAQAccordion } from '..';
 
 const faqItems = [
   {
-    question: 'What is the Tangle Incentivized Testnet?',
+    question: 'What is the Tangle Testnet Incentivization Campaign??',
     answer:
-      'Kasd eos sanctus sea sed voluptua dolores est accusam sed, et lorem labore est stet, rebum sed dolor rebum erat sed. At labore sea ipsum diam. Sanctus amet takimata elitr.',
+      'The introduction of rewards within the testnet creates a dynamic ecosystem that fosters collaboration among developers, encourages innovation, and incentivizes active engagement. By offering recognition and benefits, the testnet initiative aims to motivate participants to contribute their expertise, explore the network's capabilities, and help identify and address potential issues. The rewards system also paves the way for the future of the Tangle Network by encouraging ongoing participation and attracting a diverse range of users.',
   },
   {
     question: 'How long will the Tangle incentivized testnet run for?',
     answer:
-      'Kasd eos sanctus sea sed voluptua dolores est accusam sed, et lorem labore est stet, rebum sed dolor rebum erat sed. At labore sea ipsum diam. Sanctus amet takimata elitr.',
+      'The purpose of the testnet is to ensure the security and privacy, in addition to the overall functionality, of the network. Accordingly, we estimate that 1.5-2 months will be sufficient, but are still planning the exact duration.',
   },
   {
     question: 'What activities are rewarded with points?',
-    answer:
-      'Kasd eos sanctus sea sed voluptua dolores est accusam sed, et lorem labore est stet, rebum sed dolor rebum erat sed. At labore sea ipsum diam. Sanctus amet takimata elitr.',
+    answer: (
+      <a href="https://docs.webb.tools/docs/tangle-network/overview/" target="_blank" rel="noopener noreferrer">
+        See our documentation for the full list and instructions for each.
+      </a>
+    ),
   },
   {
     question: 'What rewards can I expect by participating?',
     answer:
-      'Kasd eos sanctus sea sed voluptua dolores est accusam sed, et lorem labore est stet, rebum sed dolor rebum erat sed. At labore sea ipsum diam. Sanctus amet takimata elitr.',
+      'We're focusing on concierge access to our team for developers, publicity for succesful validators and more. Details to come.',
   },
 ];
 
