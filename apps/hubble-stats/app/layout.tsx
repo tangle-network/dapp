6fa671f27bfd442736ce3e564c83726e07fec60d
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-06-22 21:09:59 +0700

    chore: clean up + add tailwindcss

--- apps/hubble-stats/app/layout.tsx
+++ apps/hubble-stats/app/layout.tsx
@@ -1,22 +1,36 @@
-import Head from 'next/head';
-import './global.css';
+'use client';
 
-export const metadata = {
-  title: 'Hubble Stats',
-  description: 'Hubble Stats',
-};
+import { useEffect } from 'react';
+import Head from 'next/head';
+import {
+  WebbUIProvider,
+  useDarkMode,
+  Footer,
+} from '@webb-tools/webb-ui-components';
+import '@webb-tools/webb-ui-components/tailwind.css';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [, setIsDarkMode] = useDarkMode();
+
+  useEffect(() => {
+    setIsDarkMode('light');
+  }, [setIsDarkMode]);
+
   return (
     <html lang="en">
-      <Head>
-        <title>Welcome to Hubble Stats!</title>
-      </Head>
-      <body>{children}</body>
+      <WebbUIProvider defaultThemeMode="light">
+        <Head>
+          <title>Welcome to Hubble Stats!</title>
+        </Head>
+        <body>
+          {children}
+          <Footer isMinimal />
+        </body>
+      </WebbUIProvider>
     </html>
   );
 }
--- apps/hubble-stats/app/page.tsx
+++ apps/hubble-stats/app/page.tsx
@@ -1,14 +1,3 @@
-import styles from './page.module.css';
-
 export default async function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>Hubble Stats</h1>
-    </div>
-  );
+  return <div></div>;
 }
