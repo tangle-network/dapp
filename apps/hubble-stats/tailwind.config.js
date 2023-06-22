b2a90eff51ee3cd2e8c9d919d0d94ecfadaf9d6a
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2023-06-23 01:06:06 +0700

    Initialize hubble-stats (#1366)

---
+++ apps/hubble-stats/tailwind.config.js
@@ -0,0 +1,25 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+const preset = require('@webb-tools/tailwind-preset');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [preset],
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    join(
+      __dirname,
+      '../../libs/webb-ui-components',
+      'src/{pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
