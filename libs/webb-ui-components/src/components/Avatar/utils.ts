888aa5d55c8b4a18ef732294f64af505a18166f8
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-06-29 10:02:17 +0700

    chore: refactor Avatar component code

--- libs/webb-ui-components/src/components/Avatar/utils.ts
+++ libs/webb-ui-components/src/components/Avatar/utils.ts
@@ -20,3 +20,32 @@ export function getAvatarSizeInPx(size: AvatarProps['size'] = 'md') {
     }
   }
 }
+
+export const getAvatarClassNames = (darkMode: boolean | undefined) => {
+  const borderColor =
+    typeof darkMode === 'boolean'
+      ? darkMode
+        ? ('border-mono-180' as const)
+        : ('border-mono-0' as const)
+      : ('border-mono-0 dark:border-mono-180' as const);
+
+  const bg =
+    typeof darkMode === 'boolean'
+      ? darkMode
+        ? ('bg-mono-180' as const)
+        : ('bg-mono-0' as const)
+      : ('bg-mono-0 dark:bg-mono-180' as const);
+
+  const text =
+    typeof darkMode === 'boolean'
+      ? darkMode
+        ? ('text-mono-0' as const)
+        : ('text-mono-180' as const)
+      : ('text-mono-180 dark:text-mono-0' as const);
+
+  return {
+    borderColor,
+    bg,
+    text,
+  };
+};
