e433afd46139ce4ad68e22761f06e9cbeb6271e7
AtelyPham <tinpham1412000@gmail.com>
2023-07-19 00:07:58 +0700

    chore: remove react-environment lib

--- libs/react-environment/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './RouterProvider';
-export * from './store';
-export * from './arkworks-proving-manager.worker';
-export * from './circom-proving-manager.worker';
-export * from './SettingProvider';
