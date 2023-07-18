85910ace811b63089f70176bd1062091ea88e3ea
AtelyPham <tinpham1412000@gmail.com>
2023-07-19 00:35:23 +0700

    chore: remove setting hook

--- libs/react-hooks/src/useSetting.ts
+++
@@ -1,9 +0,0 @@
-import {
-  SettingContext,
-  SettingDate,
-} from '@webb-tools/api-provider-environment';
-import { useContext } from 'react';
-
-export const useSetting = (): SettingDate => {
-  return useContext(SettingContext);
-};
