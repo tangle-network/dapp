6fb3a74442a151616ca529f2aaa64a36ef7bdbfc
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-07-20 20:47:14 +0700

    chore: format code

--- apps/hubble-stats/app/pool/[slug]/page.tsx
+++ apps/hubble-stats/app/pool/[slug]/page.tsx
@@ -10,14 +10,13 @@ export default function Pool({ params }: { params: { slug: string } }) {
 
   return (
     <div className="py-4 space-y-8">
-
       <div className="flex gap-4">
         <div className="w-[400px]">
           <PoolOverviewContainer />
         </div>
         <div className="flex-grow"></div>
       </div>
-      
+
       <NetworkTableContainer />
 
       <PoolTransactionsTableContainer />
