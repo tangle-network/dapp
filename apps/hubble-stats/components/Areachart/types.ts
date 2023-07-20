e80b3c2b1e1d26b35d1facb2c45981b35bcccd8d
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2023-07-20 13:37:36 +0700

    chore: delete Areachart directory

--- apps/hubble-stats/components/Areachart/types.ts
+++
@@ -1,8 +0,0 @@
-export type AreaChartProps = {
-  data: any;
-  setValue: (value: number | null) => void;
-  setDate: (date: Date | null) => void;
-  isDarkMode: boolean;
-  width?: number | string;
-  height?: number | string;
-};
