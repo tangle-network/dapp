dda1d9df12a97d6fef0dca1ed85ccddb4fdf9136
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-07-27 13:19:10 +0700

    feat: add forwardRef

--- libs/webb-ui-components/src/components/SideBar/SideBar.tsx
+++ libs/webb-ui-components/src/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import cx from 'classnames';
 import useLocalStorageState from 'use-local-storage-state';
 import { twMerge } from 'tailwind-merge';
@@ -28,135 +28,132 @@ import { SidebarProps } from './types';
  *  />
  * ```
  */
-export const SideBar: React.FC<SidebarProps> = ({
-  Logo,
-  ClosedLogo,
-  logoLink,
-  items,
-  footer,
-  className,
-}) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useLocalStorageState(
-    'isSidebarOpen',
-    {
-      defaultValue: true,
-    }
-  );
+export const SideBar = forwardRef<HTMLDivElement, SidebarProps>(
+  ({ Logo, ClosedLogo, logoLink, items, footer, className, ...props }, ref) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useLocalStorageState(
+      'isSidebarOpen',
+      {
+        defaultValue: true,
+      }
+    );
 
-  const [activeItem, setActiveItem] = useState<number | null>(0);
-  const [isHovering, setIsHovering] = useState(false);
+    const [activeItem, setActiveItem] = useState<number | null>(0);
+    const [isHovering, setIsHovering] = useState(false);
 
-  return (
-    <div
-      className={cx('flex gap-2 top-0 left-0 z-50 relative', className)}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
-    >
+    return (
       <div
-        className={twMerge(
-          'h-screen flex flex-col justify-between py-12 px-4 bg-mono-0 dark:bg-mono-160 transition-all duration-200 ease-in-out',
-          isSidebarOpen ? 'w-72' : 'w-16'
-        )}
+        className={cx('flex gap-2 top-0 left-0 z-50 relative', className)}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        {...props}
+        ref={ref}
       >
-        <div>
-          <div className={isSidebarOpen ? 'px-2' : ''}>
-            <a
-              href={logoLink ? logoLink : '/'}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {!isSidebarOpen && ClosedLogo ? (
-                <ClosedLogo />
-              ) : (
-                <Logo size="md" />
-              )}
-            </a>
-          </div>
-
-          <div className="flex flex-col gap-4 mt-11">
-            {items.map((itemProps, index) => (
-              <SideBarItem
-                key={index}
-                {...itemProps}
-                isSidebarOpen={isSidebarOpen}
-                isActive={activeItem === index}
-                setIsActive={() => setActiveItem(index)}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div
-          className="flex-grow"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        />
-
         <div
           className={twMerge(
-            'flex items-center justify-between',
-            isSidebarOpen ? 'p-2' : 'pl-1'
+            'h-screen flex flex-col justify-between py-12 px-4 bg-mono-0 dark:bg-mono-160 transition-all duration-200 ease-in-out',
+            isSidebarOpen ? 'w-72' : 'w-16'
           )}
         >
-          <div className="flex items-center justify-between group">
-            <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-              <footer.Icon
-                width={24}
-                height={24}
-                className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
-              />
-            </Link>
+          <div>
+            <div className={isSidebarOpen ? 'px-2' : ''}>
+              <a
+                href={logoLink ? logoLink : '/'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {!isSidebarOpen && ClosedLogo ? (
+                  <ClosedLogo />
+                ) : (
+                  <Logo size="md" />
+                )}
+              </a>
+            </div>
 
-            {isSidebarOpen && (
-              <>
-                <div className={isSidebarOpen ? 'pl-4' : ''}>
-                  <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-                    <Typography
-                      variant="body1"
-                      className="cursor-pointer text-mono-100 dark:text-mono-60 group-hover:text-mono-200 dark:group-hover:text-mono-0"
-                    >
-                      {footer.name}
-                    </Typography>
-                  </Link>
-                </div>
+            <div className="flex flex-col gap-4 mt-11">
+              {items.map((itemProps, index) => (
+                <SideBarItem
+                  key={index}
+                  {...itemProps}
+                  isSidebarOpen={isSidebarOpen}
+                  isActive={activeItem === index}
+                  setIsActive={() => setActiveItem(index)}
+                />
+              ))}
+            </div>
+          </div>
 
-                {!footer.isInternal ? (
-                  <div className={isSidebarOpen ? 'pl-[26px]' : ''}>
+          <div
+            className="flex-grow"
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          />
+
+          <div
+            className={twMerge(
+              'flex items-center justify-between',
+              isSidebarOpen ? 'p-2' : 'pl-1'
+            )}
+          >
+            <div className="flex items-center justify-between group">
+              <Link href={footer.href} aTagProps={{ target: '_blank' }}>
+                <footer.Icon
+                  width={24}
+                  height={24}
+                  className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
+                />
+              </Link>
+
+              {isSidebarOpen && (
+                <>
+                  <div className={isSidebarOpen ? 'pl-4' : ''}>
                     <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-                      <ExternalLinkLine
-                        className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
-                        width={24}
-                        height={24}
-                      />
+                      <Typography
+                        variant="body1"
+                        className="cursor-pointer text-mono-100 dark:text-mono-60 group-hover:text-mono-200 dark:group-hover:text-mono-0"
+                      >
+                        {footer.name}
+                      </Typography>
                     </Link>
                   </div>
-                ) : (
-                  ''
-                )}
-              </>
-            )}
+
+                  {!footer.isInternal ? (
+                    <div className={isSidebarOpen ? 'pl-[26px]' : ''}>
+                      <Link href={footer.href} aTagProps={{ target: '_blank' }}>
+                        <ExternalLinkLine
+                          className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
+                          width={24}
+                          height={24}
+                        />
+                      </Link>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </>
+              )}
+            </div>
+
+            {isSidebarOpen && <ThemeToggle />}
           </div>
-
-          {isSidebarOpen && <ThemeToggle />}
         </div>
-      </div>
 
-      {isHovering && (
-        <div
-          className="absolute top-0 right-0 px-3 pt-12"
-          style={{ transform: 'translateX(100%)' }}
-        >
+        {isHovering && (
           <div
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-            className="p-1 rounded-full shadow-lg cursor-pointer bg-mono-0 dark:bg-mono-180"
+            className="absolute top-0 right-0 px-3 pt-12"
+            style={{ transform: 'translateX(100%)' }}
           >
-            {isSidebarOpen ? (
-              <ChevronLeft size="md" />
-            ) : (
-              <ChevronRight size="md" />
-            )}
+            <div
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className="p-1 rounded-full shadow-lg cursor-pointer bg-mono-0 dark:bg-mono-180"
+            >
+              {isSidebarOpen ? (
+                <ChevronLeft size="md" />
+              ) : (
+                <ChevronRight size="md" />
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-  );
-};
+        )}
+      </div>
+    );
+  }
+);
--- libs/webb-ui-components/src/components/SideBar/SideBarMenu.tsx
+++ libs/webb-ui-components/src/components/SideBar/SideBarMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { useState, forwardRef } from 'react';
 import cx from 'classnames';
 import * as Dialog from '@radix-ui/react-dialog';
 import { HamburgerMenu, ExternalLinkLine } from '@webb-tools/icons';
@@ -9,95 +9,94 @@ import { ThemeToggle } from '../ThemeToggle';
 import { Link } from '../Link';
 import { SidebarProps } from './types';
 
-export const SideBarMenu: FC<SidebarProps> = ({
-  Logo,
-  logoLink,
-  items,
-  footer,
-  className,
-}) => {
-  const [activeItem, setActiveItem] = useState<number | null>(0);
+export const SideBarMenu = forwardRef<HTMLDivElement, SidebarProps>(
+  ({ Logo, logoLink, items, footer, className, ...props }, ref) => {
+    const [activeItem, setActiveItem] = useState<number | null>(0);
 
-  return (
-    <div className={cx('flex items-center', className)}>
-      <Dialog.Root>
-        <Dialog.Trigger>
-          <HamburgerMenu size="lg" className="fill-mono-200 dark:fill-mono-0" />
-        </Dialog.Trigger>
+    return (
+      <div className={cx('flex items-center', className)} {...props} ref={ref}>
+        <Dialog.Root>
+          <Dialog.Trigger>
+            <HamburgerMenu
+              size="lg"
+              className="fill-mono-200 dark:fill-mono-0"
+            />
+          </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 bg-[rgba(0,0,0,0.1)] animate-[showDialogOverlay_150ms]" />
-          <Dialog.Content
-            className={cx(
-              'w-[280px] h-full overflow-auto py-6 px-4',
-              '!bg-mono-0 dark:!bg-mono-160 fixed left-0',
-              'animate-[sideBarSlideLeftToRight_400ms]',
-              'flex flex-col justify-between'
-            )}
-          >
-            <div>
-              <a
-                href={logoLink ? logoLink : '/'}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Logo size="md" />
-              </a>
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-[rgba(0,0,0,0.1)] animate-[showDialogOverlay_150ms]" />
+            <Dialog.Content
+              className={cx(
+                'w-[280px] h-full overflow-auto py-6 px-4',
+                '!bg-mono-0 dark:!bg-mono-160 fixed left-0',
+                'animate-[sideBarSlideLeftToRight_400ms]',
+                'flex flex-col justify-between'
+              )}
+            >
+              <div>
+                <a
+                  href={logoLink ? logoLink : '/'}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Logo size="md" />
+                </a>
 
-              <div className="mt-11 flex flex-col gap-4">
-                {items.map((itemProps, index) => (
-                  <SideBarItem
-                    key={index}
-                    {...itemProps}
-                    isSidebarOpen={true}
-                    isActive={activeItem === index}
-                    setIsActive={() => setActiveItem(index)}
-                  />
-                ))}
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between gap-2 p-2">
-              <div className="group flex items-center justify-between">
-                <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-                  <footer.Icon
-                    width={24}
-                    height={24}
-                    className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
-                  />
-                </Link>
-
-                <div className="pl-2">
-                  <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-                    <Typography
-                      variant="body1"
-                      className="cursor-pointer text-mono-100 dark:text-mono-60 group-hover:text-mono-200 dark:group-hover:text-mono-0"
-                    >
-                      {footer.name}
-                    </Typography>
-                  </Link>
+                <div className="mt-11 flex flex-col gap-4">
+                  {items.map((itemProps, index) => (
+                    <SideBarItem
+                      key={index}
+                      {...itemProps}
+                      isSidebarOpen={true}
+                      isActive={activeItem === index}
+                      setIsActive={() => setActiveItem(index)}
+                    />
+                  ))}
                 </div>
+              </div>
 
-                {!footer.isInternal ? (
-                  <div className="pl-[26px]">
+              <div className="flex items-center justify-between gap-2 p-2">
+                <div className="group flex items-center justify-between">
+                  <Link href={footer.href} aTagProps={{ target: '_blank' }}>
+                    <footer.Icon
+                      width={24}
+                      height={24}
+                      className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
+                    />
+                  </Link>
+
+                  <div className="pl-2">
                     <Link href={footer.href} aTagProps={{ target: '_blank' }}>
-                      <ExternalLinkLine
-                        className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
-                        width={24}
-                        height={24}
-                      />
+                      <Typography
+                        variant="body1"
+                        className="cursor-pointer text-mono-100 dark:text-mono-60 group-hover:text-mono-200 dark:group-hover:text-mono-0"
+                      >
+                        {footer.name}
+                      </Typography>
                     </Link>
                   </div>
-                ) : (
-                  ''
-                )}
-              </div>
 
-              <ThemeToggle />
-            </div>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    </div>
-  );
-};
+                  {!footer.isInternal ? (
+                    <div className="pl-[26px]">
+                      <Link href={footer.href} aTagProps={{ target: '_blank' }}>
+                        <ExternalLinkLine
+                          className="cursor-pointer !fill-mono-100 dark:!fill-mono-60 group-hover:!fill-mono-200 dark:group-hover:!fill-mono-0"
+                          width={24}
+                          height={24}
+                        />
+                      </Link>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                </div>
+
+                <ThemeToggle />
+              </div>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </div>
+    );
+  }
+);
