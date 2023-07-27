81b3f999514133e30537cfa3bde16dd9a880ef42
AtelyPham <tinpham1412000@gmail.com>
2023-07-28 01:02:09 +0700

    fix: move StatusIndicator to svg to fix the width issue

--- libs/webb-ui-components/src/components/StatusIndicator/StatusIndicator.tsx
+++ libs/webb-ui-components/src/components/StatusIndicator/StatusIndicator.tsx
@@ -1,35 +1,64 @@
 import cx from 'classnames';
 import { forwardRef } from 'react';
 import { StatusIndicatorProps, StatusVariant } from './types';
-import { twMerge } from 'tailwind-merge';
-
-const base = cx('border-opacity-30 w-[6px] h-[6px] rounded-full border-[3px]');
 
 const classes: {
-  [key in StatusVariant]: string;
+  [key in StatusVariant]: {
+    indicator: string;
+    stroke: string;
+  };
 } = {
-  success: cx(
-    'bg-green-70 dark:bg-green-50',
-    'border-[#288E32] dark:border-[#4CB457]'
-  ),
-  warning: cx(
-    'bg-yellow-70 dark:bg-yellow-50',
-    'border-[#EAB612] dark:border-[#F8D567]'
-  ),
-  error: cx(
-    'bg-red-70 dark:bg-red-50',
-    'border-[#EF570D] dark:border-[#FF874D]'
-  ),
-  info: cx(
-    'bg-blue-70 dark:bg-blue-50',
-    'border-[#23579D] dark:border-[#23579D]'
-  ),
+  success: {
+    indicator: cx('fill-green-70 dark:fill-green-50'),
+    stroke: cx('stroke-[#288E32] dark:stroke-[#4CB457]'),
+  },
+  warning: {
+    indicator: cx('fill-yellow-70 darkcx(:fill-yellow-50'),
+    stroke: cx('stroke-[#EAB612] dark:stroke-[#F8D567]'),
+  },
+  error: {
+    indicator: cx('fill-red-70 darkcx(:fill-red-50'),
+    stroke: cx('stroke-[#EF570D] dark:stroke-[#FF874D]'),
+  },
+  info: {
+    indicator: cx('fill-blue-70 darkcx(:fill-blue-50'),
+    stroke: cx('stroke-[#23579D] dark:stroke-[#23579D]'),
+  },
 };
 
-const StatusIndicator = forwardRef<HTMLSpanElement, StatusIndicatorProps>(
-  ({ className, variant = 'info', ...props }, ref) => {
+const StatusIndicator = forwardRef<SVGSVGElement, StatusIndicatorProps>(
+  ({ variant = 'info', size = 12, ...props }, ref) => {
+    // We use haft size to make sure the component is corectly centered
+    const haftSize = size / 2;
+
     return (
-      <span ref={ref} className={twMerge(base, classes[variant], className)} />
+      <svg
+        width={haftSize * 2}
+        height={haftSize * 2}
+        viewBox={`0 0 ${haftSize * 2} ${haftSize * 2}`}
+        fill="none"
+        ref={ref}
+        {...props}
+      >
+        <rect
+          x={haftSize / 2}
+          y={haftSize / 2}
+          width={haftSize}
+          height={haftSize}
+          rx={haftSize / 2}
+          className={classes[variant].indicator}
+        />
+        <rect
+          x={haftSize / 4}
+          y={haftSize / 4}
+          width={haftSize * 1.5}
+          height={haftSize * 1.5}
+          rx={haftSize * 0.75}
+          className={classes[variant].stroke}
+          strokeOpacity="0.3"
+          strokeWidth={haftSize / 2}
+        />
+      </svg>
     );
   }
 );
--- libs/webb-ui-components/src/components/StatusIndicator/types.ts
+++ libs/webb-ui-components/src/components/StatusIndicator/types.ts
@@ -2,10 +2,16 @@ import { PropsOf } from '../../types';
 
 export type StatusVariant = 'success' | 'warning' | 'error' | 'info';
 
-export interface StatusIndicatorProps extends PropsOf<'span'> {
+export interface StatusIndicatorProps extends PropsOf<'svg'> {
   /**
    * The color variant of the status indicator.
    * @default 'info'
    */
   variant?: StatusVariant;
+
+  /**
+   * The size of the status indicator.
+   * @default 12
+   */
+  size?: number;
 }
