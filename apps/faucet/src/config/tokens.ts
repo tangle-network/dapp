d309f30461b7cae3784b3f4305585df0599ac812
Shady Khalifa <dev+github@shadykhalifa.me>
2023-07-31 15:52:13 +0300

    Remove old webbETH token and add tangle evm

--- apps/faucet/src/config/tokens.ts
+++ apps/faucet/src/config/tokens.ts
@@ -6,15 +6,15 @@ type TokenConfigType = Record<string, string>;
 // TODO: We should fetch the token info (e.g. symbol, decimals, name, ...) from the chain
 const tokens: Record<string, TokenConfigType> = {
   [PresetTypedChainId.AthenaOrbit]: {
-    webbWETH: '0x915EbFAd9c4Af2F420782a5F03BD544f9a1FF1d1',
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
   [PresetTypedChainId.HermesOrbit]: {
-    webbWETH: '0x915EbFAd9c4Af2F420782a5F03BD544f9a1FF1d1',
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
   [PresetTypedChainId.DemeterOrbit]: {
-    webbWETH: '0x915EbFAd9c4Af2F420782a5F03BD544f9a1FF1d1',
+    webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
+  },
+  [PresetTypedChainId.TangleTestnet]: {
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
 };
