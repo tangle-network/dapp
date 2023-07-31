2434ae139a4b615ecb4194adb5c79a476f8cc264
AtelyPham <tinpham1412000@gmail.com>
2023-08-01 01:19:04 +0700

    feat: add native token for the faucet

--- apps/faucet/src/config/tokens.ts
+++ apps/faucet/src/config/tokens.ts
@@ -1,4 +1,5 @@
 import { PresetTypedChainId } from '@webb-tools/dapp-types/ChainId';
+import { zeroAddress } from 'viem';
 
 // Record of token symbol to token address
 type TokenConfigType = Record<string, string>;
@@ -6,15 +7,19 @@ type TokenConfigType = Record<string, string>;
 // TODO: We should fetch the token info (e.g. symbol, decimals, name, ...) from the chain
 const tokens: Record<string, TokenConfigType> = {
   [PresetTypedChainId.AthenaOrbit]: {
+    ETH: zeroAddress,
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
   [PresetTypedChainId.HermesOrbit]: {
+    ETH: zeroAddress,
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
   [PresetTypedChainId.DemeterOrbit]: {
+    ETH: zeroAddress,
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
   [PresetTypedChainId.TangleTestnet]: {
+    tTNT: zeroAddress,
     webbtTNT: '0x6aBC6e8E4E4a1fcB43341CeC49cBd5FA3b494520',
   },
 };
