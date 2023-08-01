72b1037bc69a765e5d35b6b94fefb21d741dba5a
AtelyPham <tinpham1412000@gmail.com>
2023-08-02 04:24:34 +0700

    feat: add AccordionButtonBase component

---
+++ libs/webb-ui-components/src/components/Accordion/AccordionButtonBase.tsx
@@ -0,0 +1,20 @@
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
+import { forwardRef } from 'react';
+
+import { AccordionButtonBaseProps } from './types';
+
+/**
+ * The wrapper around Radix Accordion Trigger, must use inside `<AccordionItem></AccordionItem>` tag
+ */
+const AccordionButtonBase = forwardRef<
+  HTMLButtonElement,
+  AccordionButtonBaseProps
+>((props, ref) => {
+  return (
+    <AccordionPrimitive.Header asChild>
+      <AccordionPrimitive.Trigger {...props} ref={ref} />
+    </AccordionPrimitive.Header>
+  );
+});
+
+export default AccordionButtonBase;
--- libs/webb-ui-components/src/components/Accordion/index.ts
+++ libs/webb-ui-components/src/components/Accordion/index.ts
@@ -1,4 +1,5 @@
 export * from './Accordion';
 export * from './AccordionButton';
+export { default as AccordionButtonBase } from './AccordionButtonBase';
 export * from './AccordionContent';
 export * from './AccordionItem';
