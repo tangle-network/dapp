b10bcff25199a5f92883350a0ab2ffefff538e53
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-08-10 09:56:45 +0700

    fix: fix null error when promise is rejected

--- apps/hubble-stats/data/getOverviewChipsData.ts
+++ apps/hubble-stats/data/getOverviewChipsData.ts
@@ -4,43 +4,53 @@ import vAnchorClient from '@webb-tools/vanchor-client';
 import { vAnchorAddresses, allSubgraphUrls } from '../constants';
 
 type OverviewChipsDataType = {
-  tvl: number;
-  volume: number;
+  tvl: number | undefined;
+  volume: number | undefined;
 };
 
 export default async function getOverviewChipsData(): Promise<OverviewChipsDataType> {
-  const tvlVAnchorsByChainsData =
-    await vAnchorClient.TotalValueLocked.GetVAnchorsTotalValueLockedByChains(
-      allSubgraphUrls,
-      vAnchorAddresses
-    );
+  let tvl, volume;
 
-  const depositVAnchorsByChainsData =
-    await vAnchorClient.Deposit.GetVAnchorsDepositByChains(
-      allSubgraphUrls,
-      vAnchorAddresses
-    );
+  try {
+    const tvlVAnchorsByChainsData =
+      await vAnchorClient.TotalValueLocked.GetVAnchorsTotalValueLockedByChains(
+        allSubgraphUrls,
+        vAnchorAddresses
+      );
 
-  const tvl = tvlVAnchorsByChainsData.reduce((tvlTotal, vAnchorsByChain) => {
-    const tvlVAnchorsByChain = vAnchorsByChain.reduce(
-      (tvlTotalByChain, vAnchor) =>
-        tvlTotalByChain + +formatEther(BigInt(vAnchor.totalValueLocked)),
-      0
-    );
-    return tvlTotal + tvlVAnchorsByChain;
-  }, 0);
-
-  const volume = depositVAnchorsByChainsData.reduce(
-    (depositTotal, vAnchorsByChain) => {
-      const depositVAnchorsByChain = vAnchorsByChain.reduce(
+    tvl = tvlVAnchorsByChainsData.reduce((tvlTotal, vAnchorsByChain) => {
+      const tvlVAnchorsByChain = vAnchorsByChain.reduce(
         (tvlTotalByChain, vAnchor) =>
-          tvlTotalByChain + +formatEther(BigInt(vAnchor.deposit)),
+          tvlTotalByChain + +formatEther(BigInt(vAnchor.totalValueLocked)),
         0
       );
-      return depositTotal + depositVAnchorsByChain;
-    },
-    0
-  );
+      return tvlTotal + tvlVAnchorsByChain;
+    }, 0);
+  } catch {
+    tvl = undefined;
+  }
+
+  try {
+    const depositVAnchorsByChainsData =
+      await vAnchorClient.Deposit.GetVAnchorsDepositByChains(
+        allSubgraphUrls,
+        vAnchorAddresses
+      );
+
+    volume = depositVAnchorsByChainsData.reduce(
+      (depositTotal, vAnchorsByChain) => {
+        const depositVAnchorsByChain = vAnchorsByChain.reduce(
+          (tvlTotalByChain, vAnchor) =>
+            tvlTotalByChain + +formatEther(BigInt(vAnchor.deposit)),
+          0
+        );
+        return depositTotal + depositVAnchorsByChain;
+      },
+      0
+    );
+  } catch {
+    volume = undefined;
+  }
 
   return {
     tvl,
