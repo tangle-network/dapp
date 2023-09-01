2d57041e9f6929d16b3b879d6ce0dcb976bbb481
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-09-01 17:55:02 +0700

    fix: lint and CodeQL issues

--- libs/webb-ui-components/src/utils/isScientificNotation.ts
+++ libs/webb-ui-components/src/utils/isScientificNotation.ts
@@ -1,5 +1,11 @@
 function isScientificNotation(value: number): boolean {
-  return /\d+\.?\d*e[+-]*\d+/i.test(value.toString());
+  const stringValue = value.toString();
+  if (stringValue.length > 1000) {
+    console.error('isScientificNotation: value is too long');
+    return false;
+  }
+  const scientificNotationRegex = /^[+-]?\d+(\.\d+)?e[+-]?\d+$/i;
+  return scientificNotationRegex.test(stringValue);
 }
 
 export default isScientificNotation;
