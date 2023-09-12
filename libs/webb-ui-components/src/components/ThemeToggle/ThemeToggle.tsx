10cc655294499c061ed17843cf6746621d9ac06d
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-09-13 01:34:11 +0700

    chore: fix theme switcher issue

--- libs/webb-ui-components/src/components/ThemeToggle/ThemeToggle.tsx
+++ libs/webb-ui-components/src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 import { MoonLine, SunLine } from '@webb-tools/icons';
 import { useDarkMode } from '../../hooks/useDarkMode';
+import cx from 'classnames';
 
 /**
  * ThemeToggle (Dark/Light) Component
@@ -14,9 +15,15 @@ export const ThemeToggle = () => {
 
   return (
     <div
-      className={`relative inline-block w-14 h-8 align-middle select-none transition duration-200 ease-in rounded-full ${
-        isDarkMode ? 'bg-mono-200' : 'bg-blue-10'
-      }`}
+      className={cx(
+        'relative inline-block w-14 h-8 align-middle',
+        'select-none transition duration-200 ease-in rounded-full',
+        '[&_*]:cursor-pointer',
+        {
+          'bg-mono-200': isDarkMode,
+          'bg-blue-10': !isDarkMode,
+        }
+      )}
       onClick={(eve) => {
         eve.preventDefault();
         toggleThemeMode();
@@ -26,7 +33,7 @@ export const ThemeToggle = () => {
         type="checkbox"
         name="toggle"
         id="toggle"
-        className="hidden toggle-checkbox"
+        className="hidden"
         checked={isDarkMode}
         onChange={(eve) => {
           eve.stopPropagation();
@@ -35,12 +42,15 @@ export const ThemeToggle = () => {
       />
       <label
         htmlFor="toggle"
-        className="block h-6 overflow-hidden rounded-full cursor-pointer toggle-label"
+        className="block h-6 overflow-hidden rounded-full"
       />
       <div
-        className={`toggle-icon absolute inset-y-0 left-0 flex items-center transition-transform duration-200 ease-in  ${
-          !isDarkMode ? 'translate-x-full' : ''
-        }`}
+        className={cx(
+          'absolute inset-y-0 left-0 flex items-center transition-transform duration-200 ease-in',
+          {
+            'translate-x-full': !isDarkMode,
+          }
+        )}
       >
         {isDarkMode ? (
           <SunLine className="h-7 w-7 ml-[3px] bg-blue-30 rounded-full p-0.5 !fill-mono-200" />
