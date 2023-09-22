787106736b0c2e757f28da510e932dea15d12484
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2023-09-22 21:04:57 +0700

    chore: fix pages not reload when users navigate back and forth (#1673)

--- apps/hubble-stats/components/Breadcrumbs/Breadcrumbs.tsx
+++ apps/hubble-stats/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import cx from 'classnames';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import {
   Breadcrumbs as BreadcrumbsCmp,
   BreadcrumbsItem,
@@ -14,6 +14,7 @@ import { VANCHORS_MAP } from '../../constants';
 import { BreadcrumbType } from './types';
 
 const Breadcrumbs: FC = () => {
+  const router = useRouter();
   const pathname = usePathname();
 
   const breadCrumbs = useMemo<BreadcrumbType[]>(() => {
@@ -48,10 +49,23 @@ const Breadcrumbs: FC = () => {
     ];
   }, [pathname]);
 
+  /**
+   * Pages not reload server data when navigating to a page when go back and forth in the browser
+   * Therefore, need to refresh the page when the user navigates to a page
+   * This keeps all client-side components still remain their states
+   */
+  useEffect(() => {
+    router.refresh();
+  }, [router, pathname]);
+
   return (
     <BreadcrumbsCmp>
       {breadCrumbs.map((breadcrumb, index) => (
-        <Link key={index} href={breadcrumb.href}>
+        /**
+         * Data on the client-side needs to be up-to-date when the user navigates to a page
+         * Therefore, do not need to prefetch routes in breadcrumb items
+         */
+        <Link key={index} href={breadcrumb.href} prefetch={false}>
           <BreadcrumbsItem
             icon={breadcrumb.icon}
             className={cx('whitespace-nowrap', breadcrumb.className)}
