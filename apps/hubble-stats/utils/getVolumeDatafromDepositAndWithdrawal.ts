8991b8fd4945d168675408bb72b935bd88c26898
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-10-01 10:32:50 +0700

    chore: add stories for skeleton loader

--- apps/hubble-stats/utils/getVolumeDatafromDepositAndWithdrawal.ts
+++
@@ -1,5 +0,0 @@
-type DepositOrWithdrawalDataType = { [epoch: string]: bigint };
-
-export type VolumeDataType = {
-  [epoch: string]: { deposit: number; withdrawal: number };
-};
---
+++ libs/webb-ui-components/src/stories/molecules/SkeletonLoader.stories.jsx
@@ -0,0 +1,26 @@
+import { SkeletonLoader } from '@webb-tools/webb-ui-components/components';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Design System/Molecules/SkeletonLoader',
+  component: SkeletonLoader,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <SkeletonLoader {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {};
+
+export const Large = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Large.args = {
+  size: 'lg',
+};
+
+export const ExtraLarge = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+ExtraLarge.args = {
+  size: 'xl',
+};
