a6e026b5db79866f6706e75dc6356015bd12786d
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-10-05 03:09:55 +0700

    chore: remove unnecessary async

--- apps/hubble-stats/containers/HeaderChipsContainer/HeaderChipsContainer.tsx
+++ apps/hubble-stats/containers/HeaderChipsContainer/HeaderChipsContainer.tsx
@@ -3,9 +3,9 @@ import { ArrowRightUp, DatabaseLine } from '@webb-tools/icons';
 import { HeaderChipItem } from '../../components';
 import { getHeaderChipsTvlData, getHeaderChipsDepositData } from '../../data';
 
-export default async function HeaderChipsContainer() {
+export default function HeaderChipsContainer() {
   return (
-    <div className="hidden md:flex items-center gap-2 lg:gap-4">
+    <div className="items-center hidden gap-2 md:flex lg:gap-4">
       <HeaderChipItem
         Icon={DatabaseLine}
         label="TVL"
