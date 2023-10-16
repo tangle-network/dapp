57d1c2bd10980b7955dfa59878c32329630e77f8
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-10-16 19:18:41 +0700

    feat: add relayer info to fee details on withdraw and transfer flows

--- apps/bridge-dapp/src/components/index.ts
+++ apps/bridge-dapp/src/components/index.ts
@@ -2,6 +2,7 @@ export * from './ChainListCardWrapper';
 export * from './EducationCard';
 export * from './Header';
 export * from './InteractiveFeedbackView';
+export { default as RelayerFeeDetails } from './RelayerFeeDetails';
 export { default as SlideAnimation } from './SlideAnimation';
 export { default as SubmittedTxModal } from './SubmittedTxModal';
 export { default as TxInfoItem } from './TxInfoItem';
