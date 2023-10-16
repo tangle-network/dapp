23359c2bcfcf4b1cae1684125150030777780d4f
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-10-17 00:35:06 +0700

    refactor: move execute transaction to separate hook

--- apps/bridge-dapp/src/containers/TransferConfirmContainer/TransferConfirmContainer.tsx
+++ apps/bridge-dapp/src/containers/TransferConfirmContainer/TransferConfirmContainer.tsx
@@ -1,5 +1,6 @@
 import {
   NewNotesTxResult,
+  OptionalActiveRelayer,
   Transaction,
   TransactionState,
   TransferTransactionPayloadType,
@@ -15,10 +16,11 @@ import {
   TransferConfirm,
   getRoundedAmountString,
 } from '@webb-tools/webb-ui-components';
-import { forwardRef, useCallback, useMemo, useState } from 'react';
+import { forwardRef, useMemo, useState, type ComponentProps } from 'react';
 import type { Hash } from 'viem';
 import { ContractFunctionRevertedError, formatEther } from 'viem';
 import { useEnqueueSubmittedTx } from '../../hooks';
+import useInProgressTxInfo from '../../hooks/useInProgressTxInfo';
 import {
   captureSentryException,
   getErrorMessage,
@@ -29,7 +31,6 @@ import {
 } from '../../utils';
 import { RecipientPublicKeyTooltipContent } from './shared';
 import { TransferConfirmContainerProps } from './types';
-import useInProgressTxInfo from '../../hooks/useInProgressTxInfo';
 
 const logger = LoggerService.get('TransferConfirmContainer');
 
@@ -51,7 +52,7 @@ const TransferConfirmContainer = forwardRef<
       inputNotes,
       onResetState,
       onClose,
-      feeInWei: feeAmount,
+      feeInWei,
       feeToken,
       refundRecipient,
       refundAmount,
@@ -63,18 +64,7 @@ const TransferConfirmContainer = forwardRef<
     // State for tracking the status of the change note checkbox
     const [isChecked, setIsChecked] = useState(false);
 
-    const {
-      api: vAnchorApi,
-      addNoteToNoteManager,
-      removeNoteFromNoteManager,
-    } = useVAnchor();
-
-    const enqueueSubmittedTx = useEnqueueSubmittedTx();
-
-    const { activeApi, activeChain, apiConfig, txQueue, noteManager } =
-      useWebContext();
-
-    const { api: txQueueApi } = txQueue;
+    const { activeApi, activeChain } = useWebContext();
 
     const {
       cardTitle,
@@ -104,192 +94,35 @@ const TransferConfirmContainer = forwardRef<
         : undefined,
     });
 
-    // The callback for the transfer button
-    const handleTransferExecute = useCallback(
-      async () => {
-        if (inputNotes.length === 0) {
-          logger.error('No input notes provided');
-          captureSentryException(
-            new Error('No input notes provided'),
-            'transactionType',
-            'transfer'
-          );
-          return;
-        }
-
-        if (!vAnchorApi || !activeApi) {
-          logger.error('No vAnchor API provided');
-          captureSentryException(
-            new Error('No vAnchor API provided'),
-            'transactionType',
-            'transfer'
-          );
-          return;
-        }
-
-        if (inProgressTxId.length > 0) {
-          txQueueApi.startNewTransaction();
-          onResetState?.();
-          return;
-        }
-
-        const note: Note = inputNotes[0];
-        const {
-          sourceChainId: sourceTypedChainId,
-          targetChainId: destTypedChainId,
-          sourceIdentifyingData,
-          tokenSymbol,
-        } = note.note;
-
-        const destCurrency = apiConfig.getCurrencyBySymbolAndTypedChainId(
-          tokenSymbol,
-          +destTypedChainId
-        );
-        if (!destCurrency) {
-          console.error(`Currency not found for symbol ${tokenSymbol}`);
-          captureSentryException(
-            new Error(`Currency not found for symbol ${tokenSymbol}`),
-            'transactionType',
-            'transfer'
-          );
-          return;
-        }
-        const tokenURI = getTokenURI(destCurrency, destTypedChainId);
-
-        const tx = Transaction.new<NewNotesTxResult>('Transfer', {
-          amount,
-          tokens: [tokenSymbol, tokenSymbol],
-          wallets: {
-            src: +sourceTypedChainId,
-            dest: +destTypedChainId,
-          },
-          token: tokenSymbol,
-          tokenURI,
-          providerType: activeApi.type,
-          address: noteManager?.getKeypair().toString(),
-          recipient,
-        });
-
-        setInProgressTxId(tx.id);
-        setTotalStep(tx.totalSteps);
-        txQueueApi.registerTransaction(tx);
-
-        try {
-          const txPayload: TransferTransactionPayloadType = {
-            notes: inputNotes,
-            changeUtxo,
-            transferUtxo,
-            feeAmount: feeAmount ?? ZERO_BIG_INT,
-            refundAmount: refundAmount ?? ZERO_BIG_INT,
-            refundRecipient: refundRecipient ?? ZERO_ADDRESS,
-          };
-
-          const args = await vAnchorApi.prepareTransaction(tx, txPayload, '');
-
-          console.log('args', args);
-
-          const outputNotes = changeNote ? [changeNote] : [];
-
-          let indexBeforeInsert: number | undefined;
-          if (changeNote) {
-            const nextIdx = Number(
-              await vAnchorApi.getNextIndex(+sourceTypedChainId, currency.id)
-            );
-
-            indexBeforeInsert = nextIdx === 0 ? nextIdx : nextIdx - 1;
-          }
-
-          let transactionHash: Hash;
-
-          if (activeRelayer) {
-            transactionHash = await vAnchorApi.transactWithRelayer(
-              activeRelayer,
-              args,
-              outputNotes
-            );
-
-            await handleStoreNote(changeNote, addNoteToNoteManager);
-
-            enqueueSubmittedTx(
-              transactionHash,
-              apiConfig.chains[+sourceTypedChainId],
-              'transfer'
-            );
-          } else {
-            transactionHash = await vAnchorApi.transact(...args);
-
-            enqueueSubmittedTx(
-              transactionHash,
-              apiConfig.chains[+sourceTypedChainId],
-              'transfer'
-            );
-
-            await handleStoreNote(changeNote, addNoteToNoteManager);
-
-            await vAnchorApi.waitForFinalization(transactionHash);
-
-            // Notification Success Transaction
-            tx.next(TransactionState.Done, {
-              txHash: transactionHash,
-              outputNotes,
-            });
-          }
-
-          if (typeof indexBeforeInsert === 'number' && changeNote) {
-            const noteWithIdx = await handleMutateNoteIndex(
-              vAnchorApi,
-              transactionHash,
-              changeNote,
-              indexBeforeInsert,
-              sourceIdentifyingData
-            );
-
-            await removeNoteFromNoteManager(changeNote);
-            await addNoteToNoteManager(noteWithIdx);
-          }
-
-          // Cleanup NoteAccount state
-          for (const note of inputNotes) {
-            await removeNoteFromNoteManager(note);
-          }
-        } catch (error) {
-          console.error('Error occured while transferring', error);
-          changeNote && (await removeNoteFromNoteManager(changeNote));
-          tx.txHash = getTransactionHash(error);
-
-          let errorMessage = getErrorMessage(error);
-          if (isViemError(error)) {
-            errorMessage = error.shortMessage;
-
-            const revertError = error.walk(
-              (err) => err instanceof ContractFunctionRevertedError
-            );
-
-            if (revertError instanceof ContractFunctionRevertedError) {
-              errorMessage = revertError.reason ?? revertError.shortMessage;
-            }
-          }
-
-          tx.fail(errorMessage);
-
-          captureSentryException(error, 'transactionType', 'transfer');
-        }
-      },
-      // prettier-ignore
-      [activeApi, activeRelayer, addNoteToNoteManager, amount, apiConfig, changeNote, changeUtxo, currency.id, enqueueSubmittedTx, feeAmount, inProgressTxId.length, inputNotes, noteManager, onResetState, recipient, refundAmount, refundRecipient, removeNoteFromNoteManager, setInProgressTxId, setTotalStep, transferUtxo, txQueueApi, vAnchorApi]
-    );
+    const handleTransferExecute = useTransferExecuteHandler({
+      activeRelayer,
+      inputNotes,
+      onResetState,
+      amount,
+      recipient,
+      changeUtxo,
+      feeInWei,
+      transferUtxo,
+      refundAmount,
+      refundRecipient,
+      note: changeNote,
+      currency,
+      inProgressTxId,
+      setInProgressTxId,
+      setTotalStep,
+    });
 
     const formattedFee = useMemo(() => {
-      if (!feeAmount) {
+      if (!feeInWei) {
         return undefined;
       }
 
-      const amountNum = Number(formatEther(feeAmount));
+      const amountNum = Number(formatEther(feeInWei));
 
       return getRoundedAmountString(amountNum, 3, {
         roundingFunction: Math.round,
       });
-    }, [feeAmount]);
+    }, [feeInWei]);
 
     return (
       <TransferConfirm
@@ -354,3 +187,227 @@ const TransferConfirmContainer = forwardRef<
 );
 
 export default TransferConfirmContainer;
+
+type Args = Pick<
+  ComponentProps<typeof TransferConfirmContainer>,
+  | 'inputNotes'
+  | 'onResetState'
+  | 'amount'
+  | 'recipient'
+  | 'changeUtxo'
+  | 'feeInWei'
+  | 'transferUtxo'
+  | 'refundAmount'
+  | 'refundRecipient'
+  | 'note'
+  | 'currency'
+> &
+  Pick<
+    ReturnType<typeof useInProgressTxInfo>,
+    'inProgressTxId' | 'setInProgressTxId' | 'setTotalStep'
+  > & {
+    activeRelayer: OptionalActiveRelayer;
+  };
+
+const useTransferExecuteHandler = (args: Args) => {
+  const {
+    amount,
+    changeUtxo,
+    feeInWei: feeAmount,
+    inProgressTxId,
+    inputNotes,
+    onResetState,
+    recipient,
+    refundAmount,
+    refundRecipient,
+    setInProgressTxId,
+    setTotalStep,
+    transferUtxo,
+    note: changeNote,
+    activeRelayer,
+    currency,
+  } = args;
+
+  const {
+    activeApi,
+    apiConfig,
+    noteManager,
+    txQueue: { api: txQueueApi },
+  } = useWebContext();
+
+  const { addNoteToNoteManager, removeNoteFromNoteManager } = useVAnchor();
+
+  const enqueueSubmittedTx = useEnqueueSubmittedTx();
+
+  return async () => {
+    if (inputNotes.length === 0) {
+      logger.error('No input notes provided');
+      captureSentryException(
+        new Error('No input notes provided'),
+        'transactionType',
+        'transfer'
+      );
+      return;
+    }
+
+    const vAnchorApi = activeApi?.methods.variableAnchor.actions.inner;
+
+    if (!vAnchorApi || !activeApi) {
+      logger.error('No vAnchor API provided');
+      captureSentryException(
+        new Error('No vAnchor API provided'),
+        'transactionType',
+        'transfer'
+      );
+      return;
+    }
+
+    if (inProgressTxId.length > 0) {
+      txQueueApi.startNewTransaction();
+      onResetState?.();
+      return;
+    }
+
+    const note: Note = inputNotes[0];
+    const {
+      sourceChainId: sourceTypedChainId,
+      targetChainId: destTypedChainId,
+      sourceIdentifyingData,
+      tokenSymbol,
+    } = note.note;
+
+    const destCurrency = apiConfig.getCurrencyBySymbolAndTypedChainId(
+      tokenSymbol,
+      +destTypedChainId
+    );
+    if (!destCurrency) {
+      console.error(`Currency not found for symbol ${tokenSymbol}`);
+      captureSentryException(
+        new Error(`Currency not found for symbol ${tokenSymbol}`),
+        'transactionType',
+        'transfer'
+      );
+      return;
+    }
+    const tokenURI = getTokenURI(destCurrency, destTypedChainId);
+
+    const tx = Transaction.new<NewNotesTxResult>('Transfer', {
+      amount,
+      tokens: [tokenSymbol, tokenSymbol],
+      wallets: {
+        src: +sourceTypedChainId,
+        dest: +destTypedChainId,
+      },
+      token: tokenSymbol,
+      tokenURI,
+      providerType: activeApi.type,
+      address: noteManager?.getKeypair().toString(),
+      recipient,
+    });
+
+    setInProgressTxId(tx.id);
+    setTotalStep(tx.totalSteps);
+    txQueueApi.registerTransaction(tx);
+
+    try {
+      const txPayload: TransferTransactionPayloadType = {
+        notes: inputNotes,
+        changeUtxo,
+        transferUtxo,
+        feeAmount: feeAmount ?? ZERO_BIG_INT,
+        refundAmount: refundAmount ?? ZERO_BIG_INT,
+        refundRecipient: refundRecipient ?? ZERO_ADDRESS,
+      };
+
+      const args = await vAnchorApi.prepareTransaction(tx, txPayload, '');
+
+      console.log('args', args);
+
+      const outputNotes = changeNote ? [changeNote] : [];
+
+      let indexBeforeInsert: number | undefined;
+      if (changeNote) {
+        const nextIdx = Number(
+          await vAnchorApi.getNextIndex(+sourceTypedChainId, currency.id)
+        );
+
+        indexBeforeInsert = nextIdx === 0 ? nextIdx : nextIdx - 1;
+      }
+
+      let transactionHash: Hash;
+
+      if (activeRelayer) {
+        transactionHash = await vAnchorApi.transactWithRelayer(
+          activeRelayer,
+          args,
+          outputNotes
+        );
+
+        await handleStoreNote(changeNote, addNoteToNoteManager);
+
+        enqueueSubmittedTx(
+          transactionHash,
+          apiConfig.chains[+sourceTypedChainId],
+          'transfer'
+        );
+      } else {
+        transactionHash = await vAnchorApi.transact(...args);
+
+        enqueueSubmittedTx(
+          transactionHash,
+          apiConfig.chains[+sourceTypedChainId],
+          'transfer'
+        );
+
+        await handleStoreNote(changeNote, addNoteToNoteManager);
+
+        await vAnchorApi.waitForFinalization(transactionHash);
+
+        // Notification Success Transaction
+        tx.next(TransactionState.Done, {
+          txHash: transactionHash,
+          outputNotes,
+        });
+      }
+
+      if (typeof indexBeforeInsert === 'number' && changeNote) {
+        const noteWithIdx = await handleMutateNoteIndex(
+          vAnchorApi,
+          transactionHash,
+          changeNote,
+          indexBeforeInsert,
+          sourceIdentifyingData
+        );
+
+        await removeNoteFromNoteManager(changeNote);
+        await addNoteToNoteManager(noteWithIdx);
+      }
+
+      // Cleanup NoteAccount state
+      for (const note of inputNotes) {
+        await removeNoteFromNoteManager(note);
+      }
+    } catch (error) {
+      console.error('Error occured while transferring', error);
+      changeNote && (await removeNoteFromNoteManager(changeNote));
+      tx.txHash = getTransactionHash(error);
+
+      let errorMessage = getErrorMessage(error);
+      if (isViemError(error)) {
+        errorMessage = error.shortMessage;
+
+        const revertError = error.walk(
+          (err) => err instanceof ContractFunctionRevertedError
+        );
+
+        if (revertError instanceof ContractFunctionRevertedError) {
+          errorMessage = revertError.reason ?? revertError.shortMessage;
+        }
+      }
+
+      tx.fail(errorMessage);
+
+      captureSentryException(error, 'transactionType', 'transfer');
+    }
+  };
+};
