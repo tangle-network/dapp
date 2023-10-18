c9868068a79fca35eb6379a865040a938e4c0c05
Pavan Soratur <pavanvsoratur@gmail.com>
2023-10-18 13:47:31 -0700

    chore: fix layout

--- apps/tangle-dapp/containers/Layout/Layout.tsx
+++ apps/tangle-dapp/containers/Layout/Layout.tsx
@@ -9,24 +9,18 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <SideBar isExpandedAtDefault={isSideBarInitiallyExpanded} />
-      <main className="flex flex-col justify-between flex-1 h-full px-3 overflow-y-auto md:px-5 lg:px-10">
-        <div className="flex-[1]">
-          {/* Header */}
-          <div className="flex items-center justify-between pt-6 pb-4">
-            <div className="flex items-center gap-2">
-              <SideBarMenu />
-              {/* Breadcrumbs will go here! */}
-            </div>
-
-            {/* Wallet connection will go here! */}
+      <main className="flex flex-col justify-between flex-1 h-full overflow-y-auto max-w-[1448px] m-auto py-4 px-4 2xl:px-0">
+        <div className="flex items-center justify-between">
+          <div>
+            <SideBarMenu />
           </div>
 
-          {/* Body */}
-          {children}
+          <div>wallet connection</div>
         </div>
 
-        {/* Footer */}
-        <Footer isMinimal className="py-12 mx-0 max-w-none" />
+        {children}
+
+        <Footer isMinimal className="py-3" />
       </main>
     </>
   );
