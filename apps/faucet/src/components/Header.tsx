7ab0996a5b32c3642c103d2ce6c9ea4adee8825c
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-10-18 10:42:44 +0700

    chore: update header right side

--- apps/faucet/src/components/Header.tsx
+++ apps/faucet/src/components/Header.tsx
@@ -1,12 +1,23 @@
-import { FaucetIcon, ThreeDotsVerticalIcon } from '@webb-tools/icons';
+import { FaucetIcon } from '@webb-tools/icons';
 import {
   Breadcrumbs,
   BreadcrumbsItem,
   Button,
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
   SideBarMenu,
 } from '@webb-tools/webb-ui-components';
+import {
+  BRIDGE_URL,
+  GITHUB_REQUEST_FEATURE_URL,
+  SOCIAL_URLS_RECORD,
+  TANGLE_MKT_URL,
+  TANGLE_STANDALONE_EXPLORER_URL,
+} from '@webb-tools/webb-ui-components/constants';
 import { type FC } from 'react';
 
+import { TANGLE_DOCS_URL } from '../constants';
 import sideBarProps from '../constants/sidebar';
 
 const Header: FC = () => {
@@ -23,8 +34,27 @@ const Header: FC = () => {
 
       {/* <HeaderChipsContainer /> */}
       <div className="flex items-center gap-2">
-        <Button>Webb dApp</Button>
-        <ThreeDotsVerticalIcon size="lg" />
+        <Button href={BRIDGE_URL} target="_blank" rel="noreferrer">
+          Webb dApp
+        </Button>
+
+        <NavigationMenu>
+          <NavigationMenuTrigger />
+          {/** TODO: Refactor these links into a config file and make the menu items dynamically based on the config */}
+          <NavigationMenuContent
+            onDocsClick={() => window.open(TANGLE_DOCS_URL, '_blank')}
+            onTestnetClick={() =>
+              window.open(TANGLE_STANDALONE_EXPLORER_URL, '_blank')
+            }
+            onHelpCenterClick={() =>
+              window.open(SOCIAL_URLS_RECORD.telegram, '_blank')
+            }
+            onRequestFeaturesClick={() =>
+              window.open(GITHUB_REQUEST_FEATURE_URL, '_blank')
+            }
+            onAboutClick={() => window.open(TANGLE_MKT_URL, '_blank')}
+          />
+        </NavigationMenu>
       </div>
     </header>
   );
---
+++ apps/faucet/src/constants/index.ts
@@ -0,0 +1,6 @@
+import { WEBB_DOC_ROUTES_RECORD } from '@webb-tools/webb-ui-components/constants';
+import populateDocsUrl from '@webb-tools/webb-ui-components/utils/populateDocsUrl';
+
+export const TANGLE_DOCS_URL = populateDocsUrl(
+  WEBB_DOC_ROUTES_RECORD['tangle-network'].overview
+);
