2ac85f81c08086e1b0e596c3a090b125e85a9999
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-10-21 19:35:03 +0700

    refactor: client side fetching for pool transactions and metadata

---
+++ apps/hubble-stats/containers/PoolTransactionsTableContainer/tabs.ts
@@ -0,0 +1,4 @@
+export const allTab = 'All Transactions' as const;
+export const depositsTab = 'Deposits' as const;
+export const transfersTab = 'Transfers' as const;
+export const withdrawalsTab = 'Withdrawals' as const;
