c672176c5d35367d6059243c59e0973348ef1397
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-11-01 03:39:24 +0700

    chore: remove notes and shielded assets filter by active chain

--- apps/bridge-dapp/src/hooks/useSpendNotes.ts
+++ apps/bridge-dapp/src/hooks/useSpendNotes.ts
@@ -27,7 +27,7 @@ export const useSpendNotes = (): SpendNoteDataType[] => {
   const { allFungibleCurrencies: fungibleCurrencies, getWrappableCurrencies } =
     useCurrencies();
 
-  const { activeChain, apiConfig, chains } = useWebContext();
+  const { apiConfig, chains } = useWebContext();
 
   const { api: vAnchorApi } = useVAnchor();
 
@@ -60,11 +60,7 @@ export const useSpendNotes = (): SpendNoteDataType[] => {
             val.typedChainId === Number(sourceChainId)
         );
 
-        // Check if the current chain tag is the same as the source chain tag
-        const isCurrentTag =
-          activeChain?.tag === apiConfig.chains[+sourceChainId]?.tag;
-
-        if (!isExisted && isCurrentTag) {
+        if (!isExisted) {
           acc.push({
             fungibleCurrencyId: fungible.id,
             typedChainId: Number(sourceChainId),
@@ -74,111 +70,107 @@ export const useSpendNotes = (): SpendNoteDataType[] => {
 
       return acc;
     }, [] as Array<Omit<ArrayElement<typeof nextIndices>, 'nextIndex'>>);
-  }, [apiConfig, allNotes, activeChain?.tag]);
+  }, [apiConfig, allNotes]);
 
-  const notes = useMemo(() => {
-    return Array.from(allNotes.entries()).reduce(
-      (acc, [resourceIdStr, notes]) => {
-        const resourceId = ResourceId.fromBytes(hexToU8a(resourceIdStr));
-        const typedChainId = calculateTypedChainId(
-          resourceId.chainType,
-          resourceId.chainId
-        );
-
-        const chain = chains[typedChainId];
-        if (!chain) {
-          console.trace('Chain not found with typedChainId: ', typedChainId);
-          return acc;
-        }
-
-        if (chain.tag !== activeChain?.tag) {
-          return acc;
-        }
-
-        notes.forEach((note) => {
-          const nextIndex = nextIndices.find((item) => {
-            const fungible = apiConfig.currencies[item.fungibleCurrencyId];
-            if (!fungible) {
-              console.error(
-                'Fungible currency not found with id: ',
-                item.fungibleCurrencyId
-              );
-              return false;
-            }
-
-            return (
-              item.fungibleCurrencyId === fungible.id &&
-              item.typedChainId === Number(note.note.sourceChainId)
-            );
-          })?.nextIndex;
-
-          const subsequentDepositsNumber = nextIndex
-            ? nextIndex - Number(note.note.index)
-            : '?';
-
-          // Calculate the wrappable currencies
-          const compositionSet = new Set<string>();
-          const fungibleCurrency = fungibleCurrencies.find((currency) => {
-            return (
-              currency.view.symbol === note.note.tokenSymbol &&
-              currency.hasChain(+note.note.targetChainId)
-            );
-          });
-          if (fungibleCurrency) {
-            const foundCurrencies = getWrappableCurrencies(
-              fungibleCurrency.id,
-              false
-            );
-            foundCurrencies.forEach((c) =>
-              compositionSet.add(c.view.symbol.toUpperCase())
-            );
-          }
-
-          // Calculate the assets url
-          let assetsUrl = '#';
-          const explorerUrl = chain.blockExplorers?.default.url;
-          const address = fungibleCurrency?.getAddressOfChain(
-            calculateTypedChainId(chain.chainType, chain.id)
+  const notes = useMemo(
+    () => {
+      return Array.from(allNotes.entries()).reduce(
+        (acc, [resourceIdStr, notes]) => {
+          const resourceId = ResourceId.fromBytes(hexToU8a(resourceIdStr));
+          const typedChainId = calculateTypedChainId(
+            resourceId.chainType,
+            resourceId.chainId
           );
 
-          if (explorerUrl && address) {
-            assetsUrl = explorerUrl.endsWith('/')
-              ? `${explorerUrl}address/${address}`
-              : `${explorerUrl}/address/${address}`;
+          const chain = chains[typedChainId];
+          if (!chain) {
+            console.trace('Chain not found with typedChainId: ', typedChainId);
+            return acc;
           }
 
-          acc.push({
-            fungibleTokenSymbol: note.note.tokenSymbol,
-            chain: chain.name.toLowerCase(),
-            note: note.serialize(),
-            assetsUrl,
-            composition: Array.from(compositionSet),
-            createdTime, // TODO: get the actual created time
-            balance: Number(
-              formatUnits(BigInt(note.note.amount), +note.note.denomination)
-            ),
-            subsequentDeposits: note.note.index
-              ? subsequentDepositsNumber.toString()
-              : '0',
-            rawFungibleCurrency: fungibleCurrency,
-            rawChain: chain,
-            rawNote: note,
-          });
-        });
+          // if (chain.tag !== activeChain?.tag) {
+          //   return acc;
+          // }
 
-        return acc;
-      },
-      [] as Array<SpendNoteDataType>
-    );
-  }, [
-    allNotes,
-    chains,
-    activeChain?.tag,
-    nextIndices,
-    fungibleCurrencies,
-    apiConfig.currencies,
-    getWrappableCurrencies,
-  ]);
+          notes.forEach((note) => {
+            const nextIndex = nextIndices.find((item) => {
+              const fungible = apiConfig.currencies[item.fungibleCurrencyId];
+              if (!fungible) {
+                console.error(
+                  'Fungible currency not found with id: ',
+                  item.fungibleCurrencyId
+                );
+                return false;
+              }
+
+              return (
+                item.fungibleCurrencyId === fungible.id &&
+                item.typedChainId === Number(note.note.sourceChainId)
+              );
+            })?.nextIndex;
+
+            const subsequentDepositsNumber = nextIndex
+              ? nextIndex - Number(note.note.index)
+              : '?';
+
+            // Calculate the wrappable currencies
+            const compositionSet = new Set<string>();
+            const fungibleCurrency = fungibleCurrencies.find((currency) => {
+              return (
+                currency.view.symbol === note.note.tokenSymbol &&
+                currency.hasChain(+note.note.targetChainId)
+              );
+            });
+            if (fungibleCurrency) {
+              const foundCurrencies = getWrappableCurrencies(
+                fungibleCurrency.id,
+                false
+              );
+              foundCurrencies.forEach((c) =>
+                compositionSet.add(c.view.symbol.toUpperCase())
+              );
+            }
+
+            // Calculate the assets url
+            let assetsUrl = '#';
+            const explorerUrl = chain.blockExplorers?.default.url;
+            const address = fungibleCurrency?.getAddressOfChain(
+              calculateTypedChainId(chain.chainType, chain.id)
+            );
+
+            if (explorerUrl && address) {
+              assetsUrl = explorerUrl.endsWith('/')
+                ? `${explorerUrl}address/${address}`
+                : `${explorerUrl}/address/${address}`;
+            }
+
+            acc.push({
+              fungibleTokenSymbol: note.note.tokenSymbol,
+              chain: chain.name.toLowerCase(),
+              note: note.serialize(),
+              assetsUrl,
+              composition: Array.from(compositionSet),
+              createdTime, // TODO: get the actual created time
+              balance: Number(
+                formatUnits(BigInt(note.note.amount), +note.note.denomination)
+              ),
+              subsequentDeposits: note.note.index
+                ? subsequentDepositsNumber.toString()
+                : '0',
+              rawFungibleCurrency: fungibleCurrency,
+              rawChain: chain,
+              rawNote: note,
+            });
+          });
+
+          return acc;
+        },
+        [] as Array<SpendNoteDataType>
+      );
+    },
+    // prettier-ignore
+    [allNotes, chains, nextIndices, fungibleCurrencies, apiConfig.currencies, getWrappableCurrencies]
+  );
 
   // Effect to get next indices asynchorously
   useEffect(() => {
