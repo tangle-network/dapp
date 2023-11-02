2f890ed68019360fd858c4b340bc937e45155b15
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-11-02 20:30:38 +0700

    fix: local storage is not defined errors

--- libs/browser-utils/src/index.ts
+++ libs/browser-utils/src/index.ts
@@ -2,6 +2,7 @@ export * from './download';
 export * from './executorWithTimeout';
 export { default as executorWithTimeout } from './executorWithTimeout';
 export * from './fetchFixtures';
+export { default as isBrowser } from './isBrowser';
 export * from './logger';
 export * from './platform';
 export * from './retryPromise';
