8ed3b073970613d73c300387f63cc82c838eb50b
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-11-21 19:56:28 +0700

    feat: gen testnet-leaderboard app

---
+++ apps/testnet-leaderboard/next.config.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
