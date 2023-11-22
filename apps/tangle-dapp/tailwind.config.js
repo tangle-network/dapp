9643741b7f0300f21f2004731f780c3de89cc782
Pavan Soratur <pavanvsoratur@gmail.com>
2023-11-22 04:08:27 -0800

    chore: address review comments

--- apps/tangle-dapp/tailwind.config.js
+++ apps/tangle-dapp/tailwind.config.js
@@ -1,8 +1,8 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const plugin = require('tailwindcss/plugin');
 const { join } = require('path');
 
 const preset = require('@webb-tools/tailwind-preset');
-const scrollbarHide = require('tailwind-scrollbar-hide');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -29,5 +29,38 @@ module.exports = {
       },
     },
   },
-  plugins: [scrollbarHide],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.scrollbar-hide': {
+            /* IE and Edge */
+            '-ms-overflow-style': 'none',
+
+            /* Firefox */
+            'scrollbar-width': 'none',
+
+            /* Safari and Chrome */
+            '&::-webkit-scrollbar': {
+              display: 'none',
+            },
+          },
+
+          '.scrollbar-default': {
+            /* IE and Edge */
+            '-ms-overflow-style': 'auto',
+
+            /* Firefox */
+            'scrollbar-width': 'auto',
+
+            /* Safari and Chrome */
+            '&::-webkit-scrollbar': {
+              display: 'block',
+            },
+          },
+        },
+        ['responsive']
+      );
+    }),
+  ],
 };
