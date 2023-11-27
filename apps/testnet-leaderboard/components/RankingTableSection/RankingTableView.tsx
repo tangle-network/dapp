d2a45915a724b513dd7bcb05b0c9c7ed8535d0f7
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-11-28 00:38:45 +0700

    chore: display empty state when no data

--- apps/testnet-leaderboard/components/RankingTableSection/RankingTableView.tsx
+++ apps/testnet-leaderboard/components/RankingTableSection/RankingTableView.tsx
@@ -182,15 +182,29 @@ const RankingTableView: FC<Props> = ({
             ))}
           </thead>
           <tbody>
-            {getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell, idx) => (
-                  <td key={idx} className="px-2 py-2 md:px-6 md:py-3">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
+            {getRowModel().rows.length > 0 ? (
+              getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell, idx) => (
+                    <td key={idx} className="px-2 py-2 md:px-6 md:py-3">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))
+            ) : (
+              <tr>
+                {/** This is a hack to make the table data full width */}
+                <td colSpan={Number.MAX_SAFE_INTEGER} className="p-2">
+                  <Typography variant="mkt-body2" fw="semibold" ta="center">
+                    No participants found.
+                  </Typography>
+                </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
 
