320f2463ae70b7a9f87333037e315c9b600d82a6
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2023-11-28 01:08:20 +0700

    chore: display badge with tooltip

--- apps/testnet-leaderboard/components/RankingTableSection/BadgesCell.tsx
+++ apps/testnet-leaderboard/components/RankingTableSection/BadgesCell.tsx
@@ -1,5 +1,11 @@
+import {
+  Tooltip,
+  TooltipBody,
+  TooltipTrigger,
+} from '@webb-tools/webb-ui-components';
 import { Typography } from '@webb-tools/webb-ui-components/typography/Typography';
 import cx from 'classnames';
+import capitalize from 'lodash/capitalize';
 import type { FC } from 'react';
 
 import { BADGE_ICON_RECORD } from '../../constants';
@@ -9,18 +15,23 @@ const BadgesCell: FC<{ badges: BadgeEnum[] }> = ({ badges }) => {
   return (
     <div className="flex flex-wrap gap-[2px]">
       {badges.map((badge, idx) => (
-        <div
-          key={idx}
-          className={cx(
-            'w-6 md:w-[30px] aspect-square rounded-full',
-            'flex items-center justify-center',
-            'bg-[rgba(31,29,43,0.1)]'
-          )}
-        >
-          <Typography variant="mkt-body2">
-            {BADGE_ICON_RECORD[badge]}
-          </Typography>
-        </div>
+        <Tooltip key={idx}>
+          <TooltipTrigger asChild>
+            <div
+              className={cx(
+                'w-6 md:w-[30px] aspect-square rounded-full',
+                'flex items-center justify-center',
+                'bg-[rgba(31,29,43,0.1)] cursor-pointer'
+              )}
+            >
+              <Typography variant="mkt-body2">
+                {BADGE_ICON_RECORD[badge]}
+              </Typography>
+            </div>
+          </TooltipTrigger>
+
+          <TooltipBody>{capitalize(badge.toString())}</TooltipBody>
+        </Tooltip>
       ))}
     </div>
   );
