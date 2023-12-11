15a2b45e9f84bc338ba1d361458e607733343fbc
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-12-11 16:19:55 +0700

    chore: refactor code

--- libs/dapp-config/src/wallets/wallet-config.interface.ts
+++ libs/dapp-config/src/wallets/wallet-config.interface.ts
@@ -3,14 +3,13 @@
 
 import type { InjectedExtension } from '@polkadot/extension-inject/types';
 import type { SupportedBrowsers } from '@webb-tools/browser-utils/platform';
-import type { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import type { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import type { InjectedConnector } from 'wagmi/connectors/injected';
+import { MetaMaskConnector, RainbowConnector } from './injected';
 
 export type SupportedConnector =
   | MetaMaskConnector
   | WalletConnectConnector
-  | InjectedConnector;
+  | RainbowConnector;
 
 export interface WalletConfig {
   id: number;
