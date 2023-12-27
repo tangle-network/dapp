183a325e54016ab29952b0b2f471f6a230521c7d
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-12-27 10:21:47 +0700

    feat: project GitHub detail section

--- apps/zk-explorer/app/project/[slug]/page.tsx
+++ apps/zk-explorer/app/project/[slug]/page.tsx
@@ -12,22 +12,20 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
   // TODO: Handle invalid project id, return 404
 
   return (
-    <main>
+    <main className="space-y-6">
       {/* Header */}
       {/* TODO: update breadcrumb */}
       <Header />
 
-      <div className="space-y-6">
-        <div className="grid grid-cols-4 grid-rows-2 gap-6">
-          <ProjectGitHubInfoContainer className="row-span-2" />
+      <div className="grid grid-cols-4 grid-rows-[auto_1fr] gap-6">
+        <ProjectGitHubInfoContainer className="row-span-2" />
 
-          <ProjectHeaderContainer className="col-span-3" />
+        <ProjectHeaderContainer className="col-span-3" />
 
-          <ProjectDetailTabsContainer className="col-span-3" />
-        </div>
-
-        <RelatedProjectsContainer />
+        <ProjectDetailTabsContainer className="col-span-3" />
       </div>
+
+      <RelatedProjectsContainer />
     </main>
   );
 }
---
+++ apps/zk-explorer/constants/index.ts
@@ -0,0 +1,2 @@
+export const GITHUB_LANGUAGE_COLORS_API_URL =
+  'https://raw.githubusercontent.com/ozh/github-colors/master/colors.json';
