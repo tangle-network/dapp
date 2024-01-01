4033a8339395b205f0ae8f4502145ce7e5af6a8a
Pavan Soratur <pavanvsoratur@gmail.com>
2024-01-01 14:15:13 -0800

    chore: adds unbond & rebond flow, fix sidebar z-index

---
+++ apps/tangle-dapp/data/NominatorStats/useUnbondingAmountSubscription.ts
@@ -0,0 +1,84 @@
+'use client';
+
+import { u128 } from '@polkadot/types';
+import { WebbError, WebbErrorCodes } from '@webb-tools/dapp-types/WebbError';
+import { useEffect, useState } from 'react';
+import { type Subscription } from 'rxjs';
+
+import { formatTokenBalance, getPolkadotApiRx } from '../../constants';
+import useFormatReturnType from '../../hooks/useFormatReturnType';
+
+export default function useUnbondingAmountSubscription(
+  address: string,
+  defaultValue: { value1: string | number | null } = {
+    value1: null,
+  }
+) {
+  const [value1, setValue1] = useState(defaultValue.value1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    let sub: Subscription | null = null;
+
+    const subscribeData = async () => {
+      try {
+        const api = await getPolkadotApiRx();
+        if (!api) {
+          throw WebbError.from(WebbErrorCodes.ApiNotReady);
+        }
+
+        if (!address) {
+          setValue1(null);
+          setIsLoading(false);
+          return;
+        }
+
+        sub = api.query.staking
+          .ledger(address)
+          .subscribe(async (ledgerData) => {
+            if (isMounted) {
+              const ledger = ledgerData.unwrapOrDefault();
+
+              const unbondingAmount = ledger.unlocking.map((unlockChunk) => {
+                return unlockChunk.value.toString();
+              });
+
+              const totalUnbondingAmount = unbondingAmount.reduce(
+                (accumulator, currentValue) => {
+                  return accumulator + Number(currentValue);
+                },
+                0
+              );
+
+              const unbondingFormattedAmount = await formatTokenBalance(
+                new u128(api.registry, totalUnbondingAmount.toString())
+              );
+
+              setValue1(unbondingFormattedAmount ?? null);
+              setIsLoading(false);
+            }
+          });
+      } catch (error) {
+        if (isMounted) {
+          setError(
+            error instanceof Error
+              ? error
+              : WebbError.from(WebbErrorCodes.UnknownError)
+          );
+          setIsLoading(false);
+        }
+      }
+    };
+
+    subscribeData();
+
+    return () => {
+      isMounted = false;
+      sub?.unsubscribe();
+    };
+  }, [address]);
+
+  return useFormatReturnType({ isLoading, error, data: { value1 } });
+}
