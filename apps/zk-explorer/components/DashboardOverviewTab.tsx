1ccf674fe718608fcef4f3dd8005c835d0171e43
yurixander <101931215+yurixander@users.noreply.github.com>
2024-01-03 19:14:14 -0500

    style(zk-explorer): Make tab contents responsive

--- apps/zk-explorer/components/DashboardOverviewTab.tsx
+++ apps/zk-explorer/components/DashboardOverviewTab.tsx
@@ -1,14 +1,24 @@
 'use client';
 
+import { GithubFill, GlobalLine, TwitterFill } from '@webb-tools/icons';
+import { IconBase } from '@webb-tools/icons/types';
 import { Button, Card, Typography } from '@webb-tools/webb-ui-components';
 import { WEBB_DOCS_URL } from '@webb-tools/webb-ui-components/constants';
-import { FC, useMemo } from 'react';
+import Link from 'next/link';
+import { ComponentType, FC, useMemo } from 'react';
 import { useRequireAuth } from '../hooks/useAuth';
 import { formatTimestamp } from '../utils';
 import { LargeSquareAvatar } from './LargeSquareAvatar';
+import { SmallChip } from './SmallChip';
 
 export const DashboardOverviewTab: FC = () => {
-  const { activatedCircuitCount, createdAt } = useRequireAuth();
+  const {
+    activatedCircuitCount,
+    createdAt,
+    githubUsername,
+    website,
+    twitterHandle,
+  } = useRequireAuth();
 
   const creationTimestampString = useMemo(
     () => formatTimestamp(createdAt),
@@ -17,56 +27,74 @@ export const DashboardOverviewTab: FC = () => {
 
   return (
     <div className="flex flex-col gap-6">
-      <Card className="p-6 flex rounded-2xl space-y-0">
-        <div className="flex">
-          <div className="flex flex-col justify-center gap-1">
-            <Typography variant="body2" fw="normal">
-              Activated Circuits
-            </Typography>
+      <Card className="p-6 flex flex-col md:flex-row rounded-2xl space-y-0">
+        <div className="flex flex-col justify-center gap-1">
+          <Typography variant="body2" fw="normal">
+            Activated Circuits
+          </Typography>
 
-            <Typography variant="h5" fw="bold">
-              {activatedCircuitCount === 0 ? '-' : activatedCircuitCount}
-            </Typography>
-          </div>
-
-          <VerticalDivider />
-
-          <div className="flex flex-col justify-center gap-1">
-            <Typography variant="body2" fw="normal">
-              Member Since
-            </Typography>
-
-            <Typography variant="h5" fw="bold">
-              {creationTimestampString}
-            </Typography>
-          </div>
-
-          <VerticalDivider />
-
-          <div className="flex flex-col justify-center gap-1">
-            <Typography variant="body2" fw="normal">
-              Links
-            </Typography>
-
-            <Typography variant="h5" fw="bold">
-              Dec 11, 2023
-            </Typography>
-          </div>
-
-          <VerticalDivider />
-
-          <div className="flex flex-col justify-center gap-1">
-            <Typography variant="body2" fw="normal">
-              Short Bio
-            </Typography>
-
-            <Typography variant="h5" fw="bold">
-              -
-            </Typography>
-          </div>
-
-          <LargeSquareAvatar />
+          <Typography variant="h5" fw="bold">
+            {activatedCircuitCount === 0 ? '-' : activatedCircuitCount}
+          </Typography>
         </div>
+
+        <VerticalDivider />
+
+        <div className="flex flex-col justify-center gap-1">
+          <Typography variant="body2" fw="normal">
+            Member Since
+          </Typography>
+
+          <Typography variant="h5" fw="bold">
+            {creationTimestampString}
+          </Typography>
+        </div>
+
+        <VerticalDivider />
+
+        <div className="flex flex-col justify-center gap-1">
+          <Typography variant="body2" fw="normal">
+            Links
+          </Typography>
+
+          <div className="flex gap-2">
+            <SocialChip
+              title="View GitHub profile"
+              href={`https://github.com/${githubUsername}`}
+              Logo={GithubFill}
+            />
+
+            {twitterHandle !== undefined && (
+              <SocialChip
+                title="View X profile"
+                href={`https://twitter.com/${twitterHandle}`}
+                Logo={TwitterFill}
+              />
+            )}
+
+            {website !== undefined && (
+              <SocialChip
+                title="View website"
+                href={website}
+                Logo={GlobalLine}
+              />
+            )}
+          </div>
+        </div>
+
+        <VerticalDivider />
+
+        <div className="flex flex-col justify-center gap-1">
+          <Typography variant="body2" fw="normal">
+            Short Bio
+          </Typography>
+
+          <Typography variant="h5" fw="bold">
+            -
+          </Typography>
+        </div>
+
+        <LargeSquareAvatar />
       </Card>
 
       <Card className="p-6 flex gap-4 rounded-2xl space-y-0">
@@ -102,8 +130,25 @@ export const DashboardOverviewTab: FC = () => {
 /** @internal */
 const VerticalDivider: FC = () => {
   return (
-    <div className="inline-block px-7">
-      <div className="border-r dark:border-mono-160 min-h-[57px] h-full" />
+    <div className="inline-block py-7 md:py-0 md:px-7">
+      <div className="dark:border-mono-160 border-t w-full md:border-r md:min-h-[57px] md:h-full" />
     </div>
+  );
+};
+
+type SocialChipProps = {
+  href: string;
+  title: string;
+  Logo: ComponentType<IconBase>;
+};
+
+/** @internal */
+const SocialChip: FC<SocialChipProps> = ({ Logo, href, title }) => {
+  return (
+    <Link target="_blank" title={title} href={href}>
+      <SmallChip color="grey">
+        <Logo size="md" />
+      </SmallChip>
+    </Link>
   );
 };
