a10a0f19c4a7b94f67e316cd32306f5d2abc2299
yurixander <101931215+yurixander@users.noreply.github.com>
2024-01-04 04:44:45 -0500

    fix(zk-explorer): Do not use `NODE_ENV`

--- apps/zk-explorer/constants/index.ts
+++ apps/zk-explorer/constants/index.ts
@@ -2,5 +2,7 @@ export const GITHUB_LANGUAGE_COLORS_API_URL =
   'https://raw.githubusercontent.com/ozh/github-colors/master/colors.json';
 
 export const API_PREFIX = '/api';
-export const IS_DEBUG_MODE = process.env.NODE_ENV === 'development';
 export const ITEMS_PER_PAGE = 12;
+
+// TODO: Use a better way to detect debug mode. `NODE_ENV` seems to be clashing with Next.js.
+export const IS_DEBUG_MODE = true;
