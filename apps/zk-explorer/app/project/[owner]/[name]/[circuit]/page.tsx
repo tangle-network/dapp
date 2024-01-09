d2ba017513c124f996fde86a311133b3632e1f18
yurixander <101931215+yurixander@users.noreply.github.com>
2024-01-08 22:21:48 -0500

    feat(zk-explorer): Make plans selectable

--- apps/zk-explorer/app/project/[owner]/[name]/[circuit]/page.tsx
+++ apps/zk-explorer/app/project/[owner]/[name]/[circuit]/page.tsx
@@ -22,7 +22,8 @@ import {
   shortenHex,
 } from '@webb-tools/webb-ui-components';
 import { WEBB_DOCS_URL } from '@webb-tools/webb-ui-components/constants';
-import { FC, useCallback, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FC, useCallback, useMemo, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { FeedbackCard } from '../../../../../components/FeedbackCard';
 import { Header } from '../../../../../components/Header';
@@ -189,37 +190,45 @@ const StepTrackerSidebar: FC = () => {
   );
 };
 
+enum Plan {
+  Free = 'Free',
+  Pro = 'Pro',
+  Enterprise = 'Enterprise',
+}
+
+enum Location {
+  UsWest = 'US West',
+  EuCentral = 'EU Central',
+  AsiaEast = 'Asia East',
+}
+
+enum ColumnKey {
+  IsChecked = 'isChecked',
+  Identity = 'identity',
+  Location = 'location',
+  SlashingIncidents = 'slashingIncidents',
+  Uptime = 'uptime',
+}
+
+type RowData = {
+  [ColumnKey.IsChecked]: boolean;
+  [ColumnKey.Identity]: string;
+  [ColumnKey.Location]: Location;
+  [ColumnKey.SlashingIncidents]: number;
+  [ColumnKey.Uptime]: number;
+};
+
 /** @internal */
 const StepCards: FC = () => {
   const [step, setStep] = useState(1);
+  const [selectedPlan, setSelectedPlan] = useState<Plan | null>(null);
+  const router = useRouter();
 
   const handleNextStep = useCallback(() => {
     setStep((current) => current + 1);
   }, []);
 
-  enum Location {
-    UsWest = 'US West',
-    EuCentral = 'EU Central',
-    AsiaEast = 'Asia East',
-  }
-
-  enum ColumnKey {
-    IsChecked = 'isChecked',
-    Identity = 'identity',
-    Location = 'location',
-    SlashingIncidents = 'slashingIncidents',
-    Uptime = 'uptime',
-  }
-
-  type RowData = {
-    [ColumnKey.IsChecked]: boolean;
-    [ColumnKey.Identity]: string;
-    [ColumnKey.Location]: Location;
-    [ColumnKey.SlashingIncidents]: number;
-    [ColumnKey.Uptime]: number;
-  };
-
-  const getColumnKeyAsTitle = (key: ColumnKey): string => {
+  const getColumnKeyAsTitle = useCallback((key: ColumnKey): string => {
     switch (key) {
       case ColumnKey.IsChecked:
         return 'Select';
@@ -232,60 +241,73 @@ const StepCards: FC = () => {
       case ColumnKey.Uptime:
         return 'Uptime';
     }
-  };
+  }, []);
 
   const columnHelper = createColumnHelper<RowData>();
 
-  const columns: ColumnDef<RowData, any>[] = [
-    columnHelper.accessor(ColumnKey.IsChecked, {
-      header: getColumnKeyAsTitle(ColumnKey.IsChecked),
-      cell: (props) => <CheckBox isChecked={true} />,
-    }),
-    columnHelper.accessor(ColumnKey.Identity, {
-      header: getColumnKeyAsTitle(ColumnKey.Identity),
-      cell: (props) => (
-        <IdentityItem address="0x123456789" avatarUrl="./pending.png" />
-      ),
-    }),
-    columnHelper.accessor(ColumnKey.Location, {
-      header: getColumnKeyAsTitle(ColumnKey.Location),
-      cell: (props) => props.getValue(),
-    }),
-    columnHelper.accessor(ColumnKey.SlashingIncidents, {
-      header: getColumnKeyAsTitle(ColumnKey.SlashingIncidents),
-      cell: (props) => props.getValue(),
-    }),
-    columnHelper.accessor(ColumnKey.Uptime, {
-      header: getColumnKeyAsTitle(ColumnKey.Uptime),
-      cell: (props) => (
-        <Progress suffixLabel="%" className="max-h-7" size="lg" value={99.3} />
-      ),
-    }),
-  ];
+  const columns: ColumnDef<RowData, any>[] = useMemo(
+    () => [
+      columnHelper.accessor(ColumnKey.IsChecked, {
+        header: getColumnKeyAsTitle(ColumnKey.IsChecked),
+        cell: (props) => (
+          <CheckBox inputProps={{ readOnly: true }} isChecked={true} />
+        ),
+      }),
+      columnHelper.accessor(ColumnKey.Identity, {
+        header: getColumnKeyAsTitle(ColumnKey.Identity),
+        cell: (props) => (
+          <IdentityItem address="0x123456789" avatarUrl="./pending.png" />
+        ),
+      }),
+      columnHelper.accessor(ColumnKey.Location, {
+        header: getColumnKeyAsTitle(ColumnKey.Location),
+        cell: (props) => props.getValue(),
+      }),
+      columnHelper.accessor(ColumnKey.SlashingIncidents, {
+        header: getColumnKeyAsTitle(ColumnKey.SlashingIncidents),
+        cell: (props) => props.getValue(),
+      }),
+      columnHelper.accessor(ColumnKey.Uptime, {
+        header: getColumnKeyAsTitle(ColumnKey.Uptime),
+        cell: (props) => (
+          <Progress
+            suffixLabel="%"
+            className="max-h-7"
+            size="lg"
+            value={99.3}
+          />
+        ),
+      }),
+    ],
+    [columnHelper, getColumnKeyAsTitle]
+  );
 
-  const data: RowData[] = [
-    {
-      [ColumnKey.IsChecked]: false,
-      [ColumnKey.Identity]: '0x123...456',
-      [ColumnKey.Location]: Location.UsWest,
-      [ColumnKey.SlashingIncidents]: 0,
-      [ColumnKey.Uptime]: 100,
-    },
-    {
-      [ColumnKey.IsChecked]: false,
-      [ColumnKey.Identity]: '0x123...456',
-      [ColumnKey.Location]: Location.EuCentral,
-      [ColumnKey.SlashingIncidents]: 0,
-      [ColumnKey.Uptime]: 100,
-    },
-    {
-      [ColumnKey.IsChecked]: false,
-      [ColumnKey.Identity]: '0x123...456',
-      [ColumnKey.Location]: Location.AsiaEast,
-      [ColumnKey.SlashingIncidents]: 0,
-      [ColumnKey.Uptime]: 100,
-    },
-  ];
+  const data: RowData[] = useMemo(
+    () => [
+      {
+        [ColumnKey.IsChecked]: false,
+        [ColumnKey.Identity]: '0x123...456',
+        [ColumnKey.Location]: Location.UsWest,
+        [ColumnKey.SlashingIncidents]: 0,
+        [ColumnKey.Uptime]: 100,
+      },
+      {
+        [ColumnKey.IsChecked]: false,
+        [ColumnKey.Identity]: '0x123...456',
+        [ColumnKey.Location]: Location.EuCentral,
+        [ColumnKey.SlashingIncidents]: 0,
+        [ColumnKey.Uptime]: 100,
+      },
+      {
+        [ColumnKey.IsChecked]: false,
+        [ColumnKey.Identity]: '0x123...456',
+        [ColumnKey.Location]: Location.AsiaEast,
+        [ColumnKey.SlashingIncidents]: 0,
+        [ColumnKey.Uptime]: 100,
+      },
+    ],
+    []
+  );
 
   const mpcParticipantsTableProps = useReactTable({
     columns,
@@ -296,6 +318,23 @@ const StepCards: FC = () => {
       fuzzy: (value, search) => false,
     },
   });
+
+  const handlePlanSelection = useCallback((plan: Plan) => {
+    // The enterprise plan requires a contact form,
+    // so it is handled separately.
+    if (plan === Plan.Enterprise) {
+      setSelectedPlan(null);
+
+      // TODO: Need to change this to a contact form when available.
+      const CONTACT_HREF = 'mailto:hello@webb.tools';
+
+      window.open(CONTACT_HREF, '_blank', 'noopener noreferrer');
+
+      return;
+    }
+
+    setSelectedPlan(plan);
+  }, []);
 
   return (
     <div className="flex flex-col gap-6 flex-grow">
@@ -323,10 +362,17 @@ const StepCards: FC = () => {
         <Table isPaginated={false} tableProps={mpcParticipantsTableProps} />
       </StepCard>
 
-      <StepCard title="Select Service Tier" number={5} onNext={handleNextStep}>
+      <StepCard
+        title="Select Service Tier"
+        number={5}
+        onNext={handleNextStep}
+        isLast
+      >
         <div className="flex flex-col gap-4">
           <ServiceTierCard
-            title="Free"
+            onSelect={handlePlanSelection}
+            plan={Plan.Free}
+            isSelected={selectedPlan === Plan.Free}
             description="New users, small-scale projects, or those wanting to test the waters."
             monthlyPrice={0}
             benefits={[
@@ -337,7 +383,9 @@ const StepCards: FC = () => {
           />
 
           <ServiceTierCard
-            title="Pro"
+            onSelect={handlePlanSelection}
+            plan={Plan.Pro}
+            isSelected={selectedPlan === Plan.Pro}
             description="Medium-scale projects, businesses, and frequent users."
             monthlyPrice={349}
             benefits={[
@@ -348,7 +396,8 @@ const StepCards: FC = () => {
           />
 
           <ServiceTierCard
-            title="Enterprise"
+            onSelect={handlePlanSelection}
+            plan={Plan.Enterprise}
             description="Large-scale projects, enterprises, and those needing premium features."
             monthlyPrice={null}
             benefits={[
@@ -367,10 +416,17 @@ type StepCardProps = {
   number: number;
   title: string;
   children: React.ReactNode;
+  isLast?: boolean;
   onNext: () => void;
 };
 
-const StepCard: FC<StepCardProps> = ({ number, title, children, onNext }) => {
+const StepCard: FC<StepCardProps> = ({
+  number,
+  title,
+  children,
+  isLast = false,
+  onNext,
+}) => {
   return (
     <CollapsibleCard title={`${number}. ${title}:`}>
       <div className="flex flex-col gap-4">
@@ -382,8 +438,8 @@ const StepCard: FC<StepCardProps> = ({ number, title, children, onNext }) => {
             Learn More
           </Button>
 
-          <Button isFullWidth onClick={onNext} variant="primary">
-            Next
+          <Button onClick={onNext} isFullWidth variant="primary">
+            {isLast ? 'Initialize Proof Generation' : 'Next'}
           </Button>
         </div>
       </div>
@@ -413,34 +469,38 @@ const CollapsibleCard: FC<CollapsibleCardProps> = ({ title, children }) => {
 };
 
 type ServiceTierCardProps = {
-  title: string;
+  plan: Plan;
   description: string;
   benefits: string[];
   monthlyPrice: number | null;
+  isSelected?: boolean;
+  onSelect: (plan: Plan) => void;
 };
 
 const ServiceTierCard: FC<ServiceTierCardProps> = ({
-  title,
+  plan,
   monthlyPrice,
   description,
   benefits,
+  isSelected = false,
+  onSelect,
 }) => {
   const billingText =
     monthlyPrice === null
       ? null
       : monthlyPrice > 0
-      ? `Billed ${monthlyPrice * 12}/year`
+      ? `Billed $${monthlyPrice * 12}/year`
       : 'Free';
 
   return (
-    <Card className="flex flex-col gap-1 space-y-0 rounded-2xl w-full dark:bg-mono-160">
+    <Card className="flex flex-col gap-1 space-y-0 rounded-2xl w-full dark:bg-mono-160 px-6 py-3">
       <Typography variant="h4" fw="bold" className="dark:text-mono-0">
-        {title}
+        {plan}
       </Typography>
 
       <Typography variant="body1">{description}</Typography>
 
-      {monthlyPrice !== undefined && (
+      {monthlyPrice !== null && (
         <>
           <div className="flex items-center">
             <Typography variant="h4" fw="bold">
@@ -448,25 +508,35 @@ const ServiceTierCard: FC<ServiceTierCardProps> = ({
             </Typography>
 
             <Typography variant="body1" fw="normal">
-              {billingText}
+              /mo
             </Typography>
           </div>
 
-          <Typography variant="body1" fw="normal"></Typography>
+          <Typography variant="body1" fw="normal">
+            {billingText}
+          </Typography>
         </>
       )}
 
-      <ul>
+      <ul className="list-disc pl-5">
         {benefits.map((benefit, index) => (
-          <li key={index}>{benefit}</li>
+          <li className="list-item" key={index}>
+            {benefit}
+          </li>
         ))}
       </ul>
 
       <Button
+        isDisabled={isSelected}
+        onClick={() => onSelect(plan)}
         className="ml-auto"
         variant={monthlyPrice === null ? 'secondary' : 'primary'}
       >
-        {monthlyPrice === null ? 'Contact Us' : 'Select Plan'}
+        {monthlyPrice === null
+          ? 'Contact Us'
+          : isSelected
+          ? 'Selected'
+          : 'Select Plan'}
       </Button>
     </Card>
   );
