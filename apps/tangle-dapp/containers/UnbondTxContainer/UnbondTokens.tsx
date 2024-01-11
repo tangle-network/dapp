bce36ef2849d737f2c20227d1ef53dc739591985
Pavan Soratur <pavanvsoratur@gmail.com>
2024-01-10 17:18:34 -0800

    fix: unbond tx container ui

--- apps/tangle-dapp/containers/UnbondTxContainer/UnbondTokens.tsx
+++ apps/tangle-dapp/containers/UnbondTxContainer/UnbondTokens.tsx
@@ -1,42 +1,16 @@
-import {
-  Button,
-  CopyWithTooltip,
-  InputField,
-  Typography,
-} from '@webb-tools/webb-ui-components';
+import { Button, InputField, Typography } from '@webb-tools/webb-ui-components';
 import { type FC } from 'react';
 
 import { UnbondTokensProps } from './types';
 
 const UnbondTokens: FC<UnbondTokensProps> = ({
-  nominatorAddress,
   amountToUnbond,
   setAmountToUnbond,
   amountToUnbondError,
   remainingStakedBalanceToUnbond,
 }) => {
   return (
-    <div className="flex flex-col gap-9">
-      {/* Account */}
-      <InputField.Root>
-        <InputField.Input
-          title="Account"
-          isAddressType={true}
-          value={nominatorAddress}
-          type="text"
-          readOnly
-        />
-
-        <InputField.Slot>
-          <CopyWithTooltip
-            textToCopy={nominatorAddress}
-            isButton={false}
-            iconSize="lg"
-            className="text-mono-160 dark:text-mono-80 cursor-pointer"
-          />
-        </InputField.Slot>
-      </InputField.Root>
-
+    <div className="flex flex-col gap-4">
       {/* Amount */}
       <InputField.Root error={amountToUnbondError}>
         <InputField.Input
@@ -63,8 +37,12 @@ const UnbondTokens: FC<UnbondTokensProps> = ({
 
       <Typography variant="body1" fw="normal">
         Once unbonding, you must wait certain number of eras for your funds to
-        become available. You can check the remaining eras for your funds to
-        become available in the Unbonding tTNT tooltip.
+        become available.
+      </Typography>
+
+      <Typography variant="body1" fw="normal">
+        You can check the remaining eras for your funds to become available in
+        the Unbonding tTNT tooltip.
       </Typography>
     </div>
   );
