6ea0bd99f6f363ca34697dcea93b35aa8792bab5
yurixander <101931215+yurixander@users.noreply.github.com>
2024-01-12 02:36:05 -0500

    perf(zk-explorer): Load header image using Next.js' `Image` for optimized performance

--- apps/zk-explorer/app/page.tsx
+++ apps/zk-explorer/app/page.tsx
@@ -1,4 +1,5 @@
 import { Typography } from '@webb-tools/webb-ui-components';
+import Image from 'next/image';
 import { HeaderControls } from '../components/HeaderControls';
 import { HomepageInteractiveContents } from '../components/HomepageInteractiveContents';
 import { OverlayMask } from '../components/OverlayMask';
@@ -7,10 +8,16 @@ export default function Index() {
   return (
     <main className="flex flex-col gap-6">
       {/* Custom, landing-page-only header */}
-      <header
-        className="relative pb-12 bg-cover bg-center rounded-b-xl"
-        style={{ backgroundImage: 'url(/header-bg.jpg)' }}
-      >
+      <header className="relative pb-12 bg-cover bg-center rounded-b-xl overflow-hidden">
+        {/* Background image, this is method preferred because of Next.js' image optimization */}
+        <Image
+          className="absolute inset-0 w-full h-full object-cover object-center"
+          src="/header-bg.jpg"
+          width={1210}
+          height={300}
+          alt="A colorful vector illustration of blocks"
+        />
+
         {/* Background image mask */}
         <OverlayMask opacity={0.2} />
 
