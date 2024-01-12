8b809b4f18d357dae54c756f6d65d95cc0b786a7
yurixander <101931215+yurixander@users.noreply.github.com>
2024-01-11 20:31:48 -0500

    perf(zk-explorer): Drastically reduce header background graphic file size

--- apps/zk-explorer/app/page.tsx
+++ apps/zk-explorer/app/page.tsx
@@ -7,10 +7,9 @@ export default function Index() {
   return (
     <main className="flex flex-col gap-6">
       {/* Custom, landing-page-only header */}
-      {/* TODO: Need to figure out a way to significantly reduce the size of the background image (it's 1mb right now). Should be <=100kb for optimal SEO. One way would be to trim it to what is actually used in terms of max proportions visible to the user, or reduce its resolution. */}
       <header
         className="relative pb-12 bg-cover bg-center rounded-b-xl"
-        style={{ backgroundImage: 'url(/header-bg.png)' }}
+        style={{ backgroundImage: 'url(/header-bg.jpg)' }}
       >
         {/* Background image mask */}
         <OverlayMask opacity={0.2} />
