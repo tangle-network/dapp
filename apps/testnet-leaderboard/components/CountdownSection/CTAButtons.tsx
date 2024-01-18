545bb27f9e284c89d7469ee384b19c8dc5d64f9d
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-01-18 19:24:35 +0700

    chore: add Join Discord button

--- apps/testnet-leaderboard/components/CountdownSection/CTAButtons.tsx
+++ apps/testnet-leaderboard/components/CountdownSection/CTAButtons.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Button from '@webb-tools/webb-ui-components/components/buttons/Button';
+import { SOCIAL_URLS_RECORD } from '@webb-tools/webb-ui-components/constants';
 
 import { GUIDELINES_URL } from '../../constants';
 
@@ -9,6 +10,14 @@ const CTAButtons = () => {
     <div className="flex items-center justify-center gap-4">
       <Button href={GUIDELINES_URL} target="_blank" className="px-5 md:px-9">
         View Guidelines
+      </Button>
+      <Button
+        href={SOCIAL_URLS_RECORD.discord}
+        target="_blank"
+        variant="secondary"
+        className="px-5 md:px-9"
+      >
+        Join Discord
       </Button>
     </div>
   );
