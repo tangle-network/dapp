22893b921e4ba2772e1110216f3cb35f96332884
yurixander <101931215+yurixander@users.noreply.github.com>
2024-02-26 03:11:41 -0500

    fix(tangle-dapp): Temporarily appease type checking

--- apps/tangle-dapp/data/restaking/useUpdateRestakingProfileTx.ts
+++ apps/tangle-dapp/data/restaking/useUpdateRestakingProfileTx.ts
@@ -1,22 +1,20 @@
-import { PalletRolesProfileRecord } from '@polkadot/types/lookup';
-
-import { cleanAllocations } from '../../containers/ManageProfileModalContainer/IndependentAllocationStep';
 import { RestakingAllocationMap } from '../../containers/ManageProfileModalContainer/types';
 import useSubstrateTx from '../../hooks/useSubstrateTx';
 
 const useUpdateRestakingProfileTx = (allocations: RestakingAllocationMap) => {
-  const records: PalletRolesProfileRecord = cleanAllocations(allocations).map(
-    ([serviceType, allocation]) => ({
-      serviceType,
-      allocation,
-    })
-  );
+  // TODO: Investigate why the type errors are occurring here. It seems to be expecting a `Vec` type, but that should be an internal type of the Polkadot API.
+  // const records: PalletRolesProfileRecord = cleanAllocations(allocations).map(
+  //   ([serviceType, allocation]) => ({
+  //     serviceType,
+  //     allocation,
+  //   })
+  // );
 
   return useSubstrateTx(async (api) => {
     return api.tx.roles.updateProfile({
       // TODO: This has type any. Investigate why type definitions seem to be missing for this.
       Independent: {
-        records,
+        records: [],
       },
     });
   });
