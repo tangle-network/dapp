8e8666ca09203ac61f738099491483323412b71c
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-03-05 17:10:02 +0700

    feat: add Shared Role Distribution Chart

--- apps/tangle-dapp/app/nomination/[validatorAddress]/page.tsx
+++ apps/tangle-dapp/app/nomination/[validatorAddress]/page.tsx
@@ -2,6 +2,7 @@ import { isAddress } from '@polkadot/util-crypto';
 import { notFound } from 'next/navigation';
 
 import NodeSpecificationsTable from './NodeSpecificationsTable';
+import RoleDistributionCard from './RoleDistributionCard';
 import ServiceTableTabs from './ServiceTableTabs';
 import ValidatorOverviewCard from './ValidatorOverviewCard';
 
@@ -18,12 +19,15 @@ export default function ValidatorDetails({
 
   return (
     <div className="my-5 space-y-10">
-      <div className="flex flex-col lg:flex-row gap-5">
+      <div className="flex flex-col lg:flex-row gap-5 items-stretch">
         <ValidatorOverviewCard
           validatorAddress={validatorAddress}
           className="flex-1"
         />
-        <div className="flex-1"></div>
+        <RoleDistributionCard
+          validatorAddress={validatorAddress}
+          className="flex-1"
+        />
       </div>
 
       <NodeSpecificationsTable validatorAddress={validatorAddress} />
