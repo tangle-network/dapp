4de2a4008dd227c362890ea4361baa3651bf6309
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-03-12 12:57:44 +0700

    chore: format code

--- apps/tangle-dapp/constants/faq.tsx
+++ apps/tangle-dapp/constants/faq.tsx
@@ -1,14 +1,14 @@
-import { TESTNET_LEADERBOARD_URL } from "@webb-tools/webb-ui-components/constants";
-import type { FAQItem } from "@webb-tools/webb-ui-components/containers/FAQSection";
-const EDGEWARE_URL = "https://www.edgeware.io/";
-const ALLOCATION_DOC = "https://docs.tangle.tools/docs/tokenomics/allocation/";
+import { TESTNET_LEADERBOARD_URL } from '@webb-tools/webb-ui-components/constants';
+import type { FAQItem } from '@webb-tools/webb-ui-components/containers/FAQSection';
+const EDGEWARE_URL = 'https://www.edgeware.io/';
+const ALLOCATION_DOC = 'https://docs.tangle.tools/docs/tokenomics/allocation/';
 
 const faq = [
   {
-    question: "How can I become qualified for the TNT airdrop?",
+    question: 'How can I become qualified for the TNT airdrop?',
     answer: (
       <>
-        {"To qualify for the airdrop, you may engage in "}
+        {'To qualify for the airdrop, you may engage in '}
         <a
           href={TESTNET_LEADERBOARD_URL}
           target="_blank"
@@ -17,13 +17,13 @@ const faq = [
           Tangle Incentivized Testnet Campaign
         </a>
         {
-          ". The primary way to participate is by running a validator node on the testnet, which earns you points and a position on the testnet leaderboard.\n\nAdditionally, if you were an "
+          '. The primary way to participate is by running a validator node on the testnet, which earns you points and a position on the testnet leaderboard.\n\nAdditionally, if you were an '
         }
         <a href={EDGEWARE_URL} target="_blank" rel="noopener noreferrer">
           Edgeware
         </a>
         {
-          " genesis or 2023 snapshot participant, you are also eligible to qualify for the airdrop. DOT Validators are also eligible from a March 2024 snapshot. For the full details on the allocation, "
+          ' genesis or 2023 snapshot participant, you are also eligible to qualify for the airdrop. DOT Validators are also eligible from a March 2024 snapshot. For the full details on the allocation, '
         }
         <a href={ALLOCATION_DOC} target="_blank" rel="noopener noreferrer">
           see our documentation.
@@ -32,11 +32,11 @@ const faq = [
     ),
   },
   {
-    question: "How many TNT tokens are allocated for the airdrop?",
+    question: 'How many TNT tokens are allocated for the airdrop?',
     answer: (
       <>
         {
-          "A total of 5 million TNT tokens are allocated for the airdrop, amounting to 5% of the total genesis amount. These are distributed among several groups: 2% for the "
+          'A total of 5 million TNT tokens are allocated for the airdrop, amounting to 5% of the total genesis amount. These are distributed among several groups: 2% for the '
         }
         <a
           href={TESTNET_LEADERBOARD_URL}
@@ -45,16 +45,16 @@ const faq = [
         >
           Tangle Testnet Leaderboard
         </a>
-        {" participants, 1% for DOT Validators, 1% for "}
+        {' participants, 1% for DOT Validators, 1% for '}
         <a href={EDGEWARE_URL} target="_blank" rel="noopener noreferrer">
           Edgeware
         </a>
-        {" Genesis participants, and 1% for "}
+        {' Genesis participants, and 1% for '}
         <a href={EDGEWARE_URL} target="_blank" rel="noopener noreferrer">
           Edgeware
         </a>
         {
-          " 2023 Snapshot participants. For the full details on the allocation, "
+          ' 2023 Snapshot participants. For the full details on the allocation, '
         }
         <a href={ALLOCATION_DOC} target="_blank" rel="noopener noreferrer">
           see our documentation.
@@ -63,11 +63,11 @@ const faq = [
     ),
   },
   {
-    question: "How are claimed airdrop tokens distributed and vested?",
+    question: 'How are claimed airdrop tokens distributed and vested?',
     answer: (
       <>
         {
-          "Upon claiming, 5% of your airdropped TNT tokens will be immediately liquid. The remaining 95% will be locked for 2 years with a 1-month cliff, after which tokens will vest linearly on a monthly basis.\n\nThis means participants will receive a portion of their vested tokens each month until the full amount has been distributed over the two-year period. For the full details on the allocation, "
+          'Upon claiming, 5% of your airdropped TNT tokens will be immediately liquid. The remaining 95% will be locked for 2 years with a 1-month cliff, after which tokens will vest linearly on a monthly basis.\n\nThis means participants will receive a portion of their vested tokens each month until the full amount has been distributed over the two-year period. For the full details on the allocation, '
         }
         <a href={ALLOCATION_DOC} target="_blank" rel="noopener noreferrer">
           see our documentation.
--- apps/testnet-leaderboard/components/FAQSection/index.tsx
+++ apps/testnet-leaderboard/components/FAQSection/index.tsx
@@ -1,25 +1,25 @@
-import { Typography } from "@webb-tools/webb-ui-components";
-import { WEBB_DOC_ROUTES_RECORD } from "@webb-tools/webb-ui-components/constants";
-import FAQContainer from "@webb-tools/webb-ui-components/containers/FAQSection";
-import populateDocsUrl from "@webb-tools/webb-ui-components/utils/populateDocsUrl";
+import { Typography } from '@webb-tools/webb-ui-components';
+import { WEBB_DOC_ROUTES_RECORD } from '@webb-tools/webb-ui-components/constants';
+import FAQContainer from '@webb-tools/webb-ui-components/containers/FAQSection';
+import populateDocsUrl from '@webb-tools/webb-ui-components/utils/populateDocsUrl';
 
 const faqItems = [
   {
-    question: "What is the Tangle Testnet Incentivization Campaign??",
+    question: 'What is the Tangle Testnet Incentivization Campaign??',
     answer:
-      "The leaderboard campaign incentivizes early validators to run Tangle Network nodes in return for a portion of genesis event tokens, and tracks their duration to reward them proportionally",
+      'The leaderboard campaign incentivizes early validators to run Tangle Network nodes in return for a portion of genesis event tokens, and tracks their duration to reward them proportionally',
   },
   {
-    question: "How long will the Tangle incentivized testnet run for?",
+    question: 'How long will the Tangle incentivized testnet run for?',
     answer:
-      "The incentivized phase of the Tangle Network testnet will conclude April 9th, with the launch of mainnet on April 10.",
+      'The incentivized phase of the Tangle Network testnet will conclude April 9th, with the launch of mainnet on April 10.',
   },
   {
-    question: "What activities are rewarded with points?",
+    question: 'What activities are rewarded with points?',
     answer: (
       <a
         href={populateDocsUrl(
-          WEBB_DOC_ROUTES_RECORD["tangle-network"].overview
+          WEBB_DOC_ROUTES_RECORD['tangle-network'].overview
         )}
         target="_blank"
         rel="noopener noreferrer"
@@ -31,9 +31,9 @@ const faqItems = [
     ),
   },
   {
-    question: "What rewards can I expect by participating?",
+    question: 'What rewards can I expect by participating?',
     answer:
-      "Leaderboard participants will recieve a portion of genesis tokens via the Tangle Network Token Airdrop.",
+      'Leaderboard participants will recieve a portion of genesis tokens via the Tangle Network Token Airdrop.',
   },
 ];
 
