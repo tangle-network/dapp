22d1b20467bfb99f4dc7f963751edde3867735c2
yurixander <101931215+yurixander@users.noreply.github.com>
2024-03-26 21:27:03 -0400

    feat(tangle-dapp): Show vested amount of vesting schedules

--- apps/tangle-dapp/containers/BalancesTableContainer/BalanceCell.tsx
+++ apps/tangle-dapp/containers/BalancesTableContainer/BalanceCell.tsx
@@ -1,17 +1,22 @@
 import { BN } from '@polkadot/util';
+import StatusIndicator from '@webb-tools/icons/StatusIndicator';
 import {
   HiddenValue,
   SkeletonLoader,
+  Tooltip,
+  TooltipBody,
+  TooltipTrigger,
   Typography,
 } from '@webb-tools/webb-ui-components';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 
 import useActiveAccountAddress from '../../hooks/useActiveAccountAddress';
 import { formatTokenBalance } from '../../utils/polkadot/tokens';
 
 const BalanceCell: FC<{
   amount: BN | null;
-}> = ({ amount }) => {
+  status?: ReactNode;
+}> = ({ amount, status }) => {
   const activeAccountAddress = useActiveAccountAddress();
   const isAccountActive = activeAccountAddress !== null;
 
@@ -19,7 +24,7 @@ const BalanceCell: FC<{
     amount !== null ? formatTokenBalance(amount, true) : null;
 
   return (
-    <div className="flex flex-col justify-center p-3 gap-6 flex-grow">
+    <div className="flex flex-row p-3 gap-2">
       {formattedBalance !== null ? (
         // If the balance is not null, display it.
         <Typography variant="body1" fw="semibold">
@@ -34,6 +39,18 @@ const BalanceCell: FC<{
         <Typography variant="body1" fw="semibold">
           --
         </Typography>
+      )}
+
+      {status !== undefined && (
+        <Tooltip>
+          <TooltipTrigger className="cursor-default">
+            <StatusIndicator size={12} variant="info" />
+          </TooltipTrigger>
+
+          <TooltipBody className="break-normal max-w-[250px] text-center">
+            {status}
+          </TooltipBody>
+        </Tooltip>
       )}
     </div>
   );
--- apps/tangle-dapp/containers/BalancesTableContainer/LockedBalanceDetails/VestingScheduleBalances.tsx
+++ apps/tangle-dapp/containers/BalancesTableContainer/LockedBalanceDetails/VestingScheduleBalances.tsx
@@ -1,27 +1,60 @@
+import { BN, BN_ZERO } from '@polkadot/util';
 import { LockUnlockLineIcon } from '@webb-tools/icons';
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 
 import useVestingInfo from '../../../data/vesting/useVestingInfo';
 import useVestTx from '../../../data/vesting/useVestTx';
+import usePolkadotApiRx from '../../../hooks/usePolkadotApiRx';
 import { TxStatus } from '../../../hooks/useSubstrateTx';
+import { formatTokenBalance } from '../../../utils/polkadot';
 import BalanceAction from '../BalanceAction';
 import BalanceCell from '../BalanceCell';
 
 const VestingScheduleBalances: FC = () => {
   const { execute: executeVestTx, status: vestTxStatus } = useVestTx();
 
+  const { data: currentBlockNumber } = usePolkadotApiRx(
+    useCallback((api) => api.derive.chain.bestNumber(), [])
+  );
+
   const {
     schedulesOpt: vestingSchedulesOpt,
     hasClaimableTokens: hasClaimableVestingTokens,
   } = useVestingInfo();
 
-  return (
-    vestingSchedulesOpt !== null &&
-    !vestingSchedulesOpt.isNone &&
-    vestingSchedulesOpt.unwrap().map((schedule, index) => (
-      // TODO: For the balance, calculate how much is LEFT to claim, not the entire, original locked amount.
+  if (vestingSchedulesOpt === null || vestingSchedulesOpt.isNone) {
+    return;
+  }
+
+  return vestingSchedulesOpt.unwrap().map((schedule, index) => {
+    // Do not exceed the total locked amount in case that
+    // the ending block has already passed, and the difference
+    // between the current block number and the starting block
+    // number exceeds the total locked amount.
+    const amountAlreadyVested = currentBlockNumber?.gt(schedule.startingBlock)
+      ? BN.min(
+          schedule.locked,
+          currentBlockNumber.sub(schedule.startingBlock).mul(schedule.perBlock)
+        )
+      : BN_ZERO;
+
+    const remaining = schedule.locked.sub(amountAlreadyVested);
+    const allVested = remaining.isZero();
+
+    const status = allVested ? (
+      'All tokens have vested and are ready to be claimed.'
+    ) : amountAlreadyVested.isZero() ? (
+      'No tokens have vested yet.'
+    ) : (
+      <>
+        <strong>{formatTokenBalance(amountAlreadyVested)}</strong> has already
+        vested, with <strong>{formatTokenBalance(remaining)}</strong> remaining.
+      </>
+    );
+
+    return (
       <div key={index} className="flex flex-row justify-between items-center">
-        <BalanceCell amount={schedule.locked} />
+        <BalanceCell amount={schedule.locked} status={status} />
 
         <BalanceAction
           Icon={LockUnlockLineIcon}
@@ -43,8 +76,8 @@ const VestingScheduleBalances: FC = () => {
           }
         />
       </div>
-    ))
-  );
+    );
+  });
 };
 
 export default VestingScheduleBalances;
