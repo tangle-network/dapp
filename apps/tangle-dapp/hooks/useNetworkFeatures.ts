e760deb0fd200334e69211bf7e907c3013741c2f
yurixander <101931215+yurixander@users.noreply.github.com>
2024-04-01 20:15:52 -0400

    refactor(tangle-dapp): Make `useNetworkFeatures` hook stable

--- apps/tangle-dapp/hooks/useNetworkFeatures.ts
+++ apps/tangle-dapp/hooks/useNetworkFeatures.ts
@@ -1,12 +1,14 @@
+import { useMemo } from 'react';
+
 import { NETWORK_FEATURE_MAP } from '../constants/networks';
 import useNetworkStore from '../context/useNetworkStore';
 import { NetworkFeature } from '../types';
 
+// TODO: This should be adjusted with the following assumption in mind: There will only be 3 networks at most, and most features will be gate-kept (omitted) from mainnet specifically.
 const useNetworkFeatures = (): Readonly<NetworkFeature[]> => {
   const { network } = useNetworkStore();
-  const features = NETWORK_FEATURE_MAP[network.name];
 
-  return features ?? [];
+  return useMemo(() => NETWORK_FEATURE_MAP[network.name] ?? [], [network.name]);
 };
 
 export default useNetworkFeatures;
