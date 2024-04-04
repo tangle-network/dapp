e956ed6d371237731e33402d7ef3f96e5f77e51f
vutuanlinh2k2 <69841784+vutuanlinh2k2@users.noreply.github.com>
2024-04-05 03:00:49 +0700

    Show Network Dropdown, Key Stats and Validators Table even with no wallet connection (#2181)

--- apps/tangle-dapp/containers/WalletAndChainContainer/WalletAndChainContainer.tsx
+++ apps/tangle-dapp/containers/WalletAndChainContainer/WalletAndChainContainer.tsx
@@ -11,10 +11,15 @@ import {
   Typography,
   useCheckMobile,
 } from '@webb-tools/webb-ui-components';
+import dynamic from 'next/dynamic';
 import { type FC } from 'react';
 
 import { WalletDropdown } from '../../components';
-import NetworkSelectionButton from '../../components/NetworkSelector/NetworkSelectionButton';
+
+const NetworkSelectionButton = dynamic(
+  () => import('../../components/NetworkSelector/NetworkSelectionButton'),
+  { ssr: false }
+);
 
 const WalletAndChainContainer: FC = () => {
   const { activeAccount, activeWallet, loading, isConnecting } =
