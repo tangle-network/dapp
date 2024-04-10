1aba7fc4b296e927355c2f98f2816e49c3ad83f2
Pavan Soratur <pavanvsoratur@gmail.com>
2024-04-09 18:32:45 -0700

    chore(tangle-dapp): Update payouts table status (#2203)

--- apps/tangle-dapp/containers/NominationsPayoutsContainer/NominationsPayoutsContainer.tsx
+++ apps/tangle-dapp/containers/NominationsPayoutsContainer/NominationsPayoutsContainer.tsx
@@ -14,11 +14,12 @@ import {
 } from '@webb-tools/webb-ui-components';
 import { type FC, useEffect, useMemo, useRef, useState } from 'react';
 
-import { DelegatorTable, PayoutTable, TableStatus } from '../../components';
+import { DelegatorTable, TableStatus } from '../../components';
 import useNominations from '../../data/NominationsPayouts/useNominations';
 import usePayouts from '../../data/NominationsPayouts/usePayouts';
 import useIsFirstTimeNominator from '../../hooks/useIsFirstTimeNominator';
 import useLocalStorage, { LocalStorageKey } from '../../hooks/useLocalStorage';
+import useNetworkState from '../../hooks/useNetworkState';
 import useQueryParamKey from '../../hooks/useQueryParamKey';
 import { DelegationsAndPayoutsTab, Payout, QueryParamKey } from '../../types';
 import { evmToSubstrateAddress } from '../../utils';
@@ -50,6 +51,8 @@ const DelegationsPayoutsContainer: FC = () => {
   const [isDelegateModalOpen, setIsDelegateModalOpen] = useState(false);
   const [isPayoutAllModalOpen, setIsPayoutAllModalOpen] = useState(false);
   const [isUpdatePayeeModalOpen, setIsUpdatePayeeModalOpen] = useState(false);
+
+  const { network } = useNetworkState();
 
   const { value: queryParamsTab } = useQueryParamKey(
     QueryParamKey.DELEGATIONS_AND_PAYOUTS_TAB
@@ -90,23 +93,23 @@ const DelegationsPayoutsContainer: FC = () => {
     return delegatorsData.delegators.map((delegator) => delegator.address);
   }, [delegatorsData?.delegators]);
 
-  const { valueAfterMount: cachedPayouts } = useLocalStorage(
-    LocalStorageKey.Payouts,
-    true
-  );
+  // const { valueAfterMount: cachedPayouts } = useLocalStorage(
+  //   LocalStorageKey.Payouts,
+  //   true
+  // );
 
   const { valueAfterMount: cachedNominations } = useLocalStorage(
     LocalStorageKey.Nominations,
     true
   );
 
-  const fetchedPayouts = useMemo(() => {
-    if (payoutsData !== null) {
-      return payoutsData.payouts;
-    } else if (cachedPayouts) {
-      return cachedPayouts[substrateAddress] ?? [];
-    }
-  }, [cachedPayouts, payoutsData, substrateAddress]);
+  // const fetchedPayouts = useMemo(() => {
+  //   if (payoutsData !== null) {
+  //     return payoutsData.payouts;
+  //   } else if (cachedPayouts) {
+  //     return cachedPayouts[substrateAddress] ?? [];
+  //   }
+  // }, [cachedPayouts, payoutsData, substrateAddress]);
 
   const fetchedNominations = useMemo(() => {
     if (delegatorsData !== null) {
@@ -226,7 +229,7 @@ const DelegationsPayoutsContainer: FC = () => {
 
         {/* Payouts Table */}
         <TabContent value={DelegationsAndPayoutsTab.PAYOUTS} aria-disabled>
-          {!activeAccount ? (
+          {/* {!activeAccount ? (
             <TableStatus
               title="Wallet Not Connected"
               description="Connect your wallet to view and manage your staking details."
@@ -255,7 +258,19 @@ const DelegationsPayoutsContainer: FC = () => {
               pageSize={PAGE_SIZE}
               updateData={setUpdatedPayouts}
             />
-          )}
+          )} */}
+
+          <TableStatus
+            title="Payouts Coming Soon"
+            description="The payouts feature for EVM and Substrate users is in development for direct access here. Meanwhile, Substrate users can view and manage payouts via Polkadot Apps.
+            "
+            buttonText="Polkadot Apps"
+            buttonProps={{
+              href: `https://polkadot.js.org/apps/?rpc=${network.wsRpcEndpoint}#/staking/payout`,
+              target: '_blank',
+            }}
+            icon="🚧"
+          />
         </TabContent>
       </TableAndChartTabs>
 
