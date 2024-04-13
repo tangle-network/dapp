e88a5af8fc6a6fcb6fe8f736d19f55c67dc40cb3
yurixander <101931215+yurixander@users.noreply.github.com>
2024-04-13 06:27:07 -0400

    refactor(tangle-dapp): Remove legacy code

--- apps/tangle-dapp/utils/evm/client.ts
+++ apps/tangle-dapp/utils/evm/client.ts
@@ -1,13 +1,6 @@
-import { ensureHex } from '@webb-tools/dapp-config';
 import { chainsConfig } from '@webb-tools/dapp-config/chains/chain-config';
 import { PresetTypedChainId } from '@webb-tools/dapp-types/ChainId';
-import {
-  createPublicClient,
-  createWalletClient,
-  custom,
-  defineChain,
-  http,
-} from 'viem';
+import { createPublicClient, defineChain, http } from 'viem';
 
 const TANGLE_TESTNET_CONFIG = chainsConfig[PresetTypedChainId.TangleTestnetEVM];
 
@@ -18,18 +11,3 @@ export const evmPublicClient = createPublicClient({
   chain: TANGLE_TESTNET_CHAIN,
   transport: http(),
 });
-
-export const createEvmWalletClient = (accountAddress: string) => {
-  // TODO: Might need to handle this more gracefully. Or at least from the caller(s) (add description to the function to make it clear that it may fail and throw an error).
-  if (typeof window.ethereum === 'undefined') {
-    throw new Error(
-      'window.ethereum is undefined, but is required for EVM actions'
-    );
-  }
-
-  return createWalletClient({
-    chain: TANGLE_TESTNET_CHAIN,
-    account: ensureHex(accountAddress),
-    transport: custom(window.ethereum),
-  });
-};
