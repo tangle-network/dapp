fe7786719ebfe7f9cc88453c71cdeb6c4f2170d1
yurixander <101931215+yurixander@users.noreply.github.com>
2024-04-13 03:04:21 -0400

    feat(tangle-dapp): Create `useUpdateNominatorTx` hook

--- apps/tangle-dapp/containers/DelegateTxContainer/DelegateTxContainer.tsx
+++ apps/tangle-dapp/containers/DelegateTxContainer/DelegateTxContainer.tsx
@@ -10,7 +10,6 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
-  useWebbUI,
 } from '@webb-tools/webb-ui-components';
 import { WEBB_TANGLE_DOCS_STAKING_URL } from '@webb-tools/webb-ui-components/constants';
 import { type FC, useCallback, useState } from 'react';
@@ -20,27 +19,14 @@ import { PAYMENT_DESTINATION_OPTIONS as PAYEE_OPTIONS } from '../../constants';
 import useNetworkStore from '../../context/useNetworkStore';
 import usePaymentDestination from '../../data/NominatorStats/usePaymentDestinationSubscription';
 import useBondExtraTx from '../../data/staking/useBondExtraTx';
-import useBondTx from '../../data/staking/useBondTx';
 import useNominateTx from '../../data/staking/useNominateTx';
 import useSetPayeeTx from '../../data/staking/useSetPayeeTx';
-import useExecuteTxWithNotification from '../../hooks/useExecuteTxWithNotification';
+import useSetupNominatorTx from '../../data/staking/useSetupNominatorTx';
+import useUpdateNominatorTx from '../../data/staking/useUpdateNominatorTx';
 import useIsBondedOrNominating from '../../hooks/useIsBondedOrNominating';
 import useMaxNominationQuota from '../../hooks/useMaxNominationQuota';
 import useSubstrateAddress from '../../hooks/useSubstrateAddress';
 import { StakingPayee } from '../../types';
-import {
-  bondExtraTokens as bondExtraTokensEvm,
-  bondTokens as bondTokensEvm,
-  nominateValidators as nominateValidatorsEvm,
-  updatePaymentDestination as updatePaymentDestinationEvm,
-} from '../../utils/evm';
-import formatBnToDisplayAmount from '../../utils/formatBnToDisplayAmount';
-import {
-  bondExtraTokens as bondExtraTokensSubstrate,
-  bondTokens as bondTokensSubstrate,
-  nominateValidators as nominateValidatorsSubstrate,
-  updatePaymentDestination as updatePaymentDestinationSubstrate,
-} from '../../utils/polkadot';
 import SelectValidators from '../UpdateNominationsTxContainer/SelectValidators';
 import BondTokens from './BondTokens';
 import { DelegateTxContainerProps, DelegateTxSteps } from './types';
@@ -49,14 +35,12 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
   isModalOpen,
   setIsModalOpen,
 }) => {
-  const { notificationApi } = useWebbUI();
   const { activeAccount } = useWebContext();
   const maxNominationQuota = useMaxNominationQuota();
   const [amountToBond, setAmountToBond] = useState<BN | null>(null);
   const [selectedValidators, setSelectedValidators] = useState<string[]>([]);
-  const executeTx = useExecuteTxWithNotification();
   const activeSubstrateAddress = useSubstrateAddress();
-  const { rpcEndpoint, nativeTokenSymbol } = useNetworkStore();
+  const { nativeTokenSymbol } = useNetworkStore();
   const [payee, setPayee] = useState(StakingPayee.STAKED);
   const [hasAmountToBondError, setHasAmountToBondError] = useState(false);
 
@@ -105,10 +89,9 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
   } = useIsBondedOrNominating();
 
   // TODO: Need to change defaulting to empty/dummy strings to instead adhere to the type system. Ex. should be using `| null` instead.
-  const {
-    data: currentPaymentDestination,
-    error: currentPaymentDestinationError,
-  } = usePaymentDestination(activeSubstrateAddress ?? '0x0');
+  const { data: currentPaymentDestination } = usePaymentDestination(
+    activeSubstrateAddress ?? '0x0'
+  );
 
   const handleAmountToBondError = useCallback(
     (error: string | null) => {
@@ -116,20 +99,6 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
     },
     [setHasAmountToBondError]
   );
-
-  const canContinueToSelectDelegatesStep = !isBondedOrNominating
-    ? amountToBond !== null && amountToBond.gt(BN_ZERO)
-    : true && !hasAmountToBondError && payee && walletAddress !== '0x0'
-    ? true
-    : false;
-
-  const canContinueToAuthorizeTxStep =
-    selectedValidators.length > 0 && !isExceedingMaxNominationQuota
-      ? true
-      : false;
-
-  const canContinueToSignAndSubmitTx =
-    canContinueToSelectDelegatesStep && canContinueToAuthorizeTxStep;
 
   const closeModal = useCallback(() => {
     setIsSubmitAndSignTxLoading(false);
@@ -141,149 +110,43 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
     setDelegateTxStep(DelegateTxSteps.BOND_TOKENS);
   }, [setIsModalOpen]);
 
-  const executeDelegate: () => Promise<void> = useCallback(async () => {
-    try {
-      if (amountToBond === null) {
-        throw new Error('Amount to bond is required.');
-      }
-      const bondingAmount = +formatBnToDisplayAmount(amountToBond);
-      if (!isBondedOrNominating) {
-        await executeTx(
-          () => bondTokensEvm(walletAddress, bondingAmount, StakingPayee.STASH),
-          () =>
-            bondTokensSubstrate(
-              rpcEndpoint,
-              walletAddress,
-              amountToBond.toNumber(),
-              StakingPayee.STASH
-            ),
-          `Successfully bonded ${bondingAmount} ${nativeTokenSymbol}.`,
-          'Failed to bond tokens!'
-        );
-        await executeTx(
-          () => updatePaymentDestinationEvm(walletAddress, payee),
-          () =>
-            updatePaymentDestinationSubstrate(
-              rpcEndpoint,
-              walletAddress,
-              payee
-            ),
-          `Successfully updated payment destination to ${payee}.`,
-          'Failed to update payment destination!'
-        );
-        const hash = await executeTx(
-          () => nominateValidatorsEvm(walletAddress, selectedValidators),
-          () =>
-            nominateValidatorsSubstrate(
-              rpcEndpoint,
-              walletAddress,
-              selectedValidators
-            ),
-          `Successfully nominated ${selectedValidators.length} validators.`,
-          'Failed to nominate validators!'
-        );
-        setTxnStatus({ status: 'success', hash });
-      } else {
-        if (bondingAmount > 0) {
-          await executeTx(
-            () => bondExtraTokensEvm(walletAddress, bondingAmount),
-            () =>
-              bondExtraTokensSubstrate(
-                rpcEndpoint,
-                walletAddress,
-                bondingAmount
-              ),
-            `Successfully bonded ${bondingAmount} ${nativeTokenSymbol}.`,
-            'Failed to bond tokens!'
-          );
-        }
-        if (currentPaymentDestinationError)
-          notificationApi({
-            variant: 'error',
-            message: currentPaymentDestinationError.message,
-          });
-        const currPaymentDestination =
-          currentPaymentDestination?.value1 === 'Staked'
-            ? StakingPayee.STAKED
-            : StakingPayee.STASH;
-        if (currPaymentDestination !== payee) {
-          await executeTx(
-            () => updatePaymentDestinationEvm(walletAddress, payee),
-            () =>
-              updatePaymentDestinationSubstrate(
-                rpcEndpoint,
-                walletAddress,
-                payee
-              ),
-            `Successfully updated payment destination to ${payee}.`,
-            'Failed to update payment destination!'
-          );
-        }
-        const hash = await executeTx(
-          () => nominateValidatorsEvm(walletAddress, selectedValidators),
-          () =>
-            nominateValidatorsSubstrate(
-              rpcEndpoint,
-              walletAddress,
-              selectedValidators
-            ),
-          `Successfully nominated ${selectedValidators.length} validator(s).`,
-          'Failed to nominate validator(s)!'
-        );
-        setTxnStatus({ status: 'success', hash });
-      }
-      setTxnConfirmationModalIsOpen(true);
-    } catch {
-      setTxnStatus({ status: 'error', hash: '' });
-      setTxnConfirmationModalIsOpen(true);
-    }
-  }, [
-    amountToBond,
-    currentPaymentDestination?.value1,
-    currentPaymentDestinationError,
-    executeTx,
-    isBondedOrNominating,
-    nativeTokenSymbol,
-    notificationApi,
-    payee,
-    rpcEndpoint,
-    selectedValidators,
-    walletAddress,
-  ]);
-
-  const { execute: executeBondTx } = useBondTx();
   const { execute: executeSetPayeeTx } = useSetPayeeTx();
   const { execute: executeNominateTx } = useNominateTx();
   const { execute: executeBondExtraTx } = useBondExtraTx();
+  const { execute: executeSetupNominatorTx } = useSetupNominatorTx();
+  const { execute: executeUpdateNominatorTx } = useUpdateNominatorTx();
 
   const executeDelegate2 = useCallback(async () => {
     // Not yet ready.
     if (
-      executeBondTx === null ||
       executeSetPayeeTx === null ||
       executeNominateTx === null ||
       executeBondExtraTx === null ||
       isBondedOrNominating === null ||
+      executeSetupNominatorTx === null ||
+      executeUpdateNominatorTx === null ||
       amountToBond === null
     ) {
       return;
     }
 
+    // Setting up a new nominator.
     if (!isBondedOrNominating) {
-      (await executeBondTx({ amount: amountToBond, payee })) &&
-        (await executeSetPayeeTx({ payee })) &&
-        (await executeNominateTx({ validatorAddresses: selectedValidators }));
-    } else {
-      if (!amountToBond.isZero()) {
-        const bondingExtraSucceeded = await executeBondExtraTx({
-          amount: amountToBond,
-        });
-
-        // Stop early; something went wrong.
-        if (!bondingExtraSucceeded) {
-          return;
-        }
-      }
+      await executeSetupNominatorTx({
+        bondAmount: amountToBond,
+        payee,
+        nominees: selectedValidators,
+      });
+    }
+    // Increasing bond, changing payee, nominating new validators,
+    // or a combination of the three.
+    else {
+      // Only bond extra if the amount is greater than zero, and
+      // if the user provided an amount to bond.
+      const extraBondingAmount =
+        amountToBond.isZero() || amountToBond === null
+          ? undefined
+          : amountToBond;
 
       const currentPayee =
         currentPaymentDestination?.value1 === 'Staked'
@@ -291,23 +154,23 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
           : StakingPayee.STASH;
 
       // Update the payee if it has changed.
-      if (payee !== currentPayee) {
-        const setPayeeSucceeded = await executeSetPayeeTx({ payee });
+      const newPayee = currentPayee === payee ? undefined : payee;
 
-        if (!setPayeeSucceeded) {
-          return;
-        }
-      }
-
-      await executeNominateTx({ validatorAddresses: selectedValidators });
+      executeUpdateNominatorTx({
+        bondAmount: extraBondingAmount,
+        payee: newPayee,
+        // TODO: Only update nominees if they have changed. Use `_.isEqual` or similar to compare arrays.
+        nominees: selectedValidators,
+      });
     }
   }, [
     amountToBond,
     currentPaymentDestination?.value1,
     executeBondExtraTx,
-    executeBondTx,
     executeNominateTx,
     executeSetPayeeTx,
+    executeSetupNominatorTx,
+    executeUpdateNominatorTx,
     isBondedOrNominating,
     payee,
     selectedValidators,
@@ -332,6 +195,20 @@ const DelegateTxContainer: FC<DelegateTxContainerProps> = ({
   ) {
     return null;
   }
+
+  const canContinueToSelectDelegatesStep = !isBondedOrNominating
+    ? amountToBond !== null && amountToBond.gt(BN_ZERO)
+    : true && !hasAmountToBondError && payee && walletAddress !== '0x0'
+    ? true
+    : false;
+
+  const canContinueToAuthorizeTxStep =
+    selectedValidators.length > 0 && !isExceedingMaxNominationQuota
+      ? true
+      : false;
+
+  const canContinueToSignAndSubmitTx =
+    canContinueToSelectDelegatesStep && canContinueToAuthorizeTxStep;
 
   return (
     <>
--- apps/tangle-dapp/data/staking/useSetupNominatorTx.ts
+++ apps/tangle-dapp/data/staking/useSetupNominatorTx.ts
@@ -1,17 +1,18 @@
 import { BN } from '@polkadot/util';
-import { useEffect } from 'react';
 
 import useSubstrateTx from '../../hooks/useSubstrateTx';
 import { StakingPayee } from '../../types';
 
-export type NominatorSetupOptions = {
+export type NominationOptions = {
   bondAmount: BN;
   payee: StakingPayee;
   nominees: string[];
 };
 
 const useSetupNominatorTx = () => {
-  return useSubstrateTx<NominatorSetupOptions>(
+  // TODO: EVM support.
+
+  return useSubstrateTx<NominationOptions>(
     (api, _activeSubstrateAddress, context) => {
       return api.tx.utility.batch([
         api.tx.staking.bond(context.bondAmount, context.payee),
@@ -19,26 +20,6 @@ const useSetupNominatorTx = () => {
       ]);
     }
   );
-};
-
-// Usage
-export const useUsageExample = () => {
-  const { execute } = useSetupNominatorTx();
-  const aliceAddress = '5Gr...';
-
-  useEffect(() => {
-    // Transaction cannot be made yet; Api is not yet ready, or no active
-    // account, etc.
-    if (execute === null) {
-      return;
-    }
-
-    execute({
-      bondAmount: new BN(100),
-      payee: StakingPayee.STASH,
-      nominees: [aliceAddress],
-    });
-  }, [execute]);
 };
 
 export default useSetupNominatorTx;
