6839617e660a8a92fd4be288c36a7b1113aaa085
Pavan Soratur <pavanvsoratur@gmail.com>
2024-04-15 03:23:48 -0700

    chore: format code

--- apps/tangle-dapp/utils/polkadot/index.ts
+++ apps/tangle-dapp/utils/polkadot/index.ts
@@ -1,6 +1,6 @@
 export * from './api';
 export * from './bond';
-export * from './nominate'
+export * from './nominate';
 export * from './nominators';
 export * from './payout';
 export * from './tokens';
