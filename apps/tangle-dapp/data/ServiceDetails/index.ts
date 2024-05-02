63d9bfd5e8212b6f52bbeaac942a86a288ef1453
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-05-02 16:50:17 +0700

    chore: remove old folders from git track to fix CI

--- apps/tangle-dapp/data/ServiceDetails/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as getSigningRules } from './getSigningRules';
-export { default as useServiceInfoCard } from './useServiceInfoCard';
-export { default as useServiceJobs } from './useServiceJobs';
-export { default as useServiceParticipants } from './useServiceParticipants';
