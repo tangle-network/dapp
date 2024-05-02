193b654a039dd81f07b37858200a34b095022096
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-05-02 16:36:19 +0700

    chore: handle case-sensitive folder name

---
+++ apps/tangle-dapp/data/serviceDetails/index.ts
@@ -0,0 +1,4 @@
+export { default as getSigningRules } from './getSigningRules';
+export { default as useServiceInfoCard } from './useServiceInfoCard';
+export { default as useServiceJobs } from './useServiceJobs';
+export { default as useServiceParticipants } from './useServiceParticipants';
