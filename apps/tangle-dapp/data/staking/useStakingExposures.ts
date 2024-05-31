eab6f4fd38a5e5e87b4827b4326bef553815d0c8
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-05-31 09:37:32 +0700

    chore: fix useStakingExposures

--- apps/tangle-dapp/data/staking/useStakingExposures.ts
+++ apps/tangle-dapp/data/staking/useStakingExposures.ts
@@ -1,3 +1,6 @@
+import { Option, StorageKey, u32 } from '@polkadot/types';
+import { AccountId32 } from '@polkadot/types/interfaces';
+import { SpStakingPagedExposureMetadata } from '@polkadot/types/lookup';
 import { useCallback } from 'react';
 
 import useApiRx from '../../hooks/useApiRx';
@@ -8,13 +11,14 @@ const useStakingExposures = () => {
   // TODO: Also consider error from this hook.
   const { result: currentEra } = useCurrentEra();
 
-  const { result: exposures, ...other } = useApiRx(
+  const { result: exposures, ...other } = useApiRx<
+    [StorageKey<[u32, AccountId32]>, Option<SpStakingPagedExposureMetadata>][]
+  >(
     useCallback(
       (api) => {
         if (currentEra === null) {
           return null;
         }
-
         return api.query.staking.erasStakersOverview.entries(currentEra);
       },
       [currentEra]
