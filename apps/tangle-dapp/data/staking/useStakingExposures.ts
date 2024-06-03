c4f6a1bb3e690a8721172dfb7096db63f3e7c0c6
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-06-03 08:28:02 +0700

    chore: quick fix function type error

--- apps/tangle-dapp/data/staking/useStakingExposures.ts
+++ apps/tangle-dapp/data/staking/useStakingExposures.ts
@@ -1,5 +1,3 @@
-import '@webb-tools/tangle-substrate-types';
-
 import { Option, StorageKey, u32 } from '@polkadot/types';
 import { AccountId32 } from '@polkadot/types/interfaces';
 import { SpStakingPagedExposureMetadata } from '@polkadot/types/lookup';
