2a21b6e738ebbfbb4a96fb452c1541856cdea264
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-06-21 23:36:21 +0700

    feat(tangle-dapp): Animate chain list card with modal

---
+++ apps/tangle-dapp/app/restake/deposit/ChainList.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { useWebContext } from '@webb-tools/api-provider-environment/webb-context';
+import { calculateTypedChainId } from '@webb-tools/sdk-core/typed-chain-id';
+import ChainListCard from '@webb-tools/webb-ui-components/components/ListCard/ChainListCard';
+import type { ChainType } from '@webb-tools/webb-ui-components/components/ListCard/types';
+import { type ComponentProps, useCallback, useMemo } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { SUPPORTED_RESTAKE_DEPOSIT_TYPED_CHAIN_IDS } from '../../../constants/restake';
+import { useActions, useSourceTypedChainId } from '../../../stores/deposit';
+
+type Props = Partial<ComponentProps<typeof ChainListCard>>;
+
+const ChainList = ({ className, onClose, ...props }: Props) => {
+  const { activeChain, loading, apiConfig } = useWebContext();
+
+  const selectTypedChainId = useSourceTypedChainId();
+  const { updateSourceTypedChainId } = useActions();
+
+  const selectedChain = useMemo(
+    () => apiConfig.chains[selectTypedChainId],
+    [apiConfig.chains, selectTypedChainId],
+  );
+
+  const chains = useMemo(
+    () =>
+      SUPPORTED_RESTAKE_DEPOSIT_TYPED_CHAIN_IDS.map(
+        (typedChainId) =>
+          [typedChainId, apiConfig.chains[typedChainId]] as const,
+      )
+        .filter(([, chain]) => Boolean(chain))
+        .map(([typedChainId, chain]) => ({
+          typedChainId,
+          name: chain.name,
+          tag: chain.tag,
+          needSwitchWallet:
+            activeChain?.id !== chain.id &&
+            activeChain?.chainType !== chain.chainType,
+        })),
+    [activeChain?.chainType, activeChain?.id, apiConfig.chains],
+  );
+
+  const defaultCategory = useMemo<
+    ComponentProps<typeof ChainListCard>['defaultCategory']
+  >(() => {
+    return selectedChain?.tag ?? activeChain?.tag ?? 'test';
+  }, [activeChain?.tag, selectedChain?.tag]);
+
+  const handleChainChange = useCallback(
+    ({ typedChainId }: ChainType) => {
+      updateSourceTypedChainId(typedChainId);
+      onClose?.();
+    },
+    [onClose, updateSourceTypedChainId],
+  );
+
+  return (
+    <ChainListCard
+      chainType="source"
+      overrideTitleProps={{
+        variant: 'h4',
+      }}
+      chains={chains}
+      activeTypedChainId={
+        activeChain
+          ? calculateTypedChainId(activeChain.chainType, activeChain.id)
+          : undefined
+      }
+      defaultCategory={defaultCategory}
+      isConnectingToChain={loading}
+      onChange={handleChainChange}
+      overrideScrollAreaProps={{
+        className: 'h-[320px]',
+      }}
+      {...props}
+      onClose={onClose}
+      className={twMerge('p-0 dark:bg-transparent', className)}
+    />
+  );
+};
+
+ChainList.displayName = 'ChainList';
+
+export default ChainList;
--- apps/tangle-dapp/app/restake/deposit/page.tsx
+++ apps/tangle-dapp/app/restake/deposit/page.tsx
@@ -1,29 +1,79 @@
-import { ArrowRight } from '@webb-tools/icons/ArrowRight';
+'use client';
 
+import { Transition } from '@headlessui/react';
+import { ArrowRight } from '@webb-tools/icons/ArrowRight';
+import cx from 'classnames';
+import { useCallback, useState } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+
+import { DepositFormFields } from '../../../types/restake';
 import TabsList from '../TabsList';
 import ActionButton from './ActionButton';
+import ChainList from './ChainList';
 import DestChainInput from './DestChainInput';
 import SourceChainInput from './SourceChainInput';
 import TxDetails from './TxDetails';
 
+const onSubmit: SubmitHandler<DepositFormFields> = (data) => {
+  console.log('Submitted data: ', data);
+};
+
 export default function DepositPage() {
+  const [chainModalOpen, setChainModalOpen] = useState(false);
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DepositFormFields>({
+    mode: 'onBlur',
+  });
+
+  const openChainModal = useCallback(() => setChainModalOpen(true), []);
+  const closeChainModal = useCallback(() => setChainModalOpen(false), []);
+
   return (
-    <div className="flex flex-col space-y-4 grow">
-      <TabsList />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="relative overflow-hidden"
+    >
+      <div className="flex flex-col space-y-4 grow">
+        <TabsList />
 
-      <div className="space-y-2">
-        <SourceChainInput />
+        <div className="space-y-2">
+          <SourceChainInput
+            openChainModal={openChainModal}
+            register={register}
+            setValue={setValue}
+            amountError={errors.amount?.message}
+          />
 
-        <ArrowRight size="lg" className="mx-auto rotate-90" />
+          <ArrowRight size="lg" className="mx-auto rotate-90" />
 
-        <DestChainInput />
+          <DestChainInput />
+        </div>
+
+        <div className="flex flex-col justify-between gap-4 grow">
+          <TxDetails />
+
+          <ActionButton />
+        </div>
       </div>
 
-      <div className="flex flex-col justify-between gap-4 grow">
-        <TxDetails />
-
-        <ActionButton />
-      </div>
-    </div>
+      <Transition
+        as="div"
+        className="absolute h-full"
+        show={chainModalOpen}
+        enter={cx('transition-[top,_opacity] duration-150')}
+        enterFrom={cx('opacity-0 top-[250px]')}
+        enterTo={cx('opacity-100 top-0')}
+        leave={cx('transition-[top,_opacity] duration-150')}
+        leaveFrom={cx('opacity-100 top-0')}
+        leaveTo={cx('opacity-0 top-[250px]')}
+      >
+        <ChainList className="h-full" onClose={closeChainModal} />
+      </Transition>
+    </form>
   );
 }
