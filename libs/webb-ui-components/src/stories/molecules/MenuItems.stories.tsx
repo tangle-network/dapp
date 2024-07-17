2c7d0364b1082ff72faf5fa38267a78424014bf2
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-07-17 20:12:20 +0700

    chore: temp fix for MenuItems

---
+++ libs/webb-ui-components/src/stories/molecules/MenuItems.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownBody,
+} from '../../components/Dropdown';
+import { MenuItem } from '../../components/MenuItem';
+
+const meta: Meta<typeof MenuItem> = {
+  title: 'Design System/Molecules/MenuItems',
+  component: MenuItem,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MenuItem>;
+
+export const Default: Story = {
+  render: () => (
+    <Dropdown className="w-full">
+      <DropdownButton
+        size="sm"
+        className="w-full px-4 py-4 rounded-full"
+        label="hello"
+      />
+
+      <DropdownBody className="radix-side-top:mb-2 radix-side-bottom:mt-2 w-[var(--radix-dropdown-menu-trigger-width)]">
+        <ul>
+          {['abc', 'xyz'].map((str) => (
+            <MenuItem>{str}</MenuItem>
+          ))}
+        </ul>
+      </DropdownBody>
+    </Dropdown>
+  ),
+};
