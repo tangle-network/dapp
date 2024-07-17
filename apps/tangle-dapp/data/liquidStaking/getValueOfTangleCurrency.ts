9740adcaf26f4bdd10e545f8929652eacd92fad0
yurixander <101931215+yurixander@users.noreply.github.com>
2024-07-17 05:09:52 -0400

    feat(tangle-dapp): Progress on `useOngoingTimeUnits`

---
+++ apps/tangle-dapp/data/liquidStaking/getValueOfTangleCurrency.ts
@@ -0,0 +1,48 @@
+import { TanglePrimitivesCurrencyCurrencyId } from '@polkadot/types/lookup';
+
+import { LiquidStakingCurrency } from '../../constants/liquidStaking';
+
+const getValueOfTangleCurrency = (
+  tangleCurrencyId: TanglePrimitivesCurrencyCurrencyId,
+): LiquidStakingCurrency => {
+  // Unfortunately, there doesn't seem to be a cleaner way of
+  // going about this. This is a direct cause of the way that
+  // Rust enums are generated to be used in TypeScript/JavaScript.
+  if (tangleCurrencyId.isNative) {
+    return tangleCurrencyId.asNative.type;
+  } else if (tangleCurrencyId.isToken) {
+    return tangleCurrencyId.asToken.type;
+  } else if (tangleCurrencyId.isToken2) {
+    return tangleCurrencyId.asToken2.type;
+  } else if (tangleCurrencyId.isLstToken) {
+    return tangleCurrencyId.asLst.type;
+  } else if (tangleCurrencyId.isLst2) {
+    return tangleCurrencyId.asLst2.type;
+  } else if (tangleCurrencyId.isStable) {
+    return tangleCurrencyId.asStable.type;
+  } else if (tangleCurrencyId.isVsToken) {
+    return tangleCurrencyId.asVsToken.type;
+  } else if (tangleCurrencyId.isVsToken2) {
+    return tangleCurrencyId.asVsToken2.type;
+  } else if (tangleCurrencyId.isVsBond) {
+    return tangleCurrencyId.asVsBond.type;
+  } else if (tangleCurrencyId.isVsBond2) {
+    return tangleCurrencyId.asVsBond2.type;
+  } else if (tangleCurrencyId.isLpToken) {
+    return tangleCurrencyId.asLpToken.type;
+  } else if (tangleCurrencyId.isForeignAsset) {
+    return tangleCurrencyId.asForeignAsset.type;
+  } else if (tangleCurrencyId.isStableLpToken) {
+    return tangleCurrencyId.asStableLpToken.type;
+  } else if (tangleCurrencyId.isBlp) {
+    return tangleCurrencyId.asBlp.type;
+  } else if (tangleCurrencyId.isLend) {
+    return tangleCurrencyId.asLend.type;
+  }
+
+  throw new Error(
+    `Unknown or unsupported currency type: ${tangleCurrencyId} (was the Tangle Restaking Parachain updated?)`,
+  );
+};
+
+export default getValueOfTangleCurrency;
