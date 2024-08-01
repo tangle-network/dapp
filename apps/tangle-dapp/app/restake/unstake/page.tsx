8e835cbfa6fdd0f7b980bfd496f1a5fe382b5918
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-08-01 16:47:10 +0700

    chore(tangle-dapp): Add component to unstake page

--- apps/tangle-dapp/app/restake/unstake/page.tsx
+++ apps/tangle-dapp/app/restake/unstake/page.tsx
@@ -1,7 +1,51 @@
+'use client';
+
+import LockFillIcon from '@webb-tools/icons/LockFillIcon';
+import { LockLineIcon } from '@webb-tools/icons/LockLineIcon';
+import { TransactionInputCard } from '@webb-tools/webb-ui-components/components/TransactionInputCard';
+import { useRef } from 'react';
+
+import AssetPlaceholder from '../AssetPlaceholder';
+import RestakeTabs from '../RestakeTabs';
+
 export const dynamic = 'force-static';
 
 const Page = () => {
-  return <div>Unstake Page</div>;
+  return (
+    <div className="grid items-start grid-cols-1 gap-4 sm:grid-cols-2 justify-stretch">
+      <div className="max-w-lg">
+        <RestakeTabs />
+
+        <form className="space-y-4">
+          <TransactionInputCard.Root>
+            <TransactionInputCard.Header>
+              <TransactionInputCard.ChainSelector placeholder="Select" />
+              <TransactionInputCard.MaxAmountButton
+                tooltipBody="Staked Balance"
+                Icon={
+                  useRef({
+                    enabled: <LockLineIcon />,
+                    disabled: <LockFillIcon />,
+                  }).current
+                }
+              />
+            </TransactionInputCard.Header>
+
+            <TransactionInputCard.Body
+              tokenSelectorProps={
+                useRef({
+                  placeholder: <AssetPlaceholder />,
+                  isDisabled: true,
+                }).current
+              }
+            />
+
+            {/* <ErrorMessage>{errors.amount?.message}</ErrorMessage> */}
+          </TransactionInputCard.Root>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
