5c17ec1f889b43bd33e51a1c6ae0cde0e4e8e39c
Pavan Soratur <pavanvsoratur@gmail.com>
2024-08-05 20:57:53 +0530

    fix: typography disabled text color

--- libs/webb-ui-components/src/components/Radio/RadioItem.tsx
+++ libs/webb-ui-components/src/components/Radio/RadioItem.tsx
@@ -26,15 +26,16 @@ export const RadioItem = forwardRef<HTMLDivElement, RadioItemProps>(
         className={twMerge('flex items-center space-x-1', className)}
         ref={ref}
       >
-        <div className="size-[36px] group rounded-full hover:bg-[#5953F91A] hover:dark:bg-[#5953F933] flex items-center justify-center has-[:disabled]:hover:!bg-transparent has-[:radix-state-checked]:!bg-[#5953F933] has-[:radix-state-checked]:dark:!bg-[#5953F94D]">
+        <div className="size-[36px] rounded-full hover:bg-[#5953F91A] hover:dark:bg-[#5953F933] flex items-center justify-center has-[:disabled]:hover:!bg-transparent has-[:radix-state-checked]:!bg-[#5953F933] has-[:radix-state-checked]:dark:!bg-[#58F98F00]">
           <RadioGroup.Item
             {...overrideRadixRadioItemProps}
             className={twMerge(
-              'peer group size-[18px] rounded-full border-[2px] border-mono-100 dark:border-mono-100',
+              'size-[18px] rounded-full border-[2px] border-mono-100 dark:border-mono-100',
               'radix-disabled:border-mono-60 dark:radix-disabled:border-mono-140',
               'radix-disabled:radix-state-checked:border-mono-60 dark:radix-disabled:radix-state-checked:border-mono-140',
-              'radix-state-checked:border-blue-60 dark:radix-state-checked:border-blue-40',
+              'radix-state-checked:border-blue-50 dark:radix-state-checked:border-blue-50',
               'radix-state-checked:hover:border-blue-50 dark:radix-state-checked:hover:border-blue-50',
+              'active:border-blue-60 dark:active:border-blue-40',
               overrideRadixRadioItemProps?.className,
             )}
             value={value}
@@ -45,8 +46,9 @@ export const RadioItem = forwardRef<HTMLDivElement, RadioItemProps>(
               className={twMerge(
                 'flex items-center justify-center w-full h-full relative',
                 "after:content-[''] after:block after:w-2.5 after:h-2.5 after:rounded-[50%]",
-                'radix-state-checked:after:bg-blue-60 radix-state-checked:after:dark:bg-blue-40',
+                'radix-state-checked:after:bg-blue-50 radix-state-checked:after:dark:bg-blue-50',
                 'radix-state-checked:hover:after:bg-blue-50 radix-state-checked:hover:after:dark:bg-blue-50',
+                'radix-state-checked:active:after:bg-blue-60 radix-state-checked:active:after:dark:bg-blue-40',
                 'radix-disabled:radix-state-checked:after:bg-mono-60 dark:radix-disabled:radix-state-checked:after:bg-mono-140',
                 overrideRadixRadioIndicatorProps?.className,
               )}
@@ -60,8 +62,9 @@ export const RadioItem = forwardRef<HTMLDivElement, RadioItemProps>(
             variant="body1"
             className={twMerge(
               'text-mono-140 dark:text-mono-20',
-              'group-radix-disabled:text-mono-80 dark:group-radix-disabled:text-mono-120',
-              'peer-radix-disabled:text-mono-80 dark:peer-radix-disabled:text-mono-120',
+              overrideRadixRadioItemProps?.disabled
+                ? 'text-mono-60 dark:text-mono-140'
+                : '',
             )}
           >
             {label}
