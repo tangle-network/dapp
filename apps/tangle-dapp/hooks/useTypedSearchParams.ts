bf958b8f6280e4daeb456d9bafdab823c9eab158
yurixander <101931215+yurixander@users.noreply.github.com>
2024-08-05 18:56:32 -0400

    feat(tangle-dapp): Create `useTypedSearchParams` hook

---
+++ apps/tangle-dapp/hooks/useTypedSearchParams.ts
@@ -0,0 +1,32 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+const useTypedSearchParams = <T extends object>(parsers: {
+  [Key in keyof T]: (value: string) => T[Key] | undefined;
+}): Partial<T> => {
+  const searchParams = useSearchParams();
+
+  const entries = Object.keys(parsers).map((stringKey) => {
+    // TODO: Find a way to avoid casting here.
+    const key = stringKey as keyof T;
+
+    const parser = parsers[key];
+    const paramValue = searchParams.get(stringKey);
+    const parsedValue = paramValue !== null ? parser(paramValue) : undefined;
+
+    return [stringKey, parsedValue] as const;
+  });
+
+  const result: Partial<T> = {};
+
+  for (const [key, value] of entries) {
+    if (value !== undefined) {
+      result[key] = value;
+    }
+  }
+
+  return result;
+};
+
+export default useTypedSearchParams;
