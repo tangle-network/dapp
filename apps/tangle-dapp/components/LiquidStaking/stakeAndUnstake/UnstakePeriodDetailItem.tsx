44976498c62e3f1fdbab8d7997c9235ecd22b466
Yurixander <101931215+yurixander@users.noreply.github.com>
2024-08-06 04:06:32 -0700

    feat(tangle-dapp): Fetch onchain token unlock duration (#2498)

--- apps/tangle-dapp/components/LiquidStaking/stakeAndUnstake/UnstakePeriodDetailItem.tsx
+++ apps/tangle-dapp/components/LiquidStaking/stakeAndUnstake/UnstakePeriodDetailItem.tsx
@@ -1,18 +1,58 @@
-import { FC } from 'react';
+import { SkeletonLoader } from '@webb-tools/webb-ui-components';
+import { FC, useMemo } from 'react';
 
+import { ParachainCurrency } from '../../../constants/liquidStaking';
+import useTokenUnlockDurations from '../../../data/liquidStaking/useTokenUnlockDurations';
+import stringifyTimeUnit from '../../../utils/liquidStaking/stringifyTimeUnit';
 import DetailItem from './DetailItem';
 
-const UnstakePeriodDetailItem: FC = () => {
-  // TODO: Load this info from the chain. Currently using dummy data.
+export type UnstakePeriodDetailItemProps = {
+  currency: ParachainCurrency;
+};
+
+const UnstakePeriodDetailItem: FC<UnstakePeriodDetailItemProps> = ({
+  currency,
+}) => {
+  const unlockDurations = useTokenUnlockDurations();
+
+  const unlockPeriod = useMemo<'Unknown' | [string, string] | null>(() => {
+    if (unlockDurations === null) {
+      return null;
+    }
+
+    const unlockDuration = unlockDurations.find(
+      (entry) => entry.currency === currency && entry.isNative,
+    );
+
+    // Unlock duration is not set onchain.
+    if (unlockDuration === undefined) {
+      return 'Unknown' as const;
+    }
+
+    const parts = stringifyTimeUnit(unlockDuration.timeUnit);
+
+    return [parts[0].toString(), parts[1]];
+  }, [currency, unlockDurations]);
+
+  const value =
+    unlockPeriod === null ? (
+      // Still fetching unlocking period.
+      <SkeletonLoader className="max-w-[100px] min-w-4" />
+    ) : typeof unlockPeriod !== 'string' ? (
+      // Unlock period is set, and it is known.
+      <div>
+        <strong>{unlockPeriod[0]}</strong> {unlockPeriod[1]}
+      </div>
+    ) : (
+      // Unlocking period is unknown because it is not set onchain.
+      unlockPeriod
+    );
+
   return (
     <DetailItem
       title="Unstake period"
       tooltip="The period of time you need to wait before you can unstake your tokens."
-      value={
-        <div>
-          <strong>7</strong> days
-        </div>
-      }
+      value={value}
     />
   );
 };
