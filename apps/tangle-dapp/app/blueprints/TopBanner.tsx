a0e79145f8467b2cca0ce3c8f3443ecf8c3475f3
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-08-06 17:14:58 +0700

    chore: update banner

--- apps/tangle-dapp/app/blueprints/TopBanner.tsx
+++ apps/tangle-dapp/app/blueprints/TopBanner.tsx
@@ -9,13 +9,14 @@ const TopBanner: FC = () => {
     <div
       className={twMerge(
         'px-6 py-9 rounded-xl bg-center bg-cover bg-no-repeat',
-        'bg-[linear-gradient(79deg,rgba(17,22,50,0.60)_8.85%,rgba(17,30,95,0.60)_55.91%,rgba(89,83,249,0.60)_127.36%),url(/static/assets/blueprints/top-banner.png)]',
-        'dark:bg-[linear-gradient(79deg,rgba(17,22,50,0.80)_8.85%,rgba(17,30,95,0.80)_55.91%,rgba(89,83,249,0.80)_127.36%),url(/static/assets/blueprints/top-banner.png)]',
+        'bg-[url(/static/assets/blueprints/top-banner.png)]',
+        'dark:bg-[url(/static/assets/blueprints/top-banner-dark.png)]',
       )}
     >
       <div className="max-w-[415px] space-y-3">
         <Typography variant="h4" className="text-mono-0">
-          Create your first Blueprint
+          Create your first{' '}
+          <span className="text-blue-40 dark:text-blue-40">Blueprint</span>
         </Typography>
         <Typography variant="body1" className="text-mono-60 dark:text-mono-100">
           Set up a minimal Tangle Blueprint in minutes accompanied by a
