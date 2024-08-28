b3f31ad0e14ceb90e45040fac437c102e08d15d4
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-08-29 00:00:06 +0700

    fix(tangle-dapp): Improve mobile view of restake overview

--- apps/tangle-dapp/app/restake/page.tsx
+++ apps/tangle-dapp/app/restake/page.tsx
@@ -1,6 +1,6 @@
 import Button from '@webb-tools/webb-ui-components/components/buttons/Button';
 import { Typography } from '@webb-tools/webb-ui-components/typography/Typography';
-import cx from 'classnames';
+import { twMerge } from 'tailwind-merge';
 
 import GlassCard from '../../components/GlassCard/GlassCard';
 import StatItem from '../../components/LiquidStaking/StatItem';
@@ -18,20 +18,23 @@ const CONTENT = {
     'Tangle combines restaking with omnichain assets to provide a multi-asset crypto-economically secured compute infrastructure.',
 } as const;
 
+const minHeightClsx = 'min-h-[233px]';
+
 export default function RestakePage() {
   return (
     <div>
-      <div className="flex gap-5">
+      <div className="flex flex-col gap-5 md:flex-row">
         <GlassCard
-          className={cx(
+          className={twMerge(
             'justify-between',
+            minHeightClsx,
             '[background:linear-gradient(79deg,_#b6b8dd_8.85%,_#d9ddf2_55.91%,_#dbbdcd_127.36%),_#fff]',
             'dark:[background:linear-gradient(79deg,_rgba(30,_32,_65,_0.8)_8.85%,_rgba(38,_52,_116,_0.8)_55.91%,_rgba(113,_61,_89,_0.8)_127.36%)]',
           )}
         >
           <Typography
             variant="body1"
-            className="text-mono-200 dark:text-mono-0 max-w-[510px]"
+            className="text-mono-200 dark:text-mono-0 max-w-[510px] pb-3"
           >
             {CONTENT.OVERVIEW}
           </Typography>
@@ -43,7 +46,9 @@ export default function RestakePage() {
           </div>
         </GlassCard>
 
-        <GlassCard className="max-w-[442px] min-h-[233px] justify-between">
+        <GlassCard
+          className={twMerge(minHeightClsx, 'md:max-w-[442px] justify-between')}
+        >
           <div>
             <Typography
               variant="body1"
