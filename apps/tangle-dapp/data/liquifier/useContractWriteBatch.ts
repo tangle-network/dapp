ee19ce6ed8368f0d08672f39137dfe4189bcd019
yurixander <101931215+yurixander@users.noreply.github.com>
2024-08-31 00:32:23 -0400

    refactor(tangle-dapp): Use `multicall3` batching for `useLiquifierWithdraw`

--- apps/tangle-dapp/data/liquifier/useContractWriteBatch.ts
+++ apps/tangle-dapp/data/liquifier/useContractWriteBatch.ts
@@ -1,16 +1,8 @@
-import { useWebContext } from '@webb-tools/api-provider-environment';
-import chainsPopulated from '@webb-tools/dapp-config/chains/chainsPopulated';
-import {
-  calculateTypedChainId,
-  ChainType,
-} from '@webb-tools/sdk-core/typed-chain-id';
 import assert from 'assert';
 import { useCallback } from 'react';
-import { Abi as ViemAbi, ContractFunctionName } from 'viem';
-import { sepolia } from 'viem/chains';
+import { Abi as ViemAbi, ContractFunctionName, encodeFunctionData } from 'viem';
 import { useConnectorClient } from 'wagmi';
 
-import { IS_PRODUCTION_ENV } from '../../constants/env';
 import { MULTICALL3_CONTRACT_ADDRESS } from '../../constants/liquidStaking/constants';
 import MULTICALL3_ABI from '../../constants/liquidStaking/multicall3Abi';
 import useEvmAddress20 from '../../hooks/useEvmAddress';
@@ -20,19 +12,16 @@ export type ContractBatchWriteOptions<
   Abi extends ViemAbi,
   FunctionName extends ContractFunctionName<Abi, 'nonpayable'>,
 > = Omit<ContractWriteOptions<Abi, FunctionName>, 'args'> & {
-  args: ContractWriteOptions<Abi, FunctionName>[];
+  args: ContractWriteOptions<Abi, FunctionName>['args'][];
 };
 
-const useContractWriteBatch = <Abi extends ViemAbi>() => {
+const useContractWriteBatch = <Abi extends ViemAbi>(abi: ViemAbi) => {
   const { data: connectorClient } = useConnectorClient();
   const activeEvmAddress20 = useEvmAddress20();
-  const { activeChain, activeWallet, switchChain } = useWebContext();
   const writeMulticall3 = useContractWrite(MULTICALL3_ABI);
 
   const write = useCallback(
-    async <
-      FunctionName extends ContractFunctionName<Abi, 'nonpayable' | 'payable'>,
-    >(
+    async <FunctionName extends ContractFunctionName<Abi, 'nonpayable'>>(
       options: ContractBatchWriteOptions<Abi, FunctionName>,
     ) => {
       assert(
@@ -50,27 +39,20 @@ const useContractWriteBatch = <Abi extends ViemAbi>() => {
         'Should not be able to call this function if there is no active EVM account',
       );
 
-      // On development, switch to the Sepolia chain if it's not already active.
-      // This is because there are dummy contracts deployed to Sepolia for testing.
-      if (
-        !IS_PRODUCTION_ENV &&
-        activeChain !== null &&
-        activeChain !== undefined &&
-        activeChain.id !== sepolia.id &&
-        activeWallet !== undefined
-      ) {
-        const typedChainId = calculateTypedChainId(ChainType.EVM, sepolia.id);
-        const targetChain = chainsPopulated[typedChainId];
+      const calls = options.args.map((arg) => {
+        const callData = encodeFunctionData<typeof abi>({
+          abi,
+          functionName: options.functionName,
+          // TODO: Getting the type of `args` right has proven quite difficult.
+          args: arg as unknown[],
+        });
 
-        await switchChain(targetChain, activeWallet);
-      }
-
-      const calls = options.args.map((arg) => ({
-        target: arg.address,
-        allowFailure: true,
-        // TODO: Convert the args to callData.
-        callData: [] as any,
-      }));
+        return {
+          target: options.address,
+          allowFailure: true,
+          callData,
+        } as const;
+      });
 
       return writeMulticall3({
         address: MULTICALL3_CONTRACT_ADDRESS,
@@ -79,14 +61,7 @@ const useContractWriteBatch = <Abi extends ViemAbi>() => {
         txName: options.txName,
       });
     },
-    [
-      activeChain,
-      activeEvmAddress20,
-      activeWallet,
-      connectorClient,
-      switchChain,
-      writeMulticall3,
-    ],
+    [abi, activeEvmAddress20, connectorClient, writeMulticall3],
   );
 
   // Only provide the write function once the connector client is ready,
