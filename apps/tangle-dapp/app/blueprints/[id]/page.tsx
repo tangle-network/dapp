82fad4a8b6292dd0ac8ed5749e110741e0578b9b
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-09-29 12:03:23 +0700

    chore(tangle-dapp): Remove TVL table on blueprint detail

--- apps/tangle-dapp/app/blueprints/[id]/page.tsx
+++ apps/tangle-dapp/app/blueprints/[id]/page.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 
 import BlueprintHeader from './BlueprintHeader';
 import OperatorsTable from './OperatorsTable';
-import VaultsAndAssetsTable from './VaultsAndAssetsTable';
 
 type Props = {
   params: {
@@ -14,7 +13,6 @@ const BlueprintDetailsPage: FC<Props> = ({ params: { id } }) => {
   return (
     <div className="space-y-5">
       <BlueprintHeader blueprintId={id} />
-      <VaultsAndAssetsTable />
       <OperatorsTable />
     </div>
   );
