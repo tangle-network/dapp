7a365306795d8334b0c59f516b64f4b5b8b2ac5f
yurixander <101931215+yurixander@users.noreply.github.com>
2024-10-13 05:05:04 +0200

    feat(tangle-dapp): Add bridge onboarding

--- apps/tangle-dapp/app/bridge/page.tsx
+++ apps/tangle-dapp/app/bridge/page.tsx
@@ -1,6 +1,16 @@
+import {
+  ExchangeFunds,
+  LockLineIcon,
+  TokenSwapLineIcon,
+  WalletLineIcon,
+} from '@webb-tools/icons';
+import { TANGLE_DOCS_URL } from '@webb-tools/webb-ui-components';
 import { Metadata } from 'next';
 import { FC } from 'react';
 
+import OnboardingItem from '../../components/OnboardingModal/OnboardingItem';
+import OnboardingModal from '../../components/OnboardingModal/OnboardingModal';
+import { OnboardingPageKey } from '../../constants';
 import createPageMetadata from '../../utils/createPageMetadata';
 import BridgeContainer from './BridgeContainer';
 
@@ -11,6 +21,36 @@ export const metadata: Metadata = createPageMetadata({
 const Bridge: FC = () => {
   return (
     <div>
+      <OnboardingModal
+        title="Get Started with Bridging"
+        pageKey={OnboardingPageKey.BRIDGE}
+        learnMoreHref={TANGLE_DOCS_URL}
+      >
+        <OnboardingItem
+          Icon={WalletLineIcon}
+          title="Connect EVM or Substrate Wallet"
+          description="Connect your EVM or Substrate wallet depending on the intended source chain. For example, if bridging in assets from Ethereum, connect your MetaMask or EVM-based wallet."
+        />
+
+        <OnboardingItem
+          Icon={ExchangeFunds}
+          title="Bridge EVM Assets"
+          description="Select source and destination chains, amount, and the recipient account's address to bridge in your EVM-based assets into Tangle to start restaking."
+        />
+
+        <OnboardingItem
+          Icon={LockLineIcon}
+          title="Powered by Hyperlane"
+          description="Enjoy smooth and secure bridging powered by Hyperlane."
+        />
+
+        <OnboardingItem
+          Icon={TokenSwapLineIcon}
+          title="Restake Your Assets"
+          description="After bridging, use the restaking page to restake your assets."
+        />
+      </OnboardingModal>
+
       <BridgeContainer className="mx-auto" />
     </div>
   );
