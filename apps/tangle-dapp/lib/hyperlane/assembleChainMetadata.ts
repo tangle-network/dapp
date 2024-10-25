c515ee6aa290b6ec6b522d4b72df5b1b5662c0f7
Pavan Soratur <pavansoratur@MacBookAir.lan>
2024-10-25 09:51:23 -0700

    chore: remove hyperlane chains and add custom chains

--- apps/tangle-dapp/lib/hyperlane/assembleChainMetadata.ts
+++ apps/tangle-dapp/lib/hyperlane/assembleChainMetadata.ts
@@ -1,11 +1,9 @@
-import { chainMetadata, GithubRegistry } from '@hyperlane-xyz/registry';
+import { GithubRegistry } from '@hyperlane-xyz/registry';
 import {
   ChainMap,
   ChainMetadata,
   ChainMetadataSchema,
-  ChainTechnicalStack,
 } from '@hyperlane-xyz/sdk';
-import { ProtocolType } from '@hyperlane-xyz/utils';
 import { z } from 'zod';
 
 import { registryUrl } from './config';
@@ -13,8 +11,6 @@ import { customChains } from './consts';
 
 export default async function assembleChainMetadata() {
   const result = z.record(ChainMetadataSchema).safeParse({
-    // Note: Chains must include a cosmos chain or CosmosKit throws errors
-    cosmoshub: cosmosDefaultChain,
     ...customChains,
   });
 
@@ -25,46 +21,8 @@ export default async function assembleChainMetadata() {
   const customChainMetadata = result.data as ChainMap<ChainMetadata>;
 
   const registry = new GithubRegistry({ uri: registryUrl });
-  let defaultChainMetadata = chainMetadata;
-  if (registryUrl) {
-    defaultChainMetadata = await registry.getMetadata();
-  } else {
-    // Note: this is an optional optimization to pre-fetch the content list
-    // and avoid repeated request from the chain logos that will use this info
-    await registry.listRegistryContent();
-  }
 
-  const chains = { ...defaultChainMetadata, ...customChainMetadata };
-
-  // Handle invalid ChainTechnicalStack values - ex: opstack
-  Object.entries(chains).forEach(([_chainName, chain]) => {
-    if (
-      typeof chain.technicalStack === 'string' &&
-      !Object.values(ChainTechnicalStack).includes(chain.technicalStack)
-    ) {
-      chain.technicalStack = ChainTechnicalStack.Other;
-    }
-  });
+  const chains = customChainMetadata;
 
   return { chains, registry };
 }
-
-const cosmosDefaultChain: ChainMetadata = {
-  protocol: ProtocolType.Cosmos,
-  name: 'cosmoshub',
-  chainId: 'cosmoshub-4',
-  displayName: 'Cosmos Hub',
-  domainId: 1234,
-  bech32Prefix: 'cosmos',
-  slip44: 118,
-  grpcUrls: [{ http: 'grpc-cosmoshub-ia.cosmosia.notional.ventures:443' }],
-  rpcUrls: [{ http: 'https://rpc-cosmoshub.blockapsis.com' }],
-  restUrls: [{ http: 'https://lcd-cosmoshub.blockapsis.com' }],
-  nativeToken: {
-    name: 'Atom',
-    symbol: 'ATOM',
-    decimals: 6,
-    denom: 'uatom',
-  },
-  logoURI: '/logos/cosmos.svg',
-};
