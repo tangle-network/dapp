0e29d21474d399a2409a3b96e286b0f7182f9193
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-10-30 20:10:47 +0700

    feat(tangle-cloud): setup tangle cloud

---
+++ apps/tangle-cloud/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              <span> Hello there, </span>
+              Welcome tangle-cloud 👋
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
