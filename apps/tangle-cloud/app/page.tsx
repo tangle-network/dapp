ac3c70ff4a9e0513008d1203bccee280faae1cf7
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-11-11 16:54:16 +0700

    style(tangle-cloud): Style layout for main page

--- apps/tangle-cloud/app/page.tsx
+++ apps/tangle-cloud/app/page.tsx
@@ -1,18 +1,15 @@
 import { Typography } from '@webb-tools/webb-ui-components/typography/Typography';
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
   return (
-    <Typography
-      variant="h3"
-      ta="center"
-      className="flex items-center justify-center min-h-full"
-    >
-      Hello there, Welcome tangle-cloud 👋
-    </Typography>
+    <div className="max-w-screen-xl mx-auto">
+      <Typography
+        variant="h3"
+        ta="center"
+        className="flex items-center justify-center min-h-full"
+      >
+        Hello there, Welcome tangle-cloud 👋
+      </Typography>
+    </div>
   );
 }
