9a421b00ad8972a1f369b1858db0ff07044ab1d5
yurixander <101931215+yurixander@users.noreply.github.com>
2024-11-17 01:13:35 +0100

    ci(tangle-dapp): Fix CI

--- libs/webb-ui-components/src/components/index.ts
+++ libs/webb-ui-components/src/components/index.ts
@@ -57,7 +57,7 @@ export * from './Popover';
 export * from './Progress';
 export * from './ProposalsBadgeGroup';
 export * from './Radio';
-export * from './side';
+export * from './SideBar';
 export { default as SkeletonLoader } from './SkeletonLoader';
 export * from './Slider';
 export * from './SocialChip';
