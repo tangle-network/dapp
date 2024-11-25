2148d06c800f12b22340e08fdc11a191a8b096ae
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-11-25 20:39:23 +0700

    feat(tangle-dapp): Setup jest

--- apps/tangle-dapp/jest.config.ts
+++ apps/tangle-dapp/jest.config.ts
@@ -1,11 +1,26 @@
+import type { Config } from 'jest';
+
 /* eslint-disable */
 export default {
   displayName: 'tangle-dapp',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.cjs',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        presets: ['@nx/next/babel'],
+        plugins: ['babel-plugin-transform-import-meta'],
+      },
+    ],
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!@webb-tools/app-util|use-local-storage-state|react-syntax-highlighter|@mangatax-highlighter|wagmi|@wagmi/core|@mangata-finance/type-definitions)',
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/tangle-dapp',
-};
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/../../__mocks__/svg.js',
+  },
+} satisfies Config;
