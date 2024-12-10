ba45722a71bee84de3bf9daeb0347dc28978dc79
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-12-10 22:30:45 +0700

    fix(repo): Fix tests not exit

--- libs/icons/vite.config.ts
+++ libs/icons/vite.config.ts
@@ -54,9 +54,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest/libs/icons',
-    },
+    watch: false,
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     includeSource: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
