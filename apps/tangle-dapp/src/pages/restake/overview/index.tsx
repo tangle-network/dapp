bdc76724100893f288d94b62e646de6f4cc6513e
yurixander <101931215+yurixander@users.noreply.github.com>
2024-12-31 12:24:22 -0500

    refactor(tangle-dapp): Remove restake page banner

--- apps/tangle-dapp/src/pages/restake/overview/index.tsx
+++ apps/tangle-dapp/src/pages/restake/overview/index.tsx
@@ -6,8 +6,6 @@ import { useParams } from 'react-router';
 import { RestakeAction } from '../../../constants';
 import NotFoundPage from '../../notFound';
 import isEnumValue from '../../../utils/isEnumValue';
-import RestakeBanner from '@webb-tools/tangle-shared-ui/components/blueprints/RestakeBanner';
-import { OPERATOR_JOIN_DOCS_LINK } from '@webb-tools/webb-ui-components/constants/tangleDocs';
 
 export default function RestakePage() {
   const { action } = useParams();
@@ -23,23 +21,14 @@ export default function RestakePage() {
   }
 
   return (
-    <div className="space-y-9">
-      <RestakeBanner
-        title="Introducing: Tangle Operators"
-        description="Ready to participate on Tangle? Join as an operator to provide on-demand services and earn rewards."
-        buttonHref={OPERATOR_JOIN_DOCS_LINK}
-        buttonText="Get Started"
-      />
-
-      <TableTabs
-        delegatorTVL={delegatorTVL}
-        operatorMap={operatorMap}
-        delegatorInfo={delegatorInfo}
-        operatorTVL={operatorTVL}
-        vaultTVL={vaultTVL}
-        operatorConcentration={operatorConcentration}
-        action={action ?? RestakeAction.DEPOSIT}
-      />
-    </div>
+    <TableTabs
+      delegatorTVL={delegatorTVL}
+      operatorMap={operatorMap}
+      delegatorInfo={delegatorInfo}
+      operatorTVL={operatorTVL}
+      vaultTVL={vaultTVL}
+      operatorConcentration={operatorConcentration}
+      action={action ?? RestakeAction.DEPOSIT}
+    />
   );
 }
