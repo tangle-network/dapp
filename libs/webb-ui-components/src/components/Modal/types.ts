34e8c8112d783dfbd20f8e7029cf705ac9c7159f
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-01-01 23:38:08 +0800

    refactor(repo): Remove headlessui, using tailwind animate

--- libs/webb-ui-components/src/components/Modal/types.ts
+++ libs/webb-ui-components/src/components/Modal/types.ts
@@ -1,4 +1,3 @@
-import type { Transition, TransitionChild } from '@headlessui/react';
 import type { DialogContentProps } from '@radix-ui/react-dialog';
 import type { PropsOf } from '../../types';
 import type { WebbTypographyVariant } from '../../typography/types';
@@ -9,22 +8,9 @@ export interface ModalContentProps extends DialogContentProps {
    */
   isOpen: boolean;
 
-  /**
-   * Props to override the transition component
-   */
-  overrideTransitionRootProps?: Partial<PropsOf<typeof Transition>>;
-
-  /**
-   * Props to override the transition overlay component
-   */
-  overrideTransitionOverlayProps?: Partial<PropsOf<typeof TransitionChild>>;
-
-  /**
-   * Props to override the transition content component
-   */
-  overrideTransitionContentProps?: Partial<PropsOf<typeof TransitionChild>>;
-
   size?: 'sm' | 'md' | 'lg';
+
+  description?: string;
 }
 
 export interface ModalHeaderProps extends PropsOf<'div'> {
--- libs/webb-ui-components/src/components/SideBar/MobileSidebar.tsx
+++ libs/webb-ui-components/src/components/SideBar/MobileSidebar.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { forwardRef, Fragment, useState } from 'react';
-import { twMerge } from 'tailwind-merge';
 import * as Dialog from '@radix-ui/react-dialog';
 import { HamburgerMenu } from '@webb-tools/icons';
-import { Transition, TransitionChild } from '@headlessui/react';
+import { forwardRef, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
+import { SideBarFooter } from './Footer';
 import { SideBarLogo } from './Logo';
 import { SideBarItems } from './SideBarItems';
-import { SideBarFooter } from './Footer';
 import { MobileSidebarProps } from './types';
 
 export const MobileSidebar = forwardRef<HTMLDivElement, MobileSidebarProps>(
@@ -40,7 +39,7 @@ export const MobileSidebar = forwardRef<HTMLDivElement, MobileSidebarProps>(
         {...props}
         ref={ref}
       >
-        <Dialog.Root onOpenChange={setIsOpen}>
+        <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
           <Dialog.Trigger className="outline-none">
             <HamburgerMenu
               size="lg"
@@ -48,79 +47,65 @@ export const MobileSidebar = forwardRef<HTMLDivElement, MobileSidebarProps>(
             />
           </Dialog.Trigger>
 
-          <Transition show={isOpen}>
-            <TransitionChild
-              as={Fragment}
-              enter="ease-out duration-200"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+          <Dialog.Portal>
+            <Dialog.Overlay
+              forceMount
+              className={twMerge(
+                'fixed inset-0 z-20 bg-black/65 backdrop-blur-[1px]',
+                'animate-in duration-200 fade-in-0',
+                'data-[state=open]:ease-out data-[state=closed]:ease-in',
+              )}
+            />
+
+            <Dialog.Content
+              forceMount
+              {...overrideContentProps}
+              className={twMerge(
+                'w-[280px] h-[calc(100%-16px)] outline-none overflow-auto py-6 px-4 z-50 rounded-xl',
+                'bg-mono-0 dark:bg-mono-200 fixed left-2 top-2 bottom-2',
+                'flex flex-col gap-6 justify-between',
+                'data-[state=open]:animate-in data-[state=open]:ease-out data-[state=open]:duration-200',
+                'data-[state=open]:slide-in-from-left-full',
+                'data-[state=closed]:animate-out data-[state=closed]:ease-in data-[state=closed]:duration-100',
+                'data-[state=closed]:slide-out-to-left-full',
+                overrideContentProps?.className,
+              )}
             >
-              <Dialog.Overlay
-                forceMount
-                className="fixed inset-0 z-20 bg-black/65 backdrop-blur-[1px]"
-              />
-            </TransitionChild>
+              <Dialog.Title className="sr-only">Sidebar Menu</Dialog.Title>
 
-            <TransitionChild
-              as={Fragment}
-              enter="ease-out duration-200"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="ease-in duration-100"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <Dialog.Content
-                forceMount
-                {...overrideContentProps}
-                className={twMerge(
-                  'w-[280px] h-[calc(100%-16px)] outline-none overflow-auto py-6 px-4 z-50 rounded-xl',
-                  'bg-mono-0 dark:bg-mono-200 fixed left-2 top-2 bottom-2',
-                  'flex flex-col gap-6 justify-between',
-                  overrideContentProps?.className,
-                )}
-              >
-                <Dialog.Title className="sr-only">Sidebar Menu</Dialog.Title>
+              <Dialog.Description className="sr-only">
+                Sidebar Menu
+              </Dialog.Description>
 
-                <Dialog.Description className="sr-only">
-                  Sidebar Menu
-                </Dialog.Description>
-
-                <div className="space-y-6">
-                  <SideBarLogo
-                    logoLink={logoLink}
-                    Logo={Logo}
-                    isExpanded
-                    className="block px-3"
-                  />
-
-                  <SideBarItems
-                    ActionButton={ActionButton}
-                    items={items}
-                    isExpanded
-                    pathnameOrHash={pathnameOrHash}
-                    // Close the sidebar when an item is clicked.
-                    onItemClick={() => setIsOpen(false)}
-                  />
-                </div>
-
-                <SideBarFooter
-                  name={footer.name}
-                  Icon={footer.Icon}
-                  isInternal={footer.isInternal}
-                  href={footer.href}
-                  useNextThemesForThemeToggle={
-                    footer.useNextThemesForThemeToggle
-                  }
+              <div className="space-y-6">
+                <SideBarLogo
+                  logoLink={logoLink}
+                  Logo={Logo}
                   isExpanded
-                  className="gap-2 p-2"
+                  className="block px-3"
                 />
-              </Dialog.Content>
-            </TransitionChild>
-          </Transition>
+
+                <SideBarItems
+                  ActionButton={ActionButton}
+                  items={items}
+                  isExpanded
+                  pathnameOrHash={pathnameOrHash}
+                  // Close the sidebar when an item is clicked.
+                  onItemClick={() => setIsOpen(false)}
+                />
+              </div>
+
+              <SideBarFooter
+                name={footer.name}
+                Icon={footer.Icon}
+                isInternal={footer.isInternal}
+                href={footer.href}
+                useNextThemesForThemeToggle={footer.useNextThemesForThemeToggle}
+                isExpanded
+                className="gap-2 p-2"
+              />
+            </Dialog.Content>
+          </Dialog.Portal>
         </Dialog.Root>
       </div>
     );
