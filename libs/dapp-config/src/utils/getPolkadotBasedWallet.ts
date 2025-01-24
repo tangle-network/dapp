2ed8676c893d106132967b911dfc6482baf79cf4
yurixander <101931215+yurixander@users.noreply.github.com>
2025-01-24 18:08:43 -0500

    fix(tangle-dapp): Fix Talisman wallet not detected

--- libs/dapp-config/src/utils/getPolkadotBasedWallet.ts
+++ libs/dapp-config/src/utils/getPolkadotBasedWallet.ts
@@ -1,12 +1,13 @@
 import type { InjectedExtension } from '@polkadot/extension-inject/types';
+import { web3Enable } from '@polkadot/extension-dapp';
 
 async function getPolkadotBasedWallet(
   appName: string,
   extensionName: string,
 ): Promise<InjectedExtension | undefined> {
   try {
-    const { web3Enable } = await import('@polkadot/extension-dapp');
     const extensions = await web3Enable(appName);
+
     return extensions.find((ex) => ex.name === extensionName);
   } catch (error) {
     console.error('Error getting polkadot based wallet', error);
