04e2c5cfec0674228a42aab231a904cce52b4d95
yurixander <101931215+yurixander@users.noreply.github.com>
2025-01-24 23:56:58 -0500

    ci(tangle-dapp): Fix build error

--- libs/dapp-config/src/utils/getPolkadotBasedWallet.ts
+++ libs/dapp-config/src/utils/getPolkadotBasedWallet.ts
@@ -1,11 +1,16 @@
 import type { InjectedExtension } from '@polkadot/extension-inject/types';
-import { web3Enable } from '@polkadot/extension-dapp';
 
 async function getPolkadotBasedWallet(
   appName: string,
   extensionName: string,
 ): Promise<InjectedExtension | undefined> {
   try {
+    // Without this, compilation for Next.js dapps fails during
+    // pre-rendering due to `window` not being defined. Likely caused
+    // by the `@polkadot/extension-dapp` package using `window` in
+    // its implementation.
+    const { web3Enable } = await import('@polkadot/extension-dapp');
+
     const extensions = await web3Enable(appName);
 
     return extensions.find((ex) => ex.name === extensionName);
