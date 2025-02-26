4dd15e6afd9af215fcb29bcdd361b228f9eb3245
danielbui12 <huytung139@gmail.com>
2025-02-26 07:39:16 +0700

    refactor(tangle-dapp): remove NominatorStatsItem due to just wrapper over NominatorStatsItem

--- apps/tangle-dapp/src/components/index.ts
+++ apps/tangle-dapp/src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './BondedTokensBalanceInfo';
 export { default as CardWithTangleLogo } from './CardWithTangleLogo';
 export * from './HeaderChip';
-export * from './NominatorStatsItem';
 export * from './Sidebar';
 export * from './skeleton';
 export { default as TangleBigLogo } from './TangleBigLogo';
