cd6a9bbf77bd00f73cd1a14562e870718ef71cd5
danielbui12 <huytung139@gmail.com>
2025-03-04 10:01:49 +0700

    fix(ui-components): fix UI dropdown select network

--- libs/ui-components/src/components/Dropdown/DropdownBody.tsx
+++ libs/ui-components/src/components/Dropdown/DropdownBody.tsx
@@ -26,6 +26,7 @@ export const DropdownBody = forwardRef<HTMLDivElement, DropdownBodyProps>(
             'border-mono-80 dark:border-mono-120',
             'bg-mono-0 dark:bg-mono-180',
             'max-h-[var(--radix-dropdown-menu-content-max-height)]',
+            'z-50'
           ),
           className,
         )}
