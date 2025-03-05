0e1f10e38fc285fd60ca7d16823982fe28f6ed36
yurixander <101931215+yurixander@users.noreply.github.com>
2025-03-04 22:57:35 -0500

    fix(tangle-dapp): Fix native asset not showing up

--- apps/tangle-dapp/src/data/restake/useNativeRestakeAssetBalance.ts
+++ apps/tangle-dapp/src/data/restake/useNativeRestakeAssetBalance.ts
@@ -1,4 +1,4 @@
-import { BN } from '@polkadot/util';
+import { BN, BN_ZERO } from '@polkadot/util';
 import { NATIVE_ASSET_ID } from '@tangle-network/tangle-shared-ui/constants/restaking';
 import useRestakeDelegatorInfo from '@tangle-network/tangle-shared-ui/data/restake/useRestakeDelegatorInfo';
 import { useMemo } from 'react';
@@ -9,20 +9,26 @@ const useNativeRestakeAssetBalance = (): BN | null => {
   const { delegatorInfo } = useRestakeDelegatorInfo();
   const stakingLock = useBalancesLock(SubstrateLockId.STAKING);
 
-  const balance = useMemo(() => {
-    if (delegatorInfo === null || stakingLock.amount === null) {
-      return null;
+  const delegated = useMemo(() => {
+    if (delegatorInfo === null) {
+      return BN_ZERO;
     }
 
-    const delegated = delegatorInfo.delegations
+    return delegatorInfo.delegations
       .filter((item) => item.assetId === NATIVE_ASSET_ID)
       .reduce(
         (acc, item) => acc.add(new BN(item.amountBonded.toString())),
         new BN(0),
       );
+  }, [delegatorInfo]);
+
+  const balance = useMemo(() => {
+    if (stakingLock.amount === null) {
+      return null;
+    }
 
     return stakingLock.amount.sub(delegated);
-  }, [delegatorInfo, stakingLock.amount]);
+  }, [delegated, stakingLock.amount]);
 
   return balance;
 };
--- apps/tangle-dapp/src/pages/restake/delegate/index.tsx
+++ apps/tangle-dapp/src/pages/restake/delegate/index.tsx
@@ -189,7 +189,7 @@ const RestakeDelegateForm: FC = () => {
       } satisfies RestakeAssetTableItem);
     }
 
-    if (!isDefined(delegatorInfo)) {
+    if (delegatorInfo === null) {
       return result;
     }
 
