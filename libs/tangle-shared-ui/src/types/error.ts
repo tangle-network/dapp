b157b087b9bef67b7fc6d11914a8ccbd35ee8125
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-03-10 23:56:22 +0700

    feat(tangle-shared-ui): Add INVALID_PARAMS error code

--- libs/tangle-shared-ui/src/types/error.ts
+++ libs/tangle-shared-ui/src/types/error.ts
@@ -1,11 +1,13 @@
 export enum TangleErrorCode {
   FEATURE_NOT_SUPPORTED,
   NO_ACTIVE_ACCOUNT,
+  INVALID_PARAMS,
 }
 
 export const TangleErrorTitleMap: Record<TangleErrorCode, string> = {
   [TangleErrorCode.FEATURE_NOT_SUPPORTED]: 'Feature Not Supported',
   [TangleErrorCode.NO_ACTIVE_ACCOUNT]: 'No Active Account',
+  [TangleErrorCode.INVALID_PARAMS]: 'Invalid Params',
 };
 
 export const TangleErrorDescriptionMap: Record<TangleErrorCode, string> = {
@@ -13,6 +15,8 @@ export const TangleErrorDescriptionMap: Record<TangleErrorCode, string> = {
     'The requested feature is not supported by the current API. Please try refreshing the page or switching to a different API endpoint. If the issue persists, the feature may not be available in this version of the application.',
   [TangleErrorCode.NO_ACTIVE_ACCOUNT]:
     'No active account found. Please connect a wallet and try again.',
+  [TangleErrorCode.INVALID_PARAMS]:
+    'The parameters passed to the function are invalid.',
 };
 
 export class TangleError extends Error {
