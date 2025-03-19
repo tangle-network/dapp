6d36c10c258a5d67df77bf9343045d71ec112a21
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-03-20 00:14:36 +0700

    chore(leaderboard): Update badge icons

--- apps/leaderboard/src/constants/index.ts
+++ apps/leaderboard/src/constants/index.ts
@@ -2,9 +2,9 @@ import { BadgeEnum } from '../types/BadgeEnum';
 
 export const BADGE_ICON_RECORD = {
   [BadgeEnum.LIQUID_STAKER]: '💧',
-  [BadgeEnum.NATIVE_RESTAKER]: '🪙',
+  [BadgeEnum.NATIVE_RESTAKER]: '💎',
   [BadgeEnum.OPERATOR]: '🛠️',
-  [BadgeEnum.RESTAKE_DELEGATOR]: '🤝',
+  [BadgeEnum.RESTAKE_DELEGATOR]: '💰',
   [BadgeEnum.RESTAKE_DEPOSITOR]: '💸',
   [BadgeEnum.VALIDATOR]: '🔐',
 } as const satisfies Record<BadgeEnum, string>;
