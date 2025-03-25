8971ee4b4788bbae90285a10c6d581b27172f4bd
danielbui12 <huytung139@gmail.com>
2025-03-25 09:19:42 +0700

    fix(tangle-cloud): update services hook to work with substrate only

--- apps/tangle-cloud/src/data/services/useServicesRejectTx.ts
+++ apps/tangle-cloud/src/data/services/useServicesRejectTx.ts
@@ -3,40 +3,24 @@ import { BN } from '@polkadot/util';
 import { useCallback } from 'react';
 
 import { TxName } from '../../constants';
-import { PrecompileAddress } from '@tangle-network/tangle-shared-ui/constants/evmPrecompiles';
-import useAgnosticTx from '@tangle-network/tangle-shared-ui/hooks/useAgnosticTx';
-import { EvmTxFactory } from '@tangle-network/tangle-shared-ui/hooks/useEvmPrecompileCall';
-import { SubstrateTxFactory } from '@tangle-network/tangle-shared-ui/hooks/useSubstrateTx';
-import SERVICES_PRECOMPILE_ABI from '@tangle-network/tangle-shared-ui/abi/services';
+import { SubstrateTxFactory, useSubstrateTxWithNotification } from '@tangle-network/tangle-shared-ui/hooks/useSubstrateTx';
 
 type Context = {
   requestId: number;
 };
 
 const useServicesRejectTx = () => {
-  // @dev Services precompile does not have the `reject` function
-  const evmTxFactory: EvmTxFactory<any, any, Context> = useCallback(
-    (_context) => ({
-      functionName: 'reject',
-      arguments: [],
-    }),
-    [],
-  );
-
   const substrateTxFactory: SubstrateTxFactory<Context> = useCallback(
     (api, _activeSubstrateAddress, context) =>
       api.tx.services.reject(new BN(context.requestId)),
     [],
   );
 
-  return useAgnosticTx({
-    name: TxName.REJECT_SERVICE_REQUEST,
-    abi: SERVICES_PRECOMPILE_ABI,
-    precompileAddress: PrecompileAddress.SERVICES,
-    evmTxFactory,
+  return useSubstrateTxWithNotification(
+    TxName.REJECT_SERVICE_REQUEST,
     substrateTxFactory,
-    successMessageByTxName: SUCCESS_MESSAGES,
-  });
+    SUCCESS_MESSAGES,
+  );
 };
 
 export default useServicesRejectTx;
