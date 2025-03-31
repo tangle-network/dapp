197e501c841dd424d01d2eb87e10d74c725ba432
danielbui12 <huytung139@gmail.com>
2025-03-31 17:20:50 +0700

    chore(libs/ui-components): format code

--- libs/ui-components/src/components/select/SelectCheckboxItem.tsx
+++ libs/ui-components/src/components/select/SelectCheckboxItem.tsx
@@ -4,18 +4,17 @@ import { twMerge } from 'tailwind-merge';
 import { Label } from '../Label';
 import { Input, InputProps } from '../Input';
 
-const SelectCheckboxItem: FC<{
-  label?: React.ReactNode
-} & InputProps> = ({
-  label,
-  ...inputProps
-}) => {
+const SelectCheckboxItem: FC<
+  {
+    label?: React.ReactNode;
+  } & InputProps
+> = ({ label, ...inputProps }) => {
   const [selected, setSelected] = useState<string[]>([]);
 
   const onChange = (value: string) => {
-    setSelected(prev => [...prev, value]);
+    setSelected((prev) => [...prev, value]);
     inputProps.onChange?.(value);
-  }
+  };
 
   return (
     <div
@@ -26,17 +25,19 @@ const SelectCheckboxItem: FC<{
         'data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       )}
     >
-      <Input 
-        type="checkbox" 
-        {...inputProps} 
+      <Input
+        type="checkbox"
+        {...inputProps}
         checked={selected.includes(inputProps.value || '')}
         onChange={onChange}
         inputClassName={twMerge(
           'w-4 h-4 rounded-sm !p-0 !border-mono-80 dark:!border-mono-120',
-          inputProps.className
+          inputProps.className,
         )}
       />
-      <Label className='w-full' htmlFor={inputProps.id}>{label}</Label>
+      <Label className="w-full" htmlFor={inputProps.id}>
+        {label}
+      </Label>
     </div>
   );
 };
