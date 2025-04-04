83b00bb36880708ad1cd9b39620fb89af43155d0
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-04-04 15:38:46 +0700

    style(ui-components): Resolve account dropdown should be scrollable

--- libs/ui-components/src/components/Dropdown/AccountDropdownBody.tsx
+++ libs/ui-components/src/components/Dropdown/AccountDropdownBody.tsx
@@ -10,6 +10,7 @@ import { DropdownBody } from './DropdownBody';
 import DropdownMenuItem from './DropdownMenuItem';
 import { AccountDropdownBodyProps } from './types';
 import { isSubstrateAddress } from '../../utils';
+import { ScrollArea } from '../ScrollArea';
 
 const AccountDropdownBody: FC<AccountDropdownBodyProps> = ({
   accountItems,
@@ -23,33 +24,35 @@ const AccountDropdownBody: FC<AccountDropdownBodyProps> = ({
         className,
       )}
     >
-      <ul>
-        {accountItems.map(({ address, name, onClick }) => (
-          <li key={address} onClick={onClick}>
-            <DropdownMenuItem
-              // Don't automatically capitalize addresses.
-              textTransform=""
-              leftIcon={
-                <Avatar
-                  theme={
-                    isEthereumAddress(address)
-                      ? 'ethereum'
-                      : isSubstrateAddress(address)
-                        ? 'substrate'
-                        : undefined
-                  }
-                  value={address}
-                />
-              }
-            >
-              {name}{' '}
-              <Typography variant="mkt-caption">
-                {addressShortenFn ? addressShortenFn(address) : address}
-              </Typography>
-            </DropdownMenuItem>
-          </li>
-        ))}
-      </ul>
+      <ScrollArea className="max-h-96 overflow-y-auto">
+        <ul>
+          {accountItems.map(({ address, name, onClick }) => (
+            <li key={address} onClick={onClick}>
+              <DropdownMenuItem
+                // Don't automatically capitalize addresses.
+                textTransform=""
+                leftIcon={
+                  <Avatar
+                    theme={
+                      isEthereumAddress(address)
+                        ? 'ethereum'
+                        : isSubstrateAddress(address)
+                          ? 'substrate'
+                          : undefined
+                    }
+                    value={address}
+                  />
+                }
+              >
+                {name}{' '}
+                <Typography variant="mkt-caption">
+                  {addressShortenFn ? addressShortenFn(address) : address}
+                </Typography>
+              </DropdownMenuItem>
+            </li>
+          ))}
+        </ul>
+      </ScrollArea>
     </DropdownBody>
   );
 };
