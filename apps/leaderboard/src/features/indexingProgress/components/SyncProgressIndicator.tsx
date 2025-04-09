e82be7ae1931ba8cf87d85820ea56c2c8d2a803c
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-04-09 07:38:34 +0700

    feat(leaderboard): Add sliding number for indexing progress

--- apps/leaderboard/src/features/indexingProgress/components/SyncProgressIndicator.tsx
+++ apps/leaderboard/src/features/indexingProgress/components/SyncProgressIndicator.tsx
@@ -1,24 +1,32 @@
+import useMeasure from 'react-use-measure';
 import { CrossCircledIcon } from '@radix-ui/react-icons';
 import { StatusIndicator } from '@tangle-network/icons';
 import { Chip, EMPTY_VALUE_PLACEHOLDER } from '@tangle-network/ui-components';
-import { useMemo } from 'react';
+import { Fragment, useEffect, useId, useMemo } from 'react';
 import { useIndexingProgress } from '../queries';
+import {
+  motion,
+  motionValue,
+  MotionValue,
+  Transition,
+  useSpring,
+  useTransform,
+} from 'framer-motion';
+import { twMerge } from 'tailwind-merge';
 
 export const SyncProgressIndicator = () => {
   const { data, error, isPending } = useIndexingProgress();
-
-  const targetBlock =
-    data?.targetHeight?.toLocaleString() ?? EMPTY_VALUE_PLACEHOLDER;
-
-  const lastProcessedBlock =
-    data?.lastProcessedHeight?.toLocaleString() ?? EMPTY_VALUE_PLACEHOLDER;
 
   const progress = useMemo(() => {
     if (!data?.lastProcessedHeight || !data?.targetHeight) {
       return 0;
     }
 
-    return (data.lastProcessedHeight / data.targetHeight) * 100;
+    // Round to 2 decimal places
+    return (
+      Math.round((data.lastProcessedHeight / data.targetHeight) * 100 * 100) /
+      100
+    );
   }, [data?.lastProcessedHeight, data?.targetHeight]);
 
   const isSynced = useMemo(() => {
@@ -54,13 +62,42 @@ export const SyncProgressIndicator = () => {
           variant={isSynced ? 'success' : 'info'}
           animated={!isSynced}
         />
-        {isSynced
-          ? `Synced ${lastProcessedBlock} of ${targetBlock} `
-          : `Indexing ${lastProcessedBlock} of ${targetBlock} `}
-        ({progress.toFixed(2)}%)
+        <span className="flex items-center">
+          {isSynced ? 'Synced' : 'Indexing'}
+
+          <span className="inline-block ml-1">
+            {data?.lastProcessedHeight ? (
+              <SlidingNumber value={data.lastProcessedHeight} />
+            ) : (
+              EMPTY_VALUE_PLACEHOLDER
+            )}
+          </span>
+
+          <span className="inline-block ml-1">of</span>
+
+          <span className="inline-block ml-1">
+            {data?.targetHeight ? (
+              <SlidingNumber value={data.targetHeight} />
+            ) : (
+              EMPTY_VALUE_PLACEHOLDER
+            )}
+          </span>
+
+          <span className="items-center hidden sm:flex ml-1">
+            (<SlidingNumber value={progress} />
+            %)
+          </span>
+        </span>
       </>
     );
-  }, [isPending, error, isSynced, lastProcessedBlock, targetBlock, progress]);
+  }, [
+    isPending,
+    error,
+    isSynced,
+    data?.lastProcessedHeight,
+    data?.targetHeight,
+    progress,
+  ]);
 
   return (
     <Chip
@@ -71,3 +108,124 @@ export const SyncProgressIndicator = () => {
     </Chip>
   );
 };
+
+const TRANSITION = {
+  type: 'spring',
+  stiffness: 280,
+  damping: 18,
+  mass: 0.3,
+} as const satisfies Transition;
+
+function Number({ mv, number }: { mv: MotionValue<number>; number: number }) {
+  const uniqueId = useId();
+  const [ref, bounds] = useMeasure();
+
+  const y = useTransform(mv, (latest) => {
+    if (!bounds.height) return 0;
+    const placeValue = latest % 10;
+    const offset = (10 + number - placeValue) % 10;
+    let memo = offset * bounds.height;
+
+    if (offset > 5) {
+      memo -= 10 * bounds.height;
+    }
+
+    return memo;
+  });
+
+  // don't render the animated number until we know the height
+  if (!bounds.height) {
+    return (
+      <span ref={ref} className="invisible absolute">
+        {number}
+      </span>
+    );
+  }
+
+  return (
+    <motion.span
+      style={{ y }}
+      layoutId={`${uniqueId}-${number}`}
+      className="absolute inset-0 flex items-center justify-center"
+      transition={TRANSITION}
+      ref={ref}
+    >
+      {number}
+    </motion.span>
+  );
+}
+
+type SlidingNumberProps = {
+  value: number;
+  padStart?: boolean;
+  decimalSeparator?: string;
+  thousandSeparator?: string;
+  className?: string;
+};
+
+function Digit({ value, place }: { value: number; place: number }) {
+  const valueRoundedToPlace = Math.floor(value / place) % 10;
+  const initial = motionValue(valueRoundedToPlace);
+  const animatedValue = useSpring(initial, TRANSITION);
+
+  useEffect(() => {
+    animatedValue.set(valueRoundedToPlace);
+  }, [animatedValue, valueRoundedToPlace]);
+
+  return (
+    <div className="relative inline-block w-[1ch] overflow-x-visible overflow-y-clip leading-none">
+      <div className="invisible">0</div>
+      {Array.from({ length: 10 }, (_, i) => (
+        <Number key={i} mv={animatedValue} number={i} />
+      ))}
+    </div>
+  );
+}
+
+export function SlidingNumber({
+  value,
+  padStart = false,
+  decimalSeparator = '.',
+  thousandSeparator = ',',
+  className,
+}: SlidingNumberProps) {
+  const absValue = Math.abs(value);
+  const [integerPart, decimalPart] = absValue.toString().split('.');
+  const integerValue = parseInt(integerPart, 10);
+  const paddedInteger =
+    padStart && integerValue < 10 ? `0${integerPart}` : integerPart;
+  const integerDigits = paddedInteger.split('');
+  const integerPlaces = integerDigits.map((_, i) =>
+    Math.pow(10, integerDigits.length - i - 1),
+  );
+
+  return (
+    <div className={twMerge('flex items-center', className)}>
+      {value < 0 && '-'}
+      {integerDigits.map((_, index) => {
+        // Add comma separator every 3 digits from the right
+        const needsComma =
+          index > 0 && (integerDigits.length - index) % 3 === 0;
+
+        return (
+          <Fragment key={`pos-${integerPlaces[index]}`}>
+            {needsComma && <span>{thousandSeparator}</span>}
+            <Digit value={integerValue} place={integerPlaces[index]} />
+          </Fragment>
+        );
+      })}
+      {decimalPart && (
+        <>
+          <span>{decimalSeparator}</span>
+          {decimalPart.split('').map((_, index) => (
+            <Digit
+              key={`decimal-${index}`}
+              value={parseInt(decimalPart, 10)}
+              place={Math.pow(10, decimalPart.length - index - 1)}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
