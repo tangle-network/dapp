e0b6a79769f64c9757cf60d7c55e092ff1ae0e0a
danielbui12 <huytung139@gmail.com>
2025-04-17 08:55:33 +0700

    chore(tangle-cloud): format code

--- apps/tangle-cloud/src/pages/instances/BlueprintManagementSection.tsx
+++ apps/tangle-cloud/src/pages/instances/BlueprintManagementSection.tsx
@@ -5,11 +5,7 @@ import { FC } from 'react';
 export const BlueprintManagementSection: FC = () => {
   return (
     <>
-      <RegisteredBlueprintsTabs
-        blueprints={[]}
-        isLoading={false}
-        error={null}
-      />
+      <RegisteredBlueprintsTabs />
       <InstancesTabs />
     </>
   );
