fea64ca824d209dfd010967bb5f74aa021896c6b
Daniel Bui <79790753+danielbui12@users.noreply.github.com>
2025-04-19 09:46:58 +0700

    chore(tangle-cloud): remove un-used codes

--- apps/tangle-cloud/src/components/Sidebar.tsx
+++ apps/tangle-cloud/src/components/Sidebar.tsx
@@ -4,12 +4,6 @@ import CommandFillIcon from '@tangle-network/icons/CommandFillIcon';
 import { DocumentationIcon } from '@tangle-network/icons/DocumentationIcon';
 import GlobalLine from '@tangle-network/icons/GlobalLine';
 import { GridFillIcon } from '@tangle-network/icons/GridFillIcon';
-// import {
-//   Dropdown,
-//   DropdownBody,
-//   DropdownButton,
-//   DropdownMenuItem,
-// } from '@tangle-network/ui-components/components/Dropdown';
 import {
   MobileSidebar,
   SideBar as SideBarCmp,
@@ -22,13 +16,10 @@ import {
   TANGLE_DAPP_URL,
   TANGLE_DOCS_URL,
 } from '@tangle-network/ui-components/constants';
-// import capitalize from 'lodash/capitalize';
 import { FC } from 'react';
 import { useLocation } from 'react-router';
-// import useRoleStore, { Role, ROLE_ICON_MAP } from '../stores/roleStore';
 import { PagePath } from '../types';
-import { HomeFillIcon, UserLineIcon } from '@tangle-network/icons';
-// import { twMerge } from 'tailwind-merge';
+import { HomeFillIcon } from '@tangle-network/icons';
 
 type Props = {
   isExpandedByDefault?: boolean;
@@ -56,13 +47,6 @@ const SIDEBAR_ITEMS: SideBarItemProps[] = [
     Icon: GlobalLine,
     subItems: [],
   },
-  {
-    name: 'Account',
-    href: PagePath.ACCOUNT,
-    isInternal: true,
-    Icon: UserLineIcon,
-    subItems: [],
-  },
 
   // External links
   {
@@ -81,52 +65,6 @@ const SIDEBAR_FOOTER: SideBarFooterType = {
   name: 'Docs',
 };
 
-// const ActionButton: FC<{ isExpanded: boolean }> = ({ isExpanded }) => {
-//   const { role, setRole } = useRoleStore();
-
-//   const capitalizedRole = useMemo(() => capitalize(role), [role]);
-
-//   return (
-//     <Dropdown>
-//       <DropdownButton
-//         icon={ROLE_ICON_MAP[role]({ size: 'lg' })}
-//         isHideArrowIcon={!isExpanded}
-//         arrowElement={<ChevronRight size="lg" />}
-//         className={twMerge(
-//           'px-3 py-3 !rounded-full border-none',
-//           '!bg-purple-50 font-bold text-mono-0',
-//           'w-full',
-//         )}
-//       >
-//         <span>{isExpanded ? capitalizedRole : ''}</span>
-//       </DropdownButton>
-
-//       <DropdownBody
-//         isPortal={false}
-//         sideOffset={8}
-//         side="right"
-//         className="z-[60]"
-//         align="center"
-//       >
-//         <DropdownMenuItem
-//           isActive={role === Role.OPERATOR}
-//           onClick={() => setRole(Role.OPERATOR)}
-//           leftIcon={ROLE_ICON_MAP[Role.OPERATOR]()}
-//         >
-//           Operate
-//         </DropdownMenuItem>
-//         <DropdownMenuItem
-//           isActive={role === Role.DEPLOYER}
-//           onClick={() => setRole(Role.DEPLOYER)}
-//           leftIcon={ROLE_ICON_MAP[Role.DEPLOYER]()}
-//         >
-//           Deploy
-//         </DropdownMenuItem>
-//       </DropdownBody>
-//     </Dropdown>
-//   );
-// };
-
 const Sidebar: FC<Props> = ({ isExpandedByDefault }) => {
   const pathname = useLocation().pathname;
 
@@ -141,7 +79,6 @@ const Sidebar: FC<Props> = ({ isExpandedByDefault }) => {
         pathnameOrHash={pathname}
         className="hidden h-screen lg:block"
         isExpandedByDefault={isExpandedByDefault}
-        // ActionButton={ActionButton}
       />
 
       {/* Small screen sidebar */}
@@ -152,7 +89,6 @@ const Sidebar: FC<Props> = ({ isExpandedByDefault }) => {
         Logo={TangleCloudLogo}
         pathnameOrHash={pathname}
         className="fixed top-[34px] left-4 md:left-8 lg:hidden"
-        // ActionButton={ActionButton}
       />
     </>
   );
