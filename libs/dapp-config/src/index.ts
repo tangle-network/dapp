9fd62774e57e8b237eeab4492974ce81774b5485
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2025-12-10 22:21:39 +0700

    chore: fix minor export bug

--- libs/dapp-config/src/index.ts
+++ libs/dapp-config/src/index.ts
@@ -2,5 +2,4 @@ export * from './api-config';
 export * from './chains';
 export { default as chainsPopulated } from './chains/chainsPopulated';
 export * from './constants';
-export * from './utils';
 export * from './wallets';
